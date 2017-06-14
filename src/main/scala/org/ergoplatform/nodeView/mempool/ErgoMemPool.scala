061c4228a8b520b7346cacf7ae247a1a1f859ce0
catena <catena@protonmail.com>
2017-06-14 19:54:08 +0300

    ErgoMemPool

--- src/main/scala/org/ergoplatform/nodeView/mempool/ErgoMemPool.scala
+++ src/main/scala/org/ergoplatform/nodeView/mempool/ErgoMemPool.scala
@@ -1,31 +1,58 @@
 package org.ergoplatform.nodeView.mempool
 
 import org.ergoplatform.modifiers.transaction.AnyoneCanSpendTransaction
+import org.ergoplatform.nodeView.mempool.ErgoMemPool._
 import scorex.core.NodeViewModifier.ModifierId
 import scorex.core.transaction.MemoryPool
+import scorex.core.utils.ScorexLogging
 
+import scala.collection.concurrent.TrieMap
+import scala.collection.mutable
 import scala.util.Try
 
-class ErgoMemPool extends MemoryPool[AnyoneCanSpendTransaction, ErgoMemPool] {
-  override def getById(id: ModifierId): Option[AnyoneCanSpendTransaction] = ???
+case class ErgoMemPool(unconfirmed: TrieMap[TxKey, AnyoneCanSpendTransaction])
+  extends MemoryPool[AnyoneCanSpendTransaction, ErgoMemPool] with ScorexLogging {
+  override type NVCT = ErgoMemPool
 
-  override def contains(id: ModifierId): Boolean = ???
+  private def key(id: Array[Byte]): TxKey = new mutable.WrappedArray.ofByte(id)
 
-  override def getAll(ids: Seq[ModifierId]): Seq[AnyoneCanSpendTransaction] = ???
+  override def getById(id: ModifierId): Option[AnyoneCanSpendTransaction] = unconfirmed.get(key(id))
 
-  override def put(tx: AnyoneCanSpendTransaction): Try[ErgoMemPool] = ???
+  override def contains(id: ModifierId): Boolean = unconfirmed.contains(key(id))
 
-  override def put(txs: Iterable[AnyoneCanSpendTransaction]): Try[ErgoMemPool] = ???
+  override def getAll(ids: Seq[ModifierId]): Seq[AnyoneCanSpendTransaction] = ids.flatMap(getById)
 
-  override def putWithoutCheck(txs: Iterable[AnyoneCanSpendTransaction]): ErgoMemPool = ???
+  override def put(tx: AnyoneCanSpendTransaction): Try[ErgoMemPool] = put(Seq(tx))
 
-  override def remove(tx: AnyoneCanSpendTransaction): ErgoMemPool = ???
+  override def put(txs: Iterable[AnyoneCanSpendTransaction]): Try[ErgoMemPool] = Try {
+    txs.foreach(tx => require(!unconfirmed.contains(key(tx.id))))
+    //todo check validity
+    putWithoutCheck(txs)
+  }
 
-  override def take(limit: Int): Iterable[AnyoneCanSpendTransaction] = ???
+  override def putWithoutCheck(txs: Iterable[AnyoneCanSpendTransaction]): ErgoMemPool = {
+    txs.foreach(tx => unconfirmed.put(key(tx.id), tx))
+    this
+  }
 
-  override def filter(condition: (AnyoneCanSpendTransaction) => Boolean): ErgoMemPool = ???
+  override def remove(tx: AnyoneCanSpendTransaction): ErgoMemPool = {
+    unconfirmed.remove(key(tx.id))
+    this
+  }
 
-  override def size: Int = ???
+  override def take(limit: Int): Iterable[AnyoneCanSpendTransaction] =
+    unconfirmed.values.toSeq.sortBy(-_.fee).take(limit)
 
-  override type NVCT = this.type
+  override def filter(condition: (AnyoneCanSpendTransaction) => Boolean): ErgoMemPool = {
+    unconfirmed.retain { (k, v) =>
+      condition(v)
+    }
+    this
+  }
+
+  override def size: Int = unconfirmed.size
+}
+
+object ErgoMemPool {
+  type TxKey = scala.collection.mutable.WrappedArray.ofByte
 }

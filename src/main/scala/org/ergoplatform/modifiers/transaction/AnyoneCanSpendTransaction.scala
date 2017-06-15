5dfbe00cd65ad48f4739c985010ddd2efc7f3008
catena <catena@protonmail.com>
2017-06-15 16:56:36 +0300

    AnyoneCanSpendTransactionSerializer

--- src/main/scala/org/ergoplatform/modifiers/transaction/AnyoneCanSpendTransaction.scala
+++ src/main/scala/org/ergoplatform/modifiers/transaction/AnyoneCanSpendTransaction.scala
@@ -4,7 +4,7 @@ import com.google.common.primitives.{Bytes, Ints, Longs}
 import io.circe.Json
 import io.circe.syntax._
 import org.ergoplatform.modifiers.transaction.AnyoneCanSpendTransaction._
-import org.ergoplatform.modifiers.transaction.proposition.{AnyoneCanSpendNoncedBox, AnyoneCanSpendProposition}
+import org.ergoplatform.modifiers.transaction.proposition.{AnyoneCanSpendNoncedBox, AnyoneCanSpendProposition, AnyoneCanSpendPropositionSerializer}
 import org.ergoplatform.settings.Constants
 import scorex.core.serialization.Serializer
 import scorex.core.transaction.BoxTransaction
@@ -48,17 +48,15 @@ case class AnyoneCanSpendTransaction(from: IndexedSeq[(AnyoneCanSpendProposition
 
   override lazy val json: Json = Map(
     "id" -> Base58.encode(id).asJson,
-    "newBoxes" -> newBoxes.map(b => Base58.encode(b.id).asJson).asJson,
-    "boxesToRemove" -> boxIdsToOpen.map(id => Base58.encode(id).asJson).asJson,
+    //    "newBoxes" -> newBoxes.map(b => Base58.encode(b.id).asJson).asJson,
+    //    "boxesToRemove" -> boxIdsToOpen.map(id => Base58.encode(id).asJson).asJson,
     "from" -> from.map { s =>
       Map(
-        "proposition" -> Base58.encode(s._1.bytes).asJson,
         "nonce" -> s._2.asJson
       ).asJson
     }.asJson,
     "to" -> to.map { s =>
       Map(
-        "proposition" -> Base58.encode(s._1.bytes).asJson,
         "value" -> s._2.asJson
       ).asJson
     }.asJson,
@@ -66,7 +64,7 @@ case class AnyoneCanSpendTransaction(from: IndexedSeq[(AnyoneCanSpendProposition
     "timestamp" -> timestamp.asJson
   ).asJson
 
-  override def toString: String = s"SimpleBoxTransaction(${json.noSpaces})"
+  override def toString: String = s"AnyoneCanSpendTransaction(${json.noSpaces})"
 
   lazy val semanticValidity: Try[Unit] = Try {
     require(to.forall(_._2 >= 0))
@@ -85,7 +83,34 @@ object AnyoneCanSpendTransaction {
 
 object AnyoneCanSpendTransactionSerializer extends Serializer[AnyoneCanSpendTransaction] {
 
-  override def toBytes(m: AnyoneCanSpendTransaction): Array[Byte] = ???
+  override def toBytes(m: AnyoneCanSpendTransaction): Array[Byte] = {
+    Bytes.concat(
+      Longs.toByteArray(m.timestamp),
+      Ints.toByteArray(m.from.length),
+      Ints.toByteArray(m.to.length),
+      m.from.foldLeft(Array[Byte]())((a, b) => Bytes.concat(a, b._1.bytes, Longs.toByteArray(b._2))),
+      m.to.foldLeft(Array[Byte]())((a, b) => Bytes.concat(a, b._1.bytes, Longs.toByteArray(b._2)))
+    )
 
-  override def parseBytes(bytes: Array[Byte]): Try[AnyoneCanSpendTransaction] = ???
+  }
+
+  override def parseBytes(bytes: Array[Byte]): Try[AnyoneCanSpendTransaction] = Try {
+    val timestamp = Longs.fromByteArray(bytes.slice(0, 8))
+    val fromLength = Ints.fromByteArray(bytes.slice(8, 12))
+    val toLength = Ints.fromByteArray(bytes.slice(12, 16))
+    val elementLength = 8 + 1
+    val s = 16
+    val from = (0 until fromLength) map { i =>
+      val b = bytes.slice(s + i * elementLength, s + i * elementLength + 1)
+      val v = Longs.fromByteArray(bytes.slice(s + i * elementLength + 1, s + (i + 1) * elementLength))
+      (AnyoneCanSpendPropositionSerializer.parseBytes(b).get, v)
+    }
+    val s2 = s + fromLength * elementLength
+    val to = (0 until toLength) map { i =>
+      val b = bytes.slice(s2 + i * elementLength, s2 + i * elementLength + 1)
+      val v = Longs.fromByteArray(bytes.slice(s2 + i * elementLength + 1, s2 + (i + 1) * elementLength))
+      (AnyoneCanSpendPropositionSerializer.parseBytes(b).get, v)
+    }
+    AnyoneCanSpendTransaction(from, to, timestamp)
+  }
 }

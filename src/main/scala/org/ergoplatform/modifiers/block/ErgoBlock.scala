8d48745992b38bc2619a0788fe5fb94bea354954
catena <catena@protonmail.com>
2017-06-23 16:56:27 +0300

    FullBlock refactoring

--- src/main/scala/org/ergoplatform/modifiers/block/ErgoBlock.scala
+++ src/main/scala/org/ergoplatform/modifiers/block/ErgoBlock.scala
@@ -1,19 +1,10 @@
 package org.ergoplatform.modifiers.block
 
-import com.google.common.primitives.Bytes
 import org.ergoplatform.modifiers.transaction.AnyoneCanSpendTransaction
 import org.ergoplatform.modifiers.transaction.proposition.AnyoneCanSpendProposition
 import scorex.core.PersistentNodeViewModifier
 import scorex.core.block.Block
-import scorex.core.serialization.Serializer
-import scorex.core.utils.ScorexLogging
-
-import scala.util.Try
 
 trait ErgoBlock extends PersistentNodeViewModifier[AnyoneCanSpendProposition, AnyoneCanSpendTransaction]
-  with Block[AnyoneCanSpendProposition, AnyoneCanSpendTransaction] {
-  def stateRoot: Array[Byte]
-  val nonce: Int
-
-}
+  with Block[AnyoneCanSpendProposition, AnyoneCanSpendTransaction]
 

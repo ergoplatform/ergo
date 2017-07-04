e30473cfa4c3c3b475b3e3a9cd41bfad2e43251b
catena <catena@protonmail.com>
2017-07-04 14:49:27 +0300

    ADProofs data

--- src/main/scala/org/ergoplatform/modifiers/experimental/ADProofs.scala
+++ src/main/scala/org/ergoplatform/modifiers/experimental/ADProofs.scala
@@ -1,19 +1,26 @@
 package org.ergoplatform.modifiers.experimental
 
 import io.circe.Json
+import org.ergoplatform.settings.Constants
 import scorex.core.NodeViewModifier.{ModifierId, ModifierTypeId}
 import scorex.core.serialization.Serializer
 
-class ADProofs extends ErgoModifier {
+case class ADProofs(proofBytes: Array[Byte]) extends ErgoModifier {
   override val modifierTypeId: ModifierTypeId = ADProofs.ModifierTypeId
 
-  override def id: ModifierId = ???
+  override def id: ModifierId = Constants.hash(proofBytes)
 
   override type M = ADProofs
 
   override def serializer: Serializer[ADProofs] = ???
 
   override def json: Json = ???
+
+  def validate(startingDigest: Array[Byte]): Boolean = {
+    //TODO validate proof relative to starting digest
+    ???
+  }
+
 }
 
 object ADProofs {
--- src/main/scala/org/ergoplatform/modifiers/experimental/ErgoModifier.scala
+++ src/main/scala/org/ergoplatform/modifiers/experimental/ErgoModifier.scala
@@ -3,5 +3,6 @@ package org.ergoplatform.modifiers.experimental
 import scorex.core.NodeViewModifier
 
 trait ErgoModifier extends NodeViewModifier {
-
+  //TODO do we need version field for all modifiers?
+  //val version: Version
 }

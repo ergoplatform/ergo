d5171ddf541ccd97e74351175fe42fd53a56bb4c
catena <catena@protonmail.com>
2017-07-04 17:21:14 +0300

    Some interfaces for UTXO snapshots

--- src/main/scala/org/ergoplatform/modifiers/experimental/UTXOSnapshotManifest.scala
+++ src/main/scala/org/ergoplatform/modifiers/experimental/UTXOSnapshotManifest.scala
@@ -1,22 +1,33 @@
 package org.ergoplatform.modifiers.experimental
 
 import io.circe.Json
+import org.ergoplatform.settings.{Algos, Constants}
 import scorex.core.NodeViewModifier.{ModifierId, ModifierTypeId}
 import scorex.core.serialization.Serializer
 
-class UTXOSnapshotManifest extends ErgoModifier {
+import scala.util.Try
+
+case class UTXOSnapshotManifest(chunkRootHashes: Seq[Array[Byte]], blockId: ModifierId) extends ErgoModifier {
   override val modifierTypeId: ModifierTypeId = UTXOSnapshotManifest.ModifierTypeId
 
-  override lazy val id: ModifierId = ???
+  override lazy val id: ModifierId = Constants.hash(scorex.core.utils.concatBytes(chunkRootHashes :+ blockId))
 
   override type M = UTXOSnapshotManifest
 
   override lazy val serializer: Serializer[UTXOSnapshotManifest] = ???
 
   override lazy val json: Json = ???
+
+  lazy val rootHash: Array[Byte] = Algos.merkleTreeRoot(chunkRootHashes)
 }
 
 object UTXOSnapshotManifest {
   val ModifierTypeId: Byte = 106: Byte
+
+  def validate(manifest: UTXOSnapshotManifest, header: Header): Try[Unit] = Try {
+    require(manifest.blockId sameElements header.id)
+    require(manifest.rootHash sameElements header.stateRoot)
+    ???
+  }
 }
 

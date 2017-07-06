23e0c116d990ec4093f06d2308ee1ddc85fa2eba
catena <catena@protonmail.com>
2017-07-06 11:31:54 +0300

    Add headerID to BlockTransactinos

--- src/main/scala/org/ergoplatform/modifiers/history/BlockTransactions.scala
+++ src/main/scala/org/ergoplatform/modifiers/history/BlockTransactions.scala
@@ -3,14 +3,14 @@ package org.ergoplatform.modifiers.history
 import com.google.common.primitives.{Bytes, Shorts}
 import io.circe.Json
 import org.ergoplatform.modifiers.mempool.{AnyoneCanSpendTransaction, AnyoneCanSpendTransactionSerializer}
-import org.ergoplatform.settings.Algos
+import org.ergoplatform.settings.{Algos, Constants}
 import scorex.core.NodeViewModifier.{ModifierId, ModifierTypeId}
 import scorex.core.serialization.Serializer
 import scorex.core.utils.concatBytes
 
 import scala.util.Try
 
-case class BlockTransactions(txs: Seq[AnyoneCanSpendTransaction]) extends HistoryModifier {
+case class BlockTransactions(headerId: ModifierId, txs: Seq[AnyoneCanSpendTransaction]) extends HistoryModifier {
 
   override val modifierTypeId: ModifierTypeId = BlockTransactions.ModifierTypeId
 
@@ -33,19 +33,21 @@ object BlockTransactions {
 object BlockTransactionsSerializer extends Serializer[BlockTransactions] {
   override def toBytes(obj: BlockTransactions): Array[ModifierTypeId] = {
     val txsBytes = concatBytes(obj.txs.map(tx => Bytes.concat(Shorts.toByteArray(tx.bytes.length.toShort), tx.bytes)))
-    Bytes.concat(txsBytes)
+    Bytes.concat(obj.headerId, txsBytes)
   }
 
   override def parseBytes(bytes: Array[ModifierTypeId]): Try[BlockTransactions] = Try {
+    val headerId = bytes.slice(0, Constants.ModifierIdSize)
+
     def parseTransactions(index: Int, acc: Seq[AnyoneCanSpendTransaction]): BlockTransactions = {
       if (index == bytes.length) {
-        BlockTransactions(acc)
+        BlockTransactions(headerId, acc)
       } else {
         val txLength = Shorts.fromByteArray(bytes.slice(index, index + 2))
         val tx = AnyoneCanSpendTransactionSerializer.parseBytes(bytes.slice(index + 2, index + 2 + txLength)).get
         parseTransactions(index + 2 + txLength, acc :+ tx)
       }
     }
-    parseTransactions(0, Seq())
+    parseTransactions(Constants.ModifierIdSize, Seq())
   }
 }

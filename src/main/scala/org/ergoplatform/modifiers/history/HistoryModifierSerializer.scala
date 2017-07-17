0d6e0fbfd64cd6da66ca54513119edc4be7c5ca3
catena <catena@protonmail.com>
2017-07-17 19:13:32 +0300

    HistorySerializer

--- src/main/scala/org/ergoplatform/modifiers/history/HistoryModifierSerializer.scala
+++ src/main/scala/org/ergoplatform/modifiers/history/HistoryModifierSerializer.scala
@@ -6,8 +6,12 @@ import scala.util.Try
 
 object HistoryModifierSerializer extends Serializer[HistoryModifier] {
   override def toBytes(obj: HistoryModifier): Array[Byte] = obj match {
-    case h: Header =>
-      Header.ModifierTypeId +: HeaderSerializer.toBytes(h)
+    case m: Header =>
+      Header.ModifierTypeId +: HeaderSerializer.toBytes(m)
+    case m: ADProofs =>
+      ADProofs.ModifierTypeId +: ADProofsSerializer.toBytes(m)
+    case m: BlockTransactions =>
+      BlockTransactions.ModifierTypeId +: BlockTransactionsSerializer.toBytes(m)
     case m =>
       throw new Error(s"Serialization for unknown modifier: ${m.json.noSpaces}")
   }
@@ -16,6 +20,10 @@ object HistoryModifierSerializer extends Serializer[HistoryModifier] {
     bytes.head match {
       case Header.ModifierTypeId =>
         HeaderSerializer.parseBytes(bytes.tail).get
+      case ADProofs.ModifierTypeId =>
+        ADProofsSerializer.parseBytes(bytes.tail).get
+      case BlockTransactions.ModifierTypeId =>
+        BlockTransactionsSerializer.parseBytes(bytes.tail).get
       case m =>
         throw new Error(s"Deserialization for unknown type byte: $m")
     }

e7e13922eda11199d627e19190d933e0dc8ac0a2
Alex Chepurnoy <alex.chepurnoy@iohk.io>
2017-07-25 12:58:16 +0300

    ErgoState made abstract

--- src/main/scala/org/ergoplatform/nodeView/state/DigestState.scala
+++ src/main/scala/org/ergoplatform/nodeView/state/DigestState.scala
@@ -1,5 +1,23 @@
 package org.ergoplatform.nodeView.state
 
-class DigestState extends ErgoState[DigestState] {
+import org.ergoplatform.modifiers.ErgoPersistentModifier
+import scorex.core.transaction.state.MinimalState.VersionTag
+import scorex.core.utils.ScorexLogging
 
+import scala.util.Try
+
+class DigestState extends ErgoState[DigestState] with ScorexLogging {
+  override lazy val rootHash = ???
+
+  override def version: VersionTag = ???
+
+  override def validate(mod: ErgoPersistentModifier): Try[Unit] = mod match {
+    case a: Any => log.info(s"Modifier not validated: $a"); Try(this)
+  }
+
+  override def applyModifier(mod: ErgoPersistentModifier): Try[DigestState] = mod match {
+    case a: Any => log.info(s"Unhandlend modifier: $a"); Try(this)
+  }
+
+  override def rollbackTo(version: VersionTag): Try[DigestState] = ???
 }

832399baab80f4b07da64e7d4fb87e062f0ad094
Alex Chepurnoy <alex.chepurnoy@iohk.io>
2017-07-25 13:41:41 +0300

    ErgoState: operations() implementation

--- src/main/scala/org/ergoplatform/nodeView/state/ErgoState.scala
+++ src/main/scala/org/ergoplatform/nodeView/state/ErgoState.scala
@@ -4,7 +4,7 @@ import org.ergoplatform.modifiers.ErgoPersistentModifier
 import org.ergoplatform.modifiers.mempool.AnyoneCanSpendTransaction
 import org.ergoplatform.modifiers.mempool.proposition.{AnyoneCanSpendNoncedBox, AnyoneCanSpendProposition}
 import org.ergoplatform.settings.ErgoSettings
-import scorex.core.transaction.state.MinimalState
+import scorex.core.transaction.state.{BoxStateChanges, Insertion, MinimalState, Removal}
 import scorex.core.transaction.state.MinimalState.VersionTag
 import ErgoState.Digest
 import scorex.core.utils.ScorexLogging
@@ -38,6 +38,15 @@ trait ErgoState[IState <: MinimalState[AnyoneCanSpendProposition,
   //TODO implement correctly
   def stateHeight: Int = 0
 
+  /**
+    * Extract ordered sequence of operations on UTXO set from set of transactions
+    */
+  def operations(txs: Seq[AnyoneCanSpendTransaction]): BoxStateChanges[AnyoneCanSpendProposition, AnyoneCanSpendNoncedBox] =
+    BoxStateChanges[AnyoneCanSpendProposition, AnyoneCanSpendNoncedBox](txs.flatMap { tx =>
+      tx.boxIdsToOpen.map(id => Removal[AnyoneCanSpendProposition, AnyoneCanSpendNoncedBox](id)) ++
+        tx.newBoxes.map(b => Insertion[AnyoneCanSpendProposition, AnyoneCanSpendNoncedBox](b))
+    })
+
   override def version: VersionTag
 
   override def validate(mod: ErgoPersistentModifier): Try[Unit]
@@ -54,7 +63,7 @@ object ErgoState {
   type Digest = Array[Byte]
 
   def readOrGenerate(settings: ErgoSettings) =
-    if(settings.ADState) new DigestState else new UtxoState
+    if (settings.ADState) new DigestState else new UtxoState
 
   //Initial state even before genesis block application
   val initialState: UtxoState = new UtxoState

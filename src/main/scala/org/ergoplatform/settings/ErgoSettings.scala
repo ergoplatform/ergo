ac5f2c59d15af94f853e08624d65ee5b93c4f5cb
catena <catena@protonmail.com>
2017-08-02 12:11:52 +0300

    Comments to settings. This closes #9

--- src/main/scala/org/ergoplatform/settings/ErgoSettings.scala
+++ src/main/scala/org/ergoplatform/settings/ErgoSettings.scala
@@ -5,9 +5,27 @@ import scorex.core.settings.Settings
 trait ErgoSettings extends Settings {
   val dataDir: String = dataDirOpt.getOrElse("/tmp/ergo")
   //todo read all from settings file
+
+  /**
+    * Keep state roothash only and validate transactions via ADProofs
+    */
   val ADState: Boolean = false
-  val verifyTransactions: Boolean = false
-  val poPoWBootstrap: Boolean = false
+  /**
+    * Download block transactions and verify them (requires BlocksToKeep == 0)
+    */
+  val verifyTransactions: Boolean = true
+  /**
+    * Number of last blocks to keep with transactions and ADproofs, for all other blocks only header will be stored.
+    * Keep all blocks from genesis if negative
+    */
   val blocksToKeep: Int = 10
+  /**
+    * Download PoPoW proof on node bootstrtap
+    */
+  val poPoWBootstrap: Boolean = false
+  /**
+    * Minimal suffix size for PoPoW proof (may be pre-defined constant or settings parameter)
+    */
   val minimalSuffix: Int = 10
+
 }

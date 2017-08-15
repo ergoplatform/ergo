938061f9f4563509c17f72f9df38c16744a85f2b
Alex Chepurnoy <alex.chepurnoy@iohk.io>
2017-08-15 20:42:50 +0300

    MempoolFilterPerformanceTest todo

--- src/test/scala/org/ergoplatform/ErgoSanity.scala
+++ src/test/scala/org/ergoplatform/ErgoSanity.scala
@@ -23,6 +23,7 @@ class ErgoSanity extends HistoryAppendBlockTest[P, TX, PM, SI, HT]
   //with WalletSecretsTest[P, TX, PM]
   //with StateRollbackTest[P, TX, PM, B, ST, SI, HT, MPool]
   with MempoolTransactionsTest[P, TX, MPool]
+  // todo: convert MempoolFilterPerformanceTest to benchmark
   //  with MempoolFilterPerformanceTest[P, TX, MPool]
   // with MempoolRemovalTest[P, TX, MPool, PM, HT, SI]
   //  with BoxStateChangesGenerationTest[P, TX, PM, B, ST, SI, HT]

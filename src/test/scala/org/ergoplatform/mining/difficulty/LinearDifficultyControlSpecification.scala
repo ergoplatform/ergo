8eab74a204eabdd4a602aaa449dd9a80d87bddfb
catena <catena@protonmail.com>
2017-08-16 18:42:11 +0300

    Calculate() in integer arithmetics

--- src/test/scala/org/ergoplatform/mining/difficulty/LinearDifficultyControlSpecification.scala
+++ src/test/scala/org/ergoplatform/mining/difficulty/LinearDifficultyControlSpecification.scala
@@ -2,6 +2,7 @@ package org.ergoplatform.mining.difficulty
 
 import org.ergoplatform.settings.Constants
 import org.ergoplatform.utils.ErgoGenerators
+import org.scalacheck.{Arbitrary, Gen}
 import org.scalatest.prop.{GeneratorDrivenPropertyChecks, PropertyChecks}
 import org.scalatest.{Matchers, PropSpec}
 
@@ -14,6 +15,8 @@ class LinearDifficultyControlSpecification extends PropSpec
   with Matchers
   with ErgoGenerators {
 
+  val precision = 0.0001
+  val minDiff = (BigDecimal(1) / precision).toBigInt()
   val Epoch = 123
   val control = new LinearDifficultyControl(1.minute, Epoch)
   val UseLastEpochs = control.UseLastEpochs
@@ -48,15 +51,61 @@ class LinearDifficultyControlSpecification extends PropSpec
     }
   }
 
+  property("calculate() vectors") {
+    val diff = BigInt("675204474840679645414180963439886534428")
+    control.calculate(Seq((799167010, diff), (799167133, diff), (799167256, diff), (799167379, diff))) shouldBe diff
+
+    control.calculate(Seq((123, diff), (246, diff), (369, diff), (492, diff))) shouldBe diff
+
+    control.calculate(Vector((123, diff), (246, diff * 2), (369, diff * 2), (492, diff))) shouldBe (diff * 3 / 2)
+
+    control.calculate(Vector((123, diff), (246, diff * 2), (369, diff * 3), (492, diff * 4))) shouldBe BigInt("3376022374203398227070904817199432672139")
+
+  }
+
   property("calculate() for constant hashrate") {
-    val precision = 0.0001
-    forAll { (startEpoch: Int, diff: BigInt) =>
-      whenever(BigDecimal(diff) > BigDecimal(1 / precision) && startEpoch >= 0 && startEpoch < Int.MaxValue - 4) {
+    forAll(epochGen, diffGen) { (startEpoch: Int, diff: BigInt) =>
+      exitOnError {
         val previousDifficulties = (startEpoch * Epoch until (UseLastEpochs + startEpoch) * Epoch by Epoch).map(i => (i, diff))
         val newDiff = control.calculate(previousDifficulties)
         (BigDecimal(newDiff - diff) / BigDecimal(diff)).toDouble should be < precision
       }
     }
   }
+
+
+  property("calculate() for linear hashrate growth") {
+    forAll(epochGen, diffGen) { (startEpoch: Int, diff: BigInt) =>
+      exitOnError {
+        val previousDifficulties = (startEpoch * Epoch until (UseLastEpochs + startEpoch) * Epoch by Epoch).map(i => (i, diff * i))
+        val newDiff = control.calculate(previousDifficulties)
+        val expected = previousDifficulties.map(_._2).max + diff
+        equalsWithPrecision(expected, newDiff)
+      }
+    }
+  }
+
+  def exitOnError(r: => Unit): Unit = Try {
+    r
+  }.recoverWith { case e =>
+    e.printStackTrace()
+    System.exit(1)
+    throw e
+  }
+
+  def equalsWithPrecision(i: BigInt, j: BigInt): Unit = {
+    require((BigDecimal(i - j) / BigDecimal(j)).toDouble < precision, s"$i and $j are too different")
+  }
+
+  def diffGen: Gen[BigInt] = for {
+    rnd <- Arbitrary.arbitrary[BigInt]
+    diff = if (rnd < 0) -rnd + minDiff else rnd + minDiff
+    _ = assert(diff > 0, s"$diff,$minDiff,$rnd")
+  } yield diff
+
+
+  def epochGen: Gen[Int] = for {
+    epoch <- Arbitrary.arbitrary[Int] if epoch > 0 && epoch < Int.MaxValue - 4
+  } yield epoch
 
 }

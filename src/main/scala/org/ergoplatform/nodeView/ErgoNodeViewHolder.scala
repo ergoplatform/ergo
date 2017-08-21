bfcbaf23056df40f83fa5bd994506826773efc16
Alex Chepurnoy <alex.chepurnoy@iohk.io>
2017-08-21 11:52:40 +0300

    DigestErgoNodeViewHolder.genesisState() impl

--- src/main/scala/org/ergoplatform/nodeView/ErgoNodeViewHolder.scala
+++ src/main/scala/org/ergoplatform/nodeView/ErgoNodeViewHolder.scala
@@ -1,7 +1,9 @@
 package org.ergoplatform.nodeView
 
+import java.io.File
+
 import org.ergoplatform.modifiers.ErgoPersistentModifier
-import org.ergoplatform.modifiers.mempool.proposition.{AnyoneCanSpendNoncedBox, AnyoneCanSpendProposition}
+import org.ergoplatform.modifiers.mempool.proposition.AnyoneCanSpendProposition
 import org.ergoplatform.modifiers.mempool.{AnyoneCanSpendTransaction, AnyoneCanSpendTransactionSerializer}
 import org.ergoplatform.nodeView.history.{ErgoHistory, ErgoSyncInfo}
 import org.ergoplatform.nodeView.mempool.ErgoMemPool
@@ -24,6 +26,7 @@ abstract class ErgoNodeViewHolder(settings: ErgoSettings)
   override type VL = ErgoWallet
   override type MP = ErgoMemPool
 
+  //todo: complete this
   override val modifierCompanions: Map[ModifierTypeId, Serializer[_ <: NodeViewModifier]] =
     Map(???,
       Transaction.ModifierTypeId -> AnyoneCanSpendTransactionSerializer)
@@ -33,24 +36,48 @@ abstract class ErgoNodeViewHolder(settings: ErgoSettings)
     reason.printStackTrace()
     System.exit(100) // this actor shouldn't be restarted at all so kill the whole app if that happened
   }
+}
+
+class UtxoErgoNodeViewHolder(settings: ErgoSettings) extends ErgoNodeViewHolder(settings){
+  override type MS = UtxoState
 
   /**
     * Hard-coded initial view all the honest nodes in a network are making progress from.
     */
-  override protected def genesisState: (HIS, MS, VL, MP) = ???
+  override protected def genesisState: (ErgoHistory, UtxoState, ErgoWallet, ErgoMemPool) = ???
 
   /**
     * Restore a local view during a node startup. If no any stored view found
     * (e.g. if it is a first launch of a node) None is to be returned
     */
-  override def restoreState(): Option[(HIS, MS, VL, MP)] = ???
-}
-
-class UtxoErgoNodeViewHolder(settings: ErgoSettings) extends ErgoNodeViewHolder(settings){
-  override type MS = UtxoState
+  override def restoreState(): Option[(ErgoHistory, UtxoState, ErgoWallet, ErgoMemPool)] = ???
 }
 
 
 class DigestErgoNodeViewHolder(settings: ErgoSettings) extends ErgoNodeViewHolder(settings){
   override type MS = DigestState
+
+  /**
+    * Hard-coded initial view all the honest nodes in a network are making progress from.
+    */
+  override protected def genesisState: (ErgoHistory, DigestState, ErgoWallet, ErgoMemPool) = {
+    val dir = new File("/tmp/view")
+
+    val digestState = ErgoState.generateGenesisDigestState(dir)
+
+    //todo: ensure that history is in certain mode
+    val history = ErgoHistory.readOrGenerate(settings)
+
+    val wallet = ErgoWallet.readOrGenerate(settings)
+
+    val memPool = ErgoMemPool.empty
+
+    (history, digestState, wallet, memPool)
+  }
+
+  /**
+    * Restore a local view during a node startup. If no any stored view found
+    * (e.g. if it is a first launch of a node) None is to be returned
+    */
+  override def restoreState(): Option[(ErgoHistory, DigestState, ErgoWallet, ErgoMemPool)] = ???
 }

cd99c45bf4f8962ae0adb7bd3e3c850f3941e287
Alex Chepurnoy <alex.chepurnoy@iohk.io>
2017-08-22 14:07:44 +0300

    random folder in DigestErgoNodeViewHolderSpecification

--- src/test/scala/org/ergoplatform/nodeView/DigestErgoNodeViewHolderSpecification.scala
+++ src/test/scala/org/ergoplatform/nodeView/DigestErgoNodeViewHolderSpecification.scala
@@ -1,8 +1,11 @@
 package org.ergoplatform.nodeView
 
+import java.io.File
+
 import akka.actor.{ActorSystem, Props}
-import akka.testkit.{ImplicitSender, TestActors, TestKit}
+import akka.testkit.{ImplicitSender, TestKit}
 import io.circe
+import org.ergoplatform.modifiers.history.Header
 import org.ergoplatform.nodeView.history.ErgoHistory
 import org.ergoplatform.nodeView.mempool.ErgoMemPool
 import org.ergoplatform.nodeView.state.{DigestState, ErgoState}
@@ -13,6 +16,9 @@ import org.scalatest.{BeforeAndAfterAll, Matchers, PropSpecLike}
 import org.scalatest.prop.{GeneratorDrivenPropertyChecks, PropertyChecks}
 import scorex.core.NodeViewHolder.GetDataFromCurrentView
 import scorex.testkit.TestkitHelpers
+
+import scala.reflect.io.Path
+import scala.util.Random
 
 class DigestErgoNodeViewHolderSpecification extends
   TestKit(ActorSystem("DigestErgoNodeViewHolderSpec"))
@@ -26,20 +32,36 @@ class DigestErgoNodeViewHolderSpecification extends
   with BeforeAndAfterAll {
 
   lazy val settings: ErgoSettings = new ErgoSettings {
+    override lazy val dataDir: String = s"/tmp/ergo/${Random.nextInt}"
     override def settingsJSON: Map[String, circe.Json] = Map()
   }
 
+  override def beforeAll {
+    new File(settings.dataDir).mkdirs()
+  }
+
   override def afterAll {
+    Path(new File(settings.dataDir)).deleteRecursively()
     TestKit.shutdownActorSystem(system)
   }
 
-  property("genesis state digest") {
+  property("genesis - state digest") {
     val digestHolder = system.actorOf(Props(classOf[DigestErgoNodeViewHolder], settings))
 
     digestHolder ! GetDataFromCurrentView[ErgoHistory, DigestState, ErgoWallet, ErgoMemPool, Boolean] { v =>
-      println("rh: " + v.state.rootHash.sameElements(ErgoState.afterGenesisStateDigest))
       v.state.rootHash.sameElements(ErgoState.afterGenesisStateDigest)
     }
     expectMsg(true)
   }
+
+  /*
+  //todo: fix
+  property("genesis - history") {
+    val digestHolder = system.actorOf(Props(classOf[DigestErgoNodeViewHolder], settings))
+
+    digestHolder ! GetDataFromCurrentView[ErgoHistory, DigestState, ErgoWallet, ErgoMemPool, Header] { v =>
+      v.history.bestHeader
+    }
+    expectNoMsg()
+  } */
 }

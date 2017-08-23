96012651264ffff1ff943ecf9a63f6e09137d6f4
Alex Chepurnoy <alex.chepurnoy@iohk.io>
2017-08-23 22:27:59 +0300

    Interlink vector genesis block fix, hanging tests fix

--- src/main/scala/org/ergoplatform/modifiers/history/PoPoWProof.scala
+++ src/main/scala/org/ergoplatform/modifiers/history/PoPoWProof.scala
@@ -60,20 +60,25 @@ object PoPoWProof {
   }
 
   def constructInterlinkVector(parent: Header): Seq[Array[Byte]] = {
-    val genesisId = parent.interlinks.head
+    if (parent.height == 0) {                            //genesis block
+      Seq(parent.id)
+    } else {
 
-    def generateInnerchain(curDifficulty: BigInt, acc: Seq[Array[Byte]]): Seq[Array[Byte]] = {
-      if (parent.realDifficulty >= curDifficulty) {
-        generateInnerchain(curDifficulty * 2, acc :+ parent.id)
-      } else {
-        parent.interlinks.find(pId => Algos.blockIdDifficulty(pId) >= curDifficulty) match {
-          case Some(id) if !(id sameElements genesisId) => generateInnerchain(curDifficulty * 2, acc :+ id)
-          case _ => acc
+      val genesisId = parent.interlinks.head
+
+      def generateInnerchain(curDifficulty: BigInt, acc: Seq[Array[Byte]]): Seq[Array[Byte]] = {
+        if (parent.realDifficulty >= curDifficulty) {
+          generateInnerchain(curDifficulty * 2, acc :+ parent.id)
+        } else {
+          parent.interlinks.find(pId => Algos.blockIdDifficulty(pId) >= curDifficulty) match {
+            case Some(id) if !(id sameElements genesisId) => generateInnerchain(curDifficulty * 2, acc :+ id)
+            case _ => acc
+          }
         }
       }
-    }
 
-    genesisId +: generateInnerchain(Constants.InitialDifficulty * 2, Seq[Array[Byte]]())
+      genesisId +: generateInnerchain(Constants.InitialDifficulty * 2, Seq[Array[Byte]]())
+    }
   }
 
 }
@@ -102,6 +107,7 @@ object PoPoWProofSerializer extends Serializer[PoPoWProof] {
     val i = bytes(2)
     val headSuffixLength = Shorts.fromByteArray(bytes.slice(3, 5))
     val headSuffix = HeaderSerializer.parseBytes(bytes.slice(5, 5 + headSuffixLength)).get
+
     def parseSuffixes(index: Int, acc: Seq[Header]): (Int, Seq[Header]) = {
       if (acc.length == k) (index, acc.reverse)
       else {
@@ -111,6 +117,7 @@ object PoPoWProofSerializer extends Serializer[PoPoWProof] {
         parseSuffixes(index + 2 + l, headerWithoutInterlinks.copy(interlinks = interlinks) +: acc)
       }
     }
+
     var (index, suffix) = parseSuffixes(5 + headSuffixLength, Seq(headSuffix))
     val innerchainLength = Shorts.fromByteArray(bytes.slice(index, index + 2))
     index = index + 2
--- src/test/scala/org/ergoplatform/nodeView/history/NonVerifyADHistorySpecification.scala
+++ src/test/scala/org/ergoplatform/nodeView/history/NonVerifyADHistorySpecification.scala
@@ -11,17 +11,17 @@ import scala.util.Random
 class NonVerifyADHistorySpecification extends HistorySpecification {
 
   private def genHistory() =
-    generateHistory(verify = false, adState = true, popow = false, toKeep = 0, nonce = Random.nextLong())
+    generateHistory(verify = false, adState = true, popow = false, toKeep = 0, nonce = Random.nextLong(), epoch = 1000)
       .ensuring(_.bestFullBlockOpt.isEmpty)
 
-  property("Should apply UTXOSnapshotChunks") {
-    val history = genHistory()
+  private lazy val popowHistory = ensureMinimalHeight(genHistory(), 300)
 
+  property("Should apply UTXOSnapshotChunks") {
     forAll(randomUTXOSnapshotChunkGen) { snapshot: UTXOSnapshotChunk =>
-      history.applicable(snapshot) shouldBe true
-      val processInfo = history.append(snapshot).get._2
+      popowHistory.applicable(snapshot) shouldBe true
+      val processInfo = popowHistory.append(snapshot).get._2
       processInfo.toApply shouldEqual Seq(snapshot)
-      history.applicable(snapshot) shouldBe false
+      popowHistory.applicable(snapshot) shouldBe false
     }
   }
 
@@ -38,10 +38,8 @@ class NonVerifyADHistorySpecification extends HistorySpecification {
 
 
   property("PoPoW history should be able to apply PoPoWProof proofs") {
-    var history = genHistory()
+    val proof = popowHistory.constructPoPoWProof(5, 5).get
 
-    history = ensureMinimalHeight(history, 100)
-    val proof = history.constructPoPoWProof(5, 5).get
     var newHistory = generateHistory(verify = false, adState = true, popow = true, 0, System.nanoTime())
     newHistory.applicable(proof) shouldBe true
     newHistory = newHistory.append(proof).get._1
@@ -49,31 +47,24 @@ class NonVerifyADHistorySpecification extends HistorySpecification {
   }
 
   property("non-PoPoW history should ignore PoPoWProof proofs") {
-    var history = genHistory()
-    history = ensureMinimalHeight(history, 100)
-    val proof = history.constructPoPoWProof(5, 5).get
+    val proof = popowHistory.constructPoPoWProof(5, 5).get
+
     val newHistory = generateHistory(verify = false, adState = true, popow = false, 0)
     newHistory.applicable(proof) shouldBe false
   }
 
   property("constructPoPoWProof() should generate valid proof") {
-    var history = genHistory()
-
-    history = ensureMinimalHeight(history, 100)
     forAll(smallInt, smallInt) { (m, k) =>
-      val proof = history.constructPoPoWProof(m + 1, k + 1).get
+      val proof = popowHistory.constructPoPoWProof(m + 1, k + 1).get
       PoPoWProof.validate(proof) shouldBe 'success
     }
   }
 
   property("lastHeaders() should return correct number of blocks") {
-    var history = genHistory()
-
-    history = ensureMinimalHeight(history, BlocksInChain + 1)
     forAll(smallInt) { m =>
-      val lastHeaders = history.lastHeaders(m)
+      val lastHeaders = popowHistory.lastHeaders(m)
       if (m > 0) {
-        lastHeaders.last shouldBe history.bestHeaderOpt.get
+        lastHeaders.last shouldBe popowHistory.bestHeaderOpt.get
       }
       lastHeaders.length shouldBe m
     }

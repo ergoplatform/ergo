86f0d828a6f1bb6d7d1eb11e9df1923991e9070c
Sergey Tolmachev <tolsi.ru@gmail.com>
2017-08-24 19:54:27 +0300

    Some invalid cases

--- src/main/scala/org/ergoplatform/mining/MinerTest.scala
+++ src/main/scala/org/ergoplatform/mining/MinerTest.scala
@@ -12,5 +12,9 @@ object MinerTest extends App {
     RequiredDifficulty.encodeCompactBits(Constants.InitialDifficulty),
     ErgoFullBlock.genesis.header, Array.emptyByteArray, ErgoFullBlock.genesis.aDProofs.get, Seq(AnyoneCanSpendTransaction(IndexedSeq.empty, IndexedSeq(10L))), 1L, Array.emptyByteArray, n, k)
   assert(Miner.isBlockValid(b, n, k))
+  val invB = b.header.equihashSolutions.clone()
+  invB(0) = 1
+  assert(!Miner.isBlockValid(b.copy(header = b.header.copy(equihashSolutions = invB)), n, k))
+  assert(!Miner.isBlockValid(b.copy(header = b.header.copy(height = 3)), n, k))
   println(b.id)
 }

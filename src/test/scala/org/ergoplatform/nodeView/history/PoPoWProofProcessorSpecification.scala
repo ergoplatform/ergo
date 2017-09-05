5b817c2583b1e0409bbdad3beb05ff5b24a1a37c
Alex Chepurnoy <alex.chepurnoy@iohk.io>
2017-09-05 23:36:21 +0300

    PoPoWProof.constructInterlinkVector test, FakePowScheme improvements

--- src/test/scala/org/ergoplatform/nodeView/history/PoPoWProofProcessorSpecification.scala
+++ src/test/scala/org/ergoplatform/nodeView/history/PoPoWProofProcessorSpecification.scala
@@ -1,6 +1,6 @@
 package org.ergoplatform.nodeView.history
 
-import org.ergoplatform.modifiers.history.{PoPoWProof, PoPoWProofSerializer}
+import org.ergoplatform.modifiers.history.{FakePowScheme, PoPoWProof, PoPoWProofSerializer}
 import org.ergoplatform.settings.Constants
 import org.ergoplatform.utils.NoShrink
 import org.scalacheck.Gen
@@ -19,6 +19,56 @@ class PoPoWProofProcessorSpecification extends HistorySpecification with NoShrin
     history.bestHeaderOpt.toSeq).ensuring(_.headers.count(_.realDifficulty > Constants.InitialDifficulty * 2) > MaxK)
   private lazy val popowHistory = applyHeaderChain(history, chain)
 
+  property("PoPoWProof.constructInterlinkVector"){
+
+    //genesis
+    val h1 = powScheme.prove(None, Constants.InitialNBits, Array.fill(33)(0: Byte), Array.fill(32)(0: Byte),
+      Array.fill(32)(0: Byte), 0L, Array.fill(5)(0:Byte))
+
+    h1.interlinks.length shouldBe 0
+
+    val zeroLevelPowScheme = new FakePowScheme(Some(0))
+
+    //first after genesis
+    val h2 = zeroLevelPowScheme.prove(Some(h1), Constants.InitialNBits, Array.fill(33)(0: Byte), Array.fill(32)(0: Byte),
+      Array.fill(32)(0: Byte), 0L, Array.fill(5)(0:Byte))
+
+    h2.interlinks.length shouldBe 1
+
+    val h3 = zeroLevelPowScheme.prove(Some(h2), Constants.InitialNBits, Array.fill(33)(0: Byte), Array.fill(32)(0: Byte),
+      Array.fill(32)(0: Byte), 0L, Array.fill(5)(0:Byte))
+
+    h3.interlinks.length shouldBe 1
+
+    val oneLevelPowScheme = new FakePowScheme(Some(1))
+
+    val h4 = oneLevelPowScheme.prove(Some(h3), Constants.InitialNBits, Array.fill(33)(0: Byte), Array.fill(32)(0: Byte),
+      Array.fill(32)(0: Byte), 0L, Array.fill(5)(0:Byte))
+
+    h4.interlinks.length shouldBe 1
+
+    val h5 = zeroLevelPowScheme.prove(Some(h4), Constants.InitialNBits, Array.fill(33)(0: Byte), Array.fill(32)(0: Byte),
+      Array.fill(32)(0: Byte), 0L, Array.fill(5)(0:Byte))
+
+    h5.interlinks.length shouldBe 2
+
+    val h6 = zeroLevelPowScheme.prove(Some(h5), Constants.InitialNBits, Array.fill(33)(0: Byte), Array.fill(32)(0: Byte),
+      Array.fill(32)(0: Byte), 0L, Array.fill(5)(0:Byte))
+
+    h6.interlinks.length shouldBe 2
+
+    val twoLevelPowScheme = new FakePowScheme(Some(2))
+
+    val h7 = twoLevelPowScheme.prove(Some(h6), Constants.InitialNBits, Array.fill(33)(0: Byte), Array.fill(32)(0: Byte),
+      Array.fill(32)(0: Byte), 0L, Array.fill(5)(0:Byte))
+
+    h7.interlinks.length shouldBe 2
+
+    val h8 = zeroLevelPowScheme.prove(Some(h7), Constants.InitialNBits, Array.fill(33)(0: Byte), Array.fill(32)(0: Byte),
+      Array.fill(32)(0: Byte), 0L, Array.fill(5)(0:Byte))
+
+    h8.interlinks.length shouldBe 3
+  }
 
   property("Valid PoPoWProof generation") {
     forAll(mkGen) { case (m, k) =>

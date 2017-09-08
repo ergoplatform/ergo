fc891bb7683b263128745b3ae857b4a42dc4dad8
catena <catena@protonmail.com>
2017-09-08 15:57:23 +0300

    Rollback should be to version, not a rootHash

--- src/main/scala/org/ergoplatform/utils/package.scala
+++ src/main/scala/org/ergoplatform/utils/package.scala
@@ -2,13 +2,10 @@ package org.ergoplatform
 
 import io.iohk.iodb.ByteArrayWrapper
 import scorex.core.{ModifierId, VersionTag}
-import scorex.crypto.authds._
 
 package object utils {
   implicit def byteArrayWrapper(a: Array[Byte]): ByteArrayWrapper = ByteArrayWrapper(a)
 
   implicit def idToTag(id: ModifierId): VersionTag = VersionTag @@ id
-
-  implicit def digestToTag(id: ADDigest): VersionTag = VersionTag @@ id
 
 }

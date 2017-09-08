b5449372e1f6e9218015626fbcd67a6f4e2c3c13
catena <catena@protonmail.com>
2017-09-08 13:18:21 +0300

    FIx digest state rollback test

--- src/main/scala/org/ergoplatform/utils/package.scala
+++ src/main/scala/org/ergoplatform/utils/package.scala
@@ -7,6 +7,8 @@ import scorex.crypto.authds._
 package object utils {
   implicit def byteArrayWrapper(a: Array[Byte]): ByteArrayWrapper = ByteArrayWrapper(a)
 
+  implicit def idToTag(id: ModifierId): VersionTag = VersionTag @@ id
+
   implicit def digestToTag(id: ADDigest): VersionTag = VersionTag @@ id
 
 }

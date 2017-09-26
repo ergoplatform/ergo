c7efe4e2c3147c59b5f62eb8cdf2777813a9ceb6
Alex Chepurnoy <alex.chepurnoy@iohk.io>
2017-09-26 16:19:30 +0300

    restore for utxo nvh

--- src/main/scala/org/ergoplatform/ErgoApp.scala
+++ src/main/scala/org/ergoplatform/ErgoApp.scala
@@ -1,9 +1,7 @@
 package org.ergoplatform
 
-import java.io.File
 
 import akka.actor.ActorRef
-import com.typesafe.config.{Config, ConfigFactory}
 import org.ergoplatform.modifiers.ErgoPersistentModifier
 import org.ergoplatform.modifiers.mempool.AnyoneCanSpendTransaction
 import org.ergoplatform.modifiers.mempool.proposition.AnyoneCanSpendProposition
@@ -23,19 +21,18 @@ class ErgoApp(args: Seq[String]) extends Application {
   val ergoSettings = ErgoSettings.read(args.headOption)
 
   //TODO remove after Scorex update
-  implicit lazy val settings: Settings = ergoSettings.scorexSettings
+  override implicit lazy val settings: Settings = ergoSettings.scorexSettings
 
   override val apiRoutes: Seq[ApiRoute] = Seq()
   override val apiTypes: Set[Class[_]] = Set()
+
   override protected val additionalMessageSpecs: Seq[MessageSpec[_]] = Seq()
   override val nodeViewHolderRef: ActorRef = ???
   override val nodeViewSynchronizer: ActorRef = ???
   override val localInterface: ActorRef = ???
-
 }
 
 object ErgoApp extends App {
   new ErgoApp(args).run()
   def forceStopApplication(): Unit = new Thread(() => System.exit(1), "ergo-platform-shutdown-thread").start()
-
 }

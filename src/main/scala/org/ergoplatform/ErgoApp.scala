59908d6df9e47847b3c0dac4d1f5f1297e06cda0
Alex Chepurnoy <alex.chepurnoy@iohk.io>
2017-09-27 16:43:38 +0300

    first runnable version

--- src/main/scala/org/ergoplatform/ErgoApp.scala
+++ src/main/scala/org/ergoplatform/ErgoApp.scala
@@ -1,15 +1,15 @@
 package org.ergoplatform
 
-
-import akka.actor.ActorRef
-import io.circe.Json
+import akka.actor.{ActorRef, Props}
 import org.ergoplatform.modifiers.ErgoPersistentModifier
 import org.ergoplatform.modifiers.mempool.AnyoneCanSpendTransaction
 import org.ergoplatform.modifiers.mempool.proposition.AnyoneCanSpendProposition
 import org.ergoplatform.nodeView.ErgoNodeViewHolder
+import org.ergoplatform.nodeView.history.{ErgoSyncInfo, ErgoSyncInfoMessageSpec}
 import org.ergoplatform.settings.ErgoSettings
 import scorex.core.api.http.ApiRoute
 import scorex.core.app.Application
+import scorex.core.network.NodeViewSynchronizer
 import scorex.core.network.message.MessageSpec
 import scorex.core.settings.Settings
 
@@ -19,20 +19,22 @@ class ErgoApp(args: Seq[String]) extends Application {
   override type PMOD = ErgoPersistentModifier
   override type NVHT = ErgoNodeViewHolder[_]
 
-  val ergoSettings = ErgoSettings.read(args.headOption)
+  lazy val ergoSettings: ErgoSettings = ErgoSettings.read(args.headOption)
 
   //TODO remove after Scorex update
-  override implicit lazy val settings: Settings = new Settings {
-    override def settingsJSON: Map[String, Json] = Map()
-  } // ergoSettings.scorexSettings
+  override implicit lazy val settings: Settings = ergoSettings.scorexSettings
 
-  override val apiRoutes: Seq[ApiRoute] = Seq()
-  override val apiTypes: Set[Class[_]] = Set()
+  override lazy val apiRoutes: Seq[ApiRoute] = Seq()
+  override lazy val apiTypes: Set[Class[_]] = Set()
 
-  override protected val additionalMessageSpecs: Seq[MessageSpec[_]] = Seq()
+  override protected lazy val additionalMessageSpecs: Seq[MessageSpec[_]] = Seq()
   override val nodeViewHolderRef: ActorRef = ErgoNodeViewHolder.createActor(actorSystem, ergoSettings)
-  override val nodeViewSynchronizer: ActorRef = ???
-  override val localInterface: ActorRef = ???
+
+  override val localInterface: ActorRef = actorSystem.actorOf(Props(classOf[ErgoLocalInterface], nodeViewHolderRef))
+
+  override val nodeViewSynchronizer: ActorRef = actorSystem.actorOf(
+    Props(classOf[NodeViewSynchronizer[P, TX, ErgoSyncInfo, ErgoSyncInfoMessageSpec.type]],
+    networkController, nodeViewHolderRef, localInterface, ErgoSyncInfoMessageSpec))
 }
 
 object ErgoApp extends App {

9ff6d45aafd3a36a9bbb4385355efaf917c25b95
Alex Chepurnoy <alex.chepurnoy@iohk.io>
2017-10-05 23:07:42 +0300

    FetchBoxes

--- src/main/scala/org/ergoplatform/local/TransactionGenerator.scala
+++ src/main/scala/org/ergoplatform/local/TransactionGenerator.scala
@@ -1,7 +1,7 @@
 package org.ergoplatform.local
 
 import akka.actor.{Actor, ActorRef, Cancellable}
-import org.ergoplatform.local.TransactionGenerator.{GetState, StartGeneration, StopGeneration}
+import org.ergoplatform.local.TransactionGenerator.{FetchBoxes, StartGeneration, StopGeneration}
 import org.ergoplatform.modifiers.mempool.AnyoneCanSpendTransaction
 import org.ergoplatform.modifiers.mempool.proposition.{AnyoneCanSpendNoncedBox, AnyoneCanSpendProposition}
 import org.ergoplatform.nodeView.history.ErgoHistory
@@ -21,13 +21,13 @@ class TransactionGenerator(viewHolder: ActorRef) extends Actor with ScorexLoggin
 
   override def receive: Receive = {
     case StartGeneration =>
-      txGenerator = context.system.scheduler.schedule(50.millis, 500.millis)(self ! GetState)
+      txGenerator = context.system.scheduler.schedule(50.millis, 500.millis)(self ! FetchBoxes)
 
-    case GetState =>
+    case FetchBoxes =>
       viewHolder ! GetDataFromCurrentView[ErgoHistory, UtxoState, ErgoWallet, ErgoMemPool,
         IndexedSeq[AnyoneCanSpendNoncedBox]] { v =>
 
-        val boxesToSpentCount = Random.nextInt(5) + 1
+        val boxesToSpentCount = Random.nextInt(10) + 2
         (1 to boxesToSpentCount).flatMap(_ => v.state.randomBox())
       }
 
@@ -42,6 +42,6 @@ class TransactionGenerator(viewHolder: ActorRef) extends Actor with ScorexLoggin
 
 object TransactionGenerator {
   case object StartGeneration
-  case object GetState
+  case object FetchBoxes
   case object StopGeneration
 }

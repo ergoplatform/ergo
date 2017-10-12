326c6fa50935e793b4cc181703238f37526c7ec3
Alex Chepurnoy <alex.chepurnoy@iohk.io>
2017-10-12 15:35:47 +0300

    fix for conflicting transactions

--- src/main/scala/org/ergoplatform/local/ErgoMiner.scala
+++ src/main/scala/org/ergoplatform/local/ErgoMiner.scala
@@ -1,6 +1,7 @@
 package org.ergoplatform.local
 
 import akka.actor.{Actor, ActorRef}
+import io.iohk.iodb.ByteArrayWrapper
 import org.ergoplatform.local.ErgoMiner.{MineBlock, ProduceCandidate, StartMining, StopMining}
 import org.ergoplatform.modifiers.history.CandidateBlock
 import org.ergoplatform.modifiers.mempool.AnyoneCanSpendTransaction
@@ -12,7 +13,6 @@ import org.ergoplatform.settings.{Constants, ErgoSettings}
 import scorex.core.LocalInterface.LocallyGeneratedModifier
 import scorex.core.NodeViewHolder.GetDataFromCurrentView
 import scorex.core.utils.ScorexLogging
-import scorex.crypto.authds.ADKey
 
 import scala.util.{Failure, Try}
 import scala.concurrent.duration._
@@ -40,19 +40,24 @@ class ErgoMiner(ergoSettings: ErgoSettings, viewHolder: ActorRef) extends Actor 
         if (v.pool.size > 0) {
           Try {
             //only transactions valid from against the current utxo state we take from the mem pool
+            //todo: move magic number to testnet settings
             val txs = v.state.filterValid(v.pool.take(1000).toSeq)
 
             //we also filter transactions which are trying to spend the same box. Currently, we pick just the first one
             //of conflicting transaction. Another strategy is possible(e.g. transaction with highest fee)
             //todo: move this logic to MemPool.put? Problem we have now is that conflicting transactions are still in
             // the pool
-            val txsNoConflict = txs.foldLeft((Seq[AnyoneCanSpendTransaction](), Set[ADKey]())){case ((s, keys), tx) =>
-                if(tx.boxIdsToOpen.forall(k => !keys.contains(k))){
-                  (s :+ tx) -> (keys ++ tx.boxIdsToOpen)
-                } else {
-                  (s, keys)
-                }
+            val txsNoConflict = txs.foldLeft((Seq[AnyoneCanSpendTransaction](), Set[ByteArrayWrapper]())) { case ((s, keys), tx) =>
+              val bxsBaw = tx.boxIdsToOpen.map(ByteArrayWrapper.apply)
+              if (bxsBaw.forall(k => !keys.contains(k)) && bxsBaw.size == bxsBaw.toSet.size) {
+                (s :+ tx) -> (keys ++ bxsBaw)
+              } else {
+                (s, keys)
+              }
             }._1
+
+            val bxsbaw = txsNoConflict.flatMap(_.boxIdsToOpen).map(k => ByteArrayWrapper(k))
+            println("seq size: " + bxsbaw.size + " set size: " + bxsbaw.toSet.size)
 
             val (adProof, adDigest) = v.state.proofsForTransactions(txsNoConflict).get
 
@@ -60,7 +65,7 @@ class ErgoMiner(ergoSettings: ErgoSettings, viewHolder: ActorRef) extends Actor 
             val votes = Array.fill(5)(0: Byte)
             CandidateBlock(v.history.bestHeaderOpt, Constants.InitialNBits, adDigest,
               adProof, txsNoConflict, timestamp, votes)
-          }.recoverWith{case thr =>
+          }.recoverWith { case thr =>
             log.warn("Error when trying to generate a block: ", thr)
             Failure(thr)
           }.toOption
@@ -96,9 +101,9 @@ class ErgoMiner(ergoSettings: ErgoSettings, viewHolder: ActorRef) extends Actor 
             viewHolder ! LocallyGeneratedModifier(adp)
           }
 
-          self ! ProduceCandidate
+          context.system.scheduler.scheduleOnce(100.millis)(self ! ProduceCandidate)
         case None =>
-          self ! MineBlock(candidate)
+          self ! ProduceCandidate
       }
   }
 }
@@ -113,4 +118,5 @@ object ErgoMiner {
   case object StopMining
 
   case class MineBlock(candidate: CandidateBlock)
+
 }

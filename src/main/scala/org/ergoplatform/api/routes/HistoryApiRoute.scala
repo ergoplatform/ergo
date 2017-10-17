cf1d2eb17c35cdf018dd5e3bc632bb416d04b5eb
Sergey Tolmachev <tolsi.ru@gmail.com>
2017-10-17 21:32:07 +0300

    Code updated for scorex case classes settings

--- src/main/scala/org/ergoplatform/api/routes/HistoryApiRoute.scala
+++ src/main/scala/org/ergoplatform/api/routes/HistoryApiRoute.scala
@@ -14,7 +14,7 @@ import org.ergoplatform.nodeView.wallet.ErgoWallet
 import org.ergoplatform.settings.ErgoSettings
 import scorex.core.NodeViewHolder.GetDataFromCurrentView
 import scorex.core.api.http.{ScorexApiResponse, SuccessApiResponse}
-import scorex.core.settings.Settings
+import scorex.core.settings.RESTApiSettings
 
 import scala.concurrent.Future
 
@@ -23,7 +23,7 @@ import scala.concurrent.Future
 case class HistoryApiRoute(nodeViewActorRef: ActorRef, ergoSettings: ErgoSettings)
                           (implicit val context: ActorRefFactory) extends ErgoBaseApiRoute {
 
-  override val settings: Settings = ergoSettings.scorexSettings
+  override val settings: RESTApiSettings = ergoSettings.scorexSettings.restApi
 
   private val digest: Boolean = ergoSettings.nodeSettings.ADState
 

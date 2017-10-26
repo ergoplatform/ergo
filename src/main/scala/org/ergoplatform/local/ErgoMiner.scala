5e4655b9018f591c853be51f90ecdde3b7045239
catena <catena@protonmail.com>
2017-10-26 21:36:31 +0300

    Do not mine genesis block when offlineGeneration=false

--- src/main/scala/org/ergoplatform/local/ErgoMiner.scala
+++ src/main/scala/org/ergoplatform/local/ErgoMiner.scala
@@ -12,7 +12,7 @@ import org.ergoplatform.nodeView.wallet.ErgoWallet
 import org.ergoplatform.settings.{Constants, ErgoSettings}
 import scorex.core.LocalInterface.LocallyGeneratedModifier
 import scorex.core.NodeViewHolder.GetDataFromCurrentView
-import scorex.core.utils.ScorexLogging
+import scorex.core.utils.{NetworkTime, ScorexLogging}
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
@@ -38,39 +38,45 @@ class ErgoMiner(ergoSettings: ErgoSettings, viewHolder: ActorRef) extends Actor 
     case ProduceCandidate =>
       viewHolder ! GetDataFromCurrentView[ErgoHistory, UtxoState, ErgoWallet, ErgoMemPool, Option[CandidateBlock]] { v =>
         val bestHeaderOpt = v.history.bestFullBlockOpt.map(_.header)
-        val coinbase: AnyoneCanSpendTransaction = {
-          val txBoxes = v.state.anyoneCanSpendBoxesAtHeight(bestHeaderOpt.map(_.height + 1).getOrElse(0))
-          AnyoneCanSpendTransaction(txBoxes.map(_.nonce), txBoxes.map(_.value))
+        if (bestHeaderOpt.isDefined || ergoSettings.nodeSettings.offlineGeneration) {
+          val coinbase: AnyoneCanSpendTransaction = {
+            val txBoxes = v.state.anyoneCanSpendBoxesAtHeight(bestHeaderOpt.map(_.height + 1).getOrElse(0))
+            AnyoneCanSpendTransaction(txBoxes.map(_.nonce), txBoxes.map(_.value))
+          }
+
+          Try {
+            //only transactions valid from against the current utxo state we take from the mem pool
+            //todo: move magic number to testnet settings
+            val txs = coinbase +: v.state.filterValid(v.pool.take(1000).toSeq)
+
+            //we also filter transactions which are trying to spend the same box. Currently, we pick just the first one
+            //of conflicting transaction. Another strategy is possible(e.g. transaction with highest fee)
+            //todo: move this logic to MemPool.put? Problem we have now is that conflicting transactions are still in
+            // the pool
+            val txsNoConflict = txs.foldLeft((Seq[AnyoneCanSpendTransaction](), Set[ByteArrayWrapper]())) { case ((s, keys), tx) =>
+              val bxsBaw = tx.boxIdsToOpen.map(ByteArrayWrapper.apply)
+              if (bxsBaw.forall(k => !keys.contains(k)) && bxsBaw.size == bxsBaw.toSet.size) {
+                (s :+ tx) -> (keys ++ bxsBaw)
+              } else {
+                (s, keys)
+              }
+            }._1
+
+            val (adProof, adDigest) = v.state.proofsForTransactions(txsNoConflict).get
+
+            val timestamp = NetworkTime.time()
+            val votes = Array.fill(5)(0: Byte)
+            CandidateBlock(bestHeaderOpt, Constants.InitialNBits, adDigest,
+              adProof, txsNoConflict, timestamp, votes)
+
+          }.recoverWith { case thr =>
+            log.warn("Error when trying to generate a block: ", thr)
+            Failure(thr)
+          }.toOption
+        } else {
+          //Do not try to mine genesis block when offlineGeneration = false
+          None
         }
-
-        Try {
-          //only transactions valid from against the current utxo state we take from the mem pool
-          //todo: move magic number to testnet settings
-          val txs = coinbase +: v.state.filterValid(v.pool.take(1000).toSeq)
-
-          //we also filter transactions which are trying to spend the same box. Currently, we pick just the first one
-          //of conflicting transaction. Another strategy is possible(e.g. transaction with highest fee)
-          //todo: move this logic to MemPool.put? Problem we have now is that conflicting transactions are still in
-          // the pool
-          val txsNoConflict = txs.foldLeft((Seq[AnyoneCanSpendTransaction](), Set[ByteArrayWrapper]())) { case ((s, keys), tx) =>
-            val bxsBaw = tx.boxIdsToOpen.map(ByteArrayWrapper.apply)
-            if (bxsBaw.forall(k => !keys.contains(k)) && bxsBaw.size == bxsBaw.toSet.size) {
-              (s :+ tx) -> (keys ++ bxsBaw)
-            } else {
-              (s, keys)
-            }
-          }._1
-
-          val (adProof, adDigest) = v.state.proofsForTransactions(txsNoConflict).get
-
-          val timestamp = System.currentTimeMillis()
-          val votes = Array.fill(5)(0: Byte)
-          CandidateBlock(bestHeaderOpt, Constants.InitialNBits, adDigest,
-            adProof, txsNoConflict, timestamp, votes)
-        }.recoverWith { case thr =>
-          log.warn("Error when trying to generate a block: ", thr)
-          Failure(thr)
-        }.toOption
       }
 
     case candidateOpt: Option[CandidateBlock] =>
@@ -79,7 +85,8 @@ class ErgoMiner(ergoSettings: ErgoSettings, viewHolder: ActorRef) extends Actor 
           startingNonce = Long.MinValue
           self ! MineBlock(candidateBlock)
         case None =>
-          context.system.scheduler.scheduleOnce(100.millis)(self ! ProduceCandidate)
+          //Failed to create candidate, e.g. we're not trying to mine now
+          context.system.scheduler.scheduleOnce(1.second)(self ! ProduceCandidate)
       }
 
     case StopMining =>

5343df386389e91751c869c19353a279712e0748
catena <catena@protonmail.com>
2017-11-09 13:18:19 +0300

    Fix ErgoSanityDigest

--- src/test/scala/org/ergoplatform/nodeView/DigestWrappedState.scala
+++ src/test/scala/org/ergoplatform/nodeView/DigestWrappedState.scala
@@ -4,4 +4,4 @@ import org.ergoplatform.nodeView.state.DigestState
 import org.ergoplatform.settings.ErgoSettings
 
 class DigestWrappedState(val digestState: DigestState, val wrappedUtxoState: WrappedUtxoState, val settings: ErgoSettings)
-  extends DigestState(digestState.version, digestState.rootHash, wrappedUtxoState.store, settings.nodeSettings)
+  extends DigestState(digestState.version, digestState.rootHash, digestState.store, settings.nodeSettings)

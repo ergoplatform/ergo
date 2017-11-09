5eaaa83d55b5acb4839e372c1f3d4997cff4d546
catena <catena@protonmail.com>
2017-11-09 13:22:14 +0300

    DigestWrappedState => WrappedDigestState,

--- src/test/scala/org/ergoplatform/ErgoSanityDigest.scala
+++ src/test/scala/org/ergoplatform/ErgoSanityDigest.scala
@@ -2,21 +2,21 @@ package org.ergoplatform
 
 import org.ergoplatform.ErgoSanity.{DIGEST_ST, HT, PM}
 import org.ergoplatform.nodeView.state.DigestState
-import org.ergoplatform.nodeView.{DigestWrappedState, WrappedUtxoState}
+import org.ergoplatform.nodeView.{WrappedDigestState, WrappedUtxoState}
 import org.scalacheck.Gen
 
 class ErgoSanityDigest extends ErgoSanity[DIGEST_ST] {
   override val historyGen: Gen[HT] = generateHistory(verifyTransactions = true, ADState = true, PoPoWBootstrap = false, -1)
 
-  override val stateGen: Gen[DigestWrappedState] = {
+  override val stateGen: Gen[WrappedDigestState] = {
     boxesHolderGen.map(WrappedUtxoState(_, createTempDir)).map{ wus =>
       val digestState = DigestState
         .create(Some(wus.version), Some(wus.rootHash), createTempDir, settings.nodeSettings).get
-      new DigestWrappedState(digestState, wus, settings)
+      new WrappedDigestState(digestState, wus, settings)
     }
   }
 
-  override def semanticallyValidModifier(state: DIGEST_ST): PM = validFullBlock(None, state.asInstanceOf[DigestWrappedState].wrappedUtxoState)
+  override def semanticallyValidModifier(state: DIGEST_ST): PM = validFullBlock(None, state.asInstanceOf[WrappedDigestState].wrappedUtxoState)
 
   override def semanticallyInvalidModifier(state: DIGEST_ST): PM = invalidErgoFullBlockGen.sample.get
 }

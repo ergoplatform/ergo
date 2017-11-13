e9222fe768a5a9b7087cd5db9b45a3c9296608a2
Sergey Tolmachev <tolsi.ru@gmail.com>
2017-11-13 16:07:34 +0300

    log nodes heights

--- src/it/scala/org/ergoplatform/it/SyncronizationTestSpec.scala
+++ src/it/scala/org/ergoplatform/it/SyncronizationTestSpec.scala
@@ -2,6 +2,7 @@ package org.ergoplatform.it
 
 import org.scalatest.{FreeSpec, Matchers}
 import org.scalatest.concurrent.{IntegrationPatience, ScalaFutures}
+import scorex.core.utils.ScorexLogging
 
 import scala.concurrent.Await.result
 import scala.concurrent.Future.traverse
@@ -9,14 +10,17 @@ import scala.concurrent.duration._
 import scala.concurrent.ExecutionContext.Implicits.global
 
 class SyncronizationTestSpec(nodes: Seq[Node]) extends FreeSpec with ScalaFutures with IntegrationPatience
-  with Matchers {
+  with Matchers with ScorexLogging {
 
   "Generate 30 blocks" in {
     val headerIdsAtSameHeight = result(for {
       b <- traverse(nodes)(_.height).map(_.min)
       _ <- traverse(nodes)(_.waitForHeight(b + 30))
       headers <- traverse(nodes)(_.headerIdsByHeight(b + 29))
-    } yield headers, 10.minutes)
+    } yield {
+      log.debug(s"Headers at height ${b + 29}: ${headers.mkString(",")}")
+      headers
+    }, 10.minutes)
 
     val someHeaderId = headerIdsAtSameHeight.head.head
     require(headerIdsAtSameHeight.forall(_.contains(someHeaderId)))

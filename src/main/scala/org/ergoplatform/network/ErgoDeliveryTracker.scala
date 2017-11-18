04a974f58a0ac0db99530e4b79d0353e7d130941
catena <catena@protonmail.com>
2017-11-18 14:28:42 +0300

    Request modifiers via toDownload from ProgressINfo

--- src/main/scala/org/ergoplatform/network/ErgoDeliveryTracker.scala
+++ src/main/scala/org/ergoplatform/network/ErgoDeliveryTracker.scala
@@ -1,8 +1,22 @@
 package org.ergoplatform.network
 
-import scorex.core.network.DeliveryTracker
+import scorex.core.network.{ConnectedPeer, DeliveryTracker}
+import scorex.core.utils.NetworkTime
+import scorex.core.{ModifierId, ModifierTypeId}
+
+import scala.collection.mutable
 
 class ErgoDeliveryTracker extends DeliveryTracker {
 
+  val toDownload = mutable.Map[MapKey, (ModifierTypeId, Long)]()
+
+  def downloadRequested(modifierTypeId: ModifierTypeId, modifierId: ModifierId): Unit = {
+    toDownload.put(key(modifierId), (modifierTypeId, NetworkTime.time()))
+  }
+
+  override def receive(mtid: ModifierTypeId, mid: ModifierId, cp: ConnectedPeer): Unit = {
+    toDownload.remove(key(mid))
+    super.receive(mtid, mid, cp)
+  }
 
 }

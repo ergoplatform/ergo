ca9ef755b04695901b87ab2fbdb4a4bf19101666
catena <catena@protonmail.com>
2017-12-13 18:02:23 +0300

    Extract ErgoHistoryReader

---
+++ src/main/scala/org/ergoplatform/nodeView/history/ErgoHistoryReader.scala
@@ -0,0 +1,271 @@
+package org.ergoplatform.nodeView.history
+
+import java.io.File
+
+import io.iohk.iodb.{ByteArrayWrapper, LSMStore, Store}
+import org.ergoplatform.modifiers.history._
+import org.ergoplatform.modifiers.state.UTXOSnapshotChunk
+import org.ergoplatform.modifiers.{ErgoFullBlock, ErgoPersistentModifier}
+import org.ergoplatform.nodeView.history.storage._
+import org.ergoplatform.nodeView.history.storage.modifierprocessors._
+import org.ergoplatform.nodeView.history.storage.modifierprocessors.adproofs.{ADProofsProcessor, ADStateProofsProcessor, EmptyADProofsProcessor, FullStateProofsProcessor}
+import org.ergoplatform.nodeView.history.storage.modifierprocessors.blocktransactions.{BlockTransactionsProcessor, EmptyBlockTransactionsProcessor, FullnodeBlockTransactionsProcessor}
+import org.ergoplatform.nodeView.history.storage.modifierprocessors.popow.{EmptyPoPoWProofsProcessor, FullPoPoWProofsProcessor, PoPoWProofsProcessor}
+import org.ergoplatform.settings.{Algos, ChainSettings, ErgoSettings, NodeConfigurationSettings}
+import scorex.core._
+import scorex.core.consensus.History.{HistoryComparisonResult, ModifierIds, ProgressInfo}
+import scorex.core.consensus.{History, HistoryReader, ModifierSemanticValidity}
+import scorex.core.utils.ScorexLogging
+import scorex.crypto.encode.Base58
+
+import scala.util.{Failure, Try}
+
+/**
+  * History implementation. It is processing persistent modifiers generated locally or coming from network.
+  * Depending on chosen node settings, it will process modifiers in a different way, different processors define how to
+  * process different type of modifiers.
+  *
+  * HeadersProcessor: processor of block headers. It's the same for all node settings
+  * ADProofsProcessor: processor of ADProofs. ADProofs may
+  *   1. Be downloaded from other nodes (ADState == true)
+  *   2. Be calculated by using local state (ADState == false)
+  *   3. Be ignored by history in light mode (verifyTransactions == false)
+  * PoPoWProofsProcessor: processor of PoPoWProof. PoPoWProof may
+  *   1. Be downloaded once during bootstrap from other peers (poPoWBootstrap == true)
+  *   2. Be ignored by history (poPoWBootstrap == false)
+  * BlockTransactionsProcessor: Processor of BlockTransactions. BlockTransactions may
+  *   1. Be downloaded from other peers (verifyTransactions == true)
+  *   2. Be ignored by history (verifyTransactions == false)
+  */
+trait ErgoHistoryReader
+  extends HistoryReader[ErgoPersistentModifier, ErgoSyncInfo]
+    with HeadersProcessor
+    with ADProofsProcessor
+    with PoPoWProofsProcessor
+    with UTXOSnapshotChunkProcessor
+    with BlockTransactionsProcessor
+    with ScorexLogging {
+
+  protected val chainSettings: ChainSettings
+  protected val config: NodeConfigurationSettings
+  protected val storage: Store
+
+  protected lazy val historyStorage: HistoryStorage = new HistoryStorage(storage)
+
+  /**
+    * Is there's no history, even genesis block
+    */
+  def isEmpty: Boolean = bestHeaderIdOpt.isEmpty
+
+  /**
+    * Header of best Header chain. Empty if no genesis block is applied yet (from a chain or a PoPoW proof).
+    * Transactions and ADProofs for this Header may be missed, to get block from best full chain (in mode that support
+    * it) call bestFullBlockOpt.
+    */
+  def bestHeaderOpt: Option[Header] = bestHeaderIdOpt.flatMap(typedModifierById[Header])
+
+  /**
+    * Complete block of the best chain with transactions.
+    * Always None for an SPV mode, Some(fullBLock) for fullnode regime after initial bootstrap.
+    */
+  def bestFullBlockOpt: Option[ErgoFullBlock] =
+    bestFullBlockIdOpt.flatMap(id => typedModifierById[Header](id)).flatMap(getFullBlock)
+
+  /**
+    * Get ErgoPersistentModifier by it's id if it is in history
+    */
+  override def modifierById(id: ModifierId): Option[ErgoPersistentModifier] = {
+    val modifier = historyStorage.modifierById(id)
+    assert(modifier.forall(_.id sameElements id), s"Modifier $modifier id is incorrect, ${Algos.encode(id)} expected")
+    modifier
+  }
+
+  /**
+    * Get ErgoPersistentModifier of type T by it's id if it is in history
+    */
+  def typedModifierById[T <: ErgoPersistentModifier](id: ModifierId): Option[T] = modifierById(id) match {
+    case Some(m: T@unchecked) if m.isInstanceOf[T] => Some(m)
+    case _ => None
+  }
+
+  /**
+    * Id of best block to mine
+    */
+  override def openSurfaceIds(): Seq[ModifierId] = bestFullBlockIdOpt.orElse(bestHeaderIdOpt).toSeq
+
+  /**
+    * Check, that it's possible to apply modifier to history
+    */
+  override def applicable(modifier: ErgoPersistentModifier): Boolean = applicableTry(modifier).isSuccess
+
+  /**
+    * Whether another's node syncinfo shows that another node is ahead or behind ours
+    *
+    * @param info other's node sync info
+    * @return Equal if nodes have the same history, Younger if another node is behind, Older if a new node is ahead
+    */
+  override def compare(info: ErgoSyncInfo): HistoryComparisonResult.Value = {
+    bestHeaderIdOpt match {
+      case Some(id) if info.lastHeaderIds.lastOption.exists(_ sameElements id) =>
+        HistoryComparisonResult.Equal
+      case Some(id) if info.lastHeaderIds.exists(_ sameElements id) =>
+        HistoryComparisonResult.Older
+      case Some(_) if info.lastHeaderIds.isEmpty =>
+        HistoryComparisonResult.Younger
+      case Some(_) =>
+        //Compare headers chain
+        val ids = info.lastHeaderIds
+        ids.view.reverse.find(m => contains(m)) match {
+          case Some(lastId) =>
+            //TODO We anyway have our fork to send. is it ok?
+            HistoryComparisonResult.Younger
+          case None => HistoryComparisonResult.Nonsense
+        }
+      case None =>
+        log.warn("Trying to compare with other node while our history is empty")
+        HistoryComparisonResult.Older
+    }
+  }
+
+  /**
+    * @param info other's node sync info
+    * @param size max return size
+    * @return Ids of headerss, that node with info should download and apply to synchronize
+    */
+  override def continuationIds(info: ErgoSyncInfo, size: Int): Option[ModifierIds] = Try {
+    if (isEmpty) {
+      info.startingPoints
+    } else if (info.lastHeaderIds.isEmpty) {
+      val heightFrom = Math.min(headersHeight, size - 1)
+      val startId = headerIdsAtHeight(heightFrom).head
+      val startHeader = typedModifierById[Header](startId).get
+      val headers = headerChainBack(size, startHeader, _ => false)
+      assert(headers.headers.exists(_.height == 0), "Should always contain genesis header")
+      headers.headers.flatMap(h => Seq((Header.modifierTypeId, h.id)))
+    } else {
+      val ids = info.lastHeaderIds
+      val lastHeaderInOurBestChain: ModifierId = ids.view.reverse.find(m => isInBestChain(m)).get
+      val theirHeight = heightOf(lastHeaderInOurBestChain).get
+      val heightFrom = Math.min(headersHeight, theirHeight + size)
+      val startId = headerIdsAtHeight(heightFrom).head
+      val startHeader = typedModifierById[Header](startId).get
+      val headerIds = headerChainBack(size, startHeader, h => h.parentId sameElements lastHeaderInOurBestChain)
+        .headers.map(h => Header.modifierTypeId -> h.id)
+      headerIds
+    }
+  }.toOption
+
+  /**
+    * @return all possible forks, that contains specified header
+    */
+  protected[history] def continuationHeaderChains(header: Header): Seq[HeaderChain] = {
+    def loop(acc: Seq[Header]): Seq[HeaderChain] = {
+      val bestHeader = acc.last
+      val currentHeight = heightOf(bestHeader.id).get
+      val nextLevelHeaders = headerIdsAtHeight(currentHeight + 1).map(id => typedModifierById[Header](id).get)
+        .filter(_.parentId sameElements bestHeader.id)
+      if (nextLevelHeaders.isEmpty) Seq(HeaderChain(acc))
+      else nextLevelHeaders.map(h => acc :+ h).flatMap(chain => loop(chain))
+    }
+
+    loop(Seq(header))
+  }
+
+
+  /**
+    *
+    * @param answer - whether it is answer to other node request or not
+    * @return Node ErgoSyncInfo
+    */
+  override def syncInfo(answer: Boolean): ErgoSyncInfo = if (isEmpty) {
+    ErgoSyncInfo(answer, Seq())
+  } else {
+    ErgoSyncInfo(answer, lastHeaders(ErgoSyncInfo.MaxBlockIds).headers.map(_.id))
+  }
+
+  /**
+    * Return last count headers from best headers chain if exist or chain up to genesis otherwise
+    */
+  def lastHeaders(count: Int): HeaderChain = bestHeaderOpt
+    .map(bestHeader => headerChainBack(count, bestHeader, b => false)).getOrElse(HeaderChain.empty)
+
+  protected def applicableTry(modifier: ErgoPersistentModifier): Try[Unit] = {
+    modifier match {
+      case header: Header =>
+        validate(header)
+      case m: BlockTransactions =>
+        validate(m)
+      case m: ADProofs =>
+        validate(m)
+      case m: PoPoWProof =>
+        validate(m)
+      case chunk: UTXOSnapshotChunk =>
+        validate(chunk)
+      case m: Any =>
+        Failure(new Error(s"Modifier $m has incorrect type"))
+    }
+  }
+
+  def getFullBlock(header: Header): Option[ErgoFullBlock] = {
+    val aDProofs = typedModifierById[ADProofs](header.ADProofsId)
+    typedModifierById[BlockTransactions](header.transactionsId).map { txs =>
+      ErgoFullBlock(header, txs, aDProofs)
+    }
+  }
+
+  def missedModifiersForFullChain(): Seq[(ModifierTypeId, ModifierId)] = {
+    if (config.verifyTransactions) {
+      bestHeaderOpt.toSeq
+        .flatMap(h => headerChainBack(headersHeight + 1, h, p => contains(p.ADProofsId) && contains(p.transactionsId)).headers)
+        .flatMap(h => Seq((BlockTransactions.modifierTypeId, h.transactionsId), (ADProofs.modifierTypeId, h.ADProofsId)))
+        .filter(id => !contains(id._2))
+    } else {
+      Seq()
+    }
+  }
+
+  protected[history] def commonBlockThenSuffixes(header1: Header, header2: Header): (HeaderChain, HeaderChain) = {
+    assert(contains(header1))
+    assert(contains(header2))
+
+    def loop(numberBack: Int, otherChain: HeaderChain): (HeaderChain, HeaderChain) = {
+      val r = commonBlockThenSuffixes(otherChain, header1, numberBack)
+      if (r._1.head == r._2.head) {
+        r
+      } else {
+        val biggerOther = headerChainBack(numberBack, otherChain.head, _ => false) ++ otherChain.tail
+        if (!otherChain.head.isGenesis) {
+          loop(biggerOther.size, biggerOther)
+        } else {
+          throw new Error(s"Common point not found for headers $header1 and $header2")
+        }
+      }
+    }
+
+    loop(2, HeaderChain(Seq(header2)))
+  }
+
+  protected[history] def commonBlockThenSuffixes(otherChain: HeaderChain,
+                                                 startHeader: Header,
+                                                 limit: Int): (HeaderChain, HeaderChain) = {
+    def until(h: Header): Boolean = otherChain.exists(_.id sameElements h.id)
+
+    val ourChain = headerChainBack(limit, startHeader, until)
+    val commonBlock = ourChain.head
+    val commonBlockThenSuffixes = otherChain.takeAfter(commonBlock)
+    (ourChain, commonBlockThenSuffixes)
+  }
+
+
+  override def isSemanticallyValid(modifierId: ModifierId): ModifierSemanticValidity.Value = {
+    historyStorage.db.get(validityKey(modifierId)) match {
+      case Some(b) if b.data.headOption.contains(1.toByte) => ModifierSemanticValidity.Valid
+      case Some(b) if b.data.headOption.contains(0.toByte) => ModifierSemanticValidity.Invalid
+      case None if contains(modifierId) => ModifierSemanticValidity.Unknown
+      case None => ModifierSemanticValidity.Absent
+      case m =>
+        log.error(s"Incorrect validity status: $m")
+        ModifierSemanticValidity.Absent
+    }
+  }
+}

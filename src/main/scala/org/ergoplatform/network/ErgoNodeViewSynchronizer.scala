3959176c767368595940ddd82bc714013ad0b58e
catena <catena@protonmail.com>
2017-12-18 16:55:26 +0300

    Too much Sync messages hunting

--- src/main/scala/org/ergoplatform/network/ErgoNodeViewSynchronizer.scala
+++ src/main/scala/org/ergoplatform/network/ErgoNodeViewSynchronizer.scala
@@ -1,20 +1,21 @@
 package org.ergoplatform.network
 
 import akka.actor.ActorRef
-import org.ergoplatform.modifiers.{ErgoFullBlock, ErgoPersistentModifier}
 import org.ergoplatform.modifiers.mempool.AnyoneCanSpendTransaction
 import org.ergoplatform.modifiers.mempool.proposition.AnyoneCanSpendProposition
+import org.ergoplatform.modifiers.{ErgoFullBlock, ErgoPersistentModifier}
 import org.ergoplatform.network.ErgoNodeViewSynchronizer.{CheckModifiersToDownload, MissedModifiers}
 import org.ergoplatform.nodeView.history.{ErgoHistory, ErgoSyncInfo, ErgoSyncInfoMessageSpec}
 import org.ergoplatform.nodeView.mempool.ErgoMemPool
 import org.ergoplatform.nodeView.state.UtxoState
 import org.ergoplatform.nodeView.wallet.ErgoWallet
 import scorex.core.NodeViewHolder._
-import scorex.core.consensus.History
 import scorex.core.network.NetworkController.SendToNetwork
+import scorex.core.network.NodeViewSynchronizer.GetLocalSyncInfo
 import scorex.core.network.message.Message
 import scorex.core.network.{NodeViewSynchronizer, SendToRandom}
 import scorex.core.settings.NetworkSettings
+import scorex.core.utils.NetworkTime
 import scorex.core.{ModifierId, ModifierTypeId, NodeViewHolder}
 
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -39,6 +40,21 @@ class ErgoNodeViewSynchronizer(networkControllerRef: ActorRef,
     context.system.scheduler.schedule(toDownloadCheckInterval, toDownloadCheckInterval)(self ! CheckModifiersToDownload)
     initializeToDownload()
   }
+
+  /**
+    * To send out regular sync signal, we first send a request to node view holder to get current syncing information
+    */
+  override protected def getLocalSyncInfo: Receive = {
+    case GetLocalSyncInfo =>
+      val currentTime = NetworkTime.time()
+      log.debug(s"GetLocalSyncInfo at $currentTime")
+      if (currentTime - lastSyncInfoSentTime >= (networkSettings.syncInterval.toMillis / 2)) {
+        historyReaderOpt.foreach { r =>
+          sender() ! CurrentSyncInfo(r.syncInfo(false))
+        }
+      }
+  }
+
 
   protected def initializeToDownload(): Unit = {
     viewHolderRef ! GetDataFromCurrentView[ErgoHistory, UtxoState, ErgoWallet, ErgoMemPool, MissedModifiers] { v =>

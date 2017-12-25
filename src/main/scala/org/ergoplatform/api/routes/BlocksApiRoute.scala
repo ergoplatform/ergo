1c9cd6cc2447f2815d1c48718610cd1d676cba0c
Aleksei <daron666@yandex.ru>
2017-12-25 21:24:05 +0400

    specs and routes improvements

--- src/main/scala/org/ergoplatform/api/routes/BlocksApiRoute.scala
+++ src/main/scala/org/ergoplatform/api/routes/BlocksApiRoute.scala
@@ -21,7 +21,6 @@ import scorex.core.utils.ScorexLogging
 import scorex.crypto.encode.Base58
 
 import scala.concurrent.Future
-import scala.util.{Failure, Success, Try}
 
 case class BlocksApiRoute(readersHolder: ActorRef, miner: ActorRef, ergoSettings: ErgoSettings, nodeId: Array[Byte], digest: Boolean)
                          (implicit val context: ActorRefFactory) extends ErgoBaseApiRoute with ScorexLogging {
@@ -29,46 +28,38 @@ case class BlocksApiRoute(readersHolder: ActorRef, miner: ActorRef, ergoSettings
   private val powScheme = ergoSettings.chainSettings.poWScheme
 
   override val route: Route = pathPrefix("blocks") {
-    concat(getBlocksR, postBlocksR, getLastHeadersR, getBlockIdsAtHeightR, getBlockHeaderByHeaderIdR, getBlockTransactionsByHeaderIdR, candidateBlockR, getFullBlockByHeaderIdR)
+    getBlocksR ~
+    postBlocksR ~
+    getLastHeadersR ~
+    getBlockIdsAtHeightR ~
+    getBlockHeaderByHeaderIdR ~
+    getBlockTransactionsByHeaderIdR ~
+    getFullBlockByHeaderIdR ~
+    candidateBlockR
   }
 
   override val settings: RESTApiSettings = ergoSettings.scorexSettings.restApi
 
-  private val request = if (digest) {
-    GetDataFromCurrentView[ErgoHistory, DigestState, ErgoWallet, ErgoMemPool, ErgoHistory](_.history)
-  } else {
-    GetDataFromCurrentView[ErgoHistory, UtxoState, ErgoWallet, ErgoMemPool, ErgoHistory](_.history)
-  }
-
   private def getHistory = (readersHolder ? GetDataFromHistory[ErgoHistoryReader](r => r)).mapTo[ErgoHistoryReader]
 
-  private def getHeaderIdsAtHeight(h: Int): Future[Json] = getHistory.map {
-    _.headerIdsAtHeight(h)
-  }.map {
-    headerIds =>
-      headerIds.map(Base58.encode).asJson
+  private def getHeaderIdsAtHeight(h: Int): Future[Json] = getHistory.map { history =>
+    history.headerIdsAtHeight(h).map(Base58.encode).asJson
   }
 
-  private def getLastHeaders(n: Int): Future[Json] = getHistory.map {
-    _.lastHeaders(n)
-  }.map { v =>
-    v.headers.map(_.json).asJson
+  private def getLastHeaders(n: Int): Future[Json] = getHistory.map { history =>
+    history.lastHeaders(n).headers.map(_.json).asJson
   }
 
-  private def getHeaderIds(limit: Int, offset: Int): Future[Json] = getHistory.map(_.headerIdsAt(limit, offset)
-    .map(Base58.encode).asJson)
-
-  private def getFullBlockByHeaderId(headerId: ModifierId): Future[Option[ErgoFullBlock]] = {
-    getHistory.map { h =>
-      h.typedModifierById[Header](headerId).flatMap(h.getFullBlock)
-    }
+  private def getHeaderIds(limit: Int, offset: Int): Future[Json] = getHistory.map { history =>
+    history.headerIdsAt(limit, offset).map(Base58.encode).asJson
   }
 
-  def getBlocksR: Route = get {
-    parameters('limit.as[Int] ? 50, 'offset.as[Int] ? 0) {
-      case (limit, offset) =>
-        toJsonResponse(getHeaderIds(limit, offset))
-    }
+  private def getFullBlockByHeaderId(headerId: ModifierId): Future[Option[ErgoFullBlock]] = getHistory.map { history =>
+    history.typedModifierById[Header](headerId).flatMap(history.getFullBlock)
+  }
+
+  def getBlocksR: Route = (pathEndOrSingleSlash & get & paging) { (offset, limit) =>
+    getHeaderIds(limit, offset).okJson()
   }
 
   def postBlocksR: Route = post {
@@ -92,70 +83,25 @@ case class BlocksApiRoute(readersHolder: ActorRef, miner: ActorRef, ergoSettings
     */
   }
 
-  def getLastHeadersR: Route =
-    path("lastHeaders" / Segment) { countString =>
-      get {
-        toJsonResponse {
-          Try {
-            countString.toInt
-          } match {
-            case Success(count) =>
-              getLastHeaders(count)
-            case Failure(e) =>
-              Future.failed(e)
-          }
-        }
-      }
-    }
+  def getLastHeadersR: Route = (pathPrefix("lastHeaders" / IntNumber) & get) { count => getLastHeaders(count).okJson() }
 
-  // todo: heightString validation
-  def getBlockIdsAtHeightR: Route = path("at" / Segment) { heightString =>
-    get {
-      toJsonResponse {
-        Try {
-          heightString.toInt
-        } match {
-          case Success(height) =>
-            getHeaderIdsAtHeight(height)
-          case Failure(e) =>
-            Future.failed(e)
-        }
-      }
-    }
+  def getBlockIdsAtHeightR: Route = (pathPrefix("at" / IntNumber) & get) { height =>
+    getHeaderIdsAtHeight(height).okJson()
   }
 
-  // todo: headerId validation
-  def getBlockHeaderByHeaderIdR: Route = path(Segment / "header") { headerId =>
-    get {
-      toJsonOptionalResponse {
-        getFullBlockByHeaderId(ModifierId @@ Base58.decode(headerId).get)
-          .map(_.map(_.header.json))
-      }
-    }
+  def getBlockHeaderByHeaderIdR: Route = (headerId & pathPrefix("header") & get) { id =>
+      getFullBlockByHeaderId(id).map(_.map(_.header.json)).okJson()
   }
 
-  // todo: headerId validation
-  def getBlockTransactionsByHeaderIdR: Route = path(Segment / "transactions") { headerId =>
-    get {
-      toJsonOptionalResponse {
-        getFullBlockByHeaderId(ModifierId @@ Base58.decode(headerId).get)
-          .map(_.map(_.transactions.map(_.json).asJson))
-      }
-    }
+  def getBlockTransactionsByHeaderIdR: Route = (headerId & pathPrefix("transactions") & get) { id =>
+        getFullBlockByHeaderId(id).map(_.map(_.transactions.map(_.json).asJson)).okJson()
   }
 
-  def candidateBlockR: Route = path("candidateBlock") {
-    get(toJsonResponse((miner ? MiningStatusRequest).mapTo[MiningStatusResponse].map(r =>
-      r.json)))
+  def candidateBlockR: Route = (path("candidateBlock") & pathEndOrSingleSlash & get) {
+    (miner ? MiningStatusRequest).mapTo[MiningStatusResponse].map(_.json).okJson()
   }
 
-  // todo: headerId validation
-  def getFullBlockByHeaderIdR: Route = path(Segment) { headerId =>
-    get {
-      toJsonOptionalResponse {
-        getFullBlockByHeaderId(ModifierId @@ Base58.decode(headerId).get)
-          .map(_.map(_.json))
-      }
-    }
+  def getFullBlockByHeaderIdR: Route = (headerId & get) { id =>
+    getFullBlockByHeaderId(id).map(_.map(_.json)).okJson()
   }
 }

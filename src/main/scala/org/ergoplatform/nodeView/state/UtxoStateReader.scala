0d3a3287e5ee2ec52b4c5eb9dd439c3a30c0eced
catena <catena@protonmail.com>
2018-01-11 20:33:49 +0300

    Use readers in ErgoMiner

---
+++ src/main/scala/org/ergoplatform/nodeView/state/UtxoStateReader.scala
@@ -0,0 +1,86 @@
+package org.ergoplatform.nodeView.state
+
+import io.iohk.iodb.Store
+import org.ergoplatform.modifiers.history.ADProofs
+import org.ergoplatform.modifiers.mempool.AnyoneCanSpendTransaction
+import org.ergoplatform.modifiers.mempool.proposition.{AnyoneCanSpendNoncedBox, AnyoneCanSpendNoncedBoxSerializer, AnyoneCanSpendProposition}
+import scorex.core.transaction.state.TransactionValidation
+import scorex.core.utils.ScorexLogging
+import scorex.crypto.authds.avltree.batch.{BatchAVLProver, NodeParameters, PersistentBatchAVLProver, VersionedIODBAVLStorage}
+import scorex.crypto.authds.{ADDigest, ADKey, ADValue, SerializedAdProof}
+import scorex.crypto.hash.{Blake2b256Unsafe, Digest32}
+
+import scala.util.{Failure, Success, Try}
+
+trait UtxoStateReader extends ErgoStateReader with ScorexLogging with TransactionValidation[AnyoneCanSpendProposition.type, AnyoneCanSpendTransaction] {
+
+  implicit val hf = new Blake2b256Unsafe
+
+  val store: Store
+  private lazy val np = NodeParameters(keySize = 32, valueSize = ErgoState.BoxSize, labelSize = 32)
+  protected lazy val storage = new VersionedIODBAVLStorage(store, np)
+
+  protected lazy val persistentProver: PersistentBatchAVLProver[Digest32, Blake2b256Unsafe] = {
+    val bp = new BatchAVLProver[Digest32, Blake2b256Unsafe](keyLength = 32, valueLengthOpt = Some(ErgoState.BoxSize))
+    PersistentBatchAVLProver.create(bp, storage).get
+  }
+
+  override def validate(tx: AnyoneCanSpendTransaction): Try[Unit] = if (tx.boxIdsToOpen.forall { k =>
+    persistentProver.unauthenticatedLookup(k).isDefined
+  }) Success()
+  else Failure(new Exception(s"Not all boxes of the transaction $tx are in the state"))
+
+  /**
+    * @return boxes, that miner (or any user) can take to himself when he creates a new block
+    */
+  def anyoneCanSpendBoxesAtHeight(height: Int): IndexedSeq[AnyoneCanSpendNoncedBox] = {
+    //TODO fix
+    randomBox().toIndexedSeq
+  }
+
+  def boxById(id: ADKey): Option[AnyoneCanSpendNoncedBox] =
+    persistentProver
+      .unauthenticatedLookup(id)
+      .map(AnyoneCanSpendNoncedBoxSerializer.parseBytes)
+      .flatMap(_.toOption)
+
+  def randomBox(): Option[AnyoneCanSpendNoncedBox] =
+    persistentProver.avlProver.randomWalk().map(_._1).flatMap(boxById)
+
+  //TODO not efficient at all
+  def proofsForTransactions(txs: Seq[AnyoneCanSpendTransaction]): Try[(SerializedAdProof, ADDigest)] = {
+
+    def rollback(): Try[Unit] = persistentProver.rollback(rootHash)
+      .ensuring(persistentProver.digest.sameElements(rootHash))
+
+    Try {
+      require(txs.nonEmpty)
+      require(persistentProver.digest.sameElements(rootHash))
+      require(storage.version.get.sameElements(rootHash))
+      require(store.lastVersionID.get.data.sameElements(rootHash))
+
+      //todo: make a special config flag, "paranoid mode", and use it for checks like one commented below
+      //persistentProver.checkTree(true)
+
+      val mods = boxChanges(txs).operations.map(ADProofs.changeToMod)
+      //todo .get
+      mods.foldLeft[Try[Option[ADValue]]](Success(None)) { case (t, m) =>
+        t.flatMap(_ => {
+          val opRes = persistentProver.performOneOperation(m)
+          if (opRes.isFailure) log.warn(s"modification: $m, failure $opRes")
+          opRes
+        })
+      }.get
+
+      val proof = persistentProver.generateProofAndUpdateStorage()
+
+      val digest = persistentProver.digest
+
+      proof -> digest
+    } match {
+      case Success(res) => rollback().map(_ => res)
+      case Failure(e) => rollback().flatMap(_ => Failure(e))
+    }
+  }
+
+}

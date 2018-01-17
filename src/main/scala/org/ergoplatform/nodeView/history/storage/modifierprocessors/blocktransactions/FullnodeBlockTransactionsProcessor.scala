0a4f6863ca472f0a59f461c1e39ab8e36ab647a6
catena <catena@protonmail.com>
2018-01-17 13:31:08 +0300

    Allow to apply BlockTransactions for all defined headers

--- src/main/scala/org/ergoplatform/nodeView/history/storage/modifierprocessors/blocktransactions/FullnodeBlockTransactionsProcessor.scala
+++ src/main/scala/org/ergoplatform/nodeView/history/storage/modifierprocessors/blocktransactions/FullnodeBlockTransactionsProcessor.scala
@@ -8,7 +8,7 @@ import org.ergoplatform.nodeView.history.storage.modifierprocessors.FullBlockPro
 import scorex.core.consensus.History.ProgressInfo
 import scorex.crypto.encode.Base58
 
-import scala.util.Try
+import scala.util.{Failure, Success, Try}
 
 /**
   * BlockTransactions processor for settings with verifyTransactions=true
@@ -36,25 +36,18 @@ trait FullnodeBlockTransactionsProcessor extends BlockTransactionsProcessor with
     }
   }
 
-  override protected def validate(m: BlockTransactions): Try[Unit] = Try {
-    require(!historyStorage.contains(m.id), s"Modifier $m is already in history")
-    historyStorage.modifierById(m.headerId) match {
-      case Some(header: Header) =>
-        require(header.transactionsRoot sameElements m.digest,
-          s"Header transactions root ${Base58.encode(header.transactionsRoot)} differs from block transactions $m digest")
-
-        bestFullBlockIdOpt match {
-          case None if config.blocksToKeep < 0 =>
-            require(header.isGenesis, "Trying to apply non-genesis block to empty history in fullnode mode")
-          case None if config.blocksToKeep >= 0 =>
-            //TODO State should be at this version!
-          case Some(id) =>
-            require(typedModifierById[Header](header.parentId).exists(h => contains(h.transactionsId)),
-              "Trying to apply transactions for header, which parent transactions are empty")
-        }
-
-      case _ =>
-        throw new Error(s"Header for modifier $m is no defined")
+  override protected def validate(m: BlockTransactions): Try[Unit] = {
+    if(historyStorage.contains(m.id)) {
+      Failure(new Error(s"Modifier $m is already in history"))
+    } else {
+      historyStorage.modifierById(m.headerId) match {
+        case None =>
+          Failure(new Error(s"Header for modifier $m is no defined"))
+        case Some(header: Header) if !(header.transactionsRoot sameElements m.digest) =>
+          Failure(new Error(s"Header transactions root ${Base58.encode(header.ADProofsRoot)} differs from $m digest"))
+        case Some(header: Header) =>
+          Success()
+      }
     }
   }
 }

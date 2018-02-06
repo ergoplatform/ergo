abd3a0e35986da14436586611a61195e089225fa
Aleksei <daron666@yandex.ru>
2018-02-06 17:14:12 +0400

    Fix Option.get while looping over headers in ErgoHistoryReader

--- src/main/scala/org/ergoplatform/nodeView/history/ErgoHistoryReader.scala
+++ src/main/scala/org/ergoplatform/nodeView/history/ErgoHistoryReader.scala
@@ -168,20 +168,23 @@ trait ErgoHistoryReader
   //TODO rework option.get and traversable.head
   protected[history] def continuationHeaderChains(header: Header, withFilter: Header => Boolean): Seq[Seq[Header]] = {
     @tailrec
-    def loop(currentHeight: Int, acc: Seq[Seq[Header]]): Seq[Seq[Header]] = {
-      val nextLevelHeaders = headerIdsAtHeight(currentHeight + 1).map(id => typedModifierById[Header](id).get)
-        .filter(h => withFilter(h))
+    def loop(currentHeight: Option[Int], acc: Seq[Seq[Header]]): Seq[Seq[Header]] = {
+      val nextLevelHeaders = currentHeight.toList
+        .flatMap{ h => headerIdsAt(h + 1) }
+        .flatMap { id => typedModifierById[Header](id) }
+        .filter(withFilter)
       if (nextLevelHeaders.isEmpty) {
         acc.map(chain => chain.reverse)
       } else {
-        val updatedChains = nextLevelHeaders
-          .flatMap(h => acc.find(chain => h.parentId sameElements chain.head.id).map(c => h +: c))
+        val updatedChains = nextLevelHeaders.flatMap { h =>
+          acc.find(chain => h.parentId sameElements chain.head.id).map(c => h +: c)
+        }
         val nonUpdatedChains = acc.filter(chain => !nextLevelHeaders.exists(_.parentId sameElements chain.head.id))
-        loop(currentHeight + 1, updatedChains ++ nonUpdatedChains)
+        loop(currentHeight.map(_ + 1), updatedChains ++ nonUpdatedChains)
       }
     }
 
-    loop(heightOf(header.id).get, Seq(Seq(header)))
+    loop(heightOf(header.id), Seq(Seq(header)))
   }
 
 

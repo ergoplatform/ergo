d414fc9f69dd2b78c24584d86272b612b3bcb046
catena <catena@protonmail.com>
2018-02-27 16:56:50 +0300

    ErgoStatsCollector implementation

--- src/main/scala/org/ergoplatform/api/routes/InfoRoute.scala
+++ src/main/scala/org/ergoplatform/api/routes/InfoRoute.scala
@@ -3,80 +3,21 @@ package org.ergoplatform.api.routes
 import akka.actor.{ActorRef, ActorRefFactory}
 import akka.http.scaladsl.server.Route
 import akka.pattern.ask
-import io.circe.Json
-import io.circe.syntax._
 import org.ergoplatform.Version
 import org.ergoplatform.local.ErgoMiner.{MiningStatusRequest, MiningStatusResponse}
-import org.ergoplatform.nodeView.ErgoReadersHolder.{GetReaders, Readers}
-import org.ergoplatform.nodeView.state.StateType
-import org.ergoplatform.settings.{Algos, ErgoSettings}
-import scorex.core.network.Handshake
-import scorex.core.network.peer.PeerManager
+import org.ergoplatform.local.ErgoStatsCollector.{GetNodeInfo, NodeInfo}
 import scorex.core.settings.RESTApiSettings
-import scorex.crypto.encode.Base58
 
 import scala.concurrent.Future
 
-case class InfoRoute(readersHolder: ActorRef,
-                     miner: ActorRef,
-                     peerManager: ActorRef,
-                     ergoSettings: ErgoSettings,
-                     nodeId: Array[Byte])
+case class InfoRoute(statsCollector: ActorRef,
+                     override val settings: RESTApiSettings)
                     (implicit val context: ActorRefFactory) extends ErgoBaseApiRoute {
   override val route = info
 
-  override val settings: RESTApiSettings = ergoSettings.scorexSettings.restApi
-
-  private def getConnectedPeers: Future[Int] = (peerManager ? PeerManager.GetConnectedPeers).mapTo[Seq[Handshake]].map(_.size)
-
-  private def stateType: StateType = ergoSettings.nodeSettings.stateType
-
-  private def nodeName: String = ergoSettings.scorexSettings.network.nodeName
-
-  private def getMinerInfo: Future[MiningStatusResponse] = (miner ? MiningStatusRequest).mapTo[MiningStatusResponse]
+  private def getMinerInfo: Future[MiningStatusResponse] = (statsCollector ? MiningStatusRequest).mapTo[MiningStatusResponse]
 
   def info: Route = (path("info") & get) {
-    val minerInfoF = getMinerInfo
-    val connectedPeersF = getConnectedPeers
-    val readersF: Future[Readers] = (readersHolder ? GetReaders).mapTo[Readers]
-    (for {
-      minerInfo <- minerInfoF
-      connectedPeers <- connectedPeersF
-      readers <- readersF
-    } yield {
-      InfoRoute.makeInfoJson(nodeId, minerInfo, connectedPeers, readers, stateType, nodeName)
-    }).okJson()
-  }
-}
-
-object InfoRoute {
-  def makeInfoJson(nodeId: Array[Byte],
-                   minerInfo: MiningStatusResponse,
-                   connectedPeersLength: Int,
-                   readers: Readers,
-                   stateType: StateType,
-                   nodeName: String): Json = {
-    val stateVersion = readers.s.map(_.version).map(Algos.encode)
-    val bestHeader = readers.h.flatMap(_.bestHeaderOpt)
-    val bestFullBlock = readers.h.flatMap(_.bestFullBlockOpt)
-    val unconfirmedCount = readers.m.map(_.size).getOrElse(0)
-    val stateRoot = readers.s.map(s => Algos.encode(s.rootHash)).getOrElse("null")
-    Map(
-      "name" -> nodeName.asJson,
-      "appVersion" -> Version.VersionString.asJson,
-      "headersHeight" -> bestHeader.map(_.height).getOrElse(0).asJson,
-      "fullHeight" -> bestFullBlock.map(_.header.height).getOrElse(0).asJson,
-      "bestHeaderId" -> bestHeader.map(_.encodedId).getOrElse("null").asJson,
-      "bestFullHeaderId" -> bestFullBlock.map(_.header.encodedId).getOrElse("null").asJson,
-      "previousFullHeaderId" -> bestFullBlock.map(_.header.parentId).map(Base58.encode).getOrElse("null").asJson,
-      "stateRoot" -> stateRoot.asJson,
-      "difficulty" -> bestFullBlock.map(_.header.requiredDifficulty).getOrElse(BigInt(0)).toString(10).asJson,
-      "unconfirmedCount" -> unconfirmedCount.asJson,
-      "stateType" -> stateType.asJson,
-      "stateVersion" -> stateVersion.asJson,
-      "isMining" -> minerInfo.isMining.asJson,
-      "votes" -> Algos.encode(minerInfo.votes).asJson,
-      "peersCount" -> connectedPeersLength.asJson
-    ).asJson
+    (statsCollector ? GetNodeInfo).mapTo[NodeInfo].map(_.json).okJson()
   }
 }
--- src/main/scala/org/ergoplatform/local/ErgoStatsCollector.scala
+++ src/main/scala/org/ergoplatform/local/ErgoStatsCollector.scala
@@ -1,12 +1,20 @@
 package org.ergoplatform.local
 
 import akka.actor.{ActorRef, ActorSystem, Props}
+import io.circe.syntax._
 import org.ergoplatform.Version
-import org.ergoplatform.modifiers.ErgoPersistentModifier
+import org.ergoplatform.local.ErgoStatsCollector.{GetNodeInfo, NodeInfo}
+import org.ergoplatform.modifiers.history.Header
 import org.ergoplatform.modifiers.mempool.AnyoneCanSpendTransaction
 import org.ergoplatform.modifiers.mempool.proposition.AnyoneCanSpendProposition
-import org.ergoplatform.settings.ErgoSettings
-import scorex.core.{LocalInterface, ModifierId}
+import org.ergoplatform.modifiers.{ErgoFullBlock, ErgoPersistentModifier}
+import org.ergoplatform.nodeView.history.ErgoHistory
+import org.ergoplatform.nodeView.state.{ErgoStateReader, StateType}
+import org.ergoplatform.settings.{Algos, ErgoSettings}
+import scorex.core.NodeViewHolder.{ChangedHistory, ChangedMempool, Subscribe}
+import scorex.core.transaction.state.StateReader
+import scorex.core.{LocalInterface, ModifierId, NodeViewHolder}
+import scorex.crypto.encode.Base58
 
 /**
   * Class that subscribes to NodeViewHolderEvents and collects them to provide fast response to API requests.
@@ -15,9 +23,49 @@ class ErgoStatsCollector(override val viewHolderRef: ActorRef,
                          settings: ErgoSettings)
   extends LocalInterface[AnyoneCanSpendProposition.type, AnyoneCanSpendTransaction, ErgoPersistentModifier] {
 
-  var nodeInfo = NodeInfo(settings.scorexSettings.network.nodeName, Version.VersionString, 0, 0, "null",
-    settings.nodeSettings.stateType, "null", isMining = false, "null", None, None)
+  override def preStart(): Unit = {
+    val events = Seq(
+      NodeViewHolder.EventType.StateChanged,
+      NodeViewHolder.EventType.HistoryChanged,
+      NodeViewHolder.EventType.MempoolChanged
+    )
+    viewHolderRef ! Subscribe(events)
+  }
 
+  private val votes = Algos.encode(Algos.hash(settings.scorexSettings.network.nodeName).take(5))
+
+  // TODO peersCount.
+  // TODO get actual votes and isMining from miner
+  var nodeInfo = NodeInfo(settings.scorexSettings.network.nodeName, Version.VersionString, 0, 0, "null",
+    settings.nodeSettings.stateType, "null", isMining = settings.nodeSettings.mining, votes, None, None)
+
+  override def receive: Receive = getNodeInfo orElse onMempoolChanged orElse super.receive
+
+  private def getNodeInfo: Receive = {
+    case GetNodeInfo => sender ! nodeInfo
+  }
+
+  private def onMempoolChanged: Receive = {
+    case ChangedMempool(p) =>
+      nodeInfo = nodeInfo.copy(unconfirmedCount = p.size)
+  }
+
+  private def onHistoryChanged: Receive = {
+    case ChangedHistory(h) if h.isInstanceOf[ErgoHistory] =>
+      nodeInfo = nodeInfo.copy(bestFullBlockOpt = h.asInstanceOf[ErgoHistory].bestFullBlockOpt,
+        bestHeaderOpt = h.asInstanceOf[ErgoHistory].bestHeaderOpt)
+  }
+
+  override protected def onChangedState(stateReader: StateReader): Unit = stateReader match {
+    case r: ErgoStateReader =>
+      nodeInfo = nodeInfo.copy(stateRoot = Algos.encode(r.asInstanceOf[ErgoStateReader].rootHash),
+        stateVersion = Algos.encode(r.version))
+    case _ =>
+      log.warn(s"Got state reader of incorrect type $stateReader")
+  }
+
+
+  //TODO move default empty implementations to Scorex
   override protected def onStartingPersistentModifierApplication(pmod: ErgoPersistentModifier): Unit = {}
 
   override protected def onFailedTransaction(tx: AnyoneCanSpendTransaction): Unit = {}
@@ -39,6 +87,42 @@ class ErgoStatsCollector(override val viewHolderRef: ActorRef,
   override protected def onNewSurface(newSurface: Seq[ModifierId]): Unit = {}
 
   override protected def onRollbackFailed(): Unit = {}
+}
+
+object ErgoStatsCollector {
+
+  case object GetNodeInfo
+
+  case class NodeInfo(nodeName: String,
+                      appVersion: String,
+                      unconfirmedCount: Int,
+                      peersCount: Int,
+                      stateRoot: String,
+                      stateType: StateType,
+                      stateVersion: String,
+                      isMining: Boolean,
+                      votes: String,
+                      bestHeaderOpt: Option[Header],
+                      bestFullBlockOpt: Option[ErgoFullBlock]) {
+    lazy val json = Map(
+      "name" -> nodeName.asJson,
+      "appVersion" -> Version.VersionString.asJson,
+      "headersHeight" -> bestHeaderOpt.map(_.height).getOrElse(-1).asJson,
+      "fullHeight" -> bestFullBlockOpt.map(_.header.height).getOrElse(-1).asJson,
+      "bestHeaderId" -> bestHeaderOpt.map(_.encodedId).getOrElse("null").asJson,
+      "bestFullHeaderId" -> bestFullBlockOpt.map(_.header.encodedId).getOrElse("null").asJson,
+      "previousFullHeaderId" -> bestFullBlockOpt.map(_.header.parentId).map(Base58.encode).getOrElse("null").asJson,
+      "difficulty" -> bestFullBlockOpt.map(_.header.requiredDifficulty).getOrElse(BigInt(0)).toString(10).asJson,
+      "unconfirmedCount" -> unconfirmedCount.asJson,
+      "stateRoot" -> stateRoot.asJson,
+      "stateType" -> stateType.stateTypeName.asJson,
+      "stateVersion" -> stateVersion.asJson,
+      "isMining" -> isMining.asJson,
+      "votes" -> votes.asJson,
+      "peersCount" -> peersCount.asJson
+    ).asJson
+  }
+
 }
 
 object ErgoStatsCollectorRef {

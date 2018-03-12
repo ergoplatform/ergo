f1425cf5630f8a19234f926fc8fd47b2a0b5e116
catena <catena@protonmail.com>
2018-03-12 16:43:50 +0300

    Fix history tests broken because of difficulty control initial period

--- src/test/scala/org/ergoplatform/utils/ChainGenerator.scala
+++ src/test/scala/org/ergoplatform/utils/ChainGenerator.scala
@@ -1,17 +1,19 @@
 package org.ergoplatform.utils
 
 import org.ergoplatform.mining.DefaultFakePowScheme
+import org.ergoplatform.mining.difficulty.LinearDifficultyControl
 import org.ergoplatform.modifiers.ErgoFullBlock
 import org.ergoplatform.modifiers.history._
 import org.ergoplatform.modifiers.mempool.AnyoneCanSpendTransaction
 import org.ergoplatform.nodeView.history.ErgoHistory
-import org.ergoplatform.settings.{Constants, ErgoSettings}
 import org.ergoplatform.settings.Constants.hashLength
-import scorex.core.utils.{NetworkTime, NetworkTimeProvider}
+import org.ergoplatform.settings.{Constants, ErgoSettings}
+import scorex.core.utils.NetworkTimeProvider
 import scorex.crypto.authds._
 import scorex.crypto.hash._
 
 import scala.annotation.tailrec
+import scala.concurrent.duration._
 import scala.util.Random
 
 trait ChainGenerator {
@@ -20,17 +22,22 @@ trait ChainGenerator {
   private val EmptyStateRoot = ADDigest @@ Array.fill(hashLength + 1)(0.toByte)
   private val EmptyDigest32 = Digest32 @@ Array.fill(hashLength)(0.toByte)
   val timeProvider: NetworkTimeProvider = new NetworkTimeProvider(ErgoSettings.read(None).scorexSettings.ntp)
+  val defaultDifficultyControl = new LinearDifficultyControl(1.minute, 8, 256)
 
   private def emptyProofs = SerializedAdProof @@ scorex.utils.Random.randomBytes(Random.nextInt(5000))
 
   def genHeaderChain(height: Int, history: ErgoHistory): HeaderChain =
-    genHeaderChain(height, history.bestHeaderOpt.toSeq)
+    genHeaderChain(height, history.bestHeaderOpt.toSeq, history.difficultyCalculator)
 
-  final def genHeaderChain(height: Int, accIn: Seq[Header]): HeaderChain =
-    genHeaderChain(acc => acc.length == accIn.length + height, accIn)
+  final def genHeaderChain(height: Int,
+                           accIn: Seq[Header],
+                           control: LinearDifficultyControl): HeaderChain =
+    genHeaderChain(acc => acc.length == accIn.length + height, accIn, control)
 
   @tailrec
-  final def genHeaderChain(until: Seq[Header] => Boolean, acc: Seq[Header]): HeaderChain = if (until(acc)) {
+  final def genHeaderChain(until: Seq[Header] => Boolean,
+                           acc: Seq[Header],
+                           control: LinearDifficultyControl): HeaderChain = if (until(acc)) {
     HeaderChain(acc.reverse)
   } else {
     val header = powScheme.prove(
@@ -39,10 +46,10 @@ trait ChainGenerator {
       EmptyStateRoot,
       EmptyDigest32,
       EmptyDigest32,
-      Math.max(timeProvider.time(), acc.headOption.map(_.timestamp + 1).getOrElse(timeProvider.time())),
+      acc.headOption.map(_.timestamp + control.desiredInterval.toMillis).getOrElse(0),
       Array.fill(5)(0.toByte)
     ): Header
-    genHeaderChain(until, header +: acc)
+    genHeaderChain(until, header +: acc, control)
   }
 
   @tailrec

eb8f119a2d8a7f7d19d8964d2717eca6663bde13
catena <catena@protonmail.com>
2018-03-12 11:13:56 +0300

    Initial epochs recalculation

--- src/main/scala/org/ergoplatform/mining/difficulty/LinearDifficultyControl.scala
+++ src/main/scala/org/ergoplatform/mining/difficulty/LinearDifficultyControl.scala
@@ -21,7 +21,9 @@ class LinearDifficultyControl(val desiredInterval: FiniteDuration,
     * @return heights of previous headers required for block recalculation
     */
   def previousHeadersRequiredForRecalculation(height: Height): Seq[Int] = {
-    if ((height - 1) % epochLength == 0 && height > epochLength * useLastEpochs) {
+    if ((height - 1) % epochLength == 0 && epochLength > 1) {
+      (0 to useLastEpochs).map(i => (height - 1) - i * epochLength).filter(_ >= 0).reverse
+    } else if ((height - 1) % epochLength == 0 && height > epochLength * useLastEpochs) {
       (0 to useLastEpochs).map(i => (height - 1) - i * epochLength).reverse
     } else {
       Seq(height - 1)
@@ -30,7 +32,10 @@ class LinearDifficultyControl(val desiredInterval: FiniteDuration,
 
   @SuppressWarnings(Array("TraversableHead"))
   def calculate(previousHeaders: Seq[Header]): Difficulty = {
-    if (previousHeaders.lengthCompare(useLastEpochs + 1) == 0) {
+    assert(previousHeaders.nonEmpty, "PreviousHeaders should always contain at least 1 element")
+    if (previousHeaders.lengthCompare(1) == 0 || previousHeaders.head.timestamp >= previousHeaders.last.timestamp) {
+      previousHeaders.head.requiredDifficulty
+    } else {
       val data: Seq[(Int, Difficulty)] = previousHeaders.sliding(2).toList.map { d =>
         val start = d.head
         val end = d.last
@@ -40,28 +45,31 @@ class LinearDifficultyControl(val desiredInterval: FiniteDuration,
       }
       val diff = interpolate(data)
       if (diff >= 1) diff else Constants.InitialDifficulty
-    } else {
-      previousHeaders.maxBy(_.height).requiredDifficulty
     }
   }
 
   //y = a + bx
   private[difficulty] def interpolate(data: Seq[(Int, Difficulty)]): Difficulty = {
     val size = data.size
-    val xy: Iterable[BigInt] = data.map(d => d._1 * d._2)
-    val x: Iterable[BigInt] = data.map(d => BigInt(d._1))
-    val x2: Iterable[BigInt] = data.map(d => BigInt(d._1) * d._1)
-    val y: Iterable[BigInt] = data.map(d => d._2)
-    val xySum = xy.sum
-    val x2Sum = x2.sum
-    val ySum = y.sum
-    val xSum = x.sum
+    if (size == 1) {
+      data.head._2
+    } else {
+      val xy: Iterable[BigInt] = data.map(d => d._1 * d._2)
+      val x: Iterable[BigInt] = data.map(d => BigInt(d._1))
+      val x2: Iterable[BigInt] = data.map(d => BigInt(d._1) * d._1)
+      val y: Iterable[BigInt] = data.map(d => d._2)
+      val xySum = xy.sum
+      val x2Sum = x2.sum
+      val ySum = y.sum
+      val xSum = x.sum
 
-    val k: BigInt = (xySum * size - xSum * ySum) * PrecisionConstant / (x2Sum * size - xSum * xSum)
-    val b: BigInt = (ySum * PrecisionConstant - k * xSum) / size / PrecisionConstant
+      //TODO check?
+      val b: BigInt = (xySum * size - xSum * ySum) * PrecisionConstant / (x2Sum * size - xSum * xSum)
+      val a: BigInt = (ySum * PrecisionConstant - b * xSum) / size / PrecisionConstant
 
-    val point = data.map(_._1).max + epochLength
-    b + k * point / PrecisionConstant
+      val point = data.map(_._1).max + epochLength
+      a + b * point / PrecisionConstant
+    }
   }
 
 }

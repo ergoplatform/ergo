7e4be1f92ef2d7e3343d72ebde0d8d9801664685
catena <catena@protonmail.com>
2018-03-19 12:02:59 +0300

    ErgoDeliveryTracker add comments to methods

--- src/main/scala/org/ergoplatform/network/ErgoDeliveryTracker.scala
+++ src/main/scala/org/ergoplatform/network/ErgoDeliveryTracker.scala
@@ -21,8 +21,14 @@ class ErgoDeliveryTracker(context: ActorContext,
   private val ToDownloadLifetime = 1.hour
   private val toDownload: mutable.Map[ModifierIdAsKey, ToDownloadStatus] = mutable.Map[ModifierIdAsKey, ToDownloadStatus]()
 
+  /**
+    * @return ids we're going to download
+    */
   def toDownloadQueue: Iterable[ModifierId] = ModifierId @@ toDownload.keys.map(_.array)
 
+  /**
+    * Process download request of modifier of type modifierTypeId with id modifierId
+    */
   def downloadRequested(modifierTypeId: ModifierTypeId, modifierId: ModifierId): Unit = {
     val downloadRequestTime = timeProvider.time()
     val newValue = toDownload.get(key(modifierId))
@@ -31,13 +37,18 @@ class ErgoDeliveryTracker(context: ActorContext,
     toDownload.put(key(modifierId), newValue)
   }
 
+  /**
+    * Remove old modifiers from download queue
+    */
   def removeOutdatedToDownload(historyReaderOpt: Option[ErgoHistoryReader]): Unit = {
     val currentTime = timeProvider.time()
-    toDownload.filter(td => (td._2.firstViewed < currentTime - ToDownloadLifetime.toMillis)
-      || historyReaderOpt.exists(hr => hr.contains(ModifierId @@ td._1.array)))
+    toDownload.filter(td => td._2.firstViewed < currentTime - ToDownloadLifetime.toMillis)
       .foreach(i => toDownload.remove(i._1))
   }
 
+  /**
+    * Id's that are already in queue to download but are not downloaded yet
+    */
   def idsToRetry(): Seq[(ModifierTypeId, ModifierId)] = {
     val currentTime = timeProvider.time()
     toDownload.filter(_._2.lastTry < currentTime - ToDownloadRetryInterval.toMillis).toSeq
@@ -45,6 +56,9 @@ class ErgoDeliveryTracker(context: ActorContext,
       .map(i => (i._2.tp, ModifierId @@ i._1.array))
   }
 
+  /**
+    * MOdifier downloaded
+    */
   override def receive(mtid: ModifierTypeId, mid: ModifierId, cp: ConnectedPeer): Unit = {
     toDownload.remove(key(mid))
     super.receive(mtid, mid, cp)

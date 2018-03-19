7edd01e026ce40fd5730f33e43487ecd416310f2
catena <catena@protonmail.com>
2018-03-19 21:08:17 +0300

    Change ErgoHistoryReader class comments

--- src/main/scala/org/ergoplatform/nodeView/history/ErgoHistoryReader.scala
+++ src/main/scala/org/ergoplatform/nodeView/history/ErgoHistoryReader.scala
@@ -18,21 +18,7 @@ import scala.annotation.tailrec
 import scala.util.{Failure, Try}
 
 /**
-  * History implementation. It is processing persistent modifiers generated locally or coming from network.
-  * Depending on chosen node settings, it will process modifiers in a different way, different processors define how to
-  * process different type of modifiers.
-  *
-  * HeadersProcessor: processor of block headers. It's the same for all node settings
-  * ADProofsProcessor: processor of ADProofs. ADProofs may
-  *   1. Be downloaded from other nodes (ADState == true)
-  *   2. Be calculated by using local state (ADState == false)
-  *   3. Be ignored by history in light mode (verifyTransactions == false)
-  * PoPoWProofsProcessor: processor of PoPoWProof. PoPoWProof may
-  *   1. Be downloaded once during bootstrap from other peers (poPoWBootstrap == true)
-  *   2. Be ignored by history (poPoWBootstrap == false)
-  * BlockTransactionsProcessor: Processor of BlockTransactions. BlockTransactions may
-  *   1. Be downloaded from other peers (verifyTransactions == true)
-  *   2. Be ignored by history (verifyTransactions == false)
+  * Read-only copy of ErgoHistory
   */
 trait ErgoHistoryReader
   extends HistoryReader[ErgoPersistentModifier, ErgoSyncInfo]

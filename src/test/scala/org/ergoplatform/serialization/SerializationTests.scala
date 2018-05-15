5d7cc1d1ce6a5cf428aa51b5fae8d0c8807b9ed2
catena <catena@protonmail.com>
2018-05-15 12:12:55 +0300

    Remove println

--- src/test/scala/org/ergoplatform/serialization/SerializationTests.scala
+++ src/test/scala/org/ergoplatform/serialization/SerializationTests.scala
@@ -25,7 +25,6 @@ class SerializationTests extends PropSpec
   property("Header serialization") {
     val serializer = HeaderSerializer
     forAll(invalidHeaderGen) { b: Header =>
-      println(b)
       val recovered = serializer.parseBytes(b.bytes)
       recovered.get shouldBe b
     }

ede1981ac6a5569d0bfb3ac7f3110e47b2a96a26
Mike Aksarin <mike.aksarin@gmail.com>
2018-05-24 14:01:38 +0300

    style fixes

--- src/main/scala/org/ergoplatform/api/InfoRoute.scala
+++ src/main/scala/org/ergoplatform/api/InfoRoute.scala
@@ -4,20 +4,15 @@ import akka.actor.{ActorRef, ActorRefFactory}
 import akka.http.scaladsl.server.Route
 import akka.pattern.ask
 import io.circe.syntax._
-import org.ergoplatform.local.ErgoMiner.{MiningStatusRequest, MiningStatusResponse}
 import org.ergoplatform.local.ErgoStatsCollector.{GetNodeInfo, NodeInfo}
 import scorex.core.settings.RESTApiSettings
 import scorex.core.utils.NetworkTimeProvider
-
-import scala.concurrent.Future
 
 case class InfoRoute(statsCollector: ActorRef,
                      override val settings: RESTApiSettings,
                      timeProvider: NetworkTimeProvider)
                     (implicit val context: ActorRefFactory) extends ErgoBaseApiRoute {
   override val route = info
-
-  private def getMinerInfo: Future[MiningStatusResponse] = (statsCollector ? MiningStatusRequest).mapTo[MiningStatusResponse]
 
   def info: Route = (path("info") & get) {
     val timeJson = Map("currentTime" -> timeProvider.time().asJson).asJson

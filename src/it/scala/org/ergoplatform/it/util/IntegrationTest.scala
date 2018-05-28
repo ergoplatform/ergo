680a549e70cea0b9c11334d838e99b0901216e22
Mike Aksarin <mike.aksarin@gmail.com>
2018-05-28 14:00:48 +0300

    added test classpath to integration tests

--- src/it/scala/org/ergoplatform/it/util/IntegrationTest.scala
+++ src/it/scala/org/ergoplatform/it/util/IntegrationTest.scala
@@ -1,22 +1,13 @@
 package org.ergoplatform.it.util
 
-import java.util.concurrent.Executors
-
-import org.scalatest.Matchers
+import org.ergoplatform.utils.ErgoTestHelpers
 import org.scalatest.concurrent.{IntegrationPatience, ScalaFutures}
 import scorex.core.utils.ScorexLogging
 
 import scala.concurrent.ExecutionContext
 
-trait IntegrationTest extends Matchers with ScalaFutures with IntegrationPatience with ScorexLogging {
+trait IntegrationTest extends ErgoTestHelpers with ScalaFutures with IntegrationPatience with ScorexLogging {
 
-  implicit def ec: ExecutionContext = IntegrationTest.defaultExecutionContext
-
-}
-
-object IntegrationTest {
-
-  implicit val defaultExecutionContext: ExecutionContext =
-    ExecutionContext.fromExecutor(Executors.newFixedThreadPool(10))
+  implicit def ec: ExecutionContext = ErgoTestHelpers.defaultExecutionContext
 
 }

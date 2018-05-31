d44652fd63d7f4b2106b55f5cfb87045dbb6a984
catena <catena@protonmail.com>
2018-05-31 14:56:52 +0300

    simplify coins emission calculation

--- src/main/scala/org/ergoplatform/mining/emission/CoinsEmission.scala
+++ src/main/scala/org/ergoplatform/mining/emission/CoinsEmission.scala
@@ -14,36 +14,25 @@ package org.ergoplatform.mining.emission
   * @param blocksPerHour - munber of blocks per hour. Should be correlated with LinearDifficultyControl.desiredInterval
   */
 class CoinsEmission(val coinsInOneErgo: Long = 100000000, val blocksPerHour: Int = 30) {
-
-  // Number of blocks per year on average
-  private val blocksPerYear: Int = 365 * 24 * blocksPerHour
-
-  // 8 years of emission
-  val blocksTotal: Int = blocksPerYear * 8
-
-  // reward reduction every 3 month
-  private lazy val rewardReductionPeriod: Int = 90 * 24 * blocksPerHour
-
-  // Number of blocks for initial fixed rate
-  private lazy val fixedRatePeriod = 2 * blocksPerYear - rewardReductionPeriod
-
-  // NUmber of coins issued per block during FixedRatePeriod
+  // 2 years of fixed rate
+  private val fixedRatePeriod = blocksPerHour * 2 * 24 * 365
+  // 75 coins per block
   private val fixedRate = 2250 * coinsInOneErgo / blocksPerHour
+  // 3 months of epoch
+  private val epochLength: Int = 90 * 24 * blocksPerHour
+  // 3 coins reduction every epoch
+  private val oneEpochReduction = 3 * coinsInOneErgo
+  val blocksTotal = fixedRatePeriod * 4
 
-  //Total number of epochs with different number of coins issued
-  private val decreasingEpochs = (blocksTotal - fixedRatePeriod) / rewardReductionPeriod
 
   def emissionAtHeight(h: Long): Long = {
-    if (h <= fixedRatePeriod) {
+    if (h < fixedRatePeriod) {
       fixedRate
-    } else if (h > blocksTotal) {
-      0
     } else {
-      val epoch: Int = ((h - fixedRatePeriod) / rewardReductionPeriod).toInt
-      fixedRate - fixedRate * epoch / decreasingEpochs
+      val epoch = 1 + (h - fixedRatePeriod) / epochLength
+      Math.max(fixedRate - oneEpochReduction * epoch, 0)
     }
   }.ensuring(_ >= 0, s"Negative at $h")
-
 
 }
 

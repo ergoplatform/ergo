3f3c9341b87f242062b10a5f407a6ee7b5109445
catena <catena@protonmail.com>
2018-06-01 13:35:43 +0300

    Failing tests for ErgoState.boxChanges() double spend attempt

--- src/test/scala/org/ergoplatform/nodeView/state/ErgoStateSpecification.scala
+++ src/test/scala/org/ergoplatform/nodeView/state/ErgoStateSpecification.scala
@@ -17,7 +17,33 @@ class ErgoStateSpecification extends ErgoPropertyTest {
     rootHash shouldBe expectedRootHash
   }
 
-  property("ErgoState.boxChanges()") {
+  property("ErgoState.boxChanges() double spend attempt") {
+      var (us: UtxoState, bh) = createUtxoState()
+      bh.boxes.size shouldBe 1
+      val genesisBox = bh.boxes.head._2
+
+      forAll { seed: Int =>
+        val txs = validTransactionsFromBoxHolder(bh, new Random(seed))._1
+        whenever(txs.lengthCompare(2) > 0) {
+          // valid transaction should spend the only existing genesis box
+          ErgoState.boxChanges(txs)._1.length shouldBe 1
+          ErgoState.boxChanges(txs)._1.head shouldBe genesisBox.id
+
+          // second transaction input should be be an input, created by the first transaction
+          val inputToDoubleSpend = txs(1).inputs.head
+          txs.head.outputs.find(_.id sameElements inputToDoubleSpend.boxId) should not be None
+          val doubleSpendTx = txs.last.copy(inputs = inputToDoubleSpend +: txs.last.inputs.tail)
+          val invalidTxs = txs.dropRight(1) :+ doubleSpendTx
+          invalidTxs.length shouldBe txs.length
+          invalidTxs.count(_.inputs.contains(inputToDoubleSpend)) shouldBe 2
+
+          ErgoState.boxChanges(invalidTxs)._1.length shouldBe 2
+        }
+      }
+    }
+
+
+  property("ErgoState.stateChanges()") {
     var (us: UtxoState, bh) = createUtxoState()
     bh.boxes.size shouldBe 1
     val genesisBox = bh.boxes.head._2

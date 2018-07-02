d673e8d6994d689f3ea07084db0a0b2cf8486c5c
catena <catena@protonmail.com>
2018-07-02 20:12:49 +0300

    List of waiting modifiers in ErgoModifiersCache

--- src/main/scala/org/ergoplatform/nodeView/ErgoModifiersCache.scala
+++ src/main/scala/org/ergoplatform/nodeView/ErgoModifiersCache.scala
@@ -1,61 +1,58 @@
 package org.ergoplatform.nodeView
 
 import org.ergoplatform.modifiers.ErgoPersistentModifier
-import org.ergoplatform.modifiers.history.Header
 import org.ergoplatform.nodeView.history.ErgoHistory
-import scorex.core.{LRUCache, ModifiersCache}
-import scorex.core.utils.ScorexLogging
+import scorex.core.DefaultModifiersCache
 import scorex.core.validation.RecoverableModifierError
 
-import scala.annotation.tailrec
-import scala.collection.mutable
-import scala.util.{Failure, Random, Success}
+import scala.collection.mutable.ArrayBuffer
+import scala.util.{Failure, Success}
 
 class ErgoModifiersCache(override val maxSize: Int)
-  extends ModifiersCache[ErgoPersistentModifier, ErgoHistory]
-      with LRUCache[ErgoPersistentModifier, ErgoHistory]
-      with ScorexLogging  {
+  extends DefaultModifiersCache[ErgoPersistentModifier, ErgoHistory](maxSize) {
 
-  protected override def onPut(key: K): Unit =
-    super.onPut(key)
+  /**
+    * Id's of block sections we are waiting to create block from full block
+    */
+  private val waitingIds: ArrayBuffer[K] = ArrayBuffer[K]()
 
-  protected override def onRemove(key: K, rememberKey: Boolean): Unit =
-    super.onRemove(key, rememberKey)
-
-  override def findCandidateKey(history: ErgoHistory): Option[K] = {
-    val headersHeight = history.headersHeight
-
-    val maxIterations = 10
-
-    @tailrec
-    def cacheIteration(called: Int): Option[K] = {
-      val expectedFullBlockParts = history
-        .headerIdsAtHeight(history.fullBlockHeight + 1)
-        .flatMap(id => history.typedModifierById[Header](id))
-        .flatMap(h => Seq(mutable.WrappedArray.make[Byte](h.transactionsId), mutable.WrappedArray.make[Byte](h.ADProofsId)))
-        .find(id => contains(id))
-        .flatMap(id => cache.get(id))
-        .toSeq
-
-      val headerToApply =
-        cache
-          .find { case (_, p) => p.isInstanceOf[Header] && p.asInstanceOf[Header].height <= headersHeight + 1 }
-          .map(_._2)
-          .toSeq
-
-      val mods = expectedFullBlockParts ++ headerToApply
-
-      if(mods.isEmpty) None else {
-        val mod = mods(Random.nextInt(mods.size))
-        history.applicableTry(mod) match {
-          case Failure(e) =>
-            if (!e.isInstanceOf[RecoverableModifierError]) remove(mod.id, rememberKey = true)
-            if (called == maxIterations - 1) None else cacheIteration(called + 1)
-          case Success(_) => Some(mod.id)
-        }
-      }
+  protected override def onPut(key: K): Unit = {
+    // TODO make efficient when value will be present and add ids that are not in a cache yet
+    cache.foreach { case (k: K, v: V) =>
+      if (v.parentId sameElements key.array) waitingIds += k
     }
-
-    cacheIteration(0)
+    super.onPut(key)
   }
+
+  protected override def onRemove(key: K, rememberKey: Boolean): Unit = {
+    waitingIds -= key
+    super.onRemove(key, rememberKey)
+  }
+
+  /**
+    * Defines a candidate to be applied.
+    * Get candidate from short list if possible, do exhaustive search otherwise
+    *
+    * @param history - an interface to history which could be needed to define a candidate
+    * @return - candidate if it is found
+    */
+  override def findCandidateKey(history: ErgoHistory): Option[K] = {
+    waitingIds.flatMap(k => cache.get(k).map(v => k -> v)).find { case (k, v) =>
+      history.applicableTry(v) match {
+        case Failure(e) if e.isInstanceOf[RecoverableModifierError] =>
+          // do nothing - modifier may be applied in future
+          log.warn("Modifier from waitingIds list is not applicable now. Should avoid such situations")
+          false
+        case Failure(e) =>
+          // non-recoverable error - remove modifier from cache
+          // TODO blacklist peer who sent it
+          log.warn(s"Modifier ${v.encodedId} is permanently invalid and will be removed from cache", e)
+          remove(k, rememberKey = true)
+          false
+        case Success(_) =>
+          true
+      }
+    }.map(_._1) orElse super.findCandidateKey(history)
+  }
+
 }

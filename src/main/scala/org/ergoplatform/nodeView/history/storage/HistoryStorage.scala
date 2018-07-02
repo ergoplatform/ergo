e593e7a3c1dc970c77e955a88ad6a10f5fa083e3
catena <catena@protonmail.com>
2018-07-02 16:04:55 +0300

    Cache objects in HistoryStorage. fix #332

--- src/main/scala/org/ergoplatform/nodeView/history/storage/HistoryStorage.scala
+++ src/main/scala/org/ergoplatform/nodeView/history/storage/HistoryStorage.scala
@@ -1,26 +1,36 @@
 package org.ergoplatform.nodeView.history.storage
 
+import com.google.common.cache.CacheBuilder
 import io.iohk.iodb.{ByteArrayWrapper, Store}
 import org.ergoplatform.modifiers.ErgoPersistentModifier
 import org.ergoplatform.modifiers.history.HistoryModifierSerializer
+import org.ergoplatform.settings.Algos
 import scorex.core.ModifierId
 import scorex.core.utils.{ScorexEncoding, ScorexLogging}
 
-import scala.util.{Failure, Success}
+import scala.util.Failure
+import scalacache._
+import scalacache.guava._
+import scalacache.modes.try_.mode
 
 class HistoryStorage(indexStore: Store, objectsStore: ObjectsStore) extends ScorexLogging with AutoCloseable
   with ScorexEncoding {
 
-  def modifierById(id: ModifierId): Option[ErgoPersistentModifier] = objectsStore.get(id)
-    .flatMap { bBytes =>
-      HistoryModifierSerializer.parseBytes(bBytes) match {
-        case Success(b) =>
-          Some(b)
-        case Failure(e) =>
+  // TODO move size to config?
+  val underlyingGuavaCache = CacheBuilder.newBuilder().maximumSize(100L).build[String, Entry[ErgoPersistentModifier]]
+  implicit val modifiersCache: Cache[ErgoPersistentModifier] = GuavaCache(underlyingGuavaCache)
+
+  def modifierById(id: ModifierId): Option[ErgoPersistentModifier] = {
+    objectsStore.get(id).flatMap { bBytes =>
+      cachingF(Algos.encode(id))(ttl = None) {
+        HistoryModifierSerializer.parseBytes(bBytes).recoverWith { case e =>
           log.warn(s"Failed to parse modifier ${encoder.encode(id)} from db (bytes are: ${bBytes.mkString("-")}): ", e)
-          None
-      }
+          Failure(e)
+        }
+      }.toOption
     }
+  }
+
 
   def getIndex(id: ByteArrayWrapper): Option[ByteArrayWrapper] = indexStore.get(id)
 

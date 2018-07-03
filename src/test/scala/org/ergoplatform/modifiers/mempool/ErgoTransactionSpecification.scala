41f495fa7558a9983d7e2a03d6c323d3d52eef69
Alex Chepurnoy <alex.chepurnoy@iohk.io>
2018-07-03 19:45:36 +0300

    ergo preservation law holds

--- src/test/scala/org/ergoplatform/modifiers/mempool/ErgoTransactionSpecification.scala
+++ src/test/scala/org/ergoplatform/modifiers/mempool/ErgoTransactionSpecification.scala
@@ -1,24 +1,41 @@
 package org.ergoplatform.modifiers.mempool
 
+import org.ergoplatform.ErgoBoxCandidate
 import org.ergoplatform.nodeView.state.ErgoStateContext
 import org.ergoplatform.utils.ErgoPropertyTest
 import scorex.crypto.authds.ADDigest
 
+import scala.util.Random
+
 class ErgoTransactionSpecification extends ErgoPropertyTest {
+
+  private val context = ErgoStateContext(0, ADDigest @@ Array.fill(32)(0:Byte))
+
+  private def modifyValue(boxCandidate: ErgoBoxCandidate, delta: Int): ErgoBoxCandidate = {
+    new ErgoBoxCandidate(
+      boxCandidate.value + delta,
+      boxCandidate.proposition,
+      boxCandidate.additionalTokens,
+      boxCandidate.additionalRegisters)
+  }
 
   property("a valid transaction is valid") {
     forAll(validErgoTransactionGen){case (from, tx) =>
-      val context = ErgoStateContext(0, ADDigest @@ Array.fill(32)(0:Byte))
-
       tx.statelessValidity.isSuccess shouldBe true
-      val res = tx.statefulValidity(from, context)
-      println(res)
-      res.isSuccess shouldBe true
+      tx.statefulValidity(from, context).isSuccess shouldBe true
     }
   }
 
   property("ergo preservation law holds") {
+    forAll(validErgoTransactionGen, smallPositiveInt) { case ((from, tx), deltaAbs) =>
+      val delta = if(Random.nextBoolean()) -deltaAbs else deltaAbs
 
+      val wrongTx = tx.copy(outputCandidates =
+        modifyValue(tx.outputCandidates.head, delta) +: tx.outputCandidates.tail)
+
+      wrongTx.statelessValidity.isSuccess shouldBe true
+      wrongTx.statefulValidity(from, context).isSuccess shouldBe false
+    }
   }
 
   property("impossible to create a negative-value output") {

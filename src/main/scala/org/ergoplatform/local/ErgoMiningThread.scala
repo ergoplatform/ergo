232fe6f094036dcc41413a337237acce60305f30
Alex Chepurnoy <alex.chepurnoy@iohk.io>
2018-07-12 18:06:32 +0300

    Address separation, TxGen fixes

--- src/main/scala/org/ergoplatform/local/ErgoMiningThread.scala
+++ src/main/scala/org/ergoplatform/local/ErgoMiningThread.scala
@@ -40,7 +40,8 @@ class ErgoMiningThread(ergoSettings: ErgoSettings,
       // timestamp is increased for at least 1 as a role of a nonce
       val newTimestamp = Math.max(candidate.timestamp + 1, timeProvider.time())
       candidate = candidate.copy(timestamp = newTimestamp)
-      log.info(s"Trying to prove block with parent ${candidate.parentOpt.map(_.encodedId)} and timestamp $newTimestamp")
+      log.info(s"Trying to prove block with parent ${candidate.parentOpt.map(_.encodedId)} and timestamp $newTimestamp " +
+        s"containing ${candidate.transactions.size} transactions")
       powScheme.proveBlock(candidate) match {
         case Some(newBlock) =>
           log.info("New block found: " + newBlock)

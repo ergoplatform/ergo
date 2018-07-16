d02edc283f8bfaeb5b04d5e5e9207ceed999fb17
Mike Aksarin <mike.aksarin@gmail.com>
2018-07-16 12:56:03 +0300

    Long chain synchronization integration test added

---
+++ src/it/scala/org/ergoplatform/it/LongChainSpec.scala
@@ -0,0 +1,26 @@
+package org.ergoplatform.it
+
+import org.scalatest.FreeSpec
+
+import scala.async.Async.{async, await}
+import scala.concurrent.Await
+import scala.concurrent.duration._
+
+class LongChainSpec extends FreeSpec with IntegrationSuite {
+
+    s"Synchronize long blocks" in {
+    val minerConfig = noDelayConfig.withFallback(Docker.nodeConfigs.head)
+    val followerConfig = nonGeneratingPeerConfig.withFallback(Docker.nodeConfigs(1))
+    val miner = docker.startNode(minerConfig).success.value
+    val blocksCount = miner.settings.scorexSettings.network.maxInvObjects
+    log.info(s"Config maxInvObjects = $blocksCount")
+    val check = async {
+      await(miner.waitForHeight(blocksCount))
+      val follower = docker.startNode(followerConfig).success.value
+      await(follower.waitForHeight(blocksCount))
+      succeed
+    }
+      Await.result(check, 5.minutes)
+  }
+}
+

58197a645a99d3eb58cd6ae06153977c08e5ba14
Alex Chepurnoy <alex.chepurnoy@iohk.io>
2018-07-31 17:14:36 +0300

    tx scan test fixes

--- src/test/scala/org/ergoplatform/nodeView/wallet/ErgoWalletSpecification.scala
+++ src/test/scala/org/ergoplatform/nodeView/wallet/ErgoWalletSpecification.scala
@@ -1,6 +1,8 @@
 package org.ergoplatform.nodeView.wallet
 
 import akka.actor.{ActorRef, ActorSystem}
+import akka.pattern.ask
+import akka.util.Timeout
 import org.ergoplatform.{ErgoBoxCandidate, Input}
 import org.ergoplatform.modifiers.mempool.ErgoTransaction
 import org.ergoplatform.nodeView.ErgoNodeViewRef
@@ -18,6 +20,7 @@ import sigmastate.interpreter.{ContextExtension, ProverResult}
 import scala.concurrent.duration._
 import scala.concurrent.Await
 import scala.util.Random
+
 
 class ErgoWalletSpecification extends ErgoPropertyTest {
 
@@ -43,55 +46,52 @@ class ErgoWalletSpecification extends ErgoPropertyTest {
       new ErgoTransaction(IndexedSeq(input), IndexedSeq(new ErgoBoxCandidate(balance, script)))
     }
 
-    nodeViewHolderRef ! GetDataFromCurrentView[H, S, W, P, Any] { v =>
-      val w = v.vault
-      val bf0 = w.unconfirmedBalances()
+    val w: ErgoWallet = new ErgoWallet(actorSystem, null, null, settings)
 
-      val bs0 = Await.result(bf0, 1.second)
+    val bf0 = w.unconfirmedBalances()
 
-      bs0.balance shouldBe 0
-      bs0.assetBalances.isEmpty shouldBe true
+    val bs0 = Await.result(bf0, 1.second)
 
-      val address = P2PKAddress(pubKey)
-      w.watchFor(address)
+    bs0.balance shouldBe 0
+    bs0.assetBalances.isEmpty shouldBe true
 
-      val balance1 = Random.nextInt(1000) + 1
-      w.scanOffchain(makeTx(balance1))
+    val address = P2PKAddress(pubKey)
+    w.watchFor(address)
 
-      val bf1 = w.unconfirmedBalances()
-      val bs1 = Await.result(bf1, 1.second)
-      bs1.balance shouldBe balance1
-      bs1.assetBalances.isEmpty shouldBe true
+    val balance1 = Random.nextInt(1000) + 1
+    w.scanOffchain(makeTx(balance1))
 
-      val balance2 = Random.nextInt(1000) + 1
-      w.scanOffchain(makeTx(balance2))
+    val bf1 = w.unconfirmedBalances()
+    val bs1 = Await.result(bf1, 1.second)
+    bs1.balance shouldBe balance1
+    bs1.assetBalances.isEmpty shouldBe true
 
-      val bf2 = w.unconfirmedBalances()
-      val bs2 = Await.result(bf2, 1.second)
-      bs2.balance shouldBe (balance1 + balance2)
-      bs2.assetBalances.isEmpty shouldBe true
+    val balance2 = Random.nextInt(1000) + 1
+    w.scanOffchain(makeTx(balance2))
 
-      w.watchFor(ScriptAddress(Values.TrueLeaf))
-      val balance3 = Random.nextInt(1000) + 1
-      w.scanOffchain(makeTx(balance3, Values.TrueLeaf))
+    val bf2 = w.unconfirmedBalances()
+    val bs2 = Await.result(bf2, 1.second)
+    bs2.balance shouldBe (balance1 + balance2)
+    bs2.assetBalances.isEmpty shouldBe true
 
-      val bf3 = w.unconfirmedBalances()
-      val bs3 = Await.result(bf3, 1.second)
-      println(bs3)
-      bs3.balance shouldBe (balance1 + balance2 + balance3)
-      bs3.assetBalances.isEmpty shouldBe true
+    w.watchFor(ScriptAddress(Values.TrueLeaf))
+    val balance3 = Random.nextInt(1000) + 1
+    w.scanOffchain(makeTx(balance3, Values.TrueLeaf))
 
-      //todo: enhance the test, e.g. add assets
-    }
+    val bf3 = w.unconfirmedBalances()
+    val bs3 = Await.result(bf3, 1.second)
+    bs3.balance shouldBe (balance1 + balance2 + balance3)
+    bs3.assetBalances.isEmpty shouldBe true
 
-    Thread.sleep(5000)
+    //todo: enhance the test, e.g. add assets
+
   }
 
   property("Successfully scans an onchain transaction") {
 
   }
 
-  property("Successfully doing a rollback"){
+  property("Successfully doing a rollback") {
 
   }
 }

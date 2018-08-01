0a51efe90d7e1ed051c13f84b38298d07f6fd54f
Alex Chepurnoy <alex.chepurnoy@iohk.io>
2018-08-01 15:09:24 +0300

    test for proper selection of boxes w. assets

--- src/test/scala/org/ergoplatform/nodeView/wallet/DefaultBoxSelectorSpecification.scala
+++ src/test/scala/org/ergoplatform/nodeView/wallet/DefaultBoxSelectorSpecification.scala
@@ -4,6 +4,7 @@ import io.iohk.iodb.ByteArrayWrapper
 import org.ergoplatform.ErgoBox
 import org.ergoplatform.modifiers.mempool.ErgoTransaction
 import org.ergoplatform.utils.ErgoPropertyTest
+import scorex.crypto.hash.{Blake2b256, Digest32}
 import sigmastate.Values
 
 class DefaultBoxSelectorSpecification extends ErgoPropertyTest {
@@ -67,5 +68,52 @@ class DefaultBoxSelectorSpecification extends ErgoPropertyTest {
     s5.get.changeBoxes.size == 1
     s5.get.changeBoxes.head._1 shouldBe 50
     s5.get.boxes shouldBe Seq(box1, box2, box3)
+  }
+
+  property("properly selects coins - assets w. 1 change box") {
+
+    val assetId1 = ByteArrayWrapper(Blake2b256("hello"))
+    val assetId2 = ByteArrayWrapper(Blake2b256("world"))
+
+    val parentTx = ErgoTransaction(IndexedSeq(), IndexedSeq())
+    val box1 = ErgoBox(1, Values.TrueLeaf, Seq(Digest32 @@ assetId1.data -> 1))
+    val box2 = ErgoBox(10, Values.TrueLeaf, Seq(Digest32 @@ assetId2.data -> 10))
+    val box3 = ErgoBox(100, Values.TrueLeaf, Seq(Digest32 @@ assetId1.data -> 100))
+
+    val uBox1 = UnspentOnchainBox(parentTx, 0, 100, box1, BoxCertainty.Certain)
+    val uBox2 = UnspentOffchainBox(parentTx, 1, box2, BoxCertainty.Certain)
+    val uBox3 = UnspentOnchainBox(parentTx, 2, 100, box3, BoxCertainty.Certain)
+
+    val uBoxes = Seq(uBox1, uBox2, uBox3)
+
+    val s1 = select(uBoxes.toIterator, _ => true, 1, Map(assetId1 -> 1))
+    s1.isDefined shouldBe true
+    s1.get.changeBoxes.isEmpty shouldBe true
+    s1.get.boxes.head shouldBe box1
+
+    val s2 = select(uBoxes.toIterator, _ => true, 1, Map(assetId1 -> 11))
+    s2.isDefined shouldBe true
+    s2.get.changeBoxes.size == 1
+    s2.get.changeBoxes.head._1 shouldBe 100
+    s2.get.changeBoxes.head._2(assetId1) shouldBe 90
+    s2.get.boxes shouldBe Seq(box1, box3)
+
+    select(uBoxes.toIterator, box => box.onchain, 1, Map(assetId2 -> 1)) shouldBe None
+    select(uBoxes.toIterator, _ => true, 1, Map(assetId2 -> 11)) shouldBe None
+    select(uBoxes.toIterator, _ => true, 1, Map(assetId1 -> 1000)) shouldBe None
+
+    val s3 = select(uBoxes.toIterator, _ => true, 1, Map(assetId1 -> 11, assetId2 -> 1))
+    s3.isDefined shouldBe true
+    s3.get.changeBoxes.size == 1
+    s3.get.changeBoxes.head._1 shouldBe 110
+    s3.get.changeBoxes.head._2(assetId1) shouldBe 90
+    s3.get.changeBoxes.head._2(assetId2) shouldBe 9
+    s3.get.boxes shouldBe Seq(box1, box2, box3)
+
+    select(uBoxes.toIterator, box => box.onchain, 1, Map(assetId1 -> 11, assetId2 -> 1)) shouldBe None
+  }
+
+  property("properly selects coins - assets w. multiple change boxes") {
+
   }
 }

8fadc48f065beb2443fa5264c0e72cb5fd5ee8ba
Aleksei <daron666@yandex.ru>
2018-08-01 12:40:05 +0400

    add new tests from scorex into ergo

--- src/test/scala/org/ergoplatform/ErgoSanity.scala
+++ src/test/scala/org/ergoplatform/ErgoSanity.scala
@@ -1,25 +1,34 @@
 package org.ergoplatform
 
+import akka.actor.{ActorRef, ActorSystem, Props}
+import akka.testkit.TestProbe
+import io.iohk.iodb.ByteArrayWrapper
 import org.ergoplatform.ErgoSanity._
 import org.ergoplatform.mining.DefaultFakePowScheme
-import org.ergoplatform.modifiers.ErgoPersistentModifier
+import org.ergoplatform.mining.emission.CoinsEmission
+import org.ergoplatform.modifiers.{ErgoFullBlock, ErgoPersistentModifier}
 import org.ergoplatform.modifiers.history.Header
 import org.ergoplatform.modifiers.mempool.ErgoTransaction
+import org.ergoplatform.nodeView.ErgoNodeViewHolder
 import org.ergoplatform.nodeView.history.{ErgoHistory, ErgoSyncInfo}
 import org.ergoplatform.nodeView.mempool.ErgoMemPool
 import org.ergoplatform.nodeView.state.{DigestState, UtxoState}
-import org.ergoplatform.settings.Constants
+import org.ergoplatform.settings.{Constants, ErgoSettings}
 import org.ergoplatform.settings.Constants.hashLength
 import org.ergoplatform.utils.{ErgoTestHelpers, HistorySpecification}
 import org.scalacheck.Gen
-import scorex.core.bytesToId
+import scorex.core.{bytesToId, idToBytes, idToVersion}
 import scorex.core.transaction.state.MinimalState
+import scorex.core.utils.NetworkTimeProvider
 import scorex.crypto.authds.ADDigest
-import scorex.crypto.hash.Digest32
+import scorex.crypto.hash.{Blake2b256, Digest32}
+import scorex.testkit.generators.{ModifierProducerTemplateItem, SynInvalid, Valid}
 import scorex.testkit.properties._
 import scorex.testkit.properties.mempool.MempoolTransactionsTest
 import scorex.testkit.properties.state.StateApplicationTest
 import scorex.utils.Random
+
+import scala.concurrent.ExecutionContext
 
 //todo: currently this class parametrized with UtxoState, consider DigestState as well
 trait ErgoSanity[ST <: MinimalState[PM, ST]] extends HistoryTests[TX, PM, SI, HT]
@@ -32,6 +41,7 @@ trait ErgoSanity[ST <: MinimalState[PM, ST]] extends HistoryTests[TX, PM, SI, HT
   //with MempoolFilterPerformanceTest[P, TX, MPool]
   //with MempoolRemovalTest[P, TX, MPool, PM, PM, HT, SI]
   //with BoxStateChangesGenerationTest[P, TX, PM, B, ST]
+  with NodeViewHolderTests[TX, PM, ST, SI, HT, MPool]
   with ErgoTestHelpers
   with HistorySpecification {
 
@@ -61,6 +71,29 @@ trait ErgoSanity[ST <: MinimalState[PM, ST]] extends HistoryTests[TX, PM, SI, HT
 
   override def syntacticallyInvalidModifier(history: HT): PM =
     syntacticallyValidModifier(history).copy(parentId = bytesToId(Random.randomBytes(32)))
+
+  private val hf = Blake2b256
+
+  def makeSyntacticallyInvalid(mod: PM): PM = mod match {
+    case fb: ErgoFullBlock => {
+      val parentId = fb.header.parentId
+      val header = fb.header.copy(parentId = bytesToId(hf(parentId)))
+      fb.copy(header = header)
+    }
+    case h: Header => h.copy(parentId = bytesToId(hf(h.parentId)))
+    case v => v
+  }
+
+  def customModifiers(history: HT,
+                      state: ST,
+                      template: Seq[ModifierProducerTemplateItem]): Seq[PM] =
+    template.zip(totallyValidModifiers(history, state, template.length))
+      .map { case (templateItem, mod) =>
+        templateItem match {
+          case Valid => mod
+          case SynInvalid => makeSyntacticallyInvalid(mod)
+        }
+      }
 
 }
 
--- src/test/scala/org/ergoplatform/ErgoSanityDigest.scala
+++ src/test/scala/org/ergoplatform/ErgoSanityDigest.scala
@@ -1,9 +1,24 @@
 package org.ergoplatform
 
+import java.io.File
+
+import akka.actor.{ActorRef, ActorSystem, Props}
+import akka.testkit.TestProbe
+import io.iohk.iodb.ByteArrayWrapper
 import org.ergoplatform.ErgoSanity.{DIGEST_ST, HT, PM}
-import org.ergoplatform.nodeView.state.{DigestState, StateType}
-import org.ergoplatform.nodeView.{WrappedDigestState, WrappedUtxoState}
+import org.ergoplatform.mining.DefaultFakePowScheme
+import org.ergoplatform.mining.emission.CoinsEmission
+import org.ergoplatform.modifiers.ErgoFullBlock
+import org.ergoplatform.nodeView.mempool.ErgoMemPool
+import org.ergoplatform.nodeView.state.{DigestState, ErgoState, StateType, UtxoState}
+import org.ergoplatform.nodeView.wallet.ErgoWallet
+import org.ergoplatform.nodeView.{ErgoNodeViewHolder, ErgoNodeViewRef, WrappedDigestState, WrappedUtxoState}
+import org.ergoplatform.settings.ErgoSettings
 import org.scalacheck.Gen
+import scorex.core.utils.NetworkTimeProvider
+import scorex.core.{idToBytes, idToVersion}
+
+import scala.concurrent.ExecutionContext
 
 class ErgoSanityDigest extends ErgoSanity[DIGEST_ST] {
   override val historyGen: Gen[HT] = generateHistory(verifyTransactions = true, StateType.Digest, PoPoWBootstrap = false, -1)
@@ -19,4 +34,58 @@ class ErgoSanityDigest extends ErgoSanity[DIGEST_ST] {
 
   override def semanticallyInvalidModifier(state: DIGEST_ST): PM = invalidErgoFullBlockGen.sample.get
 
+  override def totallyValidModifier(history: HT, state: DIGEST_ST): PM = {
+    val parentOpt = history.bestHeaderOpt
+    validFullBlock(parentOpt, state.asInstanceOf[WrappedDigestState].wrappedUtxoState).header
+  }
+
+  override def totallyValidModifiers(history: HT, state: DIGEST_ST, count: Int): Seq[PM] = {
+    require(count >= 1)
+    val headerOpt = history.bestHeaderOpt
+    (0 until count).foldLeft((headerOpt, Seq.empty[PM])) { case (acc, _) =>
+      val pm = validFullBlock(headerOpt, state.asInstanceOf[WrappedDigestState].wrappedUtxoState)
+      (Some(pm.header), acc._2 :+ pm)
+    }._2.map(_.asInstanceOf[ErgoFullBlock].header)
+  }
+
+  override def nodeViewHolder(implicit system: ActorSystem): (ActorRef, TestProbe, PM, DIGEST_ST, HT) = {
+    implicit val ec = system.dispatcher
+    val dir: File = createTempDir
+    val defaultSettings: ErgoSettings = ErgoSettings.read(None).copy(directory = dir.getAbsolutePath)
+    val settings = defaultSettings.copy(
+      nodeSettings = defaultSettings.nodeSettings.copy(
+        stateType = StateType.Digest,
+        verifyTransactions = false,
+        PoPoWBootstrap = false
+      )
+    )
+    val ce = new CoinsEmission(settings.chainSettings.monetary)
+
+    val h = historyGen.sample.get
+    val s = stateGen.sample.get
+    val v = h.openSurfaceIds().last
+    s.store.update(ByteArrayWrapper(idToBytes(v)), Seq(), Seq())
+    val ref = system.actorOf(NodeViewHolderForTests.props(h, s, settings, ce))
+    val m = totallyValidModifier(h, s)
+    val eventListener = TestProbe()
+    (ref, eventListener, m, s, h)
+  }
+
+  class NodeViewHolderForTests(h: HT, s: DIGEST_ST, settings: ErgoSettings, ce: CoinsEmission)
+                              (implicit ec: ExecutionContext)
+    extends ErgoNodeViewHolder[DIGEST_ST](settings, new NetworkTimeProvider(settings.scorexSettings.ntp), ce) {
+
+    override protected def genesisState: (HIS, MS, VL, MP) = {
+      @SuppressWarnings(Array("org.wartremover.warts.OptionPartial"))
+      val w = new ErgoWallet
+      (h, s, w, ErgoMemPool.empty)
+    }
+
+    override def restoreState(): Option[(HIS, MS, VL, MP)] = None
+  }
+
+  object NodeViewHolderForTests {
+    def props(h: HT, s: DIGEST_ST, settings: ErgoSettings, ce: CoinsEmission)(implicit ec: ExecutionContext): Props =
+      Props(new NodeViewHolderForTests(h, s, settings, ce))
+  }
 }

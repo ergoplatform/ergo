40697ff3bce967c41d11cd01855dca50acc7851d
Alex Chepurnoy <alex.chepurnoy@iohk.io>
2018-08-01 13:45:59 +0300

    DefaultBoxSelector test stubs

---
+++ src/test/scala/org/ergoplatform/nodeView/wallet/DefaultBoxSelector.scala
@@ -0,0 +1,14 @@
+package org.ergoplatform.nodeView.wallet
+
+import org.ergoplatform.utils.ErgoPropertyTest
+
+class DefaultBoxSelector extends ErgoPropertyTest {
+
+  property("returns None when it is impossible to select coins") {
+
+  }
+
+  property("properly selects coins") {
+
+  }
+}

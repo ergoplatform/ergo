e7c4401cb64f117559487274d3911f15a31eaa13
Aleksei <daron666@yandex.ru>
2018-08-06 15:52:45 +0400

    Add tests to check valid size deserialization

--- src/test/scala/org/ergoplatform/serialization/SerializationTests.scala
+++ src/test/scala/org/ergoplatform/serialization/SerializationTests.scala
@@ -1,10 +1,15 @@
 package org.ergoplatform.serialization
 
+import org.ergoplatform.modifiers.ErgoNodeViewModifier
 import org.ergoplatform.modifiers.history._
-import org.ergoplatform.modifiers.mempool.{ErgoBoxSerializer, ErgoTransaction, ErgoTransactionSerializer, TransactionIdsForHeaderSerializer}
+import org.ergoplatform.modifiers.mempool.{ErgoBoxSerializer, ErgoTransactionSerializer, TransactionIdsForHeaderSerializer}
 import org.ergoplatform.nodeView.history.ErgoSyncInfoSerializer
 import org.ergoplatform.nodeView.state.ErgoStateContextSerializer
 import org.ergoplatform.utils.ErgoPropertyTest
+import org.scalacheck.Gen
+import org.scalatest.Assertion
+import org.scalatest.OptionValues._
+import scorex.core.serialization.Serializer
 
 class SerializationTests extends ErgoPropertyTest with scorex.testkit.SerializationTests {
 
@@ -13,6 +18,9 @@ class SerializationTests extends ErgoPropertyTest with scorex.testkit.Serializat
     forAll(invalidHeaderGen) { b: Header =>
       val recovered = serializer.parseBytes(serializer.bytesWithoutInterlinks(b)).get.copy(interlinks = b.interlinks)
       recovered shouldBe b
+
+      val size = serializer.bytesWithoutInterlinks(b).length
+      recovered.size.value shouldBe size
     }
   }
 
@@ -21,6 +29,7 @@ class SerializationTests extends ErgoPropertyTest with scorex.testkit.Serializat
     forAll(invalidHeaderGen) { b: Header =>
       val recovered = serializer.parseBytes(b.bytes)
       recovered.get shouldBe b
+      recovered.get.size.value shouldBe b.bytes.length
     }
   }
 
@@ -34,6 +43,7 @@ class SerializationTests extends ErgoPropertyTest with scorex.testkit.Serializat
 
   property("ErgoTransactionGen serialization") {
     checkSerializationRoundtrip(invalidErgoTransactionGen, ErgoTransactionSerializer)
+    checkSize(invalidErgoTransactionGen, ErgoTransactionSerializer)
   }
 
   property("ErgoSyncInfo serialization") {
@@ -42,17 +52,27 @@ class SerializationTests extends ErgoPropertyTest with scorex.testkit.Serializat
 
   property("ErgoHeader serialization") {
     checkSerializationRoundtrip(invalidHeaderGen, HeaderSerializer)
+    checkSize(invalidHeaderGen, HeaderSerializer)
   }
 
   property("BlockTransactions serialization") {
     checkSerializationRoundtrip(invalidBlockTransactionsGen, BlockTransactionsSerializer)
+    checkSize(invalidBlockTransactionsGen, BlockTransactionsSerializer)
   }
 
   property("ADProofs serialization") {
     checkSerializationRoundtrip(randomADProofsGen, ADProofSerializer)
+    checkSize(randomADProofsGen, ADProofSerializer)
   }
 
   property("TransactionIdsForHeader serialization") {
     checkSerializationRoundtrip(transactionIdsForHeaderGen, TransactionIdsForHeaderSerializer)
+  }
+
+  def checkSize[A <: ErgoNodeViewModifier](generator: Gen[A], serializer: Serializer[A]): Assertion = {
+    forAll(generator) { b: A =>
+      val size = serializer.parseBytes(serializer.toBytes(b)).get.size.get
+      serializer.toBytes(b).length shouldEqual size
+    }
   }
 }

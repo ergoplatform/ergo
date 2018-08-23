8784399c5ac0e4d70a6f72c639d350a126742386
Alex Chepurnoy <alex.chepurnoy@iohk.io>
2018-08-23 12:35:10 +0300

    scalastyle fixes in ErgoStateChanges

--- src/main/scala/org/ergoplatform/modifiers/state/ErgoStateChanges.scala
+++ src/main/scala/org/ergoplatform/modifiers/state/ErgoStateChanges.scala
@@ -1,23 +1,32 @@
 package org.ergoplatform.modifiers.state
 
+import com.google.common.primitives.Ints
 import org.ergoplatform.ErgoBox
 import org.ergoplatform.settings.Algos
 import scorex.crypto.authds.ADKey
 
 
-abstract class StateChangeOperation {
-  override def equals(that: scala.Any): Boolean = (this, that) match {
-    case (Removal(i1), Removal(i2)) => java.util.Arrays.equals(i1, i2)
-    case (Insertion(b1), Insertion(b2)) => b1 == b2
-    case _ => false
-  }
-}
+abstract class StateChangeOperation
 
 case class Removal(boxId: ADKey) extends StateChangeOperation {
+  override def equals(that: Any): Boolean = (this, that) match {
+    case (Removal(i1), Removal(i2)) => java.util.Arrays.equals(i1, i2)
+    case _ => false
+  }
+
+  override def hashCode(): Int = Ints.fromByteArray(boxId.take(4))
+
   override def toString: String = s"Removal(id: ${Algos.encode(boxId)})"
 }
 
 case class Insertion(box: ErgoBox) extends StateChangeOperation {
+  override def equals(that: Any): Boolean = (this, that) match {
+    case (Insertion(b1), Insertion(b2)) => b1 == b2
+    case _ => false
+  }
+
+  override def hashCode(): Int = Ints.fromByteArray(box.id.take(4))
+
   override def toString: String = s"Insertion(id: ${Algos.encode(box.id)})"
 }
 

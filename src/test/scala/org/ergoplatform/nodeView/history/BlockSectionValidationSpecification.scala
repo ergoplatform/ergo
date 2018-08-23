1abd24d50437842e6538b5ad465fcdc76546facd
catena <catena@protonmail.com>
2018-08-23 15:24:21 +0300

    BlockSectionValidationSpecification

--- src/test/scala/org/ergoplatform/nodeView/history/BlockSectionValidationSpecification.scala
+++ src/test/scala/org/ergoplatform/nodeView/history/BlockSectionValidationSpecification.scala
@@ -1,64 +1,95 @@
 package org.ergoplatform.nodeView.history
 
-import org.ergoplatform.modifiers.history.Extension
+import org.ergoplatform.modifiers.BlockSection
+import org.ergoplatform.modifiers.history.{ADProofs, BlockTransactions, Extension, Header}
 import org.ergoplatform.nodeView.state.StateType
 import org.ergoplatform.utils.HistorySpecification
+import scorex.core.ModifierId
 
 class BlockSectionValidationSpecification extends HistorySpecification {
 
-  private def genHistory() =
-    generateHistory(verifyTransactions = true, StateType.Utxo, PoPoWBootstrap = false, BlocksToKeep)
+  property("BlockTransactions validation") {
+    var (history, block) = init()
+    commonChecks(history, block.blockTransactions)
+  }
 
-  property("extension validation") {
-    var history = genHistory()
-    val chain = genChain(2, history)
-    history = applyBlock(history, chain.head)
+  property("ADProofs validation") {
+    var (history, block) = init()
+    commonChecks(history, block.adProofs.get)
+  }
 
-    val block = chain.last
+  property("Extension validation") {
+    var (history, block) = init()
+
+    val header = block.header
     val extension = block.extension
     val m = extension.mandatoryFields
     val o = extension.optionalFields
 
-    def appendHeader(e: Extension): Extension = {
-      val newHeader = block.header.copy(extensionRoot = e.digest)
-      history = history.append(newHeader).get._1
-      e.copy(headerId = newHeader.id)
-    }
-
-    // validation of header id
-    history = history.append(block.header).get._1
-    history.applicableTry(extension) shouldBe 'success
-    history.applicableTry(extension.copy(headerId = extension.id)) shouldBe 'failure
+    //common checks
+    commonChecks(history, extension)
 
     // validation of mandatory fields key size
     val imvKey = kvGen(Extension.MandatoryFieldKeySize - 1, Extension.MandatoryFieldValueSize).sample.get
-    val e2In = extension.copy(mandatoryFields = imvKey +: m)
-    val e2 = appendHeader(e2In)
-    history.applicableTry(e2) shouldBe 'failure
+    applicableCheck(extension.copy(mandatoryFields = imvKey +: m), header, history)
     // validation of mandatory fields value size
     val imvValue = kvGen(Extension.MandatoryFieldKeySize, Extension.MandatoryFieldValueSize + 1).sample.get
-    val e3In = extension.copy(mandatoryFields = imvValue +: m)
-    val e3 = appendHeader(e3In)
-    history.applicableTry(e3) shouldBe 'failure
+    applicableCheck(extension.copy(mandatoryFields = imvValue +: m), header, history)
     // validation of optional fields key size
     val omvKey = kvGen(Extension.OptionalFieldKeySize - 1, Extension.OptionalFieldValueSize).sample.get
-    val e4In = extension.copy(optionalFields = omvKey +: o)
-    val e4 = appendHeader(e4In)
-    history.applicableTry(e4) shouldBe 'failure
+    applicableCheck(extension.copy(optionalFields = omvKey +: o), header, history)
     // validation of optional fields value size
     val omvValue = kvGen(Extension.OptionalFieldKeySize, Extension.OptionalFieldValueSize + 1).sample.get
-    val e5In = extension.copy(mandatoryFields = omvValue +: o)
-    val e5 = appendHeader(e5In)
-    history.applicableTry(e5) shouldBe 'failure
+    applicableCheck(extension.copy(mandatoryFields = omvValue +: o), header, history)
     // validation of optional fields number
     val moreOMV = (0 until Extension.MaxOptionalFields + 1) map (_ => kvGen(Extension.MandatoryFieldKeySize, Extension.MandatoryFieldValueSize).sample.get)
-    val e6In = extension.copy(mandatoryFields = moreOMV)
-    val e6 = appendHeader(e6In)
-    history.applicableTry(e6) shouldBe 'success
-    val e7In = extension.copy(mandatoryFields = moreOMV ++ o)
-    val e7 = appendHeader(e7In)
-    history.applicableTry(e7) shouldBe 'failure
+    applicableCheck(extension.copy(mandatoryFields = moreOMV), header, history, correct = true)
+    applicableCheck(extension.copy(mandatoryFields = moreOMV ++ o), header, history)
   }
 
 
+  private def init() = {
+    var history = genHistory()
+    val chain = genChain(2, history)
+    history = applyBlock(history, chain.head)
+    history = history.append(chain.last.header).get._1
+    (history, chain.last)
+  }
+
+  private def commonChecks(history: ErgoHistory, section: BlockSection) = {
+    history.applicableTry(section) shouldBe 'success
+    history.applicableTry(withUpdatedHeaderId(section, section.id)) shouldBe 'failure
+  }
+
+  private def genHistory() =
+    generateHistory(verifyTransactions = true, StateType.Utxo, PoPoWBootstrap = false, BlocksToKeep)
+
+  private def withUpdatedHeaderId[T <: BlockSection](section: T, newId: ModifierId): T = section match {
+    case s: Extension => s.copy(headerId = newId).asInstanceOf[T]
+    case s: BlockTransactions => s.copy(headerId = newId).asInstanceOf[T]
+    case s: ADProofs => s.copy(headerId = newId).asInstanceOf[T]
+  }
+
+  private def withUpdatedSection(section: BlockSection, header: Header): Header = section match {
+    case _: Extension => header.copy(extensionRoot = section.digest)
+    case _: BlockTransactions => header.copy(transactionsRoot = section.digest)
+    case _: ADProofs => header.copy(ADProofsRoot = section.digest)
+  }
+
+  /**
+    * Incorrect section for `header` should not be applicable
+    */
+  private def applicableCheck(section: BlockSection,
+                              header: Header,
+                              history: ErgoHistory,
+                              correct: Boolean = false): Unit = {
+    val newHeader = withUpdatedSection(section, header)
+    val newSection = withUpdatedHeaderId(section, newHeader.id)
+    history.append(newHeader).get._1
+    if (correct) {
+      history.applicableTry(newSection) shouldBe 'success
+    } else {
+      history.applicableTry(newSection) shouldBe 'failure
+    }
+  }
 }

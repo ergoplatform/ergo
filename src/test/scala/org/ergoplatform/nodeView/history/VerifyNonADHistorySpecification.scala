d742188d7194479ce07c0020096e515db80ac132
Mike Aksarin <mike.aksarin@gmail.com>
2018-09-05 16:04:55 +0300

    more tests fixed

--- src/test/scala/org/ergoplatform/nodeView/history/VerifyNonADHistorySpecification.scala
+++ src/test/scala/org/ergoplatform/nodeView/history/VerifyNonADHistorySpecification.scala
@@ -16,7 +16,6 @@ class VerifyNonADHistorySpecification extends HistorySpecification {
     val chain = genChain(6, history)
     if (history.pruningProcessor.minimalFullBlockHeight == Int.MaxValue) {
       history.pruningProcessor.updateBestFullBlock(chain.last.header)
-      history.isHeadersChainSyncedVar = true
     }
     history = applyHeaderChain(history, HeaderChain(chain.map(_.header)))
     chain.foreach(fb => history.append(fb.extension).get)
@@ -46,7 +45,6 @@ class VerifyNonADHistorySpecification extends HistorySpecification {
 
     if (history.pruningProcessor.minimalFullBlockHeight == Int.MaxValue) {
       history.pruningProcessor.updateBestFullBlock(chain.last.header)
-      history.isHeadersChainSyncedVar = true
     }
 
     // Until UTXO snapshot synchronization is implemented, we should always start to apply full blocks from genesis

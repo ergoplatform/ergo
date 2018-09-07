6c004df916048a8aff98d80d14df52f118bcfa80
Alex Chepurnoy <alex.chepurnoy@iohk.io>
2018-09-07 14:12:48 +0300

    unused imports, formatting

--- src/main/scala/org/ergoplatform/utils/AssetUtils.scala
+++ src/main/scala/org/ergoplatform/utils/AssetUtils.scala
@@ -6,16 +6,14 @@ import scala.collection.mutable
 
 object AssetUtils {
   @inline
-  def mergeAssets(into: mutable.Map[ModifierId, Long],
-                  from: Map[ModifierId, Long]): Unit = {
+  def mergeAssets(into: mutable.Map[ModifierId, Long], from: Map[ModifierId, Long]): Unit = {
     from.foreach { case (id, amount) =>
       into.put(id, into.getOrElse(id, 0L) + amount)
     }
   }
 
   @inline
-  def subtractAssets(from: mutable.Map[ModifierId, Long],
-                     subtractor: Map[ModifierId, Long]): Unit = {
+  def subtractAssets(from: mutable.Map[ModifierId, Long], subtractor: Map[ModifierId, Long]): Unit = {
     subtractor.foreach { case (id, subtractAmt) =>
       val fromAmt = from(id)
       if (fromAmt == subtractAmt) {

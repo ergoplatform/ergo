cdffcb7b3a05474a2ae9609275df2e815eacc62e
catena <catena@protonmail.com>
2018-09-19 15:55:30 +0300

    Update number of digits in one coin. close #451

--- src/main/scala/org/ergoplatform/settings/Constants.scala
+++ src/main/scala/org/ergoplatform/settings/Constants.scala
@@ -10,11 +10,12 @@ import scorex.core.{ModifierTypeId, NodeViewModifier}
 
 
 object Constants {
-  val hashLength: Int = 32
-  val MaxTarget: BigInt = BigInt(1, Array.fill(hashLength)((-1).toByte))
+  val HashLength: Int = 32
+  val CoinsInOneErgo: Long = 1000000000
+  val MaxTarget: BigInt = BigInt(1, Array.fill(HashLength)((-1).toByte))
   val InitialDifficulty: Difficulty = BigInt(1)
   val InitialNBits: Long = RequiredDifficulty.encodeCompactBits(InitialDifficulty)
-  val ModifierIdSize: Int = hashLength
+  val ModifierIdSize: Int = HashLength
   val MaxBlockCost: Long = 1000000    //todo: move to config
 
   val modifierSerializers: Map[ModifierTypeId, Serializer[_ <: NodeViewModifier]] =

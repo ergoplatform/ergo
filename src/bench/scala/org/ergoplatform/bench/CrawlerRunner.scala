63b3a5d6c34d9f87ddc95db3506c674fc5b824a6
Mike Aksarin <mike.aksarin@gmail.com>
2018-09-25 18:33:16 +0300

    updated bench to newer scorex

--- src/bench/scala/org/ergoplatform/bench/CrawlerRunner.scala
+++ src/bench/scala/org/ergoplatform/bench/CrawlerRunner.scala
@@ -57,7 +57,7 @@ class CrawlerRunner(args: Array[String]) extends Application {
 
   override val apiRoutes: Seq[ApiRoute] = Seq(
     UtilsApiRoute(settings.restApi),
-    PeersApiRoute(peerManagerRef, networkControllerRef, settings.restApi),
+    PeersApiRoute(peerManagerRef, networkControllerRef, timeProvider, settings.restApi),
     InfoRoute(statsCollectorRef, settings.restApi, timeProvider),
     BlocksApiRoute(readersHolderRef, minerRef, ergoSettings),
     TransactionsApiRoute(readersHolderRef, nodeViewHolderRef, settings.restApi))

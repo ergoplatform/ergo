850c75508e728325e6db3f4c9db33152019e900c
Alex Chepurnoy <alex.chepurnoy@iohk.io>
2018-10-15 18:50:10 +0300

    ErgoContext externalized

--- src/main/scala/org/ergoplatform/nodeView/ErgoInterpreter.scala
+++ src/main/scala/org/ergoplatform/nodeView/ErgoInterpreter.scala
@@ -1,36 +1,13 @@
 package org.ergoplatform.nodeView
 
-import org.ergoplatform.ErgoLikeContext.{Height, Metadata}
+import org.ergoplatform.ErgoLikeContext.Height
 import org.ergoplatform._
-import org.ergoplatform.nodeView.state.ErgoStateContext
 import org.ergoplatform.settings.{Constants, Parameters}
-import scorex.crypto.authds.ADDigest
-import sigmastate.{AvlTreeData, SBoolean}
+import sigmastate.SBoolean
 import sigmastate.Values.Value
-import sigmastate.interpreter.ContextExtension
 import sigmastate.interpreter.Interpreter.VerificationResult
-
 import scala.util.Try
 
-class ErgoContext(val stateContext: ErgoStateContext,
-                  override val boxesToSpend: IndexedSeq[ErgoBox],
-                  override val spendingTransaction: ErgoLikeTransactionTemplate[_ <: UnsignedInput],
-                  override val self: ErgoBox,
-                  override val metadata: Metadata,
-                  override val extension: ContextExtension = ContextExtension(Map()))
-  extends ErgoLikeContext(stateContext.currentHeight, ErgoContext.stateTreeFromDigest(stateContext.stateDigest),
-                          boxesToSpend, spendingTransaction, self, metadata, extension) {
-
-  override def withExtension(newExtension: ContextExtension): ErgoContext =
-    new ErgoContext(stateContext, boxesToSpend, spendingTransaction, self, metadata, newExtension)
-
-  override def withTransaction(newSpendingTransaction: ErgoLikeTransactionTemplate[_ <: UnsignedInput]): ErgoContext =
-    new ErgoContext(stateContext, boxesToSpend, newSpendingTransaction, self, metadata, extension)
-}
-
-object ErgoContext {
-  def stateTreeFromDigest(digest: ADDigest): AvlTreeData = AvlTreeData(digest, Constants.HashLength)
-}
 
 class ErgoInterpreter(override val maxCost: Long = Parameters.MaxBlockCost)
   extends ErgoLikeInterpreter(maxCost) {

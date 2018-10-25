93480faceadc7a078c3bc9cf398f585ce8f2b994
Alex Chepurnoy <alex.chepurnoy@iohk.io>
2018-10-25 18:38:18 +0300

    fee simulator update

--- src/main/scala/org/ergoplatform/settings/Constants.scala
+++ src/main/scala/org/ergoplatform/settings/Constants.scala
@@ -24,6 +24,14 @@ object Constants {
 
   val BlocksPerHour = 30
 
+  val BlocksPerDay = BlocksPerHour * 24
+
+  val BlocksPerWeek = BlocksPerDay * 7
+
+  val BlocksPerMonth = BlocksPerDay * 30
+
+  val BlocksPerYear = BlocksPerDay * 365
+
   //For how many blocks a box could be put into the state with no paying.
   //4 years
   val StoragePeriod: Int = 4 * 365 * 24 * BlocksPerHour
--- src/test/scala/org/ergoplatform/tools/FeeSimulator.scala
+++ src/test/scala/org/ergoplatform/tools/FeeSimulator.scala
@@ -1,5 +1,12 @@
 package org.ergoplatform.tools
 
+import org.ergoplatform.{ErgoBoxCandidate, Input}
+import org.ergoplatform.modifiers.mempool.ErgoTransaction
+import scapi.sigma.DLogProtocol.DLogProverInput
+import scorex.crypto.authds.ADKey
+import scorex.crypto.hash.Digest32
+import sigmastate.interpreter.{ContextExtension, ProverResult}
+
 object FeeSimulator extends App {
   import org.ergoplatform.settings.Parameters._
   import org.ergoplatform.settings.Constants._
@@ -17,4 +24,25 @@ object FeeSimulator extends App {
 
   val minStdDust = MinValuePerByte * StdSize
   println(s"Min dust value of standard-size box: $minStdDust")
+
+  val k1 = DLogProverInput.random().publicImage
+  val k2 = DLogProverInput.random().publicImage
+
+  val input = Input(ADKey @@ Array.fill(32)(0:Byte), ProverResult(Array.fill(65)(0: Byte), ContextExtension(Map())))
+
+  val box1 = new ErgoBoxCandidate(1L, k1, Seq((Digest32 @@ Array.fill(32)(0:Byte)) -> 0L))
+  val box2 = new ErgoBoxCandidate(100L, k2)
+
+  val simpleTx = ErgoTransaction(IndexedSeq(input), IndexedSeq(box1, box2), None)
+
+  val outputsSize = simpleTx.outputs.map(_.bytes.length).sum
+
+  val minTxFee = K * outputsSize / CoinsInOneErgo.toDouble
+  println(s"Min tx fee: $minTxFee")
+
+  println("Four years storage fee: ")
+  println(s"Everyday relocation: ${minTxFee * StoragePeriod / BlocksPerDay}")
+  println(s"Weekly relocation: ${minTxFee * StoragePeriod / BlocksPerWeek}")
+  println(s"Monthly relocation: ${minTxFee * StoragePeriod / BlocksPerMonth}")
+  println(s"Yearly relocation: ${minTxFee * StoragePeriod / BlocksPerYear}")
 }

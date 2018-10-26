5e7c6755a59be83f70b6fd1902faf7daea10c9c5
oskin1 <ilya.arcadich@gmail.com>
2018-10-26 16:40:52 +0300

    Transaction generator specification improvements.

--- src/test/scala/org/ergoplatform/local/TransactionGeneratorSpec.scala
+++ src/test/scala/org/ergoplatform/local/TransactionGeneratorSpec.scala
@@ -2,26 +2,25 @@ package org.ergoplatform.local
 
 import akka.actor.{ActorRef, ActorSystem}
 import akka.pattern.ask
-import akka.testkit.{TestKit, TestProbe}
+import akka.testkit.TestKit
+import org.ergoplatform.P2PKAddress
 import org.ergoplatform.local.ErgoMiner.StartMining
 import org.ergoplatform.local.TransactionGenerator.StartGeneration
+import org.ergoplatform.modifiers.mempool.ErgoTransaction
 import org.ergoplatform.nodeView.ErgoReadersHolder.{GetReaders, Readers}
 import org.ergoplatform.nodeView.state.StateType
 import org.ergoplatform.nodeView.wallet.ErgoWalletReader
 import org.ergoplatform.nodeView.{ErgoNodeViewRef, ErgoReadersHolderRef}
 import org.ergoplatform.settings.ErgoSettings
 import org.ergoplatform.utils.{ErgoTestHelpers, WalletTestOps}
-import org.ergoplatform.{ErgoAddressEncoder, P2PKAddress}
 import org.scalatest.FlatSpec
 import scorex.core.network.NodeViewSynchronizer.ReceivableMessages.SuccessfulTransaction
 
+import scala.collection.immutable
 import scala.concurrent.duration._
 import scala.concurrent.{Await, Future}
 
 class TransactionGeneratorSpec extends FlatSpec with ErgoTestHelpers with WalletTestOps {
-
-  private implicit val ergoAddressEncoder: ErgoAddressEncoder =
-    new ErgoAddressEncoder(settings.chainSettings.addressPrefix)
 
   type MsgType = SuccessfulTransaction[_]
   val newTransaction: Class[MsgType] = classOf[MsgType]
@@ -45,12 +44,17 @@ class TransactionGeneratorSpec extends FlatSpec with ErgoTestHelpers with Wallet
 
   def await[A](f: Future[A]): A = Await.result[A](f, defaultAwaitDuration)
 
+  def containsAssetIssuingBox(tx: ErgoTransaction): Boolean = {
+    val firstInputId = tx.inputs.head.boxId
+    val assetIds = tx.outAssetsOpt.get.map(_._1.data).toSeq
+    assetIds.exists(x => java.util.Arrays.equals(x, firstInputId))
+  }
+
   it should "Generate valid transactions of all types" in new TestKit(ActorSystem()) {
 
     val ergoSettings: ErgoSettings = defaultSettings.copy(directory = createTempDir.getAbsolutePath)
 
-    val testProbe = new TestProbe(system)
-    system.eventStream.subscribe(testProbe.ref, newTransaction)
+    system.eventStream.subscribe(testActor, newTransaction)
 
     val nodeViewHolderRef: ActorRef = ErgoNodeViewRef(ergoSettings, timeProvider)
     val readersHolderRef: ActorRef = ErgoReadersHolderRef(nodeViewHolderRef)
@@ -71,6 +75,17 @@ class TransactionGeneratorSpec extends FlatSpec with ErgoTestHelpers with Wallet
     val txGenRef = TransactionGeneratorRef(nodeViewHolderRef, ergoSettings)
     txGenRef ! StartGeneration
 
-    testProbe.expectMsgClass(paymentTransactionAwaitDuration, newTransaction)
+    val paymentTxs: immutable.Seq[ErgoTransaction] = receiveWhile(paymentTransactionAwaitDuration) {
+      case SuccessfulTransaction(tx: ErgoTransaction) => tx
+    }
+
+    val assetTxs: immutable.Seq[ErgoTransaction] = receiveWhile(assetTransferTransactionAwaitDuration) {
+      case SuccessfulTransaction(tx: ErgoTransaction) if tx.outAssetsOpt.get.nonEmpty => tx
+    }
+
+    val assetIssueTxs: immutable.Seq[ErgoTransaction] = receiveWhile(assetIssueTransactionAwaitDuration) {
+      case SuccessfulTransaction(tx: ErgoTransaction) if containsAssetIssuingBox(tx) => tx
+    }
   }
+
 }

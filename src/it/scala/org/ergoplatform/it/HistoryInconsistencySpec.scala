3fca0781c65100583c17b6ddb34326d1b8f8e2fe
oskin1 <ilya.arcadich@gmail.com>
2018-10-29 13:49:50 +0300

    History inconsistency spec.

--- src/it/scala/org/ergoplatform/it/HistoryInconsistencySpec.scala
+++ src/it/scala/org/ergoplatform/it/HistoryInconsistencySpec.scala
@@ -3,40 +3,41 @@ package org.ergoplatform.it
 import java.io.File
 
 import com.typesafe.config.Config
+import org.ergoplatform.it.api.NodeApi.HistoryInfo
 import org.ergoplatform.it.container.{IntegrationSuite, Node}
-import org.scalatest.{FreeSpec, OptionValues}
+import org.scalatest.FreeSpec
 
 import scala.concurrent.Await
 import scala.concurrent.duration._
 
-class HistoryConsistencySpec
-  extends FreeSpec
-    with IntegrationSuite
-    with OptionValues {
+class HistoryInconsistencySpec extends FreeSpec with IntegrationSuite {
 
-  val shutdownAtHeight: Int = 5
+  val shutdownAtHeight: Int = 40
 
-  val localVolume = s"$localDataDir/history-consistency-spec/data"
+  val localVolume = "/tmp/ergo2/history-inconsistency-spec/data"
   val remoteVolume = "/app"
 
   val dir = new File(localVolume)
+
   dir.mkdirs()
 
-  val nodeConfig: Config = specialDataDirConfig(remoteVolume)
+  val offlineGeneratingPeer: Config = specialDataDirConfig(remoteVolume)
     .withFallback(shortDelayConfig)
     .withFallback(offlineGeneratingPeerConfig)
     .withFallback(nodeSeedConfigs.head)
 
-  val node: Node = docker.startNode(nodeConfig, specialVolumeOpt = Some((localVolume, remoteVolume))).get
+  val node: Node = docker.startNode(offlineGeneratingPeer, specialVolumeOpt = Some((localVolume, remoteVolume))).get
 
-  "History consistency after unexpected shutdown" in {
+  "History inconsistency after unexpected shutdown" in {
 
     val result = node.waitForHeight(shutdownAtHeight, 100.millis)
-      .flatMap(_ => node.waitForInconsistentHistory)
+      .flatMap { _ =>
+        node.waitFor[HistoryInfo](_.historyInfo, hi => hi.bestHeaderHeight > hi.bestBlockHeight, 50.millis)
+      }
       .flatMap { _ =>
         docker.forceStopNode(node.containerId)
         val restartedNode = docker
-          .startNode(nodeConfig, specialVolumeOpt = Some((localVolume, remoteVolume))).get
+          .startNode(offlineGeneratingPeer, specialVolumeOpt = Some((localVolume, remoteVolume))).get
         restartedNode.historyInfo
           .flatMap(hi => restartedNode.waitForHeight(hi.bestBlockHeight + 1, 100.millis))
           .flatMap(_ => restartedNode.historyInfo)

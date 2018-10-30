f77ca12e793d0c1114d2854fac94e44789e57c8d
oskin1 <ilya.arcadich@gmail.com>
2018-10-30 13:43:18 +0300

    Fork resolution spec improvements.

--- src/it/scala/org/ergoplatform/it/ForkResolutionSpec.scala
+++ src/it/scala/org/ergoplatform/it/ForkResolutionSpec.scala
@@ -2,22 +2,25 @@ package org.ergoplatform.it
 
 import java.io.File
 
+import cats.implicits._
 import com.typesafe.config.Config
+import org.ergoplatform.it.container.Docker.{ExtraConfig, noExtraConfig}
 import org.ergoplatform.it.container.{IntegrationSuite, Node}
 import org.scalatest.FreeSpec
 
-import scala.concurrent.{Await, Future}
+import scala.concurrent.{Await, Future, blocking}
 import scala.concurrent.duration._
+import scala.util.Try
 
 class ForkResolutionSpec extends FreeSpec with IntegrationSuite {
 
-  val initialCommonChainLength: Int = 4
+  val nodesQty: Int = 4
+
+  val initialCommonChainLength: Int = 5
   val forkLength: Int = 5
-  val syncLength: Int = 5
+  val syncLength: Int = 10
 
-  def localVolume(n: Int): String = s"$localDataDir/fork-resolution-spec/node$n/data"
-
-  val localVolumes: Seq[String] = (0 to 3).map(localVolume)
+  val localVolumes: Seq[String] = (1 to nodesQty).map(localVolume)
   val remoteVolume = "/app"
 
   val volumesMapping: Seq[(String, String)] = localVolumes.map(_ -> remoteVolume)
@@ -26,33 +29,48 @@ class ForkResolutionSpec extends FreeSpec with IntegrationSuite {
   dirs.foreach(_.mkdirs())
 
   val minerConfig: Config = nodeSeedConfigs.head
-  val onlineMiningNodesConfig: List[Config] = nodeSeedConfigs.slice(1, 4).map(_.withFallback(onlineGeneratingPeerConfig))
-  val offlineMiningNodesConfig: List[Config] = nodeSeedConfigs.slice(1, 4)
+  val onlineMiningNodesConfig: List[Config] = nodeSeedConfigs.slice(1, nodesQty)
+    .map(_.withFallback(onlineGeneratingPeerConfig))
+  val offlineMiningNodesConfig: List[Config] = nodeSeedConfigs.slice(1, nodesQty)
+
+  def localVolume(n: Int): String = s"$localDataDir/fork-resolution-spec/node$n/data"
+
+  def startNodesWithBinds(nodeConfigs: List[Config],
+                          configEnrich: ExtraConfig = noExtraConfig): Try[List[Node]] = {
+    log.trace(s"Starting ${nodeConfigs.size} containers")
+    val nodes: Try[List[Node]] = nodeConfigs
+      .map(_.withFallback(specialDataDirConfig(remoteVolume)))
+      .zip(volumesMapping)
+      .map { case (cfg, vol) => docker.startNode(cfg, configEnrich, Some(vol)) }
+      .sequence
+    blocking(Thread.sleep(nodeConfigs.size * 3000))
+    nodes
+  }
 
   "Fork resolution after isolated mining" in {
 
-    val nodes: List[Node] = docker.startNodes(minerConfig +: onlineMiningNodesConfig, specialVolumes = volumesMapping).get
+    val nodes: List[Node] = startNodesWithBinds(minerConfig +: onlineMiningNodesConfig).get
 
     val result = for {
       b <- Future.traverse(nodes)(_.height).map(_.max)
       _ <- Future.traverse(nodes)(_.waitForHeight(b + initialCommonChainLength))
       isolatedNodes <- {
         nodes.foreach(node => docker.stopNode(node.containerId))
-        Future.successful(docker.startNodes(minerConfig +: offlineMiningNodesConfig, isolatedPeersConfig, volumesMapping).get)
+        Future.successful(startNodesWithBinds(minerConfig +: offlineMiningNodesConfig, isolatedPeersConfig).get)
       }
       _ <- Future.traverse(isolatedNodes)(_.waitForHeight(b + initialCommonChainLength + forkLength))
       regularNodes <- {
         isolatedNodes.foreach(node => docker.stopNode(node.containerId))
-        Future.successful(docker.startNodes(minerConfig +: offlineMiningNodesConfig, specialVolumes = volumesMapping).get)
+        Future.successful(startNodesWithBinds(minerConfig +: offlineMiningNodesConfig).get)
       }
       _ <- Future.traverse(regularNodes)(_.waitForHeight(b + initialCommonChainLength + forkLength + syncLength))
       headers <- Future.traverse(regularNodes)(_.headerIdsByHeight(b + initialCommonChainLength + forkLength))
     } yield {
       log.debug(s"Headers at height $b: ${headers.mkString(",")}")
-      val headerIdsAtSameHeight = headers.flatten
+      val headerIdsAtSameHeight = headers.map(_.headOption.value)
       val sample = headerIdsAtSameHeight.head
       headerIdsAtSameHeight should contain only sample
     }
-    Await.result(result, 4.minutes)
+    Await.result(result, 10.minutes)
   }
 }

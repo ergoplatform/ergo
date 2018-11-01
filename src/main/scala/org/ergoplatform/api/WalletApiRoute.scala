9b6b1e6e2ffb2492729bff54a4e5ae14321274a3
oskin1 <ilya.arcadich@gmail.com>
2018-11-01 14:21:33 +0300

    New address routes.

--- src/main/scala/org/ergoplatform/api/WalletApiRoute.scala
+++ src/main/scala/org/ergoplatform/api/WalletApiRoute.scala
@@ -9,11 +9,14 @@ import org.ergoplatform.nodeView.ErgoReadersHolder.{GetReaders, Readers}
 import org.ergoplatform.nodeView.wallet._
 import org.ergoplatform.nodeView.wallet.requests._
 import org.ergoplatform.settings.{Constants, ErgoSettings}
-import org.ergoplatform.{ErgoAddress, ErgoAddressEncoder, Pay2SAddress}
+import org.ergoplatform.{ErgoAddress, ErgoAddressEncoder, Pay2SAddress, Pay2SHAddress}
 import scorex.core.NodeViewHolder.ReceivableMessages.LocallyGeneratedTransaction
 import scorex.core.api.http.ApiError.BadRequest
 import scorex.core.api.http.ApiResponse
 import scorex.core.settings.RESTApiSettings
+import sigmastate.SBoolean
+import sigmastate.Values.Value
+import sigmastate.lang.SigmaCompiler
 
 import scala.concurrent.Future
 import scala.util.{Failure, Success, Try}
@@ -24,27 +27,46 @@ case class WalletApiRoute(readersHolder: ActorRef, nodeViewActorRef: ActorRef, e
   implicit val paymentRequestDecoder: PaymentRequestDecoder = new PaymentRequestDecoder(ergoSettings)
   implicit val assetIssueRequestDecoder: AssetIssueRequestDecoder = new AssetIssueRequestDecoder(ergoSettings)
   implicit val requestsHolderDecoder: RequestsHolderDecoder = new RequestsHolderDecoder(ergoSettings)
-  implicit val ergoAddressEncoder: ErgoAddressEncoder = ErgoAddressEncoder(ergoSettings.chainSettings.addressPrefix)
-  implicit val addressEncoder: Encoder[ErgoAddress] = paymentRequestDecoder.addressEncoders.encoder
+  implicit val addressEncoder: ErgoAddressEncoder = ErgoAddressEncoder(ergoSettings.chainSettings.addressPrefix)
+  implicit val addressJsonEncoder: Encoder[ErgoAddress] = paymentRequestDecoder.addressEncoders.encoder
 
   val settings: RESTApiSettings = ergoSettings.scorexSettings.restApi
 
   override val route: Route = (pathPrefix("wallet") & withCors & withAuth) {
     balancesR ~
-      unconfirmedBalanceR ~
       addressesR ~
-      generateTransactionR ~
-      generatePaymentTransactionR ~
-      generateAssetIssueTransactionR ~
+      p2sAddressR ~
+      p2shAddressR ~
       sendTransactionR ~
+      unconfirmedBalanceR ~
+      generateTransactionR ~
       sendPaymentTransactionR ~
       sendAssetIssueTransactionR
+      generatePaymentTransactionR ~
+      generateAssetIssueTransactionR
   }
 
   private val fee: Directive1[Option[Long]] = entity(as[Json]).flatMap { p =>
     Try(p.hcursor.downField("fee").as[Long]) match {
       case Success(Right(value)) => provide(Some(value))
       case _ => provide(None)
+    }
+  }
+
+  private val source: Directive1[String] = entity(as[Json]).flatMap { p =>
+    p.hcursor.downField("source").as[String]
+      .fold(_ => reject, s => provide(s))
+  }
+
+  private def adderessResponse(address: ErgoAddress): Json = Json.obj("address" -> addressJsonEncoder(address))
+
+  private def compileSource(source: String): Try[Value[SBoolean.type]] = {
+    val compiler = new SigmaCompiler
+    Try(compiler.compile(Map.empty, source)).flatMap {
+      case script: Value[SBoolean.type@unchecked] if script.tpe.isInstanceOf[SBoolean.type] =>
+        Success(script)
+      case other =>
+        Failure(new Exception(s"Source compilation result is of type ${other.tpe}, but `SBoolean` expected"))
     }
   }
 
@@ -107,6 +129,20 @@ case class WalletApiRoute(readersHolder: ActorRef, nodeViewActorRef: ActorRef, e
 
   def unconfirmedBalanceR: Route = (path("balances" / "with_unconfirmed") & get) {
     withWallet(_.balancesWithUnconfirmed())
+  }
+
+  def p2sAddressR: Route = (path("p2s_address") & post & source) { source =>
+    compileSource(source).map(Pay2SAddress.apply).fold(
+      e => BadRequest(e.getMessage),
+      address => ApiResponse(adderessResponse(address))
+    )
+  }
+
+  def p2shAddressR: Route = (path("p2sh_address") & post & source) { source =>
+    compileSource(source).map(Pay2SHAddress.apply).fold(
+      e => BadRequest(e.getMessage),
+      address => ApiResponse(adderessResponse(address))
+    )
   }
 
   def addressesR: Route = (path("addresses") & get) {

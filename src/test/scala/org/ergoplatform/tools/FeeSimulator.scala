8b4cf7d8b5748b53b69364d82e788e698cfea0fe
Alex Chepurnoy <alex.chepurnoy@iohk.io>
2018-11-01 19:02:57 +0300

    better println in FeeSimulator

--- src/test/scala/org/ergoplatform/tools/FeeSimulator.scala
+++ src/test/scala/org/ergoplatform/tools/FeeSimulator.scala
@@ -12,15 +12,15 @@ object FeeSimulator extends App {
   import org.ergoplatform.settings.Constants._
 
   //standard output size is about 80 bytes
-  val StdSize = 80
+  private val StdSize = 80
 
-  println("K: " + K )
+  println(s"K: $K")
 
   lazy val perOutputFee = StdSize * K  / CoinsInOneErgo.toDouble
-  println("Storage fee for ordinary output: " + perOutputFee)
+  println(s"Storage fee for ordinary output: $perOutputFee")
 
   val bitcoinUtxos = 60000000
-  println("Reward per block: " + perOutputFee * (bitcoinUtxos / StoragePeriod))
+  println(s"Reward per block: ${perOutputFee * (bitcoinUtxos / StoragePeriod)}")
 
   val minStdDust = MinValuePerByte * StdSize
   println(s"Min dust value of standard-size box: $minStdDust")

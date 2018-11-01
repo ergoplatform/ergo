c84b92eb6427a75c94ee03ce53f8148346336518
oskin1 <ilya.arcadich@gmail.com>
2018-11-01 16:57:04 +0300

    Tests for new routes.

--- src/test/scala/org/ergoplatform/api/routes/WalletApiRouteSpec.scala
+++ src/test/scala/org/ergoplatform/api/routes/WalletApiRouteSpec.scala
@@ -4,14 +4,15 @@ import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.testkit.ScalatestRouteTest
 import de.heikoseeberger.akkahttpcirce.FailFastCirceSupport
-import io.circe.Json
 import io.circe.syntax._
+import io.circe.{Decoder, Json}
 import org.ergoplatform.api.WalletApiRoute
 import org.ergoplatform.modifiers.mempool.ErgoTransaction
+import org.ergoplatform.nodeView.wallet.ErgoAddressJsonEncoder
 import org.ergoplatform.nodeView.wallet.requests.{AssetIssueRequest, AssetIssueRequestEncoder, PaymentRequest, PaymentRequestEncoder, _}
 import org.ergoplatform.settings.ErgoSettings
 import org.ergoplatform.utils.Stubs
-import org.ergoplatform.{ErgoAddressEncoder, Pay2SAddress}
+import org.ergoplatform.{ErgoAddress, ErgoAddressEncoder, Pay2SAddress, Pay2SHAddress}
 import org.scalatest.{FlatSpec, Matchers, TryValues}
 import sigmastate.Values
 
@@ -33,10 +34,12 @@ class WalletApiRouteSpec extends FlatSpec
   implicit val assetIssueRequestEncoder: AssetIssueRequestEncoder = new AssetIssueRequestEncoder(ergoSettings)
   implicit val requestsHolderEncoder: RequestsHolderEncoder = new RequestsHolderEncoder(ergoSettings)
   implicit val ergoAddressEncoder: ErgoAddressEncoder = new ErgoAddressEncoder(ergoSettings.chainSettings.addressPrefix)
+  implicit val addressJsonDecoder: Decoder[ErgoAddress] = ErgoAddressJsonEncoder(settings).decoder
 
   val paymentRequest = PaymentRequest(Pay2SAddress(Values.FalseLeaf), 100L, None, None)
   val assetIssueRequest = AssetIssueRequest(Pay2SAddress(Values.FalseLeaf), 100L, "TEST", "Test", 8)
   val requestsHolder = RequestsHolder((0 to 10).flatMap(_ => Seq(paymentRequest, assetIssueRequest)), 10000L)
+  val source = """{ if (HEIGHT < 100) getVar[SigmaProp](1).get else getVar[SigmaProp](2).get }"""
 
   it should "generate arbitrary transaction" in {
     Post(prefix + "/transaction/generate", requestsHolder.asJson) ~> route ~> check {
@@ -97,6 +100,24 @@ class WalletApiRouteSpec extends FlatSpec
     Post(prefix + "/assets/issue", Seq(assetIssueRequest).asJson) ~> route ~> check {
       status shouldBe StatusCodes.OK
       responseAs[String] should not be empty
+    }
+  }
+
+  it should "generate valid P2SAddress form source" in {
+    val p = Json.obj("source" -> source.asJson)
+    Post(prefix + "p2s_address", p) ~> route ~> check {
+      status shouldBe StatusCodes.OK
+      val addressStr = responseAs[Json].hcursor.downField("address").as[String].right.get
+      ergoAddressEncoder.fromString(addressStr).get.addressTypePrefix shouldEqual Pay2SAddress.addressTypePrefix
+    }
+  }
+
+  it should "generate valid P2SHAddress form source" in {
+    val p = Json.obj("source" -> source.asJson)
+    Post(prefix + "p2sh_address", p) ~> route ~> check {
+      status shouldBe StatusCodes.OK
+      val addressStr = responseAs[Json].hcursor.downField("address").as[String].right.get
+      ergoAddressEncoder.fromString(addressStr).get.addressTypePrefix shouldEqual Pay2SHAddress.addressTypePrefix
     }
   }
 

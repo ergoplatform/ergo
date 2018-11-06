7152f210a34a36fd325290ef00c41dd0717efb97
oskin1 <ilya.arcadich@gmail.com>
2018-11-06 18:10:51 +0300

    Digest sate node sync spec.

---
+++ src/it/scala/org/ergoplatform/it/DigestStateNodeSyncSpec.scala
@@ -0,0 +1,40 @@
+package org.ergoplatform.it
+
+import com.typesafe.config.Config
+import org.ergoplatform.it.container.{IntegrationSuite, Node}
+import org.scalatest.FreeSpec
+
+import scala.concurrent.{Await, Future}
+import scala.concurrent.duration._
+
+class DigestStateNodeSyncSpec extends FreeSpec with IntegrationSuite {
+
+  val blocksQty = 10
+
+  val forkDepth: Int = blocksQty
+  val minerConfig: Config = nodeSeedConfigs.head
+
+  val digestConfig: Config = digestStatePeerConfig
+    .withFallback(prunedHistoryPeerConfig(blocksQty / 2))
+    .withFallback(nonGeneratingPeerConfig)
+    .withFallback(nodeSeedConfigs(1))
+  val onlineGeneratingConfigs: List[Config] = nodeSeedConfigs.slice(2, 4).map(onlineGeneratingPeerConfig.withFallback)
+  val nodeConfigs: List[Config] = minerConfig +: digestConfig +: onlineGeneratingConfigs
+
+  val nodes: List[Node] = docker.startNodes(nodeConfigs).get
+
+  s"Digest mode synchronization ($blocksQty blocks)" in {
+    val result = for {
+      initHeight <- Future.traverse(nodes)(_.height).map(_.max)
+      _ <- Future.traverse(nodes)(_.waitForHeight(initHeight + blocksQty))
+      headers <- Future.traverse(nodes)(_.headerIdsByHeight(initHeight + blocksQty - forkDepth))
+    } yield {
+      log.debug(s"Headers at height ${initHeight + blocksQty - forkDepth}: ${headers.mkString(",")}")
+      val headerIdsAtSameHeight = headers.flatten
+      val sample = headerIdsAtSameHeight.head
+      headerIdsAtSameHeight should contain only sample
+    }
+    Await.result(result, 10.minutes)
+  }
+
+}

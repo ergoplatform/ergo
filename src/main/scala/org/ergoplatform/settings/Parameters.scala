de4416faaec3ec0315bc178b778ed0b2ff087b88
Alex Chepurnoy <alex.chepurnoy@iohk.io>
2018-11-06 14:42:21 +0300

    parametersTable

--- src/main/scala/org/ergoplatform/settings/Parameters.scala
+++ src/main/scala/org/ergoplatform/settings/Parameters.scala
@@ -6,11 +6,11 @@ package org.ergoplatform.settings
   */
 object Parameters {
 
-  // Max total computation cost of a block.
-  val MaxBlockCost: Long = 1000000
-
   // Max size of transactions section of a block.
-  val MaxBlockSize: Int = 512 * 1024
+  lazy val MaxBlockSize: Int = parametersTable(MaxBlockSizeIncrease)
+
+  // Max total computation cost of a block.
+  lazy val MaxBlockCost: Long = parametersTable(MaxBlockCostIncrease)
 
   val Kdefault = 1250000
   val Kmax = 2500000
@@ -19,12 +19,43 @@ object Parameters {
 
   /** Cost of storing 1 byte per Constants.StoragePeriod blocks, in nanoErgs
     */
-  val K: Long = Kdefault
+  lazy val K: Int = parametersTable(KIncrease)
 
   /** To prevent creation of dust which is not profitable to charge storage fee from, we have this min-value per-byte
     * parameter.
     */
-  val MinValuePerByte: Long = 30 * 12
+  val MinValuePerByteDefault: Int = 30 * 12
   val MinValueStep = 10
   val MinValueMin = 0
+  val MinValueMax = 100000000 //0.1 Erg
+
+  lazy val MinValuePerByte: Int = parametersTable(MinValuePerByteIncrease)
+
+  //A vote for nothing
+  val NoParameter = 0: Byte
+
+  //Parameter identifiers
+  val KIncrease = 1: Byte
+  val KDecrease = -KIncrease
+
+  val MinValuePerByteIncrease = 2: Byte
+  val MinValuePerByteDecrease = -MinValuePerByteIncrease
+
+  val MaxBlockSizeIncrease = 3: Byte
+  val MaxBlockSizeDecrease = -MaxBlockSizeIncrease
+
+  val MaxBlockCostIncrease = 4: Byte
+  val MaxBlockCostDecrease = -MaxBlockCostIncrease
+
+  val parametersTable: Map[Byte, Int] = Map(
+    KIncrease -> Kdefault,
+    MinValuePerByteIncrease -> MinValuePerByteDefault,
+    MaxBlockSizeIncrease -> 512 * 1024,
+    MaxBlockCostIncrease -> 1000000
+  )
+
+ /*
+  def changeParameter(paramId: Byte) = {
+    ???
+  }*/
 }

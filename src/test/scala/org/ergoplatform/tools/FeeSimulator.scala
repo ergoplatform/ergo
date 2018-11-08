269bef33b5c1543256c4ff0d39d1e8e95749bde8
catena <catena@protonmail.com>
2018-11-08 12:18:44 +0300

    More info in FeeSimulator

--- src/test/scala/org/ergoplatform/tools/FeeSimulator.scala
+++ src/test/scala/org/ergoplatform/tools/FeeSimulator.scala
@@ -1,78 +1,99 @@
 package org.ergoplatform.tools
 
-import org.ergoplatform.{ErgoBoxCandidate, Input}
 import org.ergoplatform.modifiers.mempool.ErgoTransaction
+import org.ergoplatform.{ErgoBoxCandidate, Input}
 import scapi.sigma.DLogProtocol.DLogProverInput
 import scorex.crypto.authds.ADKey
 import scorex.crypto.hash.Digest32
 import sigmastate.interpreter.{ContextExtension, ProverResult}
 
 object FeeSimulator extends App {
-  import org.ergoplatform.settings.Parameters._
+
   import org.ergoplatform.settings.Constants._
+  import org.ergoplatform.settings.Parameters._
 
-  //standard output size is about 80 bytes
-  private val StdSize = 80
+  //Block size is 500Kb
+  private val BlockSize = 500 * 1024
 
-  println(s"K: $K")
-
-  lazy val perOutputFee = StdSize * K  / CoinsInOneErgo.toDouble
-  println(s"Storage fee for ordinary output: $perOutputFee")
-
-  val bitcoinUtxos = 60000000
-  println(s"Reward per block: ${perOutputFee * (bitcoinUtxos / StoragePeriod)}")
-
-  val minStdDust = MinValuePerByte * StdSize
-  println(s"Min dust value of standard-size box: $minStdDust")
-
-
-  //Now assume that a miner is requiring upfront payment equal to storage fee to move a box
   val k1 = DLogProverInput.random().publicImage
   val k2 = DLogProverInput.random().publicImage
 
-  val input = Input(ADKey @@ Array.fill(32)(0:Byte), ProverResult(Array.fill(65)(0: Byte), ContextExtension(Map())))
+  val input = Input(ADKey @@ Array.fill(32)(0: Byte), ProverResult(Array.fill(65)(0: Byte), ContextExtension(Map())))
 
-  val box1 = new ErgoBoxCandidate(1L, k1, Seq((Digest32 @@ Array.fill(32)(0:Byte)) -> 0L))
+  val box1 = new ErgoBoxCandidate(1L, k1, Seq((Digest32 @@ Array.fill(32)(0: Byte)) -> 0L))
   val box2 = new ErgoBoxCandidate(100L, k2)
 
   val simpleTx = ErgoTransaction(IndexedSeq(input, input), IndexedSeq(box1, box2), None)
-
+  val stdSize = simpleTx.outputs.map(_.bytes.length).sum / simpleTx.outputs.length
+  val simpleTxSize = simpleTx.bytes.length
   val outputsSize = simpleTx.outputs.map(_.bytes.length).sum
+  lazy val perOutputFee = stdSize * K / CoinsInOneErgo.toDouble
+  val minStdDust = MinValuePerByte * stdSize
+  val byteFeeBitcoin = 0.00039128734 * CoinsInOneErgo
+
+  println("=====================")
+  println("Global parameters:")
+  println(s"K: $K")
+  println(s"Output size: $stdSize B")
+  println(s"Simple tx size: $simpleTxSize B")
+  println(s"Block size: $BlockSize B")
+  println(s"Storage fee for ordinary output: $perOutputFee")
+  println(s"Min dust value of standard-size box: $minStdDust")
+
+  println("=====================")
+  println(s"Assume that Ergo state have the same number of outputs($bitcoinUtxos), as Bitcion.")
+
+  def bitcoinUtxos = 60000000
+
+  println(s"Reward per block: ${perOutputFee * (bitcoinUtxos.toDouble / StoragePeriod)} Erg")
+
+
+  println("=====================")
+  println(s"Assume that blocks are full and miner is requiring upfront payment equal to storage fee to move a box")
+
+  println(s"Reward per block: ${BlockSize * K / CoinsInOneErgo.toDouble} Erg")
 
   val minTxFee = K * outputsSize / CoinsInOneErgo.toDouble
-  println(s"Min tx fee: $minTxFee")
+  println(s"Tx fee: $minTxFee")
 
-  println("Four years storage fee: ")
   println(s"Everyday relocation: ${minTxFee * StoragePeriod / BlocksPerDay}")
   println(s"Weekly relocation: ${minTxFee * StoragePeriod / BlocksPerWeek}")
   println(s"Monthly relocation: ${minTxFee * StoragePeriod / BlocksPerMonth}")
   println(s"Yearly relocation: ${minTxFee * StoragePeriod / BlocksPerYear}")
 
   println("=====================")
+  println("Assume tx byte in Ergo has the same USD cost as in Bitcoin")
 
-
-  //Assume tx byte in Ergo has the same cost as in Bitcoin (not very realistic, as we take current market price of
-  // Ergo token, but it will be much higher probably under the assumption of equality)
-  val byteFeeBitcoin = 0.00039128734 * CoinsInOneErgo
-  println(s"bytefee Bitcoin: $byteFeeBitcoin")
-  val simpleTxSize = simpleTx.bytes.length
-  println(s"Simple tx size: $simpleTxSize")
-  val txsBlock = MaxBlockSize / simpleTxSize
-  println(s"Transactions in a block: $txsBlock")
+  //(not very realistic, as we take current market price of Ergo token, but it will be much higher probably under the assumption of equality)
   val blockFee = MaxBlockSize * byteFeeBitcoin
-  println(s"Block fee: ${blockFee / CoinsInOneErgo.toDouble}")
+  println(s"Reward per block: ${blockFee / CoinsInOneErgo.toDouble} Erg")
+  println(s"bytefee Bitcoin: ${byteFeeBitcoin / CoinsInOneErgo.toDouble} Erg")
+
 
   println("=====================")
+  println("Assume that a miner is requiring minimum tx fee proportional to lifetime of its inputs and average" +
+    " box lifetime equals to Bitcoin (8182 blocks)")
 
-
-  //et's assume that a miner is requiring minimum tx fee proportional to lifetime of its inputs (as a part of
-  // StoragePeriod = 4 years). Mean lifetime of a box in Bitcoin = 8182 blocks. Assume average tx which has 2 inputs
-  // of mean lifetime and creating 2 outputs, then:
-
+  // Mean lifetime of a box in Bitcoin = 8182 blocks.
   val LBitcoin = 8182
 
-  val meanMinTxFee = 2 * StdSize * K * LBitcoin / StoragePeriod.toDouble
+  val meanMinTxFee = outputsSize * K * LBitcoin / StoragePeriod.toDouble
 
-  println(meanMinTxFee / CoinsInOneErgo)
-  println(meanMinTxFee * txsBlock / CoinsInOneErgo)
+  println(s"Reward per block: ${BlockSize * K * LBitcoin / StoragePeriod.toDouble / CoinsInOneErgo.toDouble} Erg")
+
+  println(s"Tx fee: ${meanMinTxFee / CoinsInOneErgo.toDouble}")
+
+  println("=====================")
+  println("Assume that a miner is requiring minimum tx fee proportional to lifetime of its inputs and average" +
+    " box lifetime equals to Bitcoin (56,8 days)")
+
+  // Mean lifetime of a box in Ergo = 56,8 days
+  val LErgo = LBitcoin * 5
+
+  val meanMinTxFeeE = outputsSize * K * LErgo / StoragePeriod.toDouble
+
+  println(s"Reward per block: ${BlockSize * K * LErgo / StoragePeriod.toDouble  / CoinsInOneErgo.toDouble} Erg")
+
+  println(s"Tx fee: ${meanMinTxFeeE / CoinsInOneErgo.toDouble}")
+
 }

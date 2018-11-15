83e9c1c33c38c7351f4b078cba851607b175aa58
Alex Chepurnoy <alex.chepurnoy@iohk.io>
2018-11-15 19:16:27 +0300

    constants moved to the object Parameters

--- src/main/scala/org/ergoplatform/local/TransactionGenerator.scala
+++ src/main/scala/org/ergoplatform/local/TransactionGenerator.scala
@@ -90,7 +90,7 @@ class TransactionGenerator(viewHolder: ActorRef,
           val tokenToSpend = balances.assetBalances.toSeq(Random.nextInt(balances.assetBalances.size))
           val tokenAmountToSpend = tokenToSpend._2 / 4
           val approximateBoxSize = 200
-          val minimalErgoAmount = approximateBoxSize * LaunchParameters.MinValueMax
+          val minimalErgoAmount = approximateBoxSize * Parameters.MinValueMax
           Algos.decode(tokenToSpend._1).map { id =>
             PaymentRequest(randProposition, minimalErgoAmount, Some(Seq(Digest32 @@ id -> tokenAmountToSpend)), None)
           }.toOption

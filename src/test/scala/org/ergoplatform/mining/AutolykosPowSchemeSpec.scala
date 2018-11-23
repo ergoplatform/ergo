c7dc333abbe65130c43398b13c62fe7cc7f5d216
catena <catena@protonmail.com>
2018-11-23 18:13:42 +0300

    Leave on-fly calculation only

--- src/test/scala/org/ergoplatform/mining/AutolykosPowSchemeSpec.scala
+++ src/test/scala/org/ergoplatform/mining/AutolykosPowSchemeSpec.scala
@@ -11,7 +11,7 @@ class AutolykosPowSchemeSpec extends ErgoPropertyTest with NoShrink {
   val k = 21
   val N = 134217728
 
-  property("generate valid solution for small difficulty") {
+  property("generated solution should be valid") {
     val pow = new AutolykosPowScheme(k, N)
     forAll(invalidHeaderGen, Gen.choose(1, 20)) { (inHeader, difficulty) =>
       val nBits = RequiredDifficulty.encodeCompactBits(difficulty)
@@ -20,25 +20,10 @@ class AutolykosPowSchemeSpec extends ErgoPropertyTest with NoShrink {
       val sk = randomSecret()
       val msg = pow.msgByHeader(h)
       val b = pow.getB(h.nBits)
-      pow.initializeIfNeeded(msg, sk, b)
       val newHeader = pow.checkNonces(msg, sk, b, 0, 1000).map(s => h.copy(powSolution = s)).get
       pow.validate(newHeader) shouldBe 'success
     }
   }
 
-  property("generate valid solution for big difficulty") {
-    val pow = new AutolykosPowScheme(k, k + 1)
-    forAll(invalidHeaderGen, Gen.choose(k + 1, 1000)) { (inHeader, difficulty) =>
-      val nBits = RequiredDifficulty.encodeCompactBits(difficulty)
-      val h = inHeader.copy(nBits = nBits)
-      pow.validate(h) shouldBe 'failure
-      val sk = randomSecret()
-      val msg = pow.msgByHeader(h)
-      val b = pow.getB(h.nBits)
-      pow.initializeIfNeeded(msg, sk, b)
-      val newHeader = pow.checkNonces(msg, sk, b, 0, Int.MaxValue).map(s => h.copy(powSolution = s)).get
-      pow.validate(newHeader) shouldBe 'success
-    }
-  }
 
 }

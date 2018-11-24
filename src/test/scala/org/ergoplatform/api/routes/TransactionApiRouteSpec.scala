f25db0a34aa7abd8765c393acb3e313c642dbafe
Alex Chepurnoy <alex.chepurnoy@iohk.io>
2018-11-24 17:43:59 +0300

    unused imports

--- src/test/scala/org/ergoplatform/api/routes/TransactionApiRouteSpec.scala
+++ src/test/scala/org/ergoplatform/api/routes/TransactionApiRouteSpec.scala
@@ -4,8 +4,7 @@ import java.net.InetSocketAddress
 
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Route
-import akka.http.scaladsl.testkit.{RouteTestTimeout, ScalatestRouteTest}
-import akka.testkit.TestDuration
+import akka.http.scaladsl.testkit.ScalatestRouteTest
 import de.heikoseeberger.akkahttpcirce.FailFastCirceSupport
 import io.circe.syntax._
 import org.ergoplatform.api.TransactionsApiRoute

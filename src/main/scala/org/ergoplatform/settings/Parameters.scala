16cdbe2e5b866e24901281a8523c9ec654b5ce60
Alex Chepurnoy <alex.chepurnoy@iohk.io>
2018-11-26 14:13:48 +0300

    height, parametersTable moved to Parameters constructor

--- src/main/scala/org/ergoplatform/settings/Parameters.scala
+++ src/main/scala/org/ergoplatform/settings/Parameters.scala
@@ -6,6 +6,7 @@ import io.circe.syntax._
 import org.ergoplatform.api.ApiCodecs
 import org.ergoplatform.modifiers.history.{Extension, ExtensionCandidate}
 import org.ergoplatform.nodeView.history.ErgoHistory.Height
+import org.ergoplatform.settings.Parameters.{KIncrease, MaxBlockCostIncrease, MaxBlockSizeIncrease, MinValuePerByteIncrease}
 import scorex.core.serialization.Serializer
 
 import scala.util.Try
@@ -14,12 +15,9 @@ import scala.util.Try
 /**
   * System parameters which could be readjusted via collective miners decision.
   */
-abstract class Parameters {
+class Parameters(val height: Height, val parametersTable: Map[Byte, Int]) {
+
   import Parameters._
-
-  def height: Height
-
-  def parametersTable: Map[Byte, Int]
 
   /**
     * Cost of storing 1 byte per Constants.StoragePeriod blocks, in nanoErgs.
@@ -43,7 +41,7 @@ abstract class Parameters {
 
   def update(newHeight: Height, votes: Seq[(Byte, Int)], votingEpochLength: Int): Parameters = {
     val paramsTable = votes.foldLeft(parametersTable) { case (table, (paramId, count)) =>
-      val paramIdAbs = if(paramId < 0) (-paramId).toByte else paramId
+      val paramIdAbs = if (paramId < 0) (-paramId).toByte else paramId
 
       if (count > votingEpochLength / 2) {
         val currentValue = parametersTable(paramIdAbs)
@@ -53,9 +51,9 @@ abstract class Parameters {
 
         val newValue = paramId match {
           case b: Byte if b > 0 =>
-            if(currentValue < maxValue) currentValue + step else currentValue
+            if (currentValue < maxValue) currentValue + step else currentValue
           case b: Byte if b < 0 =>
-            if(currentValue > minValue) currentValue - step else currentValue
+            if (currentValue > minValue) currentValue - step else currentValue
         }
         table.updated(paramIdAbs, newValue)
       } else {
@@ -72,18 +70,17 @@ abstract class Parameters {
 
 
   def suggestVotes(ownTargets: Map[Byte, Int]): Array[Byte] = {
-    val vs = ownTargets.flatMap{case (paramId, value) =>
-      if(value > parametersTable(paramId)) Some(paramId) else
-      if(value < parametersTable(paramId)) Some((-paramId).toByte) else None
+    val vs = ownTargets.flatMap { case (paramId, value) =>
+      if (value > parametersTable(paramId)) Some(paramId) else if (value < parametersTable(paramId)) Some((-paramId).toByte) else None
     }.take(ParamVotesCount).toArray
     padVotes(vs)
   }
 
   def vote(ownTargets: Map[Byte, Int], votes: Array[(Byte, Int)]): Array[Byte] = {
     val vs = votes.filter { case (paramId, _) =>
-      if(paramId > 0) {
+      if (paramId > 0) {
         ownTargets.get(paramId).exists(_ > parametersTable(paramId))
-      } else if(paramId < 0) {
+      } else if (paramId < 0) {
         ownTargets.get((-paramId).toByte).exists(_ < parametersTable((-paramId).toByte))
       } else {
         false
@@ -128,34 +125,31 @@ object Parameters {
   val MinValueMin = 0
   val MinValueMax = 10000000 //0.01 Erg
 
-  lazy val parametersDescs: Map[Byte, String] = Map (
-    KIncrease -> "Storage fee factor (nanoErgs per byte per storage period)",
+  lazy val parametersDescs: Map[Byte, String] = Map(
+    KIncrease -> "Storage fee factor (per byte per storage period)",
     MinValuePerByteIncrease -> "Minimum monetary value of a box",
     MaxBlockSizeIncrease -> "Maximum block size",
     MaxBlockCostIncrease -> "Maximum cumulative computational cost of a block"
   )
 
-  lazy val stepsTable: Map[Byte, Int] = Map (
+  lazy val stepsTable: Map[Byte, Int] = Map(
     KIncrease -> Kstep,
     MinValuePerByteIncrease -> MinValueStep
   )
 
-  lazy val minValues: Map[Byte, Int] = Map (
+  lazy val minValues: Map[Byte, Int] = Map(
     KIncrease -> Kmin,
     MinValuePerByteIncrease -> MinValueMin
   )
 
-  lazy val maxValues: Map[Byte, Int] = Map (
+  lazy val maxValues: Map[Byte, Int] = Map(
     KIncrease -> Kmax,
     MinValuePerByteIncrease -> MinValueMax
   )
 
   val ParamVotesCount = 2
 
-  def apply(h: Height, paramsTable: Map[Byte, Int]): Parameters = new Parameters {
-    override val height: Height = h
-    override val parametersTable: Map[Byte, Int] = paramsTable
-  }
+  def apply(h: Height, paramsTable: Map[Byte, Int]): Parameters = new Parameters(h, paramsTable)
 
   def parseExtension(h: Height, extension: Extension): Try[Parameters] = Try {
     val paramsTable = extension.mandatoryFields.map { case (k, v) =>
@@ -191,17 +185,14 @@ object ParametersSerializer extends Serializer[Parameters] with ApiCodecs {
     ).asJson
 }
 
-object LaunchParameters extends Parameters with App {
+object LaunchParameters extends Parameters(height = 0, parametersTable = Map(
+  KIncrease -> Parameters.Kdefault,
+  MinValuePerByteIncrease -> Parameters.MinValuePerByteDefault,
+  MaxBlockSizeIncrease -> 512 * 1024,
+  MaxBlockCostIncrease -> 1000000
+)) {
+
   import Parameters._
-
-  override val height = 0
-
-  override val parametersTable = Map(
-    KIncrease -> Kdefault,
-    MinValuePerByteIncrease -> MinValuePerByteDefault,
-    MaxBlockSizeIncrease -> 512 * 1024,
-    MaxBlockCostIncrease -> 1000000
-  )
 
   def parametersDescription: String = {
     """
@@ -221,5 +212,5 @@ object LaunchParameters extends Parameters with App {
       """.stripMargin
   }
 
-  println(parametersDescription)
+  //println(parametersDescription)
 }

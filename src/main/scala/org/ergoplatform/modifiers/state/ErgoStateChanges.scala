569fc49147726780e98ca3330eeac2b7ce45b854
Ergo Morphic <ergomorphic@gmail.com>
2018-12-03 00:09:28 +0300

    minimize creation of IRContext (by saving ErgoInterpreter in State)

--- src/main/scala/org/ergoplatform/modifiers/state/ErgoStateChanges.scala
+++ src/main/scala/org/ergoplatform/modifiers/state/ErgoStateChanges.scala
@@ -9,23 +9,23 @@ import scorex.crypto.authds.ADKey
 abstract class StateChangeOperation
 
 case class Removal(boxId: ADKey) extends StateChangeOperation {
-  override def equals(that: Any): Boolean = (this, that) match {
-    case (Removal(i1), Removal(i2)) => java.util.Arrays.equals(i1, i2)
+  override def equals(that: Any): Boolean = that match {
+    case that: Removal => java.util.Arrays.equals(boxId, that.boxId)
     case _ => false
   }
 
-  override def hashCode(): Int = Ints.fromByteArray(boxId.take(4))
+  override def hashCode(): Int = Ints.fromByteArray(boxId)
 
   override def toString: String = s"Removal(id: ${Algos.encode(boxId)})"
 }
 
 case class Insertion(box: ErgoBox) extends StateChangeOperation {
-  override def equals(that: Any): Boolean = (this, that) match {
-    case (Insertion(b1), Insertion(b2)) => b1 == b2
+  override def equals(that: Any): Boolean = that match {
+    case that: Insertion => box == that.box
     case _ => false
   }
 
-  override def hashCode(): Int = Ints.fromByteArray(box.id.take(4))
+  override def hashCode(): Int = Ints.fromByteArray(box.id)
 
   override def toString: String = s"Insertion(id: ${Algos.encode(box.id)})"
 }

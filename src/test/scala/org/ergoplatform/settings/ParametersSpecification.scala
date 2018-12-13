5d8b7803038505f339d1dc3eb9ca31bfd0d91b5b
Alex Chepurnoy <alex.chepurnoy@iohk.io>
2018-12-13 23:23:56 +0300

    fork start fix, ParametersSpec fixed

--- src/test/scala/org/ergoplatform/settings/ParametersSpecification.scala
+++ src/test/scala/org/ergoplatform/settings/ParametersSpecification.scala
@@ -21,40 +21,46 @@ class ParametersSpecification extends ErgoPropertyTest {
   property("simple voting - start - conditions") {
     val kInit = 1000000
 
-    val p: Parameters = Parameters(2, Map(KIncrease -> kInit))
+    val p: Parameters = Parameters(2, Map(KIncrease -> kInit, BlockVersion -> 0))
     val vr: VotingData = VotingData.empty
     val esc = new ErgoStateContext(Seq(), ADDigest @@ Array.fill(33)(0: Byte), p, vr)
     val votes = Array(KIncrease, NoParameter, NoParameter)
-    val esc2 = esc.processExtension(p, votes, 2).get
+    val h = defaultHeaderGen.sample.get.copy(votes = votes, version = 0: Byte)
+    val esc2 = esc.processExtension(p, h, 2).get
 
     //double vote
     val wrongVotes1 = Array(KIncrease, KIncrease, NoParameter)
-    esc.processExtension(p, wrongVotes1, 2).isSuccess shouldBe false
+    val hw1 = defaultHeaderGen.sample.get.copy(votes = wrongVotes1, version = 0: Byte)
+    esc.processExtension(p, hw1, 2).isSuccess shouldBe false
 
     //contradictory votes
     val wrongVotes2 = Array(KIncrease, KDecrease, NoParameter)
-    esc.processExtension(p, wrongVotes2, 2).isSuccess shouldBe false
+    val hw2 = defaultHeaderGen.sample.get.copy(votes = wrongVotes2, version = 0: Byte)
+    esc.processExtension(p, hw2, 2).isSuccess shouldBe false
 
     //too many votes - only two ordinary changes allowed per epoch
     val wrongVotes3 = Array(KIncrease, MaxBlockCostIncrease, MaxBlockSizeDecrease)
-    esc.processExtension(p, wrongVotes3, 2).isSuccess shouldBe false
+    val hw3 = defaultHeaderGen.sample.get.copy(votes = wrongVotes3, version = 0: Byte)
+    esc.processExtension(p, hw3, 2).isSuccess shouldBe false
 
     //too many votes - a vote proposed on non-existing parameter
     val wrongVotes4 = Array((-50).toByte, NoParameter, MaxBlockSizeDecrease)
-    esc.processExtension(p, wrongVotes4, 2).isSuccess shouldBe false
+    val hw4 = defaultHeaderGen.sample.get.copy(votes = wrongVotes4, version = 0: Byte)
+    esc.processExtension(p, hw4, 2).isSuccess shouldBe false
 
     //no quorum gathered - no parameter change
-    val esc30 = esc2.processExtension(p, Array.fill(3)(NoParameter), 3).get
-    val esc40 = esc30.processExtension(p, Array.fill(3)(NoParameter), 4).get
+    val he = defaultHeaderGen.sample.get.copy(votes = Array.fill(3)(NoParameter), version = 0: Byte)
+    val esc30 = esc2.processExtension(p, he, 3).get
+    val esc40 = esc30.processExtension(p, he, 4).get
     esc40.currentParameters.k shouldBe kInit
 
     //quorum gathered - parameter change
-    val esc31 = esc2.processExtension(p, votes, 3).get
-    val p4 = Parameters(4, Map(KIncrease -> (kInit + Parameters.Kstep)))
+    val esc31 = esc2.processExtension(p, h, 3).get
+    val p4 = Parameters(4, Map(KIncrease -> (kInit + Parameters.Kstep), BlockVersion -> 0))
 
     esc31.currentVoting.results.filter(_._1 == KIncrease).head._2 shouldBe 2
 
-    val esc41 = esc31.processExtension(p4, Array.fill(3)(NoParameter), 4).get
+    val esc41 = esc31.processExtension(p4, he, 4).get
     esc41.currentParameters.k shouldBe (kInit + Parameters.Kstep)
   }
 

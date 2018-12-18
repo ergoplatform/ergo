54037d188188e32a1bfb1477fd9c3dc9beae2935
oskin1 <ilya.arcadich@gmail.com>
2018-12-18 12:43:34 +0300

    Snapshot taking bench improvements.

--- src/bench/scala/org/ergoplatform/bench/StateSnapshotTakingBench.scala
+++ src/bench/scala/org/ergoplatform/bench/StateSnapshotTakingBench.scala
@@ -3,80 +3,105 @@ package org.ergoplatform.bench
 import java.io.File
 
 import akka.actor.{ActorRef, ActorSystem}
+import io.iohk.iodb.LSMStore
+import org.ergoplatform.ErgoBox
 import org.ergoplatform.bench.misc.TempDir
-import org.ergoplatform.bench.misc.Utils.{elapsedTimeOf, _}
-import org.ergoplatform.modifiers.ErgoFullBlock
-import org.ergoplatform.modifiers.history.{BlockTransactions, Extension, Header}
+import org.ergoplatform.bench.misc.Utils.elapsedTimeOf
+import org.ergoplatform.nodeView.state.ErgoState.genesisEmissionBox
+import org.ergoplatform.nodeView.state.UtxoState.createPersistentProver
 import org.ergoplatform.nodeView.state._
-import org.ergoplatform.settings.ErgoSettings
-import scorex.core.idToVersion
+import org.ergoplatform.settings.Algos.HF
+import org.ergoplatform.settings.{Algos, Constants, ErgoSettings}
+import scorex.crypto.authds.ADValue
+import scorex.crypto.authds.avltree.batch._
+import scorex.crypto.authds.avltree.batch.serialization.BatchAVLProverSerializer
+import scorex.crypto.hash.Digest32
 import scorex.util.ScorexLogging
+import scorex.util.encode.Base16
+import sigmastate.Values
 
 import scala.concurrent.ExecutionContextExecutor
 import scala.language.postfixOps
 
 object StateSnapshotTakingBench extends ScorexLogging {
 
+  // Current result for state size = 500000 is 24 millis before rollback
+  // and 51089 millis after rollback.
+
   implicit val system: ActorSystem = ActorSystem("bench")
   implicit val ec: ExecutionContextExecutor = system.dispatcher
 
   val targetDirectory = "target/bench"
+  val stateSize = 500000
 
   def main(args: Array[String]): Unit = {
     new File(targetDirectory).mkdirs()
     val threshold = args.headOption.getOrElse("1000").toInt
-    val fileUrl = args.lift(1).get
     val userDir = TempDir.createTempDir
 
     log.info(s"User dir is $userDir")
-    log.info("Starting benchmark.")
+    log.info("Starting benchmark..")
 
     val settings = ErgoSettings.read(None)
-    val nodeSettings = settings.nodeSettings.copy(stateType = StateType.Utxo, snapshotCreationInterval = threshold - 1)
 
-    lazy val ergoSettings: ErgoSettings = settings
-      .copy(directory = userDir.getAbsolutePath, nodeSettings = nodeSettings)
+    log.info(s"Settings to be used:\n$settings")
 
-    log.info(s"Setting to be used:")
-    log.info(s"$ergoSettings")
+    val constants = StateConstants(None, settings)
+    val emissionBox = genesisEmissionBox(constants.emission)
 
-    val modifiers = readModifiers(fileUrl, threshold)
+    val boxHolder = (0 to stateSize).map { i =>
+      ErgoBox(120099L * i, Values.TrueLeaf, 1L, Seq.empty, Map.empty)
+    } :+ emissionBox
 
-    val blocks = modifiers.foldLeft(Seq.empty[ErgoFullBlock]) {
-      case (acc, header: Header) =>
-        modifiers
-          .collectFirst { case bt: BlockTransactions if bt.headerId == header.id => bt }
-          .map(bt => acc :+ ErgoFullBlock(header, bt, Extension(header.id, Seq.empty, Seq.empty), None))
-          .getOrElse(acc)
-      case (acc, _) =>
-        acc
+    val additionalBoxes: Seq[Seq[ErgoBox]] = (0 to 1000).map { i =>
+      ErgoBox(140000L * i, Values.TrueLeaf, 1L, Seq.empty, Map.empty)
+    }.grouped(100).toSeq
+
+    val prover = createProver(boxHolder, constants, settings, None)
+
+    additionalBoxes.foreach { boxes =>
+      boxes.foreach(b => prover.performOneOperation(Insert(b.id, ADValue @@ b.bytes)))
+      prover.generateProofAndUpdateStorage()
     }
 
-    var state = createUtxoState(settings, None)._1
+    implicit val hf: HF = Algos.hash
+    val serializer = new BatchAVLProverSerializer[Digest32, HF]
 
-    blocks.foreach(b => state = state.applyModifier(b).get)
+    val result0 = elapsedTimeOf {
+      serializer.slice(prover.prover())
+    }
 
-    log.info(s"State is at height ${state.stateContext.currentHeight}")
+    val versions = prover.storage.rollbackVersions
+    val versionToRollbackTo = versions.dropRight(7).last
+    val rollbackResult = prover.rollback(versionToRollbackTo)
 
-    val destinationBlock = blocks.dropRight(10).last
-
-    state = state.rollbackTo(idToVersion(destinationBlock.id)).get
-
-    log.info(s"State height after rollback is ${state.stateContext.currentHeight}")
+    log.info(rollbackResult.map(_ => s"Successfully rolled back to version ${Base16.encode(versionToRollbackTo)}")
+      .getOrElse("Rollback failed"))
 
     val result = elapsedTimeOf {
-      state.takeSnapshot
+      serializer.slice(prover.prover())
     }
 
-    log.info(s"Elapsed time of taking state snapshot at height ${destinationBlock.header.height} is $result millis")
+    log.info(s"Elapsed time of taking snapshot before rollback is $result0 millis (${stateSize + 1002})")
+    log.info(s"Elapsed time of taking snapshot after rollback is $result millis (~$stateSize)")
 
     System.exit(0)
   }
 
-  private def createUtxoState(settings: ErgoSettings,
-                              nodeViewHolderRef: Option[ActorRef] = None): (UtxoState, BoxHolder) = {
-    val constants = StateConstants(nodeViewHolderRef, settings)
-    ErgoState.generateGenesisUtxoState(TempDir.createTempDir, constants, settings)
+  private def createProver(boxes: Seq[ErgoBox],
+                           constants: StateConstants,
+                           settings: ErgoSettings,
+                           nodeViewHolderRef: Option[ActorRef] = None): PersistentBatchAVLProver[Digest32, HF] = {
+    log.info(s"Generating prover with ${boxes.size} boxes inside..")
+
+    val p = new BatchAVLProver[Digest32, HF](keyLength = Constants.HashLength, valueLengthOpt = None)
+    boxes.foreach(b => p.performOneOperation(Insert(b.id, ADValue @@ b.bytes)).get)
+
+    val nodeParameters = NodeParameters(keySize = Constants.HashLength, valueSize = None, labelSize = 32)
+    val store = new LSMStore(TempDir.createTempDir, keepVersions = constants.keepVersions)
+    val defaultStateContext = ErgoStateContext.empty(p.digest)
+    val storage: VersionedIODBAVLStorage[Digest32] = new VersionedIODBAVLStorage(store, nodeParameters)(Algos.hash)
+    createPersistentProver(p, storage, ErgoState.genesisStateVersion, None, defaultStateContext)
   }
 
 }

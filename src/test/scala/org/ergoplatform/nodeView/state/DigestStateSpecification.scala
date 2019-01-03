02a3c506cf413f12c6db82adc52f4d6efe83c713
Alex Chepurnoy <alex.chepurnoy@iohk.io>
2019-01-03 12:22:35 +0300

    rollback test for digest state improved

--- src/test/scala/org/ergoplatform/nodeView/state/DigestStateSpecification.scala
+++ src/test/scala/org/ergoplatform/nodeView/state/DigestStateSpecification.scala
@@ -86,10 +86,14 @@ class DigestStateSpecification extends ErgoPropertyTest {
 
       ds2.rollbackVersions.size shouldEqual 2
 
+      ds2.stateContext.lastHeaders.size shouldEqual 1
+
       java.util.Arrays.equals(ds2.rootHash, ds.rootHash) shouldBe false
 
       val ds3 = ds2.rollbackTo(ds.version).get
       ds3.rootHash shouldBe ds.rootHash
+
+      ds3.stateContext.lastHeaders.size shouldEqual 0
 
       ds3.applyModifier(block).get.rootHash shouldBe ds2.rootHash
     }

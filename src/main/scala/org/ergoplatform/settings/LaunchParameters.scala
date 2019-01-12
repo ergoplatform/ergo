bb0e6d980d1ef314431a8d0d56bacebb4478f0b9
Alex Chepurnoy <alex.chepurnoy@iohk.io>
2019-01-12 18:07:35 +0300

    DefaultParametersPrinter

--- src/main/scala/org/ergoplatform/settings/LaunchParameters.scala
+++ src/main/scala/org/ergoplatform/settings/LaunchParameters.scala
@@ -8,25 +8,4 @@ object LaunchParameters extends Parameters(height = 0, parametersTable = Map(
   MaxBlockSizeIncrease -> 512 * 1024,
   MaxBlockCostIncrease -> 1000000,
   BlockVersion -> 1
-)) {
-
-  import Parameters._
-
-  def parametersDescription: String = {
-    """
-      |\begin{tabular}{*{6}{l}}
-      |Id & Description & Default & Step & Min & Max \\
-      |\hline
-    """.stripMargin +
-      parametersDescs.map { case (id, desc) =>
-        val defaultOpt = parametersTable.get(id)
-        val stepOpt = stepsTable.get(id)
-        val minValue = minValues.get(id)
-        val maxValue = maxValues.get(id)
-        s"$id & $desc & ${defaultOpt.getOrElse("-")} & ${stepOpt.getOrElse("-")} & ${minValue.getOrElse("-")} & ${maxValue.getOrElse("-")} \\\\"
-      }.mkString("\n") +
-      """
-        |\end{tabular}
-      """.stripMargin
-  }
-}
+))

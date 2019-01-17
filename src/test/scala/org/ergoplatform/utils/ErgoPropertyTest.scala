357226b35e5b548ff6a84699f3580c50105b79ad
catena <catena@protonmail.com>
2019-01-17 15:54:14 +0300

    Fix scalatest version

--- src/test/scala/org/ergoplatform/utils/ErgoPropertyTest.scala
+++ src/test/scala/org/ergoplatform/utils/ErgoPropertyTest.scala
@@ -2,12 +2,11 @@ package org.ergoplatform.utils
 
 import org.ergoplatform.utils.generators.{ErgoGenerators, ValidBlocksGenerators}
 import org.scalatest.PropSpec
-import org.scalatest.prop.TableDrivenPropertyChecks
-import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
+import org.scalatest.prop.{PropertyChecks, TableDrivenPropertyChecks}
 import scorex.testkit.utils.NoShrink
 
 trait ErgoPropertyTest extends PropSpec
-  with ScalaCheckPropertyChecks
+  with PropertyChecks
   with TableDrivenPropertyChecks
   with ErgoTestHelpers
   with ErgoGenerators

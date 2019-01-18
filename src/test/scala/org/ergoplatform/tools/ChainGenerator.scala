7dc14df2cd3ae0509a4b9e64a53fd563ec12823b
oskin1 <ilya.arcadich@gmail.com>
2019-01-18 18:33:08 +0300

    Transaction generation fixed.

--- src/test/scala/org/ergoplatform/tools/ChainGenerator.scala
+++ src/test/scala/org/ergoplatform/tools/ChainGenerator.scala
@@ -4,31 +4,27 @@ import java.io.File
 
 import akka.actor.ActorSystem
 import akka.testkit.TestKit
+import org.ergoplatform._
 import org.ergoplatform.local.ErgoMiner
 import org.ergoplatform.mining.difficulty.RequiredDifficulty
 import org.ergoplatform.mining.{AutolykosPowScheme, CandidateBlock}
 import org.ergoplatform.modifiers.ErgoFullBlock
 import org.ergoplatform.modifiers.history.{Extension, ExtensionCandidate, Header}
 import org.ergoplatform.modifiers.mempool.{ErgoTransaction, UnsignedErgoTransaction}
-import org.ergoplatform.nodeView.{ErgoContext, TransactionContext}
 import org.ergoplatform.nodeView.history.ErgoHistory
 import org.ergoplatform.nodeView.history.ErgoHistory.Height
 import org.ergoplatform.nodeView.history.storage.modifierprocessors.{FullBlockPruningProcessor, ToDownloadProcessor}
 import org.ergoplatform.nodeView.state._
-import org.ergoplatform.nodeView.wallet.BoxCertainty.Uncertain
 import org.ergoplatform.nodeView.wallet._
 import org.ergoplatform.settings._
 import org.ergoplatform.utils.ErgoTestHelpers
-import org.ergoplatform._
 import scorex.crypto.hash.Digest32
-import scorex.util.{ModifierId, bytesToId, idToBytes}
-import sigmastate.Values
+import scorex.util.{ModifierId, idToBytes}
 import sigmastate.basics.DLogProtocol.ProveDlog
-import sigmastate.interpreter.ContextExtension
 
 import scala.annotation.tailrec
 import scala.concurrent.duration._
-import scala.util.{Failure, Random, Success, Try}
+import scala.util.{Random, Try}
 
 /**
   * Application object for chain generation.
@@ -43,104 +39,123 @@ object ChainGenerator extends TestKit(ActorSystem()) with App with ErgoTestHelpe
 
   val EmissionTxCost: Long = 20000
   val MinTxAmount: Long = 2000000
+  val RewardDelay: Int = 720
+  val MaxTxsPerBlock: Int = 20
 
   val prover = defaultProver
+  val selfAddressScript = Pay2SAddress(prover.dlogPubkeys.head).script
 
   val pow = new AutolykosPowScheme(powScheme.k, powScheme.n)
   val blockInterval = 2.minute
 
   val boxSelector: BoxSelector = DefaultBoxSelector
-  val registry = new WalletStorage
 
   val startTime = args.headOption.map(_.toLong).getOrElse(timeProvider.time - (blockInterval * 10).toMillis)
-  val dir = if (args.length < 2) new File("/Users/oskin/Desktop/Dev/scala_dev/ergo/ergo/data") else new File(args(1))
+  val dir = if (args.length < 2) new File("/tmp/ergo/data") else new File(args(1))
   val txsSize: Int = if (args.length < 3) 100 * 1024 else args(2).toInt
 
   val miningDelay = 1.second
   val minimalSuffix = 2
   val nodeSettings: NodeConfigurationSettings = NodeConfigurationSettings(StateType.Utxo, verifyTransactions = true,
     -1, PoPoWBootstrap = false, minimalSuffix, mining = false, miningDelay, offlineGeneration = false, 200)
-  val monetarySettings = settings.chainSettings.monetary.copy(minerRewardDelay = 720)
+  val monetarySettings = settings.chainSettings.monetary.copy(
+    minerRewardDelay = RewardDelay,
+    afterGenesisStateDigestHex = "d801a0e4573d6993caa2eda7dc97aad2b4c8ed51ebb0afe0dd272c8d7e26d5fd01"
+  )
   val chainSettings = ChainSettings(0: Byte, 0: Byte, blockInterval, 256, 8, votingSettings, pow, monetarySettings)
   val fullHistorySettings: ErgoSettings = ErgoSettings(dir.getAbsolutePath, chainSettings, settings.testingSettings,
     nodeSettings, settings.scorexSettings, settings.walletSettings, CacheSettings.default)
   val stateDir = ErgoState.stateDir(fullHistorySettings)
   stateDir.mkdirs()
 
+  val genesisBox = ErgoState.genesisEmissionBox(fullHistorySettings.emission)
+
   val votingEpochLength = votingSettings.votingLength
   val protocolVersion = fullHistorySettings.chainSettings.protocolVersion
 
   val history = ErgoHistory.readOrGenerate(fullHistorySettings, timeProvider)
   allowToApplyOldBlocks(history)
-  val (state, initBoxHolder) = ErgoState.generateGenesisUtxoState(stateDir, StateConstants(None, fullHistorySettings))
+  val (state, _) = ErgoState.generateGenesisUtxoState(stateDir, StateConstants(None, fullHistorySettings))
   log.error(s"Going to generate a chain at ${dir.getAbsoluteFile} starting from ${history.bestFullBlockOpt}")
 
-  val chain = loop(state, initBoxHolder, None, Seq())
+  val chain = loop(state, IndexedSeq.empty, None, Seq())
   log.info(s"Chain of length ${chain.length} generated")
   history.bestHeaderOpt shouldBe history.bestFullBlockOpt.map(_.header)
   history.bestFullBlockOpt.get.id shouldBe chain.last
   log.info("History was generated successfully")
   System.exit(0)
 
-  private def loop(state: UtxoState, bh: BoxHolder, last: Option[Header], acc: Seq[ModifierId]): Seq[ModifierId] = {
+  private def loop(state: UtxoState,
+                   bxs: IndexedSeq[ErgoBox],
+                   last: Option[Header],
+                   acc: Seq[ModifierId]): Seq[ModifierId] = {
     val time: Long = last.map(_.timestamp + blockInterval.toMillis).getOrElse(startTime)
     if (time < timeProvider.time) {
-      val (txs, newBh) = genTransactions(last.map(_.height).getOrElse(ErgoHistory.GenesisHeight), bh, state.stateContext)
+      val (txs, leftBxs) = genTransactions(last.map(_.height).getOrElse(ErgoHistory.GenesisHeight),
+        bxs, state.stateContext)
 
       val candidate = genCandidate(prover.dlogPubkeys.head, last, time, txs, state)
-
       val block = proveCandidate(candidate.get)
+
       history.append(block.header).get
       block.blockSections.foreach(s => if (!history.contains(s)) history.append(s).get)
+
       log.info(s"Block ${block.id} at height ${block.header.height} generated")
-      loop(state.applyModifier(block).get, newBh, Some(block.header), acc :+ block.id)
+
+      val newBxs = leftBxs ++ block.transactions
+        .flatMap(_.outputs)
+        .filterNot(_.proposition == genesisBox.proposition)
+
+      loop(state.applyModifier(block).get, newBxs, Some(block.header), acc :+ block.id)
     } else {
       acc
     }
   }
 
   private def genTransactions(height: Height,
-                              bh: BoxHolder,
-                              ctx: ErgoStateContext): (Seq[ErgoTransaction], BoxHolder) = {
-    val balance = registry.confirmedBalance
+                              bxs: IndexedSeq[ErgoBox],
+                              ctx: ErgoStateContext): (Seq[ErgoTransaction], IndexedSeq[ErgoBox]) = {
+    val availableBxs = bxs.filter { bx =>
+      bx.creationHeight + RewardDelay <= height && bx.proposition != genesisBox.proposition
+    }
+    val balance = availableBxs.map(_.value).sum
     if (balance >= MinTxAmount) {
-      val qty = (balance / MinTxAmount).toInt
+      val qty = math.min((balance / MinTxAmount).toInt, MaxTxsPerBlock)
       val amount = balance / qty
-      val outs = (0 to qty).foldLeft(Seq.empty[ErgoBoxCandidate]) { case (acc, _) =>
-        val bc = new ErgoBoxCandidate(amount, Pay2SAddress(prover.dlogPubkeys.head).script, height)
-        acc :+ bc
-      }
+      val outs = (0 to qty).map(_ => new ErgoBoxCandidate(amount, selfAddressScript, height))
       val (txs, usedBoxed) = outs
-        .flatMap { out =>
-          val bxs = bh.boxes.values.map { box =>
+        .foldLeft(Seq.empty[(ErgoTransaction, IndexedSeq[ErgoBox])]) { case (acc, out) =>
+          val spentBxs = acc.flatMap(_._2)
+          val unspentBxs = (availableBxs ++ acc.flatMap(_._1.outputs)).filterNot(spentBxs.contains)
+          val trackedBxs = unspentBxs.map { box =>
             TrackedBox(invalidErgoTransactionGen.sample.get, 0, None, box, BoxCertainty.Certain)
           }
-          boxSelector.select(bxs.iterator, _ => true, amount, Map.empty).map { r =>
-            val inputs = r.boxes.toIndexedSeq
+          boxSelector.select(trackedBxs.iterator, _ => true, amount, Map.empty)
+            .map { r =>
+              val inputs = r.boxes.toIndexedSeq
 
-            val changeAddress = prover.dlogPubkeys(Random.nextInt(prover.dlogPubkeys.size))
+              val changeAddress = prover.dlogPubkeys(Random.nextInt(prover.dlogPubkeys.size))
 
-            val changeBoxCandidates = r.changeBoxes.map { case (ergChange, tokensChange) =>
-              val assets = tokensChange.map(t => Digest32 @@ idToBytes(t._1) -> t._2).toIndexedSeq
-              new ErgoBoxCandidate(ergChange, changeAddress, height, assets)
+              val changeBoxCandidates = r.changeBoxes.map { case (ergChange, tokensChange) =>
+                val assets = tokensChange.map(t => Digest32 @@ idToBytes(t._1) -> t._2).toIndexedSeq
+                new ErgoBoxCandidate(ergChange, changeAddress, height, assets)
+              }
+
+              val unsignedTx = new UnsignedErgoTransaction(
+                inputs.map(_.id).map(id => new UnsignedInput(id)),
+                (out +: changeBoxCandidates).toIndexedSeq
+              )
+
+              prover.sign(unsignedTx, inputs, ctx).fold(_ => acc, tx => acc :+ tx -> inputs)
             }
-
-            val unsignedTx = new UnsignedErgoTransaction(
-              inputs.map(_.id).map(id => new UnsignedInput(id)),
-              (out +: changeBoxCandidates).toIndexedSeq
-            )
-
-            prover.sign(unsignedTx, inputs, ctx)
-              .fold(e => Failure(new Exception(s"Failed to sign boxes: $inputs", e)), tx => Success(tx -> inputs))
-          }
+            .fold(acc)(res => res)
         }
-        .collect { case Success(res) => res }
         .foldLeft(Seq.empty[ErgoTransaction], Seq.empty[ErgoBox]) { case ((txsAcc, inputsAcc), (tx, inputs)) =>
           (txsAcc :+ tx, inputsAcc ++ inputs)
         }
-      txs -> bh.filter(!usedBoxed.contains(_))
+      txs -> bxs.filterNot(usedBoxed.contains)
     } else {
-      Seq.empty -> bh
+      Seq.empty -> bxs
     }
   }
 

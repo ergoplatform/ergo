2b517f2cf0ff7a6cd0634f194cca8d514234e3a7
andyceo <andyceo@yandex.ru>
2019-01-23 20:32:42 +0300

    TeX papers: rename main.tex to YellowPaper.tex. Also bugfix for TeX compile.sh scripts

--- papers/yellow/compile.sh
+++ papers/yellow/compile.sh
@@ -1,24 +1,26 @@
 #!/usr/bin/env sh
 
 command -v pdflatex && command -v bibtex
-if [[ "$?" != 0 ]]; then
+if [ "$?" != 0 ]; then
     echo "Command 'pdflatex' or 'bibtex' not exist, both must be installed. For Ubuntu, try:"
     echo "sudo apt install texlive-latex-base texlive-binaries"
     echo
     echo "You may also need to install additional packages like fonts, etc. For Ubuntu, try:"
-    echo "sudo apt-get install texlive-fonts-recommended latex-xcolor texlive-latex-extra cm-super"
+    echo "sudo apt install texlive-fonts-recommended texlive-science latex-xcolor texlive-latex-extra cm-super"
+    echo
+    echo "You can install all the TeX additional packages (+2Gb disk space) with: sudo apt install texlive-full"
     exit 1;
 fi
 
-rm main.pdf
-pdflatex -shell-escape main
-bibtex -shell-escape main
-pdflatex -shell-escape main
-pdflatex -shell-escape main
+rm YellowPaper.pdf
+pdflatex -shell-escape YellowPaper
+bibtex -shell-escape YellowPaper
+pdflatex -shell-escape YellowPaper
+pdflatex -shell-escape YellowPaper
 rm *.aux
 rm *.log
 rm *.blg
 rm *.bbl
 rm *.out
 rm *.toc
-rm -r _minted-main/
+rm -r _minted-YellowPaper/

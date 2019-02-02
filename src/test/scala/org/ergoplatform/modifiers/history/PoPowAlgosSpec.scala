9948fe063ca77c101c3b13812fd6527580686d0c
oskin1 <ilya.arcadich@gmail.com>
2019-02-02 22:03:56 +0300

    packInterlinks fixed.

--- src/test/scala/org/ergoplatform/modifiers/history/PoPowAlgosSpec.scala
+++ src/test/scala/org/ergoplatform/modifiers/history/PoPowAlgosSpec.scala
@@ -1,16 +1,17 @@
 package org.ergoplatform.modifiers.history
 
-import org.ergoplatform.utils.generators.ChainGenerator
-import org.scalatest.{FlatSpec, Matchers}
+import org.ergoplatform.utils.generators.{ChainGenerator, ErgoGenerators}
+import org.scalacheck.Gen
+import org.scalatest.{Matchers, PropSpec}
 import scorex.util.ModifierId
 
-class PoPowAlgosSpec extends FlatSpec with Matchers with ChainGenerator {
+class PoPowAlgosSpec extends PropSpec with Matchers with ChainGenerator with ErgoGenerators {
 
   import PoPowAlgos._
 
   private val ChainLength = 10
 
-  it should "calculate correct interlinks" in {
+  property("updateInterlinks") {
     val chain = genChain(ChainLength)
     val genesis = chain.head
     val interlinks = chain.foldLeft(Seq.empty[Seq[ModifierId]]) { case (acc, b) =>
@@ -21,6 +22,17 @@ class PoPowAlgosSpec extends FlatSpec with Matchers with ChainGenerator {
       links.head shouldEqual genesis.header.id
       links.tail should not contain genesis.header.id
     }
+  }
+
+  property("packInterlinks") {
+    val diffInterlinks = Gen.listOfN(255, modifierIdGen).sample.get
+    val modId = modifierIdGen.sample.get
+    val sameInterlinks = List.fill(255)(modId)
+    val packedDiff = PoPowAlgos.packInterlinks(diffInterlinks)
+    val packedSame = PoPowAlgos.packInterlinks(sameInterlinks)
+
+    packedDiff.map(_._1.last).toSet.size shouldEqual diffInterlinks.size
+    packedSame.map(_._1.last).toSet.size shouldEqual 1
   }
 
 }

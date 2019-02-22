64c25889ba938072565daaede1b600a52653208d
Alex Chepurnoy <alex.chepurnoy@iohk.io>
2019-02-22 22:33:49 +0300

    soft-fork w. activation comments

--- src/test/scala/org/ergoplatform/settings/ParametersSpecification.scala
+++ src/test/scala/org/ergoplatform/settings/ParametersSpecification.scala
@@ -78,39 +78,56 @@ class ParametersSpecification extends ErgoPropertyTest {
     esc41.currentParameters.storageFeeFactor shouldBe (kInit + Parameters.StorageFeeFactorStep)
   }
 
+  /**
+    * A test which is ensuring that approved soft-fork activates properly.
+    * For the test, we have:
+    *   - epoch length is about 2 blocks
+    *   - 2 epochs to vote
+    *   - 3 epochs to activate the fork
+    *
+    *   So the fork would be activated only if 4 votes out of 4 are for it.
+    */
   property("soft fork - w. activation") {
     val p: Parameters = Parameters(1, Map(BlockVersion -> 0))
     val vr: VotingData = VotingData.empty
     val esc1 = new ErgoStateContext(Seq(), ADDigest @@ Array.fill(33)(0: Byte), p, vr)
     val forkVote = Array(SoftFork, NoParameter, NoParameter)
     val emptyVotes = Array(NoParameter, NoParameter, NoParameter)
-    val h2 = defaultHeaderGen.sample.get.copy(votes = forkVote, version = 0: Byte, height = 2)
 
+    // Soft-fork vote is proposed @ height == 2
+    val h2 = defaultHeaderGen.sample.get.copy(votes = forkVote, version = 0: Byte, height = 2)
     val expectedParameters2 = Parameters(2, Map(SoftForkStartingHeight -> 2, SoftForkVotesCollected -> 0, BlockVersion -> 0))
     val esc2 = esc1.process(h2, expectedParameters2).get
     esc2.currentParameters.softForkStartingHeight.get shouldBe 2
 
+    // voting for the fork @ height == 3
     val h3 = h2.copy(height = 3)
     val esc3 = esc2.process(h3, expectedParameters2).get
     esc3.currentParameters.softForkStartingHeight.get shouldBe 2
 
+    // voting for the fork @ height == 4
+    // new epoch is starting, thus the block should contain number of votes for the fork collected in the previous epoch
     val h4 = h3.copy(height = 4)
     val expectedParameters4 = Parameters(4, Map(SoftForkStartingHeight -> 2, SoftForkVotesCollected -> 2, BlockVersion -> 0))
     val esc4 = esc3.process(h4, expectedParameters4).get
     esc4.currentParameters.softForkStartingHeight.get shouldBe 2
     esc4.currentParameters.softForkVotesCollected.get shouldBe 2
 
+    // voting for the fork @ height == 5
     val h5 = h4.copy(height = 5)
     val esc5 = esc4.process(h5, expectedParameters4).get
 
+    // voting is finished, and we check collected votes @ height == 6
     val h6 = h5.copy(height = 6, votes = emptyVotes)
     val expectedParameters6 = Parameters(6, Map(SoftForkStartingHeight -> 2, SoftForkVotesCollected -> 4, BlockVersion -> 0))
     val esc6 = esc5.process(h6, expectedParameters6).get
 
+    // voting for soft-fork is prohibited @ height == 6
     val h6w = h5.copy(height = 6)
     esc5.process(h6w, expectedParameters6).isSuccess shouldBe false
 
     val esc11 = (7 to 11).foldLeft(esc6) { case (esc, i) =>
+      // voting for soft-fork is prohibited during activation period
       val hw = h6.copy(height = i, votes = forkVote)
       esc.process(hw, expectedParameters6).isFailure shouldBe true
 
@@ -118,27 +135,31 @@ class ParametersSpecification extends ErgoPropertyTest {
       esc.process(h, expectedParameters6).get
     }
 
+    // activation period done @ height = 12, block version is increased
     val h12 = h6.copy(height = 12, version = 1: Byte)
     val expectedParameters12 = Parameters(12, Map(SoftForkStartingHeight -> 2, SoftForkVotesCollected -> 4, BlockVersion -> 1))
     val esc12 = esc11.process(h12, expectedParameters12).get
 
+    // vote for soft-fork @ activation height
     val h12w = h12.copy(votes = forkVote)
     esc11.process(h12w, expectedParameters12).isFailure shouldBe true
 
     val h13 = h12.copy(height = 13)
     val esc13 = esc12.process(h13, expectedParameters12).get
 
+    // vote for soft-fork is prohibited before next epoch after activation height
     val h13w = h13.copy(votes = forkVote)
     esc12.process(h13w, expectedParameters12).isFailure shouldBe true
 
-
+    // voting for soft-fork is possible on the first block of the next epoch after activation height
     val h14 = h13.copy(height = 14, votes = forkVote)
     val expectedParameters14 = Parameters(14, Map(SoftForkStartingHeight -> 14, SoftForkVotesCollected -> 0, BlockVersion -> 1))
     val esc14 = esc13.process(h14, expectedParameters14).get
 
-    val h14e = h13.copy(height = 14, votes = emptyVotes)
+    // next epoch after activation height - soft-fork related parameters are cleared
+    val h14b = h13.copy(height = 14, votes = emptyVotes)
     val expectedParameters14e = Parameters(14, Map(BlockVersion -> 1))
-    val esc14e = esc13.process(h14e, expectedParameters14e).get
+    val esc14b = esc13.process(h14b, expectedParameters14e).get
   }
 
   property("soft fork - unsuccessful voting") {

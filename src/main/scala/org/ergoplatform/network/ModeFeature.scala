bde92f7adb53de62d2b2b1781fdf6b7eee3d6d83
Alex Chepurnoy <alex.chepurnoy@iohk.io>
2019-03-13 15:28:02 +0300

    ModeFeature comments

--- src/main/scala/org/ergoplatform/network/ModeFeature.scala
+++ src/main/scala/org/ergoplatform/network/ModeFeature.scala
@@ -7,7 +7,16 @@ import scorex.core.network.PeerFeature.Id
 import scorex.core.serialization.ScorexSerializer
 import scorex.util.serialization.{Reader, Writer}
 
-
+/**
+  * A peer feature that is storing information on operating mode of the peer. Currently it is storing the following
+  * fields, in future more information could be stored without any problems for nodes not upgraded, see
+  * ModeFeatureSerializer comments.
+  *
+  * @param stateType - information on whether UTXO set is store (so state type is UTXO/Digest)
+  * @param verifyingTransactions - whether the peer is verifying transactions
+  * @param popowSuffix - whether the peer has has bootstrapped via PoPoW suffix, and its length
+  * @param blocksToKeep - how many last full blocks the peer is storing
+  */
 case class ModeFeature(stateType: StateType,
                        verifyingTransactions: Boolean,
                        popowSuffix: Option[Int],
@@ -33,7 +42,15 @@ object ModeFeature {
 }
 
 
+/**
+  * When the node is parsing operating mode information from a peer, it allows additional information to be stored there.
+  * Please note that the serialized mode information could be no longer than 512 bytes. Please note that serialized
+  * handshake which contains mode information (along with other features supported by the peer) has separate length
+  * limit provided in settings ("maxHandshakeSize" field in network settings).
+  */
 object ModeFeatureSerializer extends ScorexSerializer[ModeFeature] {
+
+  val MaxSize = 512
 
   //we use these methods due to absence of getBoolean in Reader atm of writing the code
   private def booleanToByte(bool: Boolean): Byte = if (bool) 1: Byte else 0: Byte
@@ -48,7 +65,7 @@ object ModeFeatureSerializer extends ScorexSerializer[ModeFeature] {
   }
 
   override def parse(r: Reader): ModeFeature = {
-    require(r.remaining < 512)
+    require(r.remaining < MaxSize)
 
     val stateType = StateType.fromCode(r.getByte())
     val verifyingTransactions = byteToBoolean(r.getByte())

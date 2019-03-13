a6a3682039bce7c49d91f36aaed9a811dd15218e
Alex Chepurnoy <alex.chepurnoy@iohk.io>
2019-03-13 13:16:41 +0300

    DigestState.validateTransactions(), new tests

--- src/test/scala/org/ergoplatform/nodeView/state/DigestStateSpecification.scala
+++ src/test/scala/org/ergoplatform/nodeView/state/DigestStateSpecification.scala
@@ -1,11 +1,15 @@
 package org.ergoplatform.nodeView.state
 
+import org.ergoplatform.{DataInput, Input}
 import org.ergoplatform.modifiers.ErgoFullBlock
+import org.ergoplatform.modifiers.history.ADProofs
+import org.ergoplatform.modifiers.mempool.ErgoTransaction
 import org.ergoplatform.utils.ErgoPropertyTest
 import scorex.core._
 import scorex.crypto.authds.ADDigest
+import sigmastate.interpreter.ProverResult
 
-import scala.util.Random
+import scala.util.{Random, Try}
 
 class DigestStateSpecification extends ErgoPropertyTest {
 
@@ -95,6 +99,36 @@ class DigestStateSpecification extends ErgoPropertyTest {
       ds3.stateContext.lastHeaders.size shouldEqual 0
 
       ds3.applyModifier(block).get.rootHash shouldBe ds2.rootHash
+    }
+  }
+
+  property("validateTransactions() - dataInputs") {
+    forAll(boxesHolderGen) { bh =>
+      val us = createUtxoState(bh)
+      val ds = createDigestState(us.version, us.rootHash)
+
+      // generate 2 independent transactions spending state boxes only
+      val headTx = validTransactionsFromBoxes(1, bh.boxes.take(10).values.toSeq, new Random())._1.head
+      val nextTx = validTransactionsFromBoxes(1, bh.boxes.takeRight(10).values.toSeq, new Random())._1.head
+      headTx.inputs.intersect(nextTx.inputs) shouldBe empty
+
+      // trying to apply transactions with data inputs same as inputs of the next tx
+      val dataInputs = nextTx.inputs.filter(i => us.boxById(i.boxId).isDefined).map(i => DataInput(i.boxId))
+      val inputs = headTx.outputs.map(b =>  Input(b.id, ProverResult.empty))
+      val txWithDataInputs = ErgoTransaction(inputs, dataInputs, headTx.outputCandidates)
+
+      val txs1 = IndexedSeq(headTx, nextTx, txWithDataInputs)
+      val (proofBytes1, digest1) = us.proofsForTransactions(txs1).get
+      val proof1 = ADProofs(defaultHeaderGen.sample.get.id, proofBytes1)
+      Try(ds.validateTransactions(txs1, digest1, proof1, emptyStateContext)) shouldBe 'success
+
+      val txs2 = IndexedSeq(headTx, txWithDataInputs, nextTx)
+      val (proofBytes2, digest2) = us.proofsForTransactions(txs2).get
+      val proof2 = ADProofs(defaultHeaderGen.sample.get.id, proofBytes2)
+      Try(ds.validateTransactions(txs2, digest2, proof2, emptyStateContext)) shouldBe 'success
+
+      val txs3 = IndexedSeq(txWithDataInputs, headTx, nextTx)
+      Try(ds.validateTransactions(txs3, digest2, proof2, emptyStateContext)) shouldBe 'failure
     }
   }
 

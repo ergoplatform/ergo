fe0a32978ff040708ad1272903fd9ed7e90de12c
oskin1 <ilya.arcadich@gmail.com>
2019-03-27 18:47:31 +0300

    External miner support improved.

--- src/test/scala/org/ergoplatform/settings/ErgoSettingsSpecification.scala
+++ src/test/scala/org/ergoplatform/settings/ErgoSettingsSpecification.scala
@@ -15,19 +15,22 @@ class ErgoSettingsSpecification extends ErgoPropertyTest {
   property("should read default settings") {
     val settings = ErgoSettings.read(None)
     settings.nodeSettings shouldBe NodeConfigurationSettings(StateType.Utxo, verifyTransactions = true, 1000,
-      poPoWBootstrap = false, 10, mining = false, 1.second, offlineGeneration = false, 200, 100000, 100000)
+      poPoWBootstrap = false, 10, mining = false, 1.second, useExternalMiner = false, offlineGeneration = false,
+      200, 100000, 100000)
   }
 
   property("should read user settings from json file") {
     val settings = ErgoSettings.read(Some("src/test/resources/settings.json"))
     settings.nodeSettings shouldBe NodeConfigurationSettings(StateType.Utxo, verifyTransactions = true, 12,
-      poPoWBootstrap = false, 10, mining = false, 1.second, offlineGeneration = false, 200, 100000, 100000)
+      poPoWBootstrap = false, 10, mining = false, 1.second, useExternalMiner = false, offlineGeneration = false,
+      200, 100000, 100000)
   }
 
   property("should read user settings from HOCON file") {
     val settings = ErgoSettings.read(Some("src/test/resources/settings.conf"))
     settings.nodeSettings shouldBe NodeConfigurationSettings(StateType.Utxo, verifyTransactions = true, 13,
-      poPoWBootstrap = false, 10, mining = false, 1.second, offlineGeneration = false, 200, 100000, 100000)
+      poPoWBootstrap = false, 10, mining = false, 1.second, useExternalMiner = false, offlineGeneration = false,
+      200, 100000, 100000)
   }
 
 }
--- src/test/scala/org/ergoplatform/utils/Stubs.scala
+++ src/test/scala/org/ergoplatform/utils/Stubs.scala
@@ -187,7 +187,8 @@ trait Stubs extends ErgoGenerators with ErgoTestHelpers with ChainGenerator with
     val miningDelay = 1.second
     val minimalSuffix = 2
     val nodeSettings: NodeConfigurationSettings = NodeConfigurationSettings(stateType, verifyTransactions, blocksToKeep,
-      PoPoWBootstrap, minimalSuffix, mining = false, miningDelay, offlineGeneration = false, 200, 100000, 100000)
+      PoPoWBootstrap, minimalSuffix, mining = false, miningDelay, useExternalMiner = false, offlineGeneration = false,
+      200, 100000, 100000)
     val scorexSettings: ScorexSettings = null
     val testingSettings: TestingSettings = null
     val walletSettings: WalletSettings = null

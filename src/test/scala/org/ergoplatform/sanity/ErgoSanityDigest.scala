4cdddbc41dd2cb2fd9c2f3cba1b115f604cc4f54
oskin1 <ilya.arcadich@gmail.com>
2019-03-28 19:47:41 +0300

    Correct interpreter for digest state.

--- src/test/scala/org/ergoplatform/sanity/ErgoSanityDigest.scala
+++ src/test/scala/org/ergoplatform/sanity/ErgoSanityDigest.scala
@@ -30,11 +30,12 @@ class ErgoSanityDigest extends ErgoSanity[DIGEST_ST] {
   override val stateGen: Gen[WrappedDigestState] = {
     boxesHolderGen.map(WrappedUtxoState(_, createTempDir, None, settings)).map { wus =>
       val digestState = DigestState.create(Some(wus.version), Some(wus.rootHash), createTempDir, stateConstants)
-      new WrappedDigestState(digestState, wus, settings, ErgoInterpreter(LaunchParameters))
+      new WrappedDigestState(digestState, wus, settings)
     }
   }
 
-  override def semanticallyValidModifier(state: DIGEST_ST): PM = validFullBlock(None, state.asInstanceOf[WrappedDigestState].wrappedUtxoState)
+  override def semanticallyValidModifier(state: DIGEST_ST): PM =
+    validFullBlock(None, state.asInstanceOf[WrappedDigestState].wrappedUtxoState)
 
   override def semanticallyInvalidModifier(state: DIGEST_ST): PM = invalidErgoFullBlockGen.sample.get
 

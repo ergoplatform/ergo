e58854dc2b80b2d6d53f70c061cd95cc4ce2603c
oskin1 <ilya.arcadich@gmail.com>
2019-04-01 17:54:28 +0300

    Miner pk from config.

--- src/test/scala/org/ergoplatform/settings/ErgoSettingsSpecification.scala
+++ src/test/scala/org/ergoplatform/settings/ErgoSettingsSpecification.scala
@@ -15,22 +15,23 @@ class ErgoSettingsSpecification extends ErgoPropertyTest {
   property("should read default settings") {
     val settings = ErgoSettings.read(None)
     settings.nodeSettings shouldBe NodeConfigurationSettings(StateType.Utxo, verifyTransactions = true, 1000,
-      poPoWBootstrap = false, 10, mining = false, 1.second, useExternalMiner = false, offlineGeneration = false,
-      200, 100000, 100000, 10.seconds)
+      poPoWBootstrap = false, 10, mining = false, 1.second, useExternalMiner = false, miningPubKeyHex = None,
+      offlineGeneration = false, keepVersions = 200, mempoolCapacity = 100000, blacklistCapacity = 100000,
+      mempoolCleanupDuration = 10.seconds)
   }
 
   property("should read user settings from json file") {
     val settings = ErgoSettings.read(Some("src/test/resources/settings.json"))
     settings.nodeSettings shouldBe NodeConfigurationSettings(StateType.Utxo, verifyTransactions = true, 12,
-      poPoWBootstrap = false, 10, mining = false, 1.second, useExternalMiner = false, offlineGeneration = false,
-      200, 100000, 100000, 10.seconds)
+      poPoWBootstrap = false, 10, mining = false, 1.second, useExternalMiner = false, miningPubKeyHex = None,
+      offlineGeneration = false, 200, 100000, 100000, 10.seconds)
   }
 
   property("should read user settings from HOCON file") {
     val settings = ErgoSettings.read(Some("src/test/resources/settings.conf"))
     settings.nodeSettings shouldBe NodeConfigurationSettings(StateType.Utxo, verifyTransactions = true, 13,
-      poPoWBootstrap = false, 10, mining = false, 1.second, useExternalMiner = false, offlineGeneration = false,
-      200, 100000, 100000, 10.seconds)
+      poPoWBootstrap = false, 10, mining = false, 1.second, useExternalMiner = false, miningPubKeyHex = None,
+      offlineGeneration = false, 200, 100000, 100000, 10.seconds)
   }
 
 }

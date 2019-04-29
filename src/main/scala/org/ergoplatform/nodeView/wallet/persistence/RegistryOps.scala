800791e65ef520aefa80542c6fbbc2a83ca916e2
Ilya Oskin <ilya.arcadich@gmail.com>
2019-04-29 20:33:06 +0300

    Compilation fixed.

--- src/main/scala/org/ergoplatform/nodeView/wallet/persistence/RegistryOps.scala
+++ src/main/scala/org/ergoplatform/nodeView/wallet/persistence/RegistryOps.scala
@@ -5,17 +5,19 @@ import org.ergoplatform.nodeView.wallet.persistence.RegistryOpA.RegistryOp
 
 object RegistryOps {
 
+  import scala.language.implicitConversions
+
   implicit def transact[A](ma: RegistryOp[A])
                           (version: Array[Byte], registry: WalletRegistry): A = {
     ma.foldMap(registry.interpreter).run((Seq.empty, Seq.empty)).value match {
-      case ((toInsert, toRemove), out: A) if toInsert.nonEmpty || toRemove.nonEmpty =>
+      case ((toInsert, toRemove), out: A @unchecked) if toInsert.nonEmpty || toRemove.nonEmpty =>
         registry.store.update(
           ByteArrayWrapper(version),
           toRemove.map(ByteArrayWrapper.apply),
           toInsert.map(x => ByteArrayWrapper(x._1) -> ByteArrayWrapper(x._2))
         )
         out
-      case (_, out: A) =>
+      case (_, out: A @unchecked) =>
         out
     }
   }

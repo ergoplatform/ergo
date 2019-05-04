de03b9e0218d732ca7eb0e0ea46694a51c85f6f5
oskin1 <ilya.arcadich@gmail.com>
2019-05-04 14:14:44 +0300

    Wallet storage and off-chain registry specs.

---
+++ src/test/scala/org/ergoplatform/nodeView/wallet/persistence/OffChainRegistrySpec.scala
@@ -0,0 +1,29 @@
+package org.ergoplatform.nodeView.wallet.persistence
+
+import org.ergoplatform.utils.WalletTestOps
+import org.ergoplatform.utils.generators.WalletGenerators
+import org.scalacheck.Gen
+import org.scalatest.prop.GeneratorDrivenPropertyChecks
+import org.scalatest.{FlatSpec, Matchers}
+
+class OffChainRegistrySpec
+  extends FlatSpec
+    with Matchers
+    with GeneratorDrivenPropertyChecks
+    with WalletGenerators
+    with WalletTestOps {
+
+  it should "calculate indexes correctly" in {
+    forAll(Gen.listOf(trackedBoxGen)) { boxes =>
+      var registry = OffChainRegistry.empty
+      registry = registry.updated(boxes, Seq.empty)
+      val balance = balanceAmount(boxes.map(_.box))
+      val assetsBalance = assetAmount(boxes.map(_.box))
+      registry.readIndex.balance shouldEqual balance
+      registry.readIndex.assetBalances shouldEqual assetsBalance
+    }
+  }
+
+  it should ""
+
+}

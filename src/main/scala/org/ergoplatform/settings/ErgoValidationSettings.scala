5a56b813053a3d4033e88cb076696e2391955648
catena <catena@protonmail.com>
2019-05-14 12:22:25 +0300

    Extract RuleStatus case class

--- src/main/scala/org/ergoplatform/settings/ErgoValidationSettings.scala
+++ src/main/scala/org/ergoplatform/settings/ErgoValidationSettings.scala
@@ -4,13 +4,23 @@ import org.ergoplatform.api.ApiCodecs
 import org.ergoplatform.modifiers.history.Extension
 import org.ergoplatform.nodeView.history.ErgoHistory.Height
 import scorex.core.serialization.ScorexSerializer
-import scorex.core.validation.MapValidationSettings
-import scorex.core.validation.ValidationResult.Invalid
+import scorex.core.validation.{ModifierValidator, ValidationResult, ValidationSettings}
 import scorex.util.serialization.{Reader, Writer}
 
 import scala.util.Try
 
-class ErgoValidationSettings(map: Map[Short, (String => Invalid, Boolean)]) extends MapValidationSettings(true, map) {
+class ErgoValidationSettings(map: Map[Short, RuleStatus]) extends ValidationSettings {
+
+
+  override val isFailFast: Boolean = true
+
+  override def getError(id: Short, details: String): ValidationResult.Invalid = {
+    map.get(id).map(_.error(details)).getOrElse(ModifierValidator.fatal("Unknown message"))
+  }
+
+  override def isActive(id: Short): Boolean = {
+    map.get(id).forall(_.isActive)
+  }
 
   def update(height: Height, forkVote: Boolean, epochVotes: Seq[(Byte, Int)], votingSettings: VotingSettings): ErgoValidationSettings = {
     //TODO

6845abbf69ff10ac12c80d9fd234209db65c9058
catena <catena@protonmail.com>
2019-05-22 18:59:45 +0300

    ErgoStateContextSpec

--- src/test/scala/org/ergoplatform/nodeView/state/ErgoStateContextSpec.scala
+++ src/test/scala/org/ergoplatform/nodeView/state/ErgoStateContextSpec.scala
@@ -1,17 +1,13 @@
 package org.ergoplatform.nodeView.state
 
 import org.ergoplatform.modifiers.ErgoFullBlock
-import org.ergoplatform.settings.LaunchParameters
-import org.ergoplatform.utils.{ErgoPropertyTest, HistoryTestHelpers}
-import scorex.crypto.authds.ADDigest
+import org.ergoplatform.modifiers.history.{Extension, PoPowAlgos}
+import org.ergoplatform.settings.Parameters._
+import org.ergoplatform.utils.HistoryTestHelpers
 
-class ErgoStateContextSpec extends ErgoPropertyTest with HistoryTestHelpers {
+class ErgoStateContextSpec extends HistoryTestHelpers {
 
   property("Header votes") {
-    import org.ergoplatform.settings.Parameters._
-    val vr: VotingData = VotingData.empty
-    val esc = new ErgoStateContext(Seq(), None, ADDigest @@ Array.fill(33)(0: Byte), LaunchParameters, validationSettingsNoIl, vr)
-
     val fb = genChain(1).head
     val header = fb.header
 
@@ -20,27 +16,62 @@ class ErgoStateContextSpec extends ErgoPropertyTest with HistoryTestHelpers {
       fb.copy(header = newHeader)
     }
 
-
     //double vote
     val wrongVotes1 = Array(StorageFeeFactorIncrease, StorageFeeFactorIncrease, NoParameter)
-    esc.appendFullBlock(fbWithVotes(wrongVotes1), votingSettings) shouldBe 'failure
-
+    emptyStateContext.appendFullBlock(fbWithVotes(wrongVotes1), votingSettings) shouldBe 'failure
 
     //contradictory votes
     val wrongVotes2 = Array(StorageFeeFactorIncrease, StorageFeeFactorDecrease, NoParameter)
-    esc.appendFullBlock(fbWithVotes(wrongVotes2), votingSettings) shouldBe 'failure
+    emptyStateContext.appendFullBlock(fbWithVotes(wrongVotes2), votingSettings) shouldBe 'failure
 
     //too many votes - only two ordinary changes allowed per epoch
     val wrongVotes3 = Array(StorageFeeFactorIncrease, MaxBlockCostIncrease, MaxBlockSizeDecrease)
-    esc.appendFullBlock(fbWithVotes(wrongVotes3), votingSettings) shouldBe 'failure
+    emptyStateContext.appendFullBlock(fbWithVotes(wrongVotes3), votingSettings) shouldBe 'failure
 
     //a vote proposed on non-existing parameter
     val wrongVotes4 = Array((-50).toByte, NoParameter, MaxBlockSizeDecrease)
-    esc.appendFullBlock(fbWithVotes(wrongVotes4, 2), votingSettings) shouldBe 'failure
+    emptyStateContext.appendFullBlock(fbWithVotes(wrongVotes4, 2), votingSettings) shouldBe 'failure
 
     //correct votes
     val correctVotes = Array(StorageFeeFactorIncrease, MaxBlockSizeDecrease, NoParameter)
-    esc.appendFullBlock(fbWithVotes(correctVotes), votingSettings) shouldBe 'success
+    emptyStateContext.appendFullBlock(fbWithVotes(correctVotes), votingSettings) shouldBe 'success
+
+  }
+
+  property("Extension validation") {
+    val chain = genChain(2)
+    val sc = emptyStateContext.appendFullBlock(chain.head, votingSettings).get
+    val fb = chain.last
+    val extension = fb.extension
+    val oldFields = extension.fields
+
+    def fbWithFields(newFields: Seq[(Array[Byte], Array[Byte])]): ErgoFullBlock = {
+      val newExtension = extension.copy(fields = newFields)
+      fb.copy(extension = newExtension)
+    }
+
+    // checks, specific for extension
+    // validation of field keys size
+    val imvKey = extensionKvGen(Extension.FieldKeySize - 1, Extension.FieldValueMaxSize).sample.get
+    sc.appendFullBlock(fbWithFields(imvKey +: oldFields), votingSettings) shouldBe 'failure
+
+    // validation of field value sizes
+    val imvValue = extensionKvGen(Extension.FieldKeySize, Extension.FieldValueMaxSize + 1).sample.get
+    sc.appendFullBlock(fbWithFields(imvValue +: oldFields), votingSettings) shouldBe 'failure
+
+    // validation of incorrect interlinks
+    val invalidInterlinks = PoPowAlgos.packInterlinks(
+      PoPowAlgos.unpackInterlinks(fb.extension.fields).get ++ Seq(fb.header.id)
+    )
+    sc.appendFullBlock(fbWithFields(invalidInterlinks ++ oldFields), votingSettings) shouldBe 'failure
+
+    // validation of key duplicates in fields
+    val validMKV = extensionKvGen(Extension.FieldKeySize, Extension.FieldValueMaxSize).sample.get
+    sc.appendFullBlock(fbWithFields(Seq(validMKV, validMKV) ++ oldFields), votingSettings) shouldBe 'failure
+
+    // valid application of correct extension
+    sc.appendFullBlock(fbWithFields(validMKV +: oldFields), votingSettings) shouldBe 'success
+
   }
 
 

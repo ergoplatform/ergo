65836bfd02c1d43ed50fe772b9c96db62e8adf73
catena <catena@protonmail.com>
2019-05-23 18:54:18 +0300

    Vote for max extension size

--- src/main/scala/org/ergoplatform/settings/LaunchParameters.scala
+++ src/main/scala/org/ergoplatform/settings/LaunchParameters.scala
@@ -5,14 +5,15 @@ import org.ergoplatform.settings.Parameters._
 object LaunchParameters extends Parameters(
   height = 0,
   parametersTable = Map(
-  StorageFeeFactorIncrease -> Parameters.StorageFeeFactorDefault,
-  MinValuePerByteIncrease -> Parameters.MinValuePerByteDefault,
-  TokenAccessCostIncrease -> Parameters.TokenAccessCostDefault,
-  InputCostIncrease -> Parameters.InputCostDefault,
-  DataInputCostIncrease -> Parameters.DataInputCostDefault,
-  OutputCostIncrease -> Parameters.OutputCostDefault,
-  MaxBlockSizeIncrease -> 512 * 1024,
-  MaxBlockCostIncrease -> 1000000,
-  BlockVersion -> 1),
+    StorageFeeFactorIncrease -> Parameters.StorageFeeFactorDefault,
+    MinValuePerByteIncrease -> Parameters.MinValuePerByteDefault,
+    TokenAccessCostIncrease -> Parameters.TokenAccessCostDefault,
+    InputCostIncrease -> Parameters.InputCostDefault,
+    DataInputCostIncrease -> Parameters.DataInputCostDefault,
+    OutputCostIncrease -> Parameters.OutputCostDefault,
+    MaxExtensionSizeIncrease -> Parameters.MaxExtensionSizeDefault,
+    MaxBlockSizeIncrease -> Parameters.MaxBlockSizeDefault,
+    MaxBlockCostIncrease -> Parameters.MaxBlockCostDefault,
+    BlockVersion -> 1),
   Seq()
 )

8f20eac140ce11d6ce226a0881a26c029e101478
oskin1 <ilya.arcadich@gmail.com>
2019-06-24 18:43:09 +0300

    Unspent boxes wallet API route. Close #804.

---
+++ src/main/scala/org/ergoplatform/nodeView/wallet/WalletBox.scala
@@ -0,0 +1,28 @@
+package org.ergoplatform.nodeView.wallet
+
+import io.circe.syntax._
+import io.circe.{Encoder, Json}
+import org.ergoplatform.ErgoAddressEncoder
+import org.ergoplatform.api.ApiCodecs
+import org.ergoplatform.api.ApiEncoderOption.{Detalization, ShowDetails}
+import org.ergoplatform.wallet.boxes.TrackedBox
+
+case class WalletBox(trackedBox: TrackedBox, confirmationsNumOpt: Option[Int])
+
+object WalletBox extends ApiCodecs {
+
+  implicit val detalization: Detalization = ShowDetails
+
+  implicit def decoder(implicit ae: ErgoAddressEncoder): Encoder[WalletBox] = { obj =>
+    obj.trackedBox.asJson.deepMerge(
+      Json.obj(
+        "confirmationsNum" -> obj.confirmationsNumOpt.asJson,
+        "address" -> ae.fromProposition(obj.trackedBox.box.ergoTree)
+          .toOption
+          .map(_.toString)
+          .asJson
+      )
+    )
+  }
+
+}

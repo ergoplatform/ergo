dce77d2be1bd3928d75e421c6c13ba28c0bd22e9
oskin1 <ilya.arcadich@gmail.com>
2019-06-24 18:43:09 +0300

    Unspent boxes wallet API route. Close #804.

--- src/test/scala/org/ergoplatform/api/routes/WalletApiRouteSpec.scala
+++ src/test/scala/org/ergoplatform/api/routes/WalletApiRouteSpec.scala
@@ -184,4 +184,22 @@ class WalletApiRouteSpec extends FlatSpec
     }
   }
 
+  it should "return wallet boxes" in {
+    val minConfirmations = 15
+    val minInclusionHeight = 20
+    val postfix = s"/boxes/unspent?minConfirmations=$minConfirmations&minInclusionHeight=$minInclusionHeight"
+    Get(prefix + postfix) ~> route ~> check {
+      status shouldBe StatusCodes.OK
+      val response = responseAs[List[Json]]
+      response.size shouldBe 1
+      response.head.hcursor.downField("confirmationsNum").as[Int].forall(_ >= minConfirmations) shouldBe true
+      response.head.hcursor.downField("inclusionHeight").as[Int].forall(_ >= minInclusionHeight) shouldBe true
+    }
+    Get(prefix + "/boxes/unspent") ~> route ~> check {
+      status shouldBe StatusCodes.OK
+      val response = responseAs[List[Json]]
+      response.size shouldBe 2
+    }
+  }
+
 }

d3fc376541aee739b0c0219f3579eaa7fb3027bf
oskin1 <ilya.arcadich@gmail.com>
2019-07-01 13:15:40 +0300

    Correct init diff.

--- src/main/scala/org/ergoplatform/local/ErgoMiner.scala
+++ src/main/scala/org/ergoplatform/local/ErgoMiner.scala
@@ -287,7 +287,7 @@ class ErgoMiner(ergoSettings: ErgoSettings,
     val nBits: Long = bestHeaderOpt
       .map(parent => history.requiredDifficultyAfter(parent))
       .map(d => RequiredDifficulty.encodeCompactBits(d))
-      .getOrElse(ergoSettings.chainSettings.initialNBits)
+      .getOrElse(RequiredDifficulty.encodeCompactBits(RequiredDifficulty.decodeCompactBits(ergoSettings.chainSettings.initialNBits)))
     val interlinksExtension = PoPowAlgos.interlinksToExtension(updateInterlinks(bestHeaderOpt, bestExtensionOpt))
 
     val (extensionCandidate, votes: Array[Byte], version: Byte) = bestHeaderOpt.map { header =>

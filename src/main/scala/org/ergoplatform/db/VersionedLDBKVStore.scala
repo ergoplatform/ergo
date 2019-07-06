bf28fb632d17cc0bfdc80dc733d303503c4d7082
oskin1 <ilya.arcadich@gmail.com>
2019-07-06 15:36:54 +0300

    Minor improvements.

--- src/main/scala/org/ergoplatform/db/VersionedLDBKVStore.scala
+++ src/main/scala/org/ergoplatform/db/VersionedLDBKVStore.scala
@@ -11,15 +11,15 @@ import scala.util.{Failure, Success, Try}
   */
 final class VersionedLDBKVStore(protected val db: DB) extends KVStore {
 
+  import VersionedLDBKVStore.VersionId
+
   private val versionsKey = ByteString("versions")
 
-  def update(toInsert: Seq[(ByteString, ByteString)], toRemove: Seq[ByteString])
-            (version: ByteString): Unit = {
+  def update(toInsert: Seq[(K, V)], toRemove: Seq[K])(version: VersionId): Unit = {
     require(version.size == Constants.HashLength, "Illegal version id size")
     val ro = new ReadOptions()
     ro.snapshot(db.getSnapshot)
-    val (insertedKeys, altered) = toInsert.foldLeft(
-      Seq.empty[ByteString], Seq.empty[(ByteString, (ByteString, ByteString))]) {
+    val (insertedKeys, altered) = toInsert.foldLeft(Seq.empty[K], Seq.empty[(K, (V, V))]) {
       case ((insertedAcc, alteredAcc), (k, v)) =>
         Option(db.get(k.toArray, ro))
           .map { oldValue =>
@@ -49,7 +49,7 @@ final class VersionedLDBKVStore(protected val db: DB) extends KVStore {
     }
   }
 
-  def rollbackTo(version: ByteString): Try[Unit] = {
+  def rollbackTo(versionId: VersionId): Try[Unit] = {
     val ro = new ReadOptions()
     ro.snapshot(db.getSnapshot)
     Option(db.get(versionsKey.toArray)) match {
@@ -57,7 +57,7 @@ final class VersionedLDBKVStore(protected val db: DB) extends KVStore {
         val batch = db.createWriteBatch()
         try {
           bytes.grouped(Constants.HashLength)
-            .takeWhile(ByteString(_) != version) // select all versions above targeted one
+            .takeWhile(ByteString(_) != versionId) // select all versions above targeted one
             .foldLeft(Seq.empty[(Array[Byte], ChangeSet)]) { case (acc, verId) =>
               val changeSetOpt = Option(db.get(verId, ro)).flatMap { changeSetBytes =>
                 ChangeSetSerializer.parseBytesTry(changeSetBytes).toOption
@@ -79,7 +79,7 @@ final class VersionedLDBKVStore(protected val db: DB) extends KVStore {
           val updatedVersions = bytes
             .grouped(Constants.HashLength)
             .map(ByteString.apply)
-            .dropWhile(_ != version)
+            .dropWhile(_ != versionId)
             .reduce(_ ++ _)
 
           batch.put(versionsKey.toArray, updatedVersions.toArray)
@@ -91,8 +91,12 @@ final class VersionedLDBKVStore(protected val db: DB) extends KVStore {
           ro.snapshot().close()
         }
       case None =>
-        Failure(new Exception(s"Version ${Algos.encode(version.toArray)} not found"))
+        Failure(new Exception(s"Version ${Algos.encode(versionId.toArray)} not found"))
     }
   }
 
+}
+
+object VersionedLDBKVStore {
+  type VersionId = ByteString
 }
--- src/test/scala/org/ergoplatform/db/LDBKVStoreSpec.scala
+++ src/test/scala/org/ergoplatform/db/LDBKVStoreSpec.scala
@@ -1,20 +1,29 @@
 package org.ergoplatform.db
 
+import akka.util.ByteString
+import org.iq80.leveldb.DB
 import org.iq80.leveldb.impl.Iq80DBFactory._
 
 class LDBKVStoreSpec extends DBSpec {
 
-  property("LDB value rewrite") {
-    withDb { db =>
-      val key = bytes("x")
-      val batch = db.createWriteBatch()
-      batch.put(key, bytes("1"))
-      batch.delete(key)
-      batch.put(key, bytes("2"))
-      db.write(batch)
+  property("put/get/getAll/delete") {
+    withStore { store =>
+      val valueA = (byteString("A"), byteString("1"))
+      val valueB = (byteString("B"), byteString("2"))
 
-      asString(db.get(key)) shouldBe "2"
+      store.update(toInsert = Seq(valueA, valueB), toRemove = Seq.empty)
+
+      store.get(valueA._1) shouldBe Some(valueA._2)
+      store.get(valueB._1) shouldBe Some(valueB._2)
+
+      store.getAll shouldBe Seq(valueA, valueB)
+
+      store.update(toInsert = Seq.empty, toRemove = Seq(valueA._1))
+      store.get(valueA._1) shouldBe None
     }
   }
+
+  private def withStore(body: LDBKVStore => Unit): Unit =
+    withDb { db: DB => body(new LDBKVStore(db)) }
 
 }

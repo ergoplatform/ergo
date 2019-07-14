07600872f2b0c0196374758bb082ad71eb6fd122
oskin1 <ilya.arcadich@gmail.com>
2019-07-15 01:40:34 +0300

    Legacy DB equality spec added.

--- src/main/scala/org/ergoplatform/db/KVStore.scala
+++ src/main/scala/org/ergoplatform/db/KVStore.scala
@@ -39,7 +39,7 @@ trait KVStore extends AutoCloseable {
   def getOrElse(key: K, default: => V): V =
     get(key).getOrElse(default)
 
-  def get(keys: Iterable[K]): Iterable[(K, Option[V])] = {
+  def get(keys: Seq[K]): Seq[(K, Option[V])] = {
     val bf = mutable.ArrayBuffer.empty[(K, Option[V])]
     keys.foreach(k => bf += (k -> get(k)))
     bf

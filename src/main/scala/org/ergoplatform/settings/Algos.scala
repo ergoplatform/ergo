738d4a2900a32870bed2b69fc5929e178445ccbe
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2019-08-01 17:20:52 +0300

    use sigma's ErgoAlgos; use sigma's json codecs and remove it's own;

--- src/main/scala/org/ergoplatform/settings/Algos.scala
+++ src/main/scala/org/ergoplatform/settings/Algos.scala
@@ -1,20 +1,15 @@
 package org.ergoplatform.settings
 
 import io.iohk.iodb.ByteArrayWrapper
-import scorex.core.utils.ScorexEncoding
 import scorex.core.{VersionTag, versionToBytes}
 import scorex.crypto.authds.LeafData
 import scorex.crypto.authds.merkle.MerkleTree
-import scorex.crypto.hash.{Blake2b256, Digest32}
+import scorex.crypto.hash.Digest32
 import scorex.util._
 
-import scala.util.Try
+object Algos extends ErgoAlgos with scorex.core.utils.ScorexEncoding {
 
-object Algos extends ScorexEncoding {
-
-  type HF = Blake2b256.type
-
-  val hash: HF = Blake2b256
+  override implicit val encoder: scorex.core.utils.ScorexEncoder = scorex.core.utils.ScorexEncoder.default
 
   lazy val emptyMerkleTreeRoot: Digest32 = Algos.hash(LeafData @@ Array[Byte]())
 
@@ -23,12 +18,6 @@ object Algos extends ScorexEncoding {
   @inline def idToBAW(id: ModifierId): ByteArrayWrapper = ByteArrayWrapper(idToBytes(id))
 
   @inline def encode(id: ModifierId): String = encoder.encode(id)
-
-  @inline def encode(bytes: Array[Byte]): String = encoder.encode(bytes)
-
-  @inline def decode(str: String): Try[Array[Byte]] = encoder.decode(str)
-
-  @inline def decodeUnsafe(str: String): Array[Byte] = decode(str).get
 
   def blockIdDifficulty(id: Array[Byte]): BigInt = {
     val blockTarget = BigInt(1, id).ensuring(_ <= Constants.MaxTarget)

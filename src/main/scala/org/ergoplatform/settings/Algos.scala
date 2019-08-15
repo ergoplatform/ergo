c8b31045230a16680fbe23ec701d6b0ee4f74e74
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2019-08-15 12:41:49 +0300

    update sigma; add comments;

--- src/main/scala/org/ergoplatform/settings/Algos.scala
+++ src/main/scala/org/ergoplatform/settings/Algos.scala
@@ -11,6 +11,8 @@ object Algos extends ErgoAlgos with scorex.core.utils.ScorexEncoding {
 
   // ErgoAlgos in sigmastate extends scorex.util.ScorexEncoding where encoder is BytesEncoder
   // but here we use scorex.core.utils.ScorexEncoding where encoder is ScorexEncoder
+  // After ScorexEncoder is moved (there is even a todo for that) from scorex.core to scorex.util
+  //  we can fix this ugliness.
   override implicit val encoder: scorex.core.utils.ScorexEncoder = scorex.core.utils.ScorexEncoder.default
 
   lazy val emptyMerkleTreeRoot: Digest32 = Algos.hash(LeafData @@ Array[Byte]())

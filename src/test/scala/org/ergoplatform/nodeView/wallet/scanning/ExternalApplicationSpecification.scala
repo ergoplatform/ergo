1522dec6d07c136788d6bf3de6dfd5b26076e9d4
Alex Chepurnoy <alex.chepurnoy@iohk.io>
2019-09-04 11:55:49 +0300

    ExternalApplicationSpecification fix

--- src/test/scala/org/ergoplatform/nodeView/wallet/scanning/ExternalApplicationSpecification.scala
+++ src/test/scala/org/ergoplatform/nodeView/wallet/scanning/ExternalApplicationSpecification.scala
@@ -1,6 +1,5 @@
 package org.ergoplatform.nodeView.wallet.scanning
 
-import io.circe.parser._
 import io.circe.Json
 import org.ergoplatform.utils.ErgoPropertyTest
 import org.ergoplatform.utils.generators.ErgoTransactionGenerators
@@ -19,33 +18,6 @@ class ExternalApplicationSpecification extends ErgoPropertyTest with ErgoTransac
     val req = ExternalApplication(2, "Example application", complexAnd)
     val j: Json = appEncoder.apply(req)
     appDecoder.decodeJson(j).toTry.get == req
-  }
-
-  property("external app json serialization - wrong id") {
-    val j: Json = parse("""{
-                          |  "appId" : -2,
-                          |  "appName" : "Example application",
-                          |  "trackingRule" : {
-                          |    "predicate" : "and",
-                          |    "args" : [
-                          |      {
-                          |        "predicate" : "contains",
-                          |        "register" : "R1",
-                          |        "bytes" : "0101010101010101010101010101010101010101010101010101010101010101"
-                          |      },
-                          |      {
-                          |        "predicate" : "equals",
-                          |        "register" : "R4",
-                          |        "bytes" : "0101010101010101010101010101010101010101010101010101010101010101"
-                          |      },
-                          |      {
-                          |        "predicate" : "containsAsset",
-                          |        "assetId" : "0101010101010101010101010101010101010101010101010101010101010101"
-                          |      }
-                          |    ]
-                          |  }
-                          |}""".stripMargin).toTry.get
-    appDecoder.decodeJson(j).toTry.isFailure shouldBe true
   }
 
 }

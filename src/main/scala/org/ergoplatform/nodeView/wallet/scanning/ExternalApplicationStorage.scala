5c64d7de707563b149f1b991abfe3b55e5df59cd
Alex Chepurnoy <alex.chepurnoy@iohk.io>
2019-09-05 19:00:57 +0300

    ApplicationApiRoute

---
+++ src/main/scala/org/ergoplatform/nodeView/wallet/scanning/ExternalApplicationStorage.scala
@@ -0,0 +1,21 @@
+package org.ergoplatform.nodeView.wallet.scanning
+
+import com.google.common.primitives.Longs
+import org.ergoplatform.db.LDBKVStore
+import org.ergoplatform.settings.Constants
+
+import scala.util.Try
+
+class ExternalApplicationStorage(store: LDBKVStore) {
+  def addApplication(appReq: ExternalAppRequest) = {
+    val id = lastUsedId + 1
+    appReq.toApp(id).flatMap {app =>
+      Try(store.insert(Seq(Longs.toByteArray(id) -> ExternalApplicationSerializer.toBytes(app))))
+        .map(_ => app)
+    }
+  }
+
+  def allApplications = store.getAll.map{case (k, v) => ExternalApplicationSerializer.parseBytes(v)}
+
+  def lastUsedId: Long = Try(Longs.fromByteArray(store.lastKey())).getOrElse(Constants.DefaultAppId)
+}

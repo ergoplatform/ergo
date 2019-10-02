55d3be80c2b70b233874c8166a9a7d378e2552fc
Konstantin Knizhnik <knizhnik@garret.ru>
2019-10-02 16:54:51 +0300

    Add comment for ExecuteRequest

--- src/main/scala/org/ergoplatform/api/ScriptApiRoute.scala
+++ src/main/scala/org/ergoplatform/api/ScriptApiRoute.scala
@@ -98,6 +98,12 @@ case class ScriptApiRoute(readersHolder: ActorRef, ergoSettings: ErgoSettings)
   }
 
 
+  /**
+   * Represent a request for execution of a script in a given context.
+   * @param script  ErgoScript source code of the contract to execute
+   * @param env      environment map of named constants used to compile the script
+   * @param cox      script execution context
+   */
   case class ExecuteRequest(script: String,
                             env: Map[String,Any],
                             ctx: ErgoLikeContext)

50835dc9cb2912dacbfab52d593eb27afe285b46
Alex Chepurnoy <alex.chepurnoy@iohk.io>
2019-10-10 16:50:45 +0300

    styling issues in ExternalApplicationStorage

--- src/main/scala/org/ergoplatform/nodeView/wallet/scanning/ExternalApplicationStorage.scala
+++ src/main/scala/org/ergoplatform/nodeView/wallet/scanning/ExternalApplicationStorage.scala
@@ -3,22 +3,24 @@ package org.ergoplatform.nodeView.wallet.scanning
 import com.google.common.primitives.Longs
 import org.ergoplatform.db.LDBKVStore
 import org.ergoplatform.settings.Constants
-import scorex.util.encode.Base16
 
 import scala.util.Try
 
 class ExternalApplicationStorage(store: LDBKVStore) {
-  def addApplication(appReq: ExternalAppRequest) = {
+
+  def addApplication(appReq: ExternalAppRequest): Try[ExternalApplication] = {
     val id = lastUsedId + 1
-    appReq.toApp(id).flatMap {app =>
+    appReq.toApp(id).flatMap { app =>
       Try(store.insert(Seq(Longs.toByteArray(id) -> ExternalApplicationSerializer.toBytes(app))))
         .map(_ => app)
     }
   }
 
-  def removeApplication(id: Long) = store.remove(Seq(Longs.toByteArray(id)))
+  def removeApplication(id: Long): Unit = store.remove(Seq(Longs.toByteArray(id)))
 
-  def allApplications = store.getAll.map{case (k, v) => ExternalApplicationSerializer.parseBytes(v)}
+  def allApplications: Seq[ExternalApplication] =
+    store.getAll.map { case (_, v) => ExternalApplicationSerializer.parseBytes(v) }
 
   def lastUsedId: Long = Try(Longs.fromByteArray(store.lastKey())).getOrElse(Constants.DefaultAppId)
+
 }

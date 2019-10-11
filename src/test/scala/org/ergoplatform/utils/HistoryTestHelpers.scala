bb20e5fa105c31ead825af05e3737d1eac2a0720
Alex Chepurnoy <alex.chepurnoy@iohk.io>
2019-10-11 12:56:16 +0300

    reBroadcastVolume -> rebroadcastCount

--- src/test/scala/org/ergoplatform/utils/HistoryTestHelpers.scala
+++ src/test/scala/org/ergoplatform/utils/HistoryTestHelpers.scala
@@ -45,7 +45,7 @@ trait HistoryTestHelpers extends ErgoPropertyTest {
     val minimalSuffix = 2
     val nodeSettings: NodeConfigurationSettings = NodeConfigurationSettings(stateType, verifyTransactions, blocksToKeep,
       PoPoWBootstrap, minimalSuffix, mining = false, Constants.DefaultComplexityLimit, miningDelay, useExternalMiner = false, miningPubKeyHex = None,
-      offlineGeneration = false, 200, 100000, 100000, 1.minute, reBroadcastVolume = 200, 1000000)
+      offlineGeneration = false, 200, 100000, 100000, 1.minute, rebroadcastCount = 200, 1000000)
     val scorexSettings: ScorexSettings = null
     val testingSettings: TestingSettings = null
     val walletSettings: WalletSettings = null

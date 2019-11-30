a982f5ce7b70f71357cc4ac847e12be3847d788f
Alex Chepurnoy <alex.chepurnoy@iohk.io>
2019-11-30 15:26:08 +0300

    arbitrary predicate check in ScanningPredicateJsonCodecsSpecification

--- src/test/scala/org/ergoplatform/nodeView/wallet/scanning/ScanningPredicateJsonCodecsSpecification.scala
+++ src/test/scala/org/ergoplatform/nodeView/wallet/scanning/ScanningPredicateJsonCodecsSpecification.scala
@@ -3,22 +3,41 @@ package org.ergoplatform.nodeView.wallet.scanning
 import io.circe.parser._
 import org.ergoplatform.ErgoBox
 import org.ergoplatform.utils.ErgoPropertyTest
-import org.ergoplatform.utils.generators.ErgoTransactionGenerators
+import org.ergoplatform.utils.generators.WalletGenerators
 import scorex.crypto.hash.Digest32
 import scorex.util.encode.Base16
 
-class ScanningPredicateJsonCodecsSpecification extends ErgoPropertyTest with ErgoTransactionGenerators {
+class ScanningPredicateJsonCodecsSpecification extends ErgoPropertyTest with WalletGenerators {
 
   import ScanningPredicateJsonCodecs.{scanningPredicateEncoder, scanningPredicateDecoder}
 
+  private val complexOr = OrScanningPredicate(
+    ContainsScanningPredicate(ErgoBox.R1, Array.fill(32)(1: Byte)),
+    EqualsScanningPredicate(ErgoBox.R4, Array.fill(32)(0: Byte)),
+    ContainsAssetPredicate(Digest32 @@ Array.fill(32)(0: Byte))
+  )
+
+  private val complexAnd = AndScanningPredicate(
+    ContainsScanningPredicate(ErgoBox.R1, Array.fill(32)(1: Byte)),
+    EqualsScanningPredicate(ErgoBox.R4, Array.fill(32)(1: Byte)),
+    ContainsAssetPredicate(Digest32 @@ Array.fill(32)(1: Byte))
+  )
+
+  property("json roundtrip for generated predicate") {
+    forAll(scanningPredicateGen){p =>
+      val j = scanningPredicateEncoder(p)
+      scanningPredicateDecoder.decodeJson(j).toTry.get == p
+    }
+  }
+
   property("complex or roundtrip") {
-    val p = ScanningPredicateTestVectors.complexOr
+    val p = complexOr
     val j = scanningPredicateEncoder(p)
     scanningPredicateDecoder.decodeJson(j).toTry.get == p
   }
 
   property("complex and roundtrip") {
-    val p = ScanningPredicateTestVectors.complexAnd
+    val p = complexAnd
     val j = scanningPredicateEncoder(p)
     scanningPredicateDecoder.decodeJson(j).toTry.get == p
   }
@@ -37,7 +56,7 @@ class ScanningPredicateJsonCodecsSpecification extends ErgoPropertyTest with Erg
     )
   }
 
-  property("example from EIP-1 w.explicit register"){
+  property("example from EIP-1 w. explicit register"){
     val j = parse(
       """{"predicate": "and", "args":[{"predicate": "contains", "register": "R4",
         |"bytes": "02dada811a888cd0dc7a0a41739a3ad9b0f427741fe6ca19700cf1a51200c96bf7"},

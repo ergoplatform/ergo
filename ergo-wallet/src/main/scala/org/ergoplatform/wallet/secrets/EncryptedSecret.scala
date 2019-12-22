ce3375526efc7cd1f13751fdccb943b9e2eb02db
Alexander Slesarenko <avslesarenko@gmail.com>
2019-12-22 14:22:16 +0300

    Json encoders for EncryptedSecret and EncryptionSettings

--- ergo-wallet/src/main/scala/org/ergoplatform/wallet/secrets/EncryptedSecret.scala
+++ ergo-wallet/src/main/scala/org/ergoplatform/wallet/secrets/EncryptedSecret.scala
@@ -1,5 +1,8 @@
 package org.ergoplatform.wallet.secrets
 
+import io.circe.parser._
+import io.circe.syntax._
+import io.circe.{Encoder, Decoder, HCursor, Json}
 import org.ergoplatform.wallet.settings.EncryptionSettings
 import scorex.util.encode.Base16
 
@@ -23,5 +26,41 @@ object EncryptedSecret {
       Base16.encode(iv),
       Base16.encode(authTag), cipherParams)
   }
+
+  implicit object EncryptedSecretEncoder extends Encoder[EncryptedSecret] {
+
+    def apply(secret: EncryptedSecret): Json = {
+      Json.obj(
+        "cipherText" -> secret.cipherText.asJson,
+        "salt" -> secret.salt.asJson,
+        "iv" -> secret.iv.asJson,
+        "authTag" -> secret.authTag.asJson,
+        "cipherParams" -> secret.cipherParams.asJson
+      )
+    }
+
+  }
+
+  implicit object EncryptedSecretDecoder extends Decoder[EncryptedSecret] {
+
+    def apply(cursor: HCursor): Decoder.Result[EncryptedSecret] = {
+      for {
+        cipherText <- cursor.downField("cipherText").as[String]
+        salt <- cursor.downField("salt").as[String]
+        iv <- cursor.downField("iv").as[String]
+        authTag <- cursor.downField("authTag").as[String]
+        cipherParams <- cursor.downField("cipherParams").as[EncryptionSettings]
+      } yield EncryptedSecret(cipherText, salt, iv, authTag, cipherParams)
+    }
+
+  }
+
+//  def main(args: Array[String]): Unit = {
+//    val res = decode[EncryptedSecret](
+//      """{ "cipherText": "", "salt": "", "iv": "", "authTag": "", "cipherParams": { "prf":"abc", "c": 10, "dkLen": 100 }}""")
+//    val jsonRaw = res.right.get.asJson.noSpaces
+//    println(res.toString)
+//    println(jsonRaw)
+//  }
 
 }

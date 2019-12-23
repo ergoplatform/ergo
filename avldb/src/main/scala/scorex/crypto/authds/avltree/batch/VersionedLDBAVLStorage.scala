e20e66ef68722ea1b4c491d42ed3d8e8af669088
Alex Chepurnoy <alex.chepurnoy@iohk.io>
2019-12-23 15:16:16 +0300

    only one versioned db left

--- avldb/src/main/scala/scorex/crypto/authds/avltree/batch/VersionedLDBAVLStorage.scala
+++ avldb/src/main/scala/scorex/crypto/authds/avltree/batch/VersionedLDBAVLStorage.scala
@@ -1,35 +1,32 @@
 package scorex.crypto.authds.avltree.batch
 
 import com.google.common.primitives.Ints
-import io.iohk.iodb.{ByteArrayWrapper, Store}
 import scorex.crypto.authds.avltree.batch.VersionedIODBAVLStorage.{InternalNodePrefix, LeafPrefix}
 import scorex.crypto.authds.{ADDigest, ADKey, ADValue, Balance}
 import scorex.util.encode.Base58
 import scorex.crypto.hash
 import scorex.crypto.hash.{CryptographicHash, Digest}
+import scorex.db.LDBVersionedStore
 import scorex.util.ScorexLogging
-import scorex.utils.ScorexEncoding
-
 import scala.util.{Failure, Try}
 
-class VersionedIODBAVLStorage[D <: Digest](store: Store, nodeParameters: NodeParameters)
+class VersionedIODBAVLStorage[D <: Digest](store: LDBVersionedStore, nodeParameters: NodeParameters)
                                           (implicit val hf: CryptographicHash[D]) extends VersionedAVLStorage[D] with ScorexLogging {
 
   private lazy val labelSize = nodeParameters.labelSize
 
-  private val TopNodeKey: ByteArrayWrapper = ByteArrayWrapper(Array.fill(labelSize)(123: Byte))
-  private val TopNodeHeight: ByteArrayWrapper = ByteArrayWrapper(Array.fill(labelSize)(124: Byte))
-  private val DigestLength = 33
-  private val InitialVersion = ADDigest @@ Array.fill(DigestLength)(11: Byte)
+  private val TopNodeKey: Array[Byte] = Array.fill(labelSize)(123: Byte)
+  private val TopNodeHeight: Array[Byte] = Array.fill(labelSize)(124: Byte)
+
   private val fixedSizeValueMode = nodeParameters.valueSize.isDefined
 
   override def update(prover: BatchAVLProver[D, _]): Try[Unit] = update(prover, Seq())
 
   override def rollback(version: ADDigest): Try[(ProverNodes[D], Int)] = Try {
-    store.rollback(ByteArrayWrapper(version))
+    store.rollback(version)
 
-    val top = VersionedIODBAVLStorage.fetch[D](ADKey @@ store.get(TopNodeKey).get.data)(hf, store, nodeParameters)
-    val topHeight = Ints.fromByteArray(store.get(TopNodeHeight).get.data)
+    val top = VersionedIODBAVLStorage.fetch[D](ADKey @@ store.get(TopNodeKey).get)(hf, store, nodeParameters)
+    val topHeight = Ints.fromByteArray(store.get(TopNodeHeight).get)
 
     top -> topHeight
   }.recoverWith { case e =>
@@ -37,24 +34,21 @@ class VersionedIODBAVLStorage[D <: Digest](store: Store, nodeParameters: NodePar
     Failure(e)
   }
 
-  override def version: Option[ADDigest] = store.lastVersionID.map(d => ADDigest @@ d.data)
+  override def version: Option[ADDigest] = store.lastVersionID.map(d => ADDigest @@ d)
 
-  def rollbackVersions: Iterable[ADDigest] = store.rollbackVersions().map(d => ADDigest @@ d.data)
+  def rollbackVersions: Iterable[ADDigest] = store.rollbackVersions().map(d => ADDigest @@ d)
 
-  def leafsIterator() = store.getAll().filter { case (_, v) => v.data.head == LeafPrefix }
+  def leafsIterator(): Iterator[(Array[Byte], Array[Byte])] =
+    store.getWithFilter{ case (_, v) => v.head == LeafPrefix }
 
   override def update[K <: Array[Byte], V <: Array[Byte]](prover: BatchAVLProver[D, _],
                                                           additionalData: Seq[(K, V)]): Try[Unit] = Try {
-    val digestWrapper = ByteArrayWrapper(prover.digest)
-    val indexes = Seq(TopNodeKey -> nodeKey(prover.topNode),
-      TopNodeHeight -> ByteArrayWrapper(Ints.toByteArray(prover.rootNodeHeight)))
+    val digestWrapper = prover.digest
+    val indexes = Seq(TopNodeKey -> nodeKey(prover.topNode), TopNodeHeight -> Ints.toByteArray(prover.rootNodeHeight))
     val toInsert = serializedVisitedNodes(prover.topNode, isTop = true)
-    val toRemove = prover.removedNodes().map(rn => ByteArrayWrapper(rn.label))
+    val toRemove = prover.removedNodes().map(rn => rn.label)
     val toUpdate = indexes ++ toInsert
-    val toUpdateWrapped = additionalData.map { case (k, v) =>
-      ByteArrayWrapper(k) -> ByteArrayWrapper(v)
-    }
-    val toUpdateWithWrapped = toUpdate ++ toUpdateWrapped
+    val toUpdateWithWrapped = toUpdate ++ additionalData
 
     //log.info(s"Update storage to version $digestWrapper: ${toUpdateWithWrapped.size} elements to insert," +
     //  s" ${toRemove.size} elements to remove")
@@ -66,10 +60,10 @@ class VersionedIODBAVLStorage[D <: Digest](store: Store, nodeParameters: NodePar
   }
 
   private def serializedVisitedNodes(node: ProverNodes[D],
-                                     isTop: Boolean): Seq[(ByteArrayWrapper, ByteArrayWrapper)] = {
+                                     isTop: Boolean): Seq[(Array[Byte], Array[Byte])] = {
     // Should always serialize top node. It may not be new if it is the creation of the tree
     if (node.isNew || isTop) {
-      val pair: (ByteArrayWrapper, ByteArrayWrapper) = (nodeKey(node), ByteArrayWrapper(toBytes(node)))
+      val pair: (Array[Byte], Array[Byte]) = (nodeKey(node), toBytes(node))
       node match {
         case n: InternalProverNode[D] =>
           val leftSubtree = serializedVisitedNodes(n.left, isTop = false)
@@ -81,7 +75,7 @@ class VersionedIODBAVLStorage[D <: Digest](store: Store, nodeParameters: NodePar
   }
 
   //TODO label or key???
-  private def nodeKey(node: ProverNodes[D]): ByteArrayWrapper = ByteArrayWrapper(node.label)
+  private def nodeKey(node: ProverNodes[D]): Array[Byte] = node.label
 
   private def toBytes(node: ProverNodes[D]): Array[Byte] = node match {
     case n: InternalProverNode[D] => InternalNodePrefix +: n.balance +: (n.key ++ n.left.label ++ n.right.label)
@@ -97,9 +91,9 @@ object VersionedIODBAVLStorage {
   val LeafPrefix: Byte = 1: Byte
 
   def fetch[D <: hash.Digest](key: ADKey)(implicit hf: CryptographicHash[D],
-                                          store: Store,
+                                          store: LDBVersionedStore,
                                           nodeParameters: NodeParameters): ProverNodes[D] = {
-    val bytes = store(ByteArrayWrapper(key)).data
+    val bytes = store(key)
     lazy val keySize = nodeParameters.keySize
     lazy val labelSize = nodeParameters.labelSize
 

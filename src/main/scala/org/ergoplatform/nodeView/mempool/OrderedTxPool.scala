4db9315ff405aac313067025ea56e27999fca2b5
Konstantin Knizhnik <knizhnik@garret.ru>
2019-12-30 17:33:59 +0300

    Support families of transactions in mempool

--- src/main/scala/org/ergoplatform/nodeView/mempool/OrderedTxPool.scala
+++ src/main/scala/org/ergoplatform/nodeView/mempool/OrderedTxPool.scala
@@ -1,6 +1,7 @@
 package org.ergoplatform.nodeView.mempool
 
 import com.google.common.primitives.Ints
+import org.ergoplatform.ErgoBox.BoxId
 import org.ergoplatform.modifiers.mempool.ErgoTransaction
 import org.ergoplatform.nodeView.mempool.OrderedTxPool.WeightedTxId
 import org.ergoplatform.settings.{Algos, ErgoSettings, MonetarySettings}
@@ -17,8 +18,10 @@ import scala.collection.immutable.TreeMap
   *                             ordered by invalidation timestamp (values)
   */
 final case class OrderedTxPool(orderedTransactions: TreeMap[WeightedTxId, ErgoTransaction],
-                               transactionsRegistry: TreeMap[ModifierId, Double],
-                               invalidated: TreeMap[ModifierId, Long])
+                               transactionsRegistry: TreeMap[ModifierId, WeightedTxId],
+                               invalidated: TreeMap[ModifierId, Long],
+                               outputs: TreeMap[BoxId, WeightedTxId]
+                              )
                               (implicit settings: ErgoSettings) extends ScorexLogging {
 
   import OrderedTxPool.weighted
@@ -32,48 +35,64 @@ final case class OrderedTxPool(orderedTransactions: TreeMap[WeightedTxId, ErgoTr
   def size: Int = orderedTransactions.size
 
   def get(id: ModifierId): Option[ErgoTransaction] = {
-    transactionsRegistry.get(id).flatMap(w => orderedTransactions.get(WeightedTxId(id, w)))
+    transactionsRegistry.get(id).flatMap(orderedTransactions.get(_))
   }
 
   def put(tx: ErgoTransaction): OrderedTxPool = {
-    val (txs, ws) = if (orderedTransactions.size >= mempoolCapacity) {
+    val (txs, ws, outs) = if (orderedTransactions.size >= mempoolCapacity) {
+      val txnToThrow = orderedTransactions.head._2
       val keyToThrow = orderedTransactions.firstKey
       log.debug(s"Memory pool overflow - throwing out transaction with id = ${keyToThrow.id}")
-      (orderedTransactions - keyToThrow) -> (transactionsRegistry - keyToThrow.id)
+      (orderedTransactions - keyToThrow, transactionsRegistry - keyToThrow.id, outputs -- txnToThrow.outputs.map(_.id))
     } else {
-      orderedTransactions -> transactionsRegistry
+      (orderedTransactions, transactionsRegistry, outputs)
     }
     val wtx = weighted(tx)
-    OrderedTxPool(txs.updated(wtx, tx), ws.updated(wtx.id, wtx.weight), invalidated)
+    OrderedTxPool(txs.updated(wtx, tx), ws.updated(wtx.id, wtx), invalidated, outs ++ tx.outputs.map(_.id -> wtx)).updateFamily(tx, wtx.weight)
   }
 
   def remove(tx: ErgoTransaction): OrderedTxPool = {
-    OrderedTxPool(orderedTransactions - weighted(tx), transactionsRegistry - tx.id, invalidated)
+    transactionsRegistry.get(tx.id).fold(this)(wtx =>
+      OrderedTxPool(orderedTransactions - wtx, transactionsRegistry - tx.id, invalidated, outputs -- tx.outputs.map(_.id)).updateFamily(tx, -wtx.weight))
   }
 
   def invalidate(tx: ErgoTransaction): OrderedTxPool = {
     val inv = if (invalidated.size >= blacklistCapacity) invalidated - invalidated.firstKey else invalidated
     val ts = System.currentTimeMillis()
-    OrderedTxPool(orderedTransactions - weighted(tx),
-      transactionsRegistry - tx.id, inv.updated(tx.id, ts))
+    transactionsRegistry.get(tx.id).fold(this)(wtx =>
+      OrderedTxPool(orderedTransactions - wtx,
+        transactionsRegistry - tx.id, inv.updated(tx.id, ts), outputs -- tx.outputs.map(_.id)).updateFamily(tx, -wtx.weight))
   }
 
   def filter(condition: ErgoTransaction => Boolean): OrderedTxPool = {
-    val txs = orderedTransactions.filter { case (_, v) => condition(v) }
-    val ws = transactionsRegistry.filter(tx => orderedTransactions.get(WeightedTxId(tx._1, tx._2)).exists(condition))
-    OrderedTxPool(txs, ws, invalidated)
+    orderedTransactions.foldLeft(this)((pool,entry) => {
+      val tx = entry._2
+      if (condition(tx)) pool else pool.remove(tx)
+    })
   }
 
   def canAccept(tx: ErgoTransaction): Boolean = {
     !isInvalidated(tx.id) && !contains(tx.id) &&
       (size < settings.nodeSettings.mempoolCapacity ||
-        weighted(tx).weight > orderedTransactions.firstKey.weight)
+        weighted(tx).weight > weighted(orderedTransactions.head._2).weight)
   }
 
   def contains(id: ModifierId): Boolean = transactionsRegistry.contains(id)
 
   def isInvalidated(id: ModifierId): Boolean = invalidated.contains(id)
 
+  private def updateFamily(tx : ErgoTransaction, weight : Double) : OrderedTxPool = {
+    tx.inputs.foldLeft(this)((pool,box) =>
+      outputs.get(box.boxId).fold(pool)(wtx => {
+        val parent = orderedTransactions(wtx)
+        val new_wtx = WeightedTxId(wtx.id, wtx.weight + weight)
+        val new_pool = OrderedTxPool(pool.orderedTransactions - wtx + (new_wtx -> parent),
+          pool.transactionsRegistry.updated(parent.id, new_wtx),
+          pool.invalidated,
+          pool.outputs)
+        new_pool.updateFamily(parent, weight)
+      }))
+  }
 }
 
 object OrderedTxPool {
@@ -88,11 +107,12 @@ object OrderedTxPool {
     override def hashCode(): Int = Ints.fromByteArray(Algos.decodeUnsafe(id.take(8)))
   }
 
-  private implicit val ord: Ordering[WeightedTxId] = Ordering[(Double, ModifierId)].on(x => (x.weight, x.id))
+  private implicit val ordWeigth: Ordering[WeightedTxId] = Ordering[(Double, ModifierId)].on(x => (x.weight, x.id))
+  private implicit val ordBoxId: Ordering[BoxId] = Ordering[String].on(b =>  Algos.encode(b))
 
   def empty(settings: ErgoSettings): OrderedTxPool = {
-    OrderedTxPool(TreeMap.empty[WeightedTxId, ErgoTransaction], TreeMap.empty[ModifierId, Double],
-      TreeMap.empty[ModifierId, Long])(settings)
+    OrderedTxPool(TreeMap.empty[WeightedTxId, ErgoTransaction], TreeMap.empty[ModifierId, WeightedTxId],
+      TreeMap.empty[ModifierId, Long], TreeMap.empty[BoxId, WeightedTxId])(settings)
   }
 
   private def weighted(tx: ErgoTransaction)(implicit ms: MonetarySettings): WeightedTxId = {

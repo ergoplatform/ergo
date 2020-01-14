e6ab07f73c978f43db4a0d6485d95a1ba9794781
Alex Chepurnoy <alex.chepurnoy@iohk.io>
2020-01-14 17:31:40 +0300

    styling issues found during code inspection fixed

--- avldb/src/test/scala/scorex/crypto/authds/avltree/batch/VersionedLDBAVLStorageStatefulSpecification.scala
+++ avldb/src/test/scala/scorex/crypto/authds/avltree/batch/VersionedLDBAVLStorageStatefulSpecification.scala
@@ -161,7 +161,6 @@ trait VersionedIODBAVLStorageStatefulCommands extends Commands { this: TestHelpe
       require(digest1.sameElements(updatedDigest))
       ops.foreach(sut.performOneOperation)
       sut.checkTree(postProof = false)
-      val sameProof = sut.generateProofAndUpdateStorage()
       sut.checkTree(postProof = true)
       val updatedPostDigest = sut.digest
 

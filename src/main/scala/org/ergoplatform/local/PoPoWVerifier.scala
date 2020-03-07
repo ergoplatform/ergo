883b6e96f3bfe5d068173d059f1614433c5af0e9
Kostis Karantias <kkarantias@gmail.com>
2020-03-07 19:47:06 +0200

    PoPoWVerifier: fix indentation

--- src/main/scala/org/ergoplatform/local/PoPoWVerifier.scala
+++ src/main/scala/org/ergoplatform/local/PoPoWVerifier.scala
@@ -5,17 +5,16 @@ import org.ergoplatform.modifiers.history.Header
 import scorex.util.ModifierId
 
 class PoPoWVerifier(poPoWParams: PoPowParams, genesisId: ModifierId) {
-    // TODO: ideally prefix would have the actual genesis but we may just not have it
-    var bestProof: Option[PoPowProof] = None
+  var bestProof: Option[PoPowProof] = None
 
-    def bestChain(): Seq[Header] = {
-        bestProof.map(_.headersChain).getOrElse(Seq())
-    }
+  def bestChain(): Seq[Header] = {
+    bestProof.map(_.headersChain).getOrElse(Seq())
+  }
 
-    def process(newProof: PoPowProof) {
-        if (newProof.headersChain.head.id == genesisId &&
-          !bestProof.exists(_.isBetterThan(newProof))) {
-          bestProof = Some(newProof)
-        }
+  def process(newProof: PoPowProof) {
+    if (newProof.headersChain.head.id == genesisId &&
+      !bestProof.exists(_.isBetterThan(newProof))) {
+      bestProof = Some(newProof)
     }
+  }
 }

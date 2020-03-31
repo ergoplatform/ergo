207425adeba9281e32af62a11aca81b61bb72664
Alex Chepurnoy <alex.chepurnoy@iohk.io>
2020-03-31 15:27:24 +0300

    TransactionMembershipProof and supporting basic functionality from i929

--- src/main/scala/org/ergoplatform/settings/Algos.scala
+++ src/main/scala/org/ergoplatform/settings/Algos.scala
@@ -5,6 +5,7 @@ import scorex.crypto.authds.merkle.MerkleTree
 import scorex.crypto.hash.Digest32
 import scorex.util._
 
+
 object Algos extends ErgoAlgos with scorex.core.utils.ScorexEncoding {
 
   // ErgoAlgos in sigmastate extends scorex.util.ScorexEncoding where encoder is BytesEncoder
@@ -17,11 +18,20 @@ object Algos extends ErgoAlgos with scorex.core.utils.ScorexEncoding {
 
   @inline def encode(id: ModifierId): String = encoder.encode(id)
 
-  def blockIdDifficulty(id: Array[Byte]): BigInt = {
-    val blockTarget = BigInt(1, id).ensuring(_ <= Constants.MaxTarget)
-    Constants.MaxTarget / blockTarget
-  }
+  /**
+    * A method to build a Merkle tree over binary objects (leafs of the tree)
+    * @param elements - Merkle tree leafs (byte arrays of arbitrary size)
+    * @return a Merkle tree built over the elements
+    */
+  def merkleTree(elements: Seq[LeafData]): MerkleTree[Digest32] = MerkleTree(elements)(hash)
 
+  /**
+    * A method which is building a Merkle tree over binary objects and returns a digest
+    * (256-bits long root hash) of the tree
+    * @param elements - tree leafs
+    * @return 256-bits (32-bytes) long digest of the tree
+    */
   def merkleTreeRoot(elements: Seq[LeafData]): Digest32 =
-    if (elements.isEmpty) emptyMerkleTreeRoot else MerkleTree(elements)(hash).rootHash
+    if (elements.isEmpty) emptyMerkleTreeRoot else merkleTree(elements).rootHash
+
 }

34f3972ee33d3929c206c2b5ee9c620493cba68a
Kostis Karantias <kkarantias@gmail.com>
2020-04-04 18:16:42 +0300

    Extension: keep merkle tree

--- src/main/scala/org/ergoplatform/mining/AutolykosPowScheme.scala
+++ src/main/scala/org/ergoplatform/mining/AutolykosPowScheme.scala
@@ -119,10 +119,9 @@ class AutolykosPowScheme(val k: Int, val n: Int) extends ScorexLogging {
 
     val transactionsRoot = BlockTransactions.transactionsRoot(transactions)
     val adProofsRoot = ADProofs.proofDigest(adProofBytes)
-    val extensionRoot: Digest32 = Extension.rootHash(extensionCandidate)
 
     prove(parentOpt, version, nBits, stateRoot, adProofsRoot, transactionsRoot,
-      timestamp, extensionRoot, votes, sk, minNonce, maxNonce).map { h =>
+      timestamp, extensionCandidate.digest, votes, sk, minNonce, maxNonce).map { h =>
       val adProofs = ADProofs(h.id, adProofBytes)
       val blockTransactions = BlockTransactions(h.id, transactions)
       val extension = extensionCandidate.toExtension(h.id)
@@ -252,7 +251,6 @@ object AutolykosPowScheme {
     val (parentId, height) = derivedHeaderFields(candidate.parentOpt)
     val transactionsRoot = BlockTransactions.transactionsRoot(candidate.transactions)
     val adProofsRoot = ADProofs.proofDigest(candidate.adProofBytes)
-    val extensionRoot: Digest32 = Extension.rootHash(candidate.extension)
 
     Header(
       candidate.version,
@@ -263,7 +261,7 @@ object AutolykosPowScheme {
       candidate.timestamp,
       candidate.nBits,
       height,
-      extensionRoot,
+      candidate.extension.digest,
       null,
       candidate.votes
     )

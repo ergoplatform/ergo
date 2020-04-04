260994e35e94255172cee65b98e8e0abe62baf59
Kostis Karantias <kkarantias@gmail.com>
2020-04-04 20:57:40 +0300

    Extension: add test for proofFor

---
+++ src/test/scala/org/ergoplatform/modifiers/history/ExtensionCandidateTest.scala
@@ -0,0 +1,19 @@
+package org.ergoplatform.modifiers.history
+
+import org.ergoplatform.utils.ErgoPropertyTest
+
+class ExtensionCandidateTest extends ErgoPropertyTest {
+  type KV = (Array[Byte], Array[Byte])
+
+  property("proofFor should return a proof for an existing value") {
+    forAll { explodedFields: (Seq[KV], KV, Seq[KV]) =>
+      val (left, middle, right) = explodedFields
+      val fields = left ++ (middle +: right)
+
+      val ext = ExtensionCandidate(fields)
+      val pf = ext.proofFor(middle._2)
+      pf shouldBe defined
+      pf.get.valid(ext.digest) shouldBe true
+    }
+  }
+}

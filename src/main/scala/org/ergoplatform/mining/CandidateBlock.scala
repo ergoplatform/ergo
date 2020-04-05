cada1ce5acaf1b18ff935748bcb5ac4d3c6468f5
Alex Chepurnoy <alex.chepurnoy@iohk.io>
2020-04-05 18:30:04 +0300

    unused imports

--- src/main/scala/org/ergoplatform/mining/CandidateBlock.scala
+++ src/main/scala/org/ergoplatform/mining/CandidateBlock.scala
@@ -2,7 +2,7 @@ package org.ergoplatform.mining
 
 import io.circe.Encoder
 import io.circe.syntax._
-import org.ergoplatform.modifiers.history.{Extension, ExtensionCandidate, Header}
+import org.ergoplatform.modifiers.history.{ExtensionCandidate, Header}
 import org.ergoplatform.modifiers.mempool.ErgoTransaction
 import org.ergoplatform.settings.Algos
 import scorex.core.block.Block

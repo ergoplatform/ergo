8b1f4749a60891c59d493f5815d88e8a2da62e1a
Alex Chepurnoy <alex.chepurnoy@iohk.io>
2020-04-06 13:27:53 +0300

    listApps, ApplicationApiRouteSpec stub

---
+++ src/test/scala/org/ergoplatform/http/routes/ApplicationApiRouteSpec.scala
@@ -0,0 +1,14 @@
+package org.ergoplatform.http.routes
+
+import akka.http.scaladsl.testkit.ScalatestRouteTest
+import de.heikoseeberger.akkahttpcirce.FailFastCirceSupport
+import org.ergoplatform.utils.Stubs
+import org.scalatest.{FlatSpec, Matchers}
+
+class ApplicationApiRouteSpec extends FlatSpec
+  with Matchers
+  with ScalatestRouteTest
+  with Stubs
+  with FailFastCirceSupport {
+
+}

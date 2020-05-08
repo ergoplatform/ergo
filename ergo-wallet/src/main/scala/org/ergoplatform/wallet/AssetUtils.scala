a58af409a7f6935025d7d8804ecd1c48a1170aec
Alexander Slesarenko <avslesarenko@gmail.com>
2020-05-08 23:06:59 +0300

    AssetUtilsSpec.scala added

--- ergo-wallet/src/main/scala/org/ergoplatform/wallet/AssetUtils.scala
+++ ergo-wallet/src/main/scala/org/ergoplatform/wallet/AssetUtils.scala
@@ -10,14 +10,33 @@ object AssetUtils {
   def mergeAssetsMut(into: mutable.Map[ModifierId, Long], from: TokensMap*): Unit = {
     from.foreach(_.foreach {
       case (id, amount) =>
-        into.put(id, into.getOrElse(id, 0L) + amount)
+        into.put(id, Math.addExact(into.getOrElse(id, 0L), amount))
     })
   }
 
   @inline
-  def mergeAssets(from: TokensMap, to: TokensMap = Map.empty): TokensMap = {
-    from.foldLeft(to) { case (acc, (id, amount)) =>
-      acc.updated(id, acc.getOrElse(id, 0L) + amount)
+  def mergeAssets(to: TokensMap, from: TokensMap*): TokensMap = {
+    from.foldLeft(to){ case (to, from) =>
+      from.foldLeft(to) { case (acc, (id, amount)) =>
+        acc.updated(id, Math.addExact(acc.getOrElse(id, 0L), amount))
+      }
+    }
+  }
+
+  @inline
+  def subtractAssets(from: TokensMap, subtractor: TokensMap*): TokensMap = {
+    subtractor.foldLeft(from){ case (from, substractor) =>
+      substractor.foldLeft(from) { case (acc, (id, amount)) =>
+        val currAmt = acc.getOrElse(id,
+          throw new IllegalArgumentException(s"Cannot subtract $amount of token $id: token not found in $acc"))
+        require(amount >= 0, s"Cannot subtract negative amount from token $id: $amount")
+        require(currAmt >= amount, s"Not enough amount of token $id -> $currAmt to subtract $amount")
+        if (currAmt == amount) {
+          acc - id
+        } else {
+          acc.updated(id, currAmt - amount)
+        }
+      }
     }
   }
 

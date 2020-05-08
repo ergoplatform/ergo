01f52840ff268b69c86ea428ded1333a8f5bcf72
Alexander Slesarenko <avslesarenko@gmail.com>
2020-05-08 14:08:38 +0300

    introduce TokensMap type synonym

--- ergo-wallet/src/main/scala/org/ergoplatform/wallet/AssetUtils.scala
+++ ergo-wallet/src/main/scala/org/ergoplatform/wallet/AssetUtils.scala
@@ -7,7 +7,7 @@ import scala.collection.mutable
 object AssetUtils {
 
   @inline
-  def mergeAssetsMut(into: mutable.Map[ModifierId, Long], from: Map[ModifierId, Long]*): Unit = {
+  def mergeAssetsMut(into: mutable.Map[ModifierId, Long], from: TokensMap*): Unit = {
     from.foreach(_.foreach {
       case (id, amount) =>
         into.put(id, into.getOrElse(id, 0L) + amount)
@@ -15,14 +15,14 @@ object AssetUtils {
   }
 
   @inline
-  def mergeAssets(from: Map[ModifierId, Long], to: Map[ModifierId, Long] = Map.empty): Map[ModifierId, Long] = {
+  def mergeAssets(from: TokensMap, to: TokensMap = Map.empty): TokensMap = {
     from.foldLeft(to) { case (acc, (id, amount)) =>
       acc.updated(id, acc.getOrElse(id, 0L) + amount)
     }
   }
 
   @inline
-  def subtractAssetsMut(from: mutable.Map[ModifierId, Long], subtractor: Map[ModifierId, Long]): Unit = {
+  def subtractAssetsMut(from: mutable.Map[ModifierId, Long], subtractor: TokensMap): Unit = {
     subtractor.foreach { case (id, subtractAmt) =>
       val fromAmt = from(id)
       if (fromAmt == subtractAmt) {
--- src/main/scala/org/ergoplatform/nodeView/wallet/BalancesSnapshot.scala
+++ src/main/scala/org/ergoplatform/nodeView/wallet/BalancesSnapshot.scala
@@ -1,8 +1,6 @@
 package org.ergoplatform.nodeView.wallet
 
 import org.ergoplatform.nodeView.history.ErgoHistory.Height
-import scorex.util.ModifierId
+import org.ergoplatform.wallet.TokensMap
 
-import scala.collection.immutable
-
-final case class BalancesSnapshot(height: Height, balance: Long, assetBalances: immutable.Map[ModifierId, Long])
+final case class BalancesSnapshot(height: Height, balance: Long, assetBalances: TokensMap)

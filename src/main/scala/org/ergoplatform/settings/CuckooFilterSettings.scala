567e9f013654b8acc047fcd5e3004899a5f01e59
Alex Chepurnoy <alex.chepurnoy@iohk.io>
2020-07-09 17:35:44 +0300

    CuckooFilterSettings comments

--- src/main/scala/org/ergoplatform/settings/CuckooFilterSettings.scala
+++ src/main/scala/org/ergoplatform/settings/CuckooFilterSettings.scala
@@ -1,7 +1,12 @@
 package org.ergoplatform.settings
 
 /**
-  * Cuckoo filter settings
+  * Cuckoo filter settings.
+  *
+  * Cuckoo filter is used by the wallet in order to recognize boxes which belong to wallet's addresses.
+  *
+  * Please note that maximum number of keys the filter can accept is entriesPerBucket * bucketsQty
+  *
   * @param entriesPerBucket - entries per bucket
   * @param bucketsQty - number of buckets in the filter
   */

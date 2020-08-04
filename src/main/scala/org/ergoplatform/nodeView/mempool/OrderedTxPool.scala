8a99b5361ba62f6691e08d0c9a4a55a2c4397d2a
Konstantin Knizhnik <knizhnik@garret.ru>
2020-08-04 23:06:23 +0300

    [refer #1156] Fix updateFamily for case when trnsaction spends multiple inputs

--- src/main/scala/org/ergoplatform/nodeView/mempool/OrderedTxPool.scala
+++ src/main/scala/org/ergoplatform/nodeView/mempool/OrderedTxPool.scala
@@ -121,12 +121,12 @@ case class OrderedTxPool(orderedTransactions: TreeMap[WeightedTxId, ErgoTransact
       pool.outputs.get(input.boxId).fold(pool)(wtx => {
         pool.orderedTransactions.get(wtx) match {
           case Some(parent) =>
-            val newWtx = WeightedTxId(wtx.id, wtx.weight + weight)
+            val newWtx = WeightedTxId(wtx.id, wtx.weight + weight/tx.inputs.size)
             val newPool = OrderedTxPool(pool.orderedTransactions - wtx + (newWtx -> parent),
               pool.transactionsRegistry.updated(parent.id, newWtx),
               invalidated,
               parent.outputs.foldLeft(pool.outputs)((newOutputs, box) => newOutputs.updated(box.id, newWtx)))
-            newPool.updateFamily(parent, weight)
+            newPool.updateFamily(parent, weight/tx.inputs.size)
           case None =>
             //shouldn't be the case, but better not to hide this possibility
             log.error("Could not find transaction in pool.orderedTransactions, please report to devs")

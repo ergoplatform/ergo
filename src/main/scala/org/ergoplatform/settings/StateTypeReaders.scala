f04b4325922052b216b4166601efc057dd218474
Alex Chepurnoy <alex.chepurnoy@iohk.io>
2020-08-10 17:19:21 +0300

    unused StateTypeReaders.stateTypeReader removed

--- src/main/scala/org/ergoplatform/settings/StateTypeReaders.scala
+++ src/main/scala/org/ergoplatform/settings/StateTypeReaders.scala
@@ -6,12 +6,6 @@ import org.ergoplatform.nodeView.state._
 
 trait StateTypeReaders {
 
-  implicit val stateTypeReader: ValueReader[StateType] = { (cfg, path) =>
-    val typeKey = s"$path.stateType"
-    val typeName = cfg.getString(typeKey)
-    stateTypeFromString(typeName, typeKey)
-  }
-
   def stateTypeFromString(typeName: String, path: String): StateType = {
     StateType.values.find(_.stateTypeName == typeName)
       .getOrElse(throw new ConfigException.BadValue(path, typeName))

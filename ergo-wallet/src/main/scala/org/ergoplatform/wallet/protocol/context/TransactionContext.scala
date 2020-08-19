5142193ef1669a97524587f0cab9c4826f57bbb0
Alex Chepurnoy <alex.chepurnoy@iohk.io>
2020-08-19 13:13:59 +0300

    InputContext and TransactionContext comments

--- ergo-wallet/src/main/scala/org/ergoplatform/wallet/protocol/context/TransactionContext.scala
+++ ergo-wallet/src/main/scala/org/ergoplatform/wallet/protocol/context/TransactionContext.scala
@@ -1,10 +1,17 @@
 package org.ergoplatform.wallet.protocol.context
 
 import org.ergoplatform.{ErgoBox, ErgoLikeTransactionTemplate, UnsignedInput}
-import sigmastate.interpreter.ContextExtension
 
-final case class TransactionContext(boxesToSpend: IndexedSeq[ErgoBox],
-                                    dataBoxes: IndexedSeq[ErgoBox],
-                                    spendingTransaction: ErgoLikeTransactionTemplate[_ <: UnsignedInput])
+/**
+  * Part of the execution context in regards with spending transaction
+  *
+  * @param boxesToSpend - inputs of the transaction
+  * @param dataBoxes - data (read-only) inputs of the transaction
+  * @param spendingTransaction - spending transaction
+  */
 
-final case class InputContext(selfIndex: Short, extension: ContextExtension)
+// TODO: it seems spendingTransaction is needed only to get output candidates in ErgoLikeContext.
+// After ErgoLikeContext refactoring in sigma, this class can be simplified.
+case class TransactionContext(boxesToSpend: IndexedSeq[ErgoBox],
+                              dataBoxes: IndexedSeq[ErgoBox],
+                              spendingTransaction: ErgoLikeTransactionTemplate[_ <: UnsignedInput])

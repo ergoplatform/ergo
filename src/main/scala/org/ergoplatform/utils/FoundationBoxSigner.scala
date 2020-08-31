f1f8a560894f1ec5095c65240e9032411b00ee0f
Alex Chepurnoy <alex.chepurnoy@iohk.io>
2020-08-31 11:44:12 +0300

    307K

--- src/main/scala/org/ergoplatform/utils/FoundationBoxSigner.scala
+++ src/main/scala/org/ergoplatform/utils/FoundationBoxSigner.scala
@@ -51,7 +51,7 @@ object FoundationBoxSigner extends App {
   val preSign: ACTION = 1
   val sign: ACTION = 2
 
-  val height = 253000
+  val height = 307000
 
   //data which should be MANUALLY changed in order to interact with the program
   val seed = "..."

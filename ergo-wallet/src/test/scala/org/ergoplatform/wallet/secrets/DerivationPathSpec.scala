b666a7dfe4168dcbdee8bc792d193f66a5b0cb40
Alex Chepurnoy <alex.chepurnoy@iohk.io>
2020-10-03 20:59:14 +0300

    old derivation comments

--- ergo-wallet/src/test/scala/org/ergoplatform/wallet/secrets/DerivationPathSpec.scala
+++ ergo-wallet/src/test/scala/org/ergoplatform/wallet/secrets/DerivationPathSpec.scala
@@ -73,14 +73,18 @@ class DerivationPathSpec
     val pk = sk.publicKey.key
     P2PKAddress(pk).toString() shouldBe address
 
+    // Derivation procedure (post-EIP3) should get m/2 after m/1 (like pre-EIP3)
     val dp2 = DerivationPath.nextPath(IndexedSeq(masterKey, sk), usePreEip3Derivation = false).get
     dp2 shouldBe DerivationPath.fromEncoded("m/2").get
 
     val sk2 = masterKey.derive(DerivationPath.fromEncoded("m/1/1").get).asInstanceOf[ExtendedSecretKey]
 
+    // Derivation procedure (post-EIP3) should get m/2 after m/1/1 (like pre-EIP3)
     val dp3 = DerivationPath.nextPath(IndexedSeq(masterKey, sk, sk2), usePreEip3Derivation = false).get
     dp3 shouldBe DerivationPath.fromEncoded("m/2").get
 
+
+    // Once a user adds EIP-3 path (e.g. m/44'/429'/0'/0/0), post-EIP3 derivation applied
     val sk3 = masterKey.derive(DerivationPath.fromEncoded("m/44'/429'/0'/0/0").get).asInstanceOf[ExtendedSecretKey]
     val dp4 = DerivationPath.nextPath(IndexedSeq(masterKey, sk, sk3), usePreEip3Derivation = false).get
     dp4 shouldBe DerivationPath.fromEncoded("m/44'/429'/0'/0/1").get

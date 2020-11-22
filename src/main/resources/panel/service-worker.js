825ffe76cd36d447efbaec8e70de4a951e23a58a
d.usov <d.usov@city-mobil.ru>
2020-11-22 23:52:37 +0300

    add assets support at the panel

--- src/main/resources/panel/service-worker.js
+++ src/main/resources/panel/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ac67956815c266c4709a936402a3fd23.js"
+  "/precache-manifest.9e1f9a16e912d1b7260f11b0d57d38ef.js"
 );
 
 self.addEventListener('message', (event) => {

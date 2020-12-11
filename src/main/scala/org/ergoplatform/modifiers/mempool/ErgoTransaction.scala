02ceaac807a1c35846625e94a0523f1a81790b60
Alex Chepurnoy <alex.chepurnoy@iohk.io>
2020-12-11 15:44:15 +0300

    formatting

--- src/main/scala/org/ergoplatform/modifiers/mempool/ErgoTransaction.scala
+++ src/main/scala/org/ergoplatform/modifiers/mempool/ErgoTransaction.scala
@@ -142,7 +142,7 @@ case class ErgoTransaction(override val inputs: IndexedSeq[Input],
       validationState
         .validate(txDust, out.value >= BoxUtils.minimalErgoAmount(out, stateContext.currentParameters), s"$id, output ${Algos.encode(out.id)}, ${out.value} >= ${BoxUtils.minimalErgoAmount(out, stateContext.currentParameters)}")
         .validate(txFuture, out.creationHeight <= stateContext.currentHeight, s" ${out.creationHeight} <= ${stateContext.currentHeight} is not true, output id: $id: output $out")
-        .validate(txNegHeight, (protocolVersion == 1) || out.creationHeight >= 0, s" ${out.creationHeight} >= 0 is not true, output id: $id: output $out" )
+        .validate(txNegHeight, (protocolVersion == 1) || out.creationHeight >= 0, s" ${out.creationHeight} >= 0 is not true, output id: $id: output $out")
         .validate(txBoxSize, out.bytes.length <= MaxBoxSize.value, s"$id: output $out")
         .validate(txBoxPropositionSize, out.propositionBytes.length <= MaxPropositionBytes.value, s"$id: output $out")
     }

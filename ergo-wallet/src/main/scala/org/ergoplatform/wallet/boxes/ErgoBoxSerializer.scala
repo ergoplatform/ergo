802f62e3ad1c60215765ddf62e9480e5a00cbb1f
Alexander Slesarenko <avslesarenko@gmail.com>
2020-12-16 17:52:29 +0300

    update sigma v4.0: removed VersionContext

--- ergo-wallet/src/main/scala/org/ergoplatform/wallet/boxes/ErgoBoxSerializer.scala
+++ ergo-wallet/src/main/scala/org/ergoplatform/wallet/boxes/ErgoBoxSerializer.scala
@@ -3,9 +3,7 @@ package org.ergoplatform.wallet.boxes
 import org.ergoplatform.ErgoBox
 import org.ergoplatform.wallet.serialization.ErgoWalletSerializer
 import scorex.util.serialization.{Reader, Writer}
-import sigmastate.SBox
-import sigmastate.interpreter.VersionContext
-import sigmastate.serialization.{DataSerializer, ConstantStore}
+import sigmastate.serialization.ConstantStore
 import sigmastate.utils.{SigmaByteReader, SigmaByteWriter}
 
 object ErgoBoxSerializer extends ErgoWalletSerializer[ErgoBox] {
@@ -16,12 +14,9 @@ object ErgoBoxSerializer extends ErgoWalletSerializer[ErgoBox] {
   }
 
   override def parse(r: Reader): ErgoBox = {
-    // TODO v4.0: obtain the ACTUAL versions
-    val versionContext = VersionContext(0) // v3.x
     val reader = new SigmaByteReader(r,
       new ConstantStore(),
-      resolvePlaceholdersToConstants = false,
-      versionContext)
+      resolvePlaceholdersToConstants = false)
     ErgoBox.sigmaSerializer.parse(reader)
   }
 

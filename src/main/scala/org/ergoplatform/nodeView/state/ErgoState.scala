3aac895f78fbbd4b25c13a8be3e72c011a614ef2
Alex Chepurnoy <alex.chepurnoy@iohk.io>
2021-02-15 00:22:58 +0300

    no txs verifications for v1 blocks

--- src/main/scala/org/ergoplatform/nodeView/state/ErgoState.scala
+++ src/main/scala/org/ergoplatform/nodeView/state/ErgoState.scala
@@ -1,9 +1,11 @@
 package org.ergoplatform.nodeView.state
 
 import java.io.File
+import java.util.Date
 
-import org.ergoplatform.ErgoBox.{R4, TokenId, allZerosModifierId, AdditionalRegisters}
+import org.ergoplatform.ErgoBox.{AdditionalRegisters, R4, TokenId}
 import org.ergoplatform._
+import org.ergoplatform.mining.difficulty.LinearDifficultyControl
 import org.ergoplatform.mining.emission.EmissionRules
 import org.ergoplatform.mining.groupElemFromBytes
 import org.ergoplatform.modifiers.ErgoPersistentModifier
@@ -12,9 +14,10 @@ import org.ergoplatform.modifiers.mempool.ErgoTransaction
 import org.ergoplatform.modifiers.state.{Insertion, Lookup, Removal, StateChanges}
 import org.ergoplatform.nodeView.history.ErgoHistory
 import org.ergoplatform.settings.ValidationRules._
-import org.ergoplatform.settings.{ChainSettings, Constants, ErgoSettings}
+import org.ergoplatform.settings.{Args, ChainSettings, Constants, ErgoSettings, NetworkType}
 import org.ergoplatform.wallet.interpreter.ErgoInterpreter
 import scorex.core.transaction.state.MinimalState
+import scorex.core.utils.NetworkTimeProvider
 import scorex.core.validation.ValidationResult.Valid
 import scorex.core.validation.{ModifierValidator, ValidationResult}
 import scorex.core.{VersionTag, idToVersion}
@@ -22,9 +25,8 @@ import scorex.crypto.authds.{ADDigest, ADKey}
 import scorex.util.encode.Base16
 import scorex.util.{ModifierId, ScorexLogging, bytesToId}
 import sigmastate.AtLeast
-import sigmastate.Values.{ByteArrayConstant, SigmaPropConstant, IntConstant, ErgoTree}
+import sigmastate.Values.{ByteArrayConstant, ErgoTree, IntConstant, SigmaPropConstant}
 import sigmastate.basics.DLogProtocol.ProveDlog
-import sigmastate.eval.CostingSigmaDslBuilder
 import sigmastate.serialization.ValueSerializer
 
 import scala.annotation.tailrec
@@ -116,7 +118,11 @@ object ErgoState extends ScorexLogging {
         accCostTry
     }
 
-    execTx(transactions.toList, Valid[Long](0L))
+    if(currentStateContext.blockVersion == 1){
+      Valid(0L)
+    } else {
+      execTx(transactions.toList, Valid[Long](0L))
+    }
   }
 
   /**

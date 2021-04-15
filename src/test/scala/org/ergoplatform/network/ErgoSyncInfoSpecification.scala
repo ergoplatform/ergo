370b2ef0e88a036ceea9f6c912edcccdd1e71577
Alex Chepurnoy <alex.chepurnoy@iohk.io>
2021-04-15 22:41:40 +0300

    ErgoSyncInfoSpecification

--- src/test/scala/org/ergoplatform/network/ErgoSyncInfoSpecification.scala
+++ src/test/scala/org/ergoplatform/network/ErgoSyncInfoSpecification.scala
@@ -10,6 +10,12 @@ import scorex.crypto.hash
 import scorex.util.ModifierId
 import scorex.util.encode.Base16
 
+
+/**
+  * syncInfo is a message sent from one peer to another in order to get (and agree) on synchronization status
+  *
+  * syncInfo v1 is about a sequence of header ids just
+  */
 class ErgoSyncInfoSpecification extends ErgoPropertyTest with DecodingUtils {
 
   property("sync info reference parser") {
@@ -27,31 +33,40 @@ class ErgoSyncInfoSpecification extends ErgoPropertyTest with DecodingUtils {
     val bs = ms.serialize(syncMessage).toArray
     val bsString = Base16.encode(bs)
 
+    // test vector got via high-level API
     bsString shouldBe "0100020441000000214f904163010101010101010101010101010101010102020202020202020202020202020202"
 
     val bb = ByteBuffer.wrap(bs)
 
+    // simple reference parser below
+
+    // read network magic (network id) bytes (4 bytes)
     val magicRead = getBytes(bb, 4)
     magicRead.toIndexedSeq shouldBe magic.toIndexedSeq
 
+    // read message type id
     val messageCode = getByte(bb)
     messageCode shouldBe syncSpec.messageCode  // 65 (in dec)
 
+    // read message length (up to 4 bytes)
     val messageLength = Ints.fromByteArray(getBytes(bb,4))
 
     messageLength shouldBe 33
 
+    // read message checksum (4 bytes)
     val checkSum = getBytes(bb, 4)
 
-
+    // read number of headers, up to 8 bytes, but there are app-level limits (400 in reference client)
     val headersCount = getULong(bb).toByte // should read one byte only
 
     headersCount shouldBe 1.toByte
 
+    // read header ids, one-by-one
     val headerId = getBytes(bb, 32)
 
     headerId.toIndexedSeq shouldBe lastHeaderId.toIndexedSeq
 
+    // checksum must be equal to first 4 bytes of message payload
     checkSum shouldBe hash.Blake2b256(headersCount +: headerId).take(4)
   }
 

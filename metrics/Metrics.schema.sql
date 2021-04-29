f31734b2534ed414f8d9abad33e5d457024405b5
Alexander Slesarenko <avslesarenko@gmail.com>
2021-04-29 20:31:01 +0300

    metrics: db schema updated

--- metrics/Metrics.schema.sql
+++ metrics/Metrics.schema.sql
@@ -1,10 +1,3 @@
-CREATE TABLE IF NOT EXISTS "applyTransactions4"
-(
-    blockId varchar not null
-        constraint blocks_pk
-            primary key,
-    height int not null
-    , tx_num int, cost decimal, time decimal);
 CREATE TABLE IF NOT EXISTS "appendFullBlock4"
 (
     blockId varchar not null
@@ -35,19 +28,8 @@ CREATE TABLE IF NOT EXISTS "verifyScript4"
 );
 CREATE UNIQUE INDEX appendFullBlock4_id_uindex
     on appendFullBlock4 (blockId);
-CREATE UNIQUE INDEX blocks4_id_uindex
-    on applyTransactions4 (blockId);
 CREATE UNIQUE INDEX createUtxoState4_id_uindex
     on createUtxoState4 (blockId);
-CREATE TABLE IF NOT EXISTS "applyTransactions"
-(
-    blockId varchar not null
-        constraint blocks_pk
-            primary key,
-    height int not null
-    , tx_num int, cost decimal, time decimal);
-CREATE UNIQUE INDEX blocks_id_uindex
-    on "applyTransactions" (blockId);
 CREATE TABLE appendFullBlock
 (
     blockId varchar not null
@@ -82,40 +64,12 @@ CREATE TABLE verifyScript
 );
 CREATE INDEX appendFullBlock_height_index
     on appendFullBlock (height);
-CREATE INDEX blocks_height_uindex
-    on applyTransactions (height);
 CREATE INDEX createUtxoState_height_index
     on createUtxoState (height);
-CREATE TABLE IF NOT EXISTS "validateTxStateful"
-(
-    blockId varchar not null,
-    txId varchar not null,
-    cost decimal,
-    time decimal,
-    constraint validateTxStateful_pk
-        primary key (blockId, txId)
-);
 CREATE INDEX appendFullBlock4_height_index
     on appendFullBlock4 (height);
-CREATE INDEX blocks4_height_uindex
-    on applyTransactions4 (height);
 CREATE INDEX createUtxoState4_height_index
     on createUtxoState4 (height);
-CREATE TABLE IF NOT EXISTS "validateTxStateful4"
-(
-    blockId varchar not null,
-    txId varchar not null,
-    cost decimal,
-    time decimal,
-    constraint validateTxStateful_pk
-        primary key (blockId, txId)
-);
-CREATE INDEX validateTxStateful_blockId_index
-    on validateTxStateful (blockId);
-CREATE INDEX validateTxStateful4_blockId_index
-    on validateTxStateful4 (blockId);
-CREATE INDEX validateTxStateful_txId_index
-    on validateTxStateful (txId);
 CREATE INDEX verifyScript_blockId_txId_index
     on verifyScript (blockId, txId);
 CREATE INDEX verifyScript4_blockId_txId_index
@@ -124,3 +78,64 @@ CREATE INDEX verifyScript_blockId_index
     on verifyScript (blockId);
 CREATE INDEX verifyScript4_blockId_index
     on verifyScript4 (blockId);
+CREATE INDEX verifyScript4_txId_boxIndex_index
+    on verifyScript4 (txId, boxIndex);
+CREATE TABLE IF NOT EXISTS "selectedInputs"
+(
+    txId varchar not null,
+    boxIndex int not null
+);
+CREATE INDEX selectedInputs_txId_inputIndex_index
+    on selectedInputs (txId, boxIndex);
+CREATE TABLE IF NOT EXISTS "applyTransactions"
+(
+    blockId varchar(64) not null
+        constraint blocks_pk
+            primary key,
+    height int not null,
+    tx_num int,
+    cost decimal,
+    time decimal
+);
+CREATE INDEX blocks_height_uindex
+    on applyTransactions (height);
+CREATE UNIQUE INDEX blocks_id_uindex
+    on applyTransactions (blockId);
+CREATE TABLE IF NOT EXISTS "validateTxStateful"
+(
+    blockId varchar(64) not null,
+    txId varchar not null,
+    cost decimal,
+    time decimal,
+    constraint validateTxStateful_pk
+        primary key (blockId, txId)
+);
+CREATE INDEX validateTxStateful_blockId_index
+    on validateTxStateful (blockId);
+CREATE INDEX validateTxStateful_txId_index
+    on validateTxStateful (txId);
+CREATE TABLE IF NOT EXISTS "validateTxStateful4"
+(
+    blockId varchar(64) not null,
+    txId varchar not null,
+    cost decimal,
+    time decimal,
+    constraint validateTxStateful_pk
+        primary key (blockId, txId)
+);
+CREATE INDEX validateTxStateful4_blockId_index
+    on validateTxStateful4 (blockId);
+CREATE TABLE IF NOT EXISTS "applyTransactions4"
+(
+    blockId varchar(64) not null
+        constraint blocks_pk
+            primary key,
+    height int not null,
+    tx_num int,
+    cost decimal,
+    time decimal
+);
+CREATE INDEX blocks4_height_uindex
+    on applyTransactions4 (height);
+CREATE UNIQUE INDEX blocks4_id_uindex
+    on applyTransactions4 (blockId);

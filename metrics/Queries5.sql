d2f693d71273b4a0154d438fe85ff6ce564542b3
Alexander Slesarenko <avslesarenko@gmail.com>
2021-05-23 21:41:59 +0300

    metrics: Report.ipynb: validation time analysis

--- metrics/Queries5.sql
+++ metrics/Queries5.sql
@@ -1,17 +1,20 @@
 ---------------------------------------------------------
 -- Validation Queries for v5.0
 
-select count(*)
-from appendFullBlock;
-select count(*)
-from applyTransactions;
-select count(*)
-from createUtxoState;
-select count(*)
-from validateTxStateful;
-select count(*)
-from verifyScript;
+select * from
+(select count(*) as appendFullBlock
+from appendFullBlock),
+(select count(*) as applyTransactions
+from applyTransactions),
+(select count(*) as createUtxoState
+from createUtxoState),
+(select count(*) as validateTxStateful
+from validateTxStateful),
+(select count(*) as verifyScript
+from verifyScript)
 
+-- The recoreded metrics files contain a header row. That row can be found using the following
+-- queries.
 select *
 from appendFullBlock
 where blockId = 'blockId';
@@ -28,6 +31,7 @@ select *
 from verifyScript
 where blockId = 'blockId';
 
+-- Uncomment these commands to remove header rows after metrics are imported from csv files.
 -- delete from appendFullBlock where blockId = 'blockId';
 -- delete from applyTransactions where blockId = 'blockId';
 -- delete from createUtxoState where blockId = 'blockId';
@@ -35,7 +39,7 @@ where blockId = 'blockId';
 -- delete from verifyScript where blockId = 'blockId';
 
 
--- tx with negative cost
+-- count tx with negative cost
 select count(*)
 from validateTxStateful
 where cost < 0;
@@ -77,6 +81,7 @@ where sum_script_costs > tx_cost
 order by script_count desc;
 
 -- top tx_time vs script_time ratio
+-- how much tx time larger than script verification time
 select tx.blockId,
        tx.txId,
        t.script_count,
@@ -94,11 +99,12 @@ from (select blockId,
 order by time_ratio desc;
 
 -- count tx by tx_time vs script_time ratio
+-- same as above but the transactions are grouped by integer ratio
 select t.time_ratio / 10,
        count(*),
-       avg(t.tx_time_us),
-       avg(t.script_time_us),
-       avg(t.script_count)
+       round(avg(t.tx_time_us), 1) as tx_time_us,
+       round(avg(t.script_time_us), 1) as script_time_us,
+       round(avg(t.script_count), 1) as script_count
 from (select tx.blockId,
              tx.txId,
              t.script_count,
@@ -116,11 +122,11 @@ group by t.time_ratio / 10
 order by t.time_ratio / 10 desc;
 
 -- small ratio: count tx by tx_time vs script_time
-select t.time_ratio,
+select round(t.time_ratio * 0.1, 1),
        count(*),
-       avg(t.tx_time_us),
-       avg(t.script_time_us),
-       avg(t.script_count)
+       round(avg(t.tx_time_us), 1) as tx_time_us,
+       round(avg(t.script_time_us), 1) as script_time_us,
+       round(avg(t.script_count), 1) as script_count
 from (select tx.blockId,
              tx.txId,
              t.script_count,
@@ -138,6 +144,24 @@ where t.time_ratio <= 19
 group by t.time_ratio
 order by t.time_ratio desc;
 
+-- Count transaction where script validation <= 80% of time
+select count(*)
+from (select tx.blockId,
+             tx.txId,
+             t.script_count,
+             tx.time / 1000                   as tx_time_us,
+             t.sum_script_time / 1000         as script_time_us,
+             tx.time * 10 / t.sum_script_time as time_ratio
+      from (select blockId,
+                   txId,
+                   sum(time) as sum_script_time,
+                   count(*)  as script_count
+            from verifyScript
+            group by blockId, txId) as t
+               join validateTxStateful as tx on tx.blockId = t.blockId and tx.txId = t.txId) as t
+where t.time_ratio >= 12;
+
+
 -- top tx block_time vs script_time ratio
 select b.blockId,
        t.script_count,
@@ -154,7 +178,29 @@ from (select blockId,
 order by time_ratio desc;
 
 -- count blocks by block_time vs script_time ratio
-select t.time_ratio / 10,
+select t.time_ratio / 10 as time_ratio,
+       count(*),
+       round(avg(t.block_time_us), 0)  as avg_block_time_us,
+       round(avg(t.tx_time_us), 0) as avg_tx_time_us,
+       round(avg(t.tx_count), 1)   as avg_tx_count
+from (select b.blockId,
+             tx.tx_count,
+             b.time / 1000                        as block_time_us,
+             tx.sum_tx_time / 1000            as tx_time_us,
+             (b.time - tx.sum_tx_time) / 1000 as time_diff_us,
+             b.time * 10 / tx.sum_tx_time     as time_ratio
+      from (select blockId,
+                   sum(time) as sum_tx_time,
+                   count(*)  as tx_count
+            from validateTxStateful
+            group by blockId) as tx
+               join applyTransactions as b on b.blockId = tx.blockId) as t
+group by t.time_ratio / 10
+order by t.time_ratio / 10;
+
+
+-- count blocks by block_time vs script_time ratio
+select t.time_ratio / 10 as time_ratio,
        count(*),
        round(avg(t.block_time_us), 0)  as avg_block_time_us,
        round(avg(t.script_time_us), 0) as avg_script_time_us,
@@ -196,7 +242,7 @@ where t.time_ratio <= 19
 group by t.time_ratio
 order by t.time_ratio desc;
 
--- v4.0: top 100 block costs vs total validation times
+-- v5.0: top 1000 block costs vs total validation times
 select b1.blockId,
        b1.height,
        b1.tx_num,
@@ -211,8 +257,8 @@ from appendFullBlock as b1
 order by time_us desc
 limit 1000;
 
--- count blocks where script time is less than 50%
-select count(*)
+-- group and count blocks by (total time / `applyTransactions` time) ratio
+select time_us / t2_us as ratio, count(*)
 from (select b1.blockId,
              b1.height,
              b1.tx_num,
@@ -223,9 +269,24 @@ from (select b1.blockId,
              (b1.time + b2.time + b3.time) / 1000 as time_us
       from appendFullBlock as b1
                join applyTransactions as b2 on b1.blockId = b2.blockId
-               join createUtxoState b3 on b1.blockId = b3.blockId
-      where time_us * 0.5 >= t3_us
-      order by b2.cost desc);
+               join createUtxoState b3 on b1.blockId = b3.blockId)
+group by ratio
+order by ratio;
+
+select round(time_us * 10 / t2_us * 0.1, 1) as ratio, count(*)
+from (select b1.blockId,
+             b1.height,
+             b1.tx_num,
+             b2.cost,
+             b1.time / 1000                       as t1_us,
+             b2.time / 1000                       as t2_us,
+             b3.time / 1000                       as t3_us,
+             (b1.time + b2.time + b3.time) / 1000 as time_us
+      from appendFullBlock as b1
+               join applyTransactions as b2 on b1.blockId = b2.blockId
+               join createUtxoState b3 on b1.blockId = b3.blockId)
+group by ratio
+order by ratio desc;
 
 select b1.blockId,
        b1.height,

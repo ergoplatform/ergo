16f77c7947e459d01ae6a51fd78b2b722f55eb83
Alex Chepurnoy <alex.chepurnoy@iohk.io>
2021-05-31 15:48:24 +0300

    support for multiple keys in ErgoUnsafeProver

--- ergo-wallet/src/main/java/org/ergoplatform/wallet/interface4j/crypto/ErgoUnsafeProver.java
+++ ergo-wallet/src/main/java/org/ergoplatform/wallet/interface4j/crypto/ErgoUnsafeProver.java
@@ -2,7 +2,9 @@ package org.ergoplatform.wallet.interface4j.crypto;
 
 import org.ergoplatform.ErgoLikeTransaction;
 import org.ergoplatform.UnsignedErgoLikeTransaction;
+import scala.collection.JavaConverters;
 import sigmastate.basics.DLogProtocol;
+import java.util.Map;
 
 /**
  * A wrapper over naive Ergo prover implementation.
@@ -16,6 +18,15 @@ public class ErgoUnsafeProver {
      */
     public ErgoLikeTransaction prove(UnsignedErgoLikeTransaction unsignedTx, DLogProtocol.DLogProverInput sk) {
         return org.ergoplatform.wallet.interpreter.ErgoUnsafeProver.prove(unsignedTx, sk);
+    }
+
+    /**
+     * Signs all inputs of a given `unsignedTx`.
+     *
+     * @return signed transaction
+     */
+    public ErgoLikeTransaction prove(UnsignedErgoLikeTransaction unsignedTx, Map<String, DLogProtocol.DLogProverInput> sks) {
+        return org.ergoplatform.wallet.interpreter.ErgoUnsafeProver.prove(unsignedTx, JavaConverters.mapAsScalaMap(sks));
     }
 
 }

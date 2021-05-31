e784a70b8fabf7ae41f2ac9aa593a647f488100c
Alexander Slesarenko <avslesarenko@gmail.com>
2021-05-31 22:03:44 +0300

    v5.0-only-eval: fix initialCost

--- src/main/scala/org/ergoplatform/modifiers/mempool/ErgoTransaction.scala
+++ src/main/scala/org/ergoplatform/modifiers/mempool/ErgoTransaction.scala
@@ -136,7 +136,7 @@ case class ErgoTransaction(override val inputs: IndexedSeq[Input],
     // Cost of transaction initialization: we should read and parse all inputs and data inputs,
     // and also iterate through all outputs to check rules
     val initialCost: Long = addExact(
-      1000, // TODO v5.0: introduce global constant
+      CostTable.interpreterInitCost, // TODO v5.0: introduce global constant
       multiplyExact(boxesToSpend.size, stateContext.currentParameters.inputCost),
       multiplyExact(dataBoxes.size, stateContext.currentParameters.dataInputCost),
       multiplyExact(outputCandidates.size, stateContext.currentParameters.outputCost),

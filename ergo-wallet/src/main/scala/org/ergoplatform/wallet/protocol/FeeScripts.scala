096d70e20828b6a2aa37673e0d7775f3ce207d84
Alex Chepurnoy <alex.chepurnoy@iohk.io>
2021-09-09 15:47:24 +0300

    testnet script

--- ergo-wallet/src/main/scala/org/ergoplatform/wallet/protocol/FeeScripts.scala
+++ ergo-wallet/src/main/scala/org/ergoplatform/wallet/protocol/FeeScripts.scala
@@ -4,7 +4,12 @@ import org.ergoplatform.{ErgoAddressEncoder, ErgoBox, ErgoScriptPredef}
 import sigmastate.Values.ErgoTree
 
 object FeeScripts extends App {
-  def addr(tree: ErgoTree) = {
+  def testnetAddr() = {
+    val testnetScript = ErgoScriptPredef.feeProposition(72)
+    ErgoAddressEncoder.apply(ErgoAddressEncoder.TestnetNetworkPrefix).fromProposition(testnetScript).get
+  }
+
+  def mainnetAddr(tree: ErgoTree) = {
     ErgoAddressEncoder.apply(ErgoAddressEncoder.MainnetNetworkPrefix).fromProposition(tree).get
   }
 
@@ -22,10 +27,12 @@ object FeeScripts extends App {
   val compilerV0 = ErgoAddressEncoder.apply(ErgoAddressEncoder.MainnetNetworkPrefix)
     .fromString("4MQyML64GnzMxZgm").get.script
 
-  println("Initial fee proposition: " + addr(initialFeeScript))
-  println("Simplest true script(v0): " + addr(trueFeeScriptV0))
-  println("Simplest true script(v1): " + addr(trueFeeScriptV1))
+  println("Initial fee proposition: " + mainnetAddr(initialFeeScript))
+  println("Simplest true script(v0): " + mainnetAddr(trueFeeScriptV0))
+  println("Simplest true script(v1): " + mainnetAddr(trueFeeScriptV1))
   println("Compiler-produced script(v0): 4MQyML64GnzMxZgm")
+
+  println("Testnet script: " + testnetAddr())
 
   val initialFeeScriptBytes = initialFeeScript.bytes
   val trueFeeScriptV0Bytes = trueFeeScriptV0.bytes

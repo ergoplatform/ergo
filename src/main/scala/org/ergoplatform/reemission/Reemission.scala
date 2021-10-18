523927ddfbc95406cd38dadbb73027ade1757128
Alex Chepurnoy <alex.chepurnoy@iohk.io>
2021-10-18 23:45:42 +0300

    reemission contract update

--- src/main/scala/org/ergoplatform/reemission/Reemission.scala
+++ src/main/scala/org/ergoplatform/reemission/Reemission.scala
@@ -1,33 +1,46 @@
 package org.ergoplatform.reemission
 
-import org.ergoplatform.ErgoScriptPredef.boxCreationHeight
-import org.ergoplatform.{Height, Outputs, Self}
+import org.ergoplatform.ErgoScriptPredef.{boxCreationHeight, expectedMinerOutScriptBytesVal}
+import org.ergoplatform.{Height, MinerPubkey, Outputs, Self}
 import org.ergoplatform.settings.MonetarySettings
 import sigmastate.{AND, EQ, GE, GT, Minus, OR}
 import sigmastate.Values.{ErgoTree, IntConstant}
-import sigmastate.utxo.{ByIndex, ExtractAmount}
+import sigmastate.utxo.{ByIndex, ExtractAmount, ExtractScriptBytes}
 
 object Reemission {
+
+  val emissionPeriod = 2080800
+
   def reemissionBoxProp(s: MonetarySettings): ErgoTree = {
-    val rewardOut = ByIndex(Outputs, IntConstant(0))
-    val heightCorrect = EQ(boxCreationHeight(rewardOut), Height)
+    val reemissionOut = ByIndex(Outputs, IntConstant(0))
+    val minerOut = ByIndex(Outputs, IntConstant(1))
+
+    val correctMinerOutput = AND(
+      EQ(ExtractScriptBytes(minerOut), expectedMinerOutScriptBytesVal(s.minerRewardDelay, MinerPubkey)),
+      EQ(Height, boxCreationHeight(minerOut))
+    )
+
+    val heightCorrect = EQ(boxCreationHeight(reemissionOut), Height)
     val heightIncreased = GT(Height, boxCreationHeight(Self))
-    val validPeriod = GE(Height, IntConstant(2080800))
+    val afterEmission = GE(Height, IntConstant(emissionPeriod))
 
+    val sameScriptRule = EQ(ExtractScriptBytes(Self), ExtractScriptBytes(rewardOut))
     val coinsToIssue = s.oneEpochReduction // 3 ERG
-    val correctCoinsIssued = EQ(coinsToIssue, Minus(ExtractAmount(Self), ExtractAmount(rewardOut)))
+    val correctCoinsIssued = EQ(coinsToIssue, Minus(ExtractAmount(Self), ExtractAmount(reemissionOut)))
 
-    val sponsored = GT(ExtractAmount(rewardOut), ExtractAmount(Self))
+    val sponsored = GT(ExtractAmount(reemissionOut), ExtractAmount(Self))
 
-    OR(
-      AND(
-        sponsored,
-        heightCorrect),
-      AND(
-        validPeriod,
-        heightIncreased,
-        heightCorrect,
-        correctCoinsIssued
+    AND(
+      sameScriptRule,
+      heightCorrect,
+      OR(
+        AND(sponsored),
+        AND(
+          correctMinerOutput,
+          afterEmission,
+          heightIncreased,
+          correctCoinsIssued
+        )
       )
     ).toSigmaProp.treeWithSegregation
   }

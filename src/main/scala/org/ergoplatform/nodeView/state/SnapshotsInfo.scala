d2ab04e1e00beb4d7db9f0a86497d46b3a378f98
Alex Chepurnoy <alex.chepurnoy@iohk.io>
2021-11-18 01:06:39 +0300

    writeSnapshotsInfo / readSnapshotsInfo

--- src/main/scala/org/ergoplatform/nodeView/state/SnapshotsInfo.scala
+++ src/main/scala/org/ergoplatform/nodeView/state/SnapshotsInfo.scala
@@ -4,11 +4,16 @@ import com.google.common.primitives.{Bytes, Ints}
 import org.ergoplatform.ErgoLikeContext.Height
 import org.ergoplatform.nodeView.state.UtxoState.ManifestId
 import org.ergoplatform.settings.ErgoSettings
+import scorex.crypto.hash.Digest32
 import scorex.db.{LDBFactory, LDBKVStore}
+
+import scala.util.Try
 
 case class SnapshotsInfo(availableManifests: Map[Height, ManifestId])
 
 class SnapshotsDb(store: LDBKVStore) {
+
+  private val snapshotInfoKey: Array[Byte] = Array.fill(32)(0: Byte)
 
   private def snapshotsInfoToBytes(snapshotsInfo: SnapshotsInfo): Array[Byte] = {
     Bytes.concat(
@@ -18,15 +23,41 @@ class SnapshotsDb(store: LDBKVStore) {
     )
   }
 
-  def writeSnapshot(height: Height, manifest: UtxoState.Manifest, subtrees: Seq[UtxoState.Subtree]): Unit = {
-    snapshotsInfoToBytes(null)
+  private def snapshotsInfoFromBytes(bytes: Array[Byte]): SnapshotsInfo = {
+    val manifests = bytes.grouped(36).map {rowBytes =>
+      val height = Ints.fromByteArray(rowBytes.take(4))
+      val manifestId = Digest32 @@ rowBytes.drop(4)
+      height -> manifestId
+    }.toMap
+    SnapshotsInfo(manifests)
+  }
+
+  def writeSnapshotsInfo(snapshotsInfo: SnapshotsInfo): Try[Unit] = {
+    store.insert(Seq(snapshotInfoKey -> snapshotsInfoToBytes(snapshotsInfo)))
+  }
+
+  def readSnapshotsInfo: Option[SnapshotsInfo] = {
+    store.get(snapshotInfoKey).map(snapshotsInfoFromBytes)
+  }
+
+  def writeSnapshot(height: Height,
+                    manifest: UtxoState.Manifest,
+                    subtrees: Seq[UtxoState.Subtree]): Unit = {
+    ???
   }
 
 }
 
 object SnapshotsDb {
-  def create(ergoSettings: ErgoSettings): Unit ={
-    val store = LDBFactory.createKvDb(s"${ergoSettings.directory}/snapshots")
+
+  def create(ergoSettings: ErgoSettings): Unit = {
+    val dir = s"${ergoSettings.directory}/snapshots"
+    create(dir)
+  }
+
+  private[state] def create(dir: String): SnapshotsDb = {
+    val store = LDBFactory.createKvDb(dir)
     new SnapshotsDb(store)
   }
+
 }
---
+++ src/test/scala/org/ergoplatform/nodeView/state/SnapshotsDbSpecification.scala
@@ -0,0 +1,26 @@
+package org.ergoplatform.nodeView.state
+
+import org.ergoplatform.utils.ErgoPropertyTest
+import org.scalacheck.Gen
+import scorex.crypto.hash.Digest32
+import scorex.util.{idToBytes, bytesToId}
+import scala.util.Random
+
+class SnapshotsDbSpecification extends ErgoPropertyTest {
+  property("snapshotsInfo round-trip") {
+    forAll(Gen.nonEmptyListOf(modifierIdGen)){manifestIds =>
+      val m = manifestIds.map{mid =>
+        Random.nextInt(1000000) -> (Digest32 @@ idToBytes(mid))
+      }.toMap
+      val si = SnapshotsInfo(m)
+      val dir = createTempDir.getAbsolutePath
+      val db = SnapshotsDb.create(dir)
+      db.writeSnapshotsInfo(si)
+
+      val read = db.readSnapshotsInfo.get.availableManifests.mapValues(bs => bytesToId(bs))
+      val siTocompare = si.availableManifests.mapValues(bs => bytesToId(bs))
+
+      read shouldBe siTocompare
+    }
+  }
+}

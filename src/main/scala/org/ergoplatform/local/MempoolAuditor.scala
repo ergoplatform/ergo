0a1c253c629b6c22c7e8c8b8debf587aef840ef6
Alex Chepurnoy <alex.chepurnoy@iohk.io>
2022-01-25 22:11:27 +0300

    reverse reverted

--- src/main/scala/org/ergoplatform/local/MempoolAuditor.scala
+++ src/main/scala/org/ergoplatform/local/MempoolAuditor.scala
@@ -112,7 +112,7 @@ class MempoolAuditor(nodeViewHolderRef: ActorRef,
   private def rebroadcastTransactions(): Unit = {
     log.debug("Rebroadcasting transactions")
     poolReaderOpt.foreach { pr =>
-      val toBroadcast = pr.random(settings.nodeSettings.rebroadcastCount).toSeq.reverse
+      val toBroadcast = pr.random(settings.nodeSettings.rebroadcastCount).toSeq
       stateReaderOpt match {
         case Some(utxoState: UtxoStateReader) =>
           val stateToCheck = utxoState.withTransactions(toBroadcast)

7b837b9fdcdd0cf26cc5cb2a7db70fae3515cd0a
ross-weir <29697678+ross-weir@users.noreply.github.com>
2022-04-06 11:23:36 +1000

    make uninitialized keystore less pronounced

--- src/main/scala/org/ergoplatform/nodeView/wallet/ErgoWalletService.scala
+++ src/main/scala/org/ergoplatform/nodeView/wallet/ErgoWalletService.scala
@@ -24,6 +24,7 @@ import scorex.util.encode.Base16
 import scorex.util.{ModifierId, bytesToId}
 import sigmastate.Values.SigmaBoolean
 
+import java.io.FileNotFoundException
 import scala.util.{Failure, Success, Try}
 
 /**
@@ -243,7 +244,12 @@ class ErgoWalletServiceImpl extends ErgoWalletService with ErgoWalletSupport wit
         log.info("Trying to read wallet in secure mode ..")
         JsonSecretStorage.readFile(secretStorageSettings).fold(
           e => {
-            log.warn(s"Failed to read wallet. Manual initialization is required. Details: ", e)
+            e match {
+              case e: FileNotFoundException =>
+                log.info(s"Wallet secret storage not found. Details: {}", e.getMessage)
+              case _ =>
+                log.warn(s"Failed to read wallet. Manual initialization is required. Details: ", e)
+            }
             state
           },
           secretStorage => {

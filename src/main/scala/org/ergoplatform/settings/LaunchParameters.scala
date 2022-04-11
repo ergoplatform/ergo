c4f43fab5bb5fd31aafac83158d8c10facea9b90
kushti <alex.chepurnoy@iohk.io>
2022-04-11 19:04:32 +0300

    eip27Supported = false in LaunchParameters

--- src/main/scala/org/ergoplatform/settings/LaunchParameters.scala
+++ src/main/scala/org/ergoplatform/settings/LaunchParameters.scala
@@ -5,5 +5,5 @@ package org.ergoplatform.settings
   */
 object LaunchParameters extends Parameters(height = 0,
   parametersTable = Parameters.DefaultParameters,
-  eip27Supported = true,
+  eip27Supported = false,
   proposedUpdate = ErgoValidationSettingsUpdate.empty)

ef68ebb354417f952ed22d29a53cedc5933739ce
Alexander Chepurnoy <alex.chepurnoy@iohk.io>
2022-09-27 15:31:15 +0300

    Update src/main/scala/org/ergoplatform/settings/Constants.scala
    Co-authored-by: Alexander Slesarenko <aslesarenko@users.noreply.github.com>

--- src/main/scala/org/ergoplatform/settings/Constants.scala
+++ src/main/scala/org/ergoplatform/settings/Constants.scala
@@ -54,7 +54,7 @@ object Constants {
   val SoftForkEpochs = 32 //about 45.5 days
 
   def TrueLeaf: ErgoTree = Values.TrueLeaf.toSigmaProp
-  val FalseLeaf: ErgoTree = Values.FalseLeaf.toSigmaProp
+  def FalseLeaf: ErgoTree = Values.FalseLeaf.toSigmaProp
 
   val StringEncoding = "UTF-8"
 

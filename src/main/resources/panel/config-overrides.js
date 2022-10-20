ffd9f290726efdd640691bd42357e7d23a570cd5
deadit <stdeadit@gmail.com>
2022-10-20 22:46:38 +0300

    update node interface

--- src/main/resources/panel/config-overrides.js
+++
@@ -1,5 +0,0 @@
-const { override } = require('customize-cra');
-const { addReactRefresh } = require('customize-cra-react-refresh');
-
-/* config-overrides.js */
-module.exports = override(addReactRefresh());

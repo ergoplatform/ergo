13068e313c92d732ac79eb7482398d70bb08ce6a
Alexander Chepurnoy <alex.chepurnoy@iohk.io>
2023-01-09 17:20:06 +0300

    UTXOSnapshotManifest removed

--- src/main/scala/org/ergoplatform/modifiers/state/UTXOSnapshotManifest.scala
+++ src/main/scala/org/ergoplatform/modifiers/state/UTXOSnapshotManifest.scala
@@ -1,40 +1,6 @@
 package org.ergoplatform.modifiers.state
 
-import org.ergoplatform.modifiers.BlockSection
-import org.ergoplatform.modifiers.history.header.Header
-import org.ergoplatform.nodeView.history.ErgoHistory.Height
-import org.ergoplatform.settings.Algos
 import scorex.core.ModifierTypeId
-import scorex.core.serialization.ScorexSerializer
-import scorex.crypto.authds.avltree.batch.serialization.BatchAVLProverManifest
-import scorex.crypto.authds.ADDigest
-import scorex.crypto.hash.Digest32
-import scorex.util.{ModifierId, bytesToId, idToBytes}
-
-import scala.util.Try
-
-case class UTXOSnapshotManifest(
-                                height: Height,
-                                blockId: ModifierId,
-                                utxoSetDigest: ADDigest, //33 bytes! extra byte with tree height here!
-                                manifest: BatchAVLProverManifest[Digest32]
-                               ) extends BlockSection {
-
-  override val modifierTypeId: ModifierTypeId = UTXOSnapshotManifest.modifierTypeId
-
-  override lazy val serializedId: Array[Byte] = Algos.hash(idToBytes(blockId) ++ manifest.id)
-
-  override lazy val id: ModifierId = bytesToId(serializedId)
-
-  override type M = UTXOSnapshotManifest
-
-  override def serializer: ScorexSerializer[UTXOSnapshotManifest] = ???
-
-  override def parentId: ModifierId = ???
-
-  override val sizeOpt: Option[Int] = None
-  
-}
 
 object UTXOSnapshotManifest {
 

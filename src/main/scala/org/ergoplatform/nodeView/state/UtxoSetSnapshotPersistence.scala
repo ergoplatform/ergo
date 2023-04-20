c1c0922f3ff0b48c3d596449ae1ddd1d3bce5fc1
Alexander Chepurnoy <alex.chepurnoy@iohk.io>
2023-04-21 00:15:50 +0300

    UtxoSetSnapshotPersistence scaladoc

--- src/main/scala/org/ergoplatform/nodeView/state/UtxoSetSnapshotPersistence.scala
+++ src/main/scala/org/ergoplatform/nodeView/state/UtxoSetSnapshotPersistence.scala
@@ -11,17 +11,33 @@ import org.ergoplatform.settings.Constants.{MakeSnapshotEvery, timeToTakeSnapsho
 import scala.concurrent.Future
 import scala.util.Try
 
+/**
+  * Functions needed for storing UTXO set snapshots and working with them
+  */
 trait UtxoSetSnapshotPersistence extends ScorexLogging {
 
   protected def constants: StateConstants
   protected def persistentProver: PersistentBatchAVLProver[Digest32, HF]
 
-  private[nodeView] val snapshotsDb = SnapshotsDb.create(constants.settings) //todo: move to some other place ?
+  private[nodeView] val snapshotsDb = SnapshotsDb.create(constants.settings)
 
+  // Dump current UTXO set snapshot to persistent snapshots database
+  // private[nodeView] as used in tests also
   private[nodeView] def dumpSnapshot(height: Height): Try[Array[Byte]] = {
     snapshotsDb.writeSnapshot(persistentProver.storage.asInstanceOf[VersionedLDBAVLStorage], height)
   }
 
+  /**
+    * Check if it is time to store UTXO set snapshot, if so, store it asynchronously,
+    * to avoid locking the thread currently working with UTXO set, and then prune snapshots which become
+    * obsolete (also in a separate thread).
+    *
+    * Thus this method is doing everything which is needed to store snapshot when needed. Client logic then
+    * may simply call it on every block applied to the state.
+    *
+    * @param height - height of a block just processed (so we work with UTXO set after block with this height applied)
+    * @param estimatedTip - estimated height of best blockchain in the network
+    */
   protected def saveSnapshotIfNeeded(height: Height, estimatedTip: Option[Height]): Unit = {
     if (constants.settings.nodeSettings.areSnapshotsStored &&
         timeToTakeSnapshot(height) &&
@@ -44,18 +60,25 @@ trait UtxoSetSnapshotPersistence extends ScorexLogging {
     }
   }
 
-  def snapshotsAvailable(): SnapshotsInfo = {
-    snapshotsDb.readSnapshotsInfo
-  }
-
+  /**
+    * @return list of stored UTXO set snapshots
+    */
   def getSnapshotInfo(): SnapshotsInfo = {
     snapshotsDb.readSnapshotsInfo
   }
 
+  /**
+    * Read snapshot manifest bytes from database without decoding. Used to serve clients over the wire.
+    * @param id - manifest id
+    */
   def getManifestBytes(id: ManifestId): Option[Array[Byte]] = {
     snapshotsDb.readManifestBytes(id)
   }
 
+  /**
+    * Read snapshot chunk (subtree) bytes from database without decoding. Used to serve clients over the wire.
+    * @param id - subtree id
+    */
   def getUtxoSnapshotChunkBytes(id: SubtreeId): Option[Array[Byte]] = {
     snapshotsDb.readSubtreeBytes(id)
   }

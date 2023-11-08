31e651a5308d5e112f18d866a0532c1fbde88960
jellymlg <erikpanta@gmail.com>
2023-11-08 18:19:12 +0100

    Undo changes

--- src/main/scala/org/ergoplatform/nodeView/history/storage/modifierprocessors/UtxoSetSnapshotProcessor.scala
+++ src/main/scala/org/ergoplatform/nodeView/history/storage/modifierprocessors/UtxoSetSnapshotProcessor.scala
@@ -58,6 +58,7 @@ trait UtxoSetSnapshotProcessor extends MinimalFullBlockHeightFunctions with Scor
       _cachedDownloadPlan.map(_.latestUpdateTime).getOrElse(0L) - _cachedDownloadPlan.map(_.createdTime).getOrElse(0L)
     }
     log.info(s"UTXO set downloading and application time: ${utxoPhaseTime / 1000.0} s.")
+    // remove downloaded utxo set snapshots chunks
     val ts0 = System.currentTimeMillis()
     _cachedDownloadPlan.foreach { plan =>
       val chunkIdsToRemove = downloadedChunkIdsIterator(plan.totalChunks)
@@ -69,6 +70,7 @@ trait UtxoSetSnapshotProcessor extends MinimalFullBlockHeightFunctions with Scor
     _cachedDownloadPlan = None
     val ts = System.currentTimeMillis()
     log.info(s"Imported UTXO set snapshots chunks removed in ${ts - ts0} ms")
+
     // set height of first full block to be downloaded
     writeMinimalFullBlockHeight(height + 1)
   }

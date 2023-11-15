280b3fe3f3d160e8ae26a33871f4bc4dd9e11094
Alexander Chepurnoy <alex.chepurnoy@iohk.io>
2023-11-15 22:55:28 +0300

    removing unused code from utils

--- src/main/scala/scorex/core/utils/utils.scala
+++ src/main/scala/scorex/core/utils/utils.scala
@@ -8,25 +8,6 @@ import scala.util.{Failure, Success, Try}
 
 package object utils {
 
-  @deprecated("Use scorex.util.ScorexLogging instead.", "scorex-util 0.1.0")
-  type ScorexLogging = scorex.util.ScorexLogging
-
-  /**
-    * @param block - function to profile
-    * @return - execution time in seconds and function result
-    */
-  def profile[R](block: => R): (Float, R) = {
-    val t0 = System.nanoTime()
-    val result = block // call-by-name
-    val t1 = System.nanoTime()
-    ((t1 - t0).toFloat / 1000000000, result)
-  }
-
-  def toTry(b: Boolean, msg: String): Try[Unit] = b match {
-    case true => Success(Unit)
-    case false => Failure(new Exception(msg))
-  }
-
   @tailrec
   final def untilTimeout[T](timeout: FiniteDuration,
                             delay: FiniteDuration = 100.milliseconds)(fn: => T): T = {
@@ -54,22 +35,6 @@ package object utils {
     seq.foreach { array =>
       System.arraycopy(array, 0, result, pos, array.length)
       pos += array.length
-    }
-    result
-  }
-
-  def concatFixLengthBytes(seq: Traversable[Array[Byte]]): Array[Byte] = seq.headOption match {
-    case None => Array[Byte]()
-    case Some(head) => concatFixLengthBytes(seq, head.length)
-  }
-
-
-  def concatFixLengthBytes(seq: Traversable[Array[Byte]], length: Int): Array[Byte] = {
-    val result: Array[Byte] = new Array[Byte](seq.toSeq.length * length)
-    var index = 0
-    seq.foreach { s =>
-      Array.copy(s, 0, result, index, length)
-      index += length
     }
     result
   }

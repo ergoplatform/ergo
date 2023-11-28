2a605ef7d1a1c37c66191558741694e5a8b2301d
Alexander Chepurnoy <alex.chepurnoy@iohk.io>
2023-11-28 14:19:16 +0300

    1111k

--- src/main/scala/org/ergoplatform/utils/FoundationBoxSigner.scala
+++ src/main/scala/org/ergoplatform/utils/FoundationBoxSigner.scala
@@ -53,7 +53,7 @@ object FoundationBoxSigner extends App {
   val preSign: ACTION = 1
   val sign: ACTION = 2
 
-  val height = 1110000
+  val height = 1111000
 
   //data which should be MANUALLY changed in order to interact with the program
   val seed = "..."
@@ -119,7 +119,7 @@ object FoundationBoxSigner extends App {
 
   val addr = enc.fromString("9g1ReLmsbGevkTjfPGMdRMoFRdrAVskZVmSBpaEdVW2DfG4HDWm").get
 
-  val withdrawalAmount = 23000 * EmissionRules.CoinsInOneErgo
+  val withdrawalAmount = 16200 * EmissionRules.CoinsInOneErgo
   val withdrawalOutputs = IndexedSeq(new ErgoBoxCandidate(withdrawalAmount, addr.script, height))
 
   val foundationOutput = new ErgoBoxCandidate(gfBox.value - withdrawalAmount - fee, gfBox.ergoTree, height,

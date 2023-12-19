e93daf02c121e2a6db13363f02480caa66da126f
Alexander Chepurnoy <alex.chepurnoy@iohk.io>
2023-12-19 20:59:01 +0300

    asInstanceOf removed in SortDirection

--- src/main/scala/org/ergoplatform/http/api/SortDirection.scala
+++ src/main/scala/org/ergoplatform/http/api/SortDirection.scala
@@ -7,8 +7,8 @@ object SortDirection {
 
   type Direction = Byte
 
-  val ASC: Direction = 1.asInstanceOf[Direction]
-  val DESC: Direction = 0.asInstanceOf[Direction]
-  val INVALID: Direction = (-1).asInstanceOf[Direction]
+  val ASC: Direction = 1.toByte
+  val DESC: Direction = 0.toByte
+  val INVALID: Direction = (-1).toByte
 
 }

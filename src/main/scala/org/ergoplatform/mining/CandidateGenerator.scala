4e0e923cf7b121d136ac4dd5001fe8b966e32b97
Alexander Chepurnoy <alex.chepurnoy@iohk.io>
2024-08-23 13:40:34 +0300

    devnet settings

--- src/main/scala/org/ergoplatform/mining/CandidateGenerator.scala
+++ src/main/scala/org/ergoplatform/mining/CandidateGenerator.scala
@@ -409,10 +409,6 @@ object CandidateGenerator extends ScorexLogging {
       ergoSettings.votingTargets.softForkOption.getOrElse(0) == 1
     }
 
-    //todo: remove after 5.0 soft-fork activation
-    log.debug(s"betterVersion: $betterVersion, forkVotingAllowed: $forkVotingAllowed, " +
-              s"forkOrdered: $forkOrdered, nextHeightCondition: $nextHeightCondition")
-
     betterVersion &&
       forkVotingAllowed &&
       forkOrdered &&

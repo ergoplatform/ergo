b7e41329d2fe1a41e6e4623b07836d097f0b1dcb
Alexander Chepurnoy <alex.chepurnoy@iohk.io>
2025-01-08 13:38:40 +0300

    unsigned big int serialization in register - initial test

--- src/test/scala/org/ergoplatform/modifiers/mempool/ErgoNodeTransactionSpec.scala
+++ src/test/scala/org/ergoplatform/modifiers/mempool/ErgoNodeTransactionSpec.scala
@@ -1,31 +1,32 @@
 package org.ergoplatform.modifiers.mempool
 
-import org.ergoplatform.ErgoBox.TokenId
+import org.ergoplatform.ErgoBox.{R4, TokenId}
 import org.ergoplatform.nodeView.state.{ErgoStateContext, VotingData}
 import org.ergoplatform.settings._
 import org.ergoplatform.utils.{ErgoCompilerHelpers, ErgoCorePropertyTest, ErgoStateContextHelpers}
 import org.ergoplatform.wallet.interpreter.ErgoInterpreter
-import org.ergoplatform.{ErgoBox, ErgoBoxCandidate, Input}
+import org.ergoplatform.{ErgoBox, ErgoBoxCandidate, ErgoTreePredef, Input}
 import scorex.util.{ModifierId, bytesToId}
 import sigmastate.eval.Extensions._
 import org.ergoplatform.nodeView.ErgoContext
 import org.ergoplatform.sdk.wallet.protocol.context.TransactionContext
 import org.ergoplatform.settings.Parameters.MaxBlockCostIncrease
 import org.ergoplatform.settings.ValidationRules.{bsBlockTransactionsCost, txAssetsInOneBox}
-import org.ergoplatform.wallet.boxes.ErgoBoxAssetExtractor
+import org.ergoplatform.wallet.boxes.{ErgoBoxAssetExtractor, ErgoBoxSerializer}
 import org.ergoplatform.wallet.interpreter.TransactionHintsBag
 import org.ergoplatform.wallet.protocol.context.InputContext
 import org.scalacheck.Gen
 import sigma.util.BenchmarkUtil
 import scorex.crypto.hash.Blake2b256
 import scorex.util.encode.Base16
-import sigma.Colls
+import sigma.{Colls, VersionContext}
 import sigma.ast.ErgoTree.DefaultHeader
-import sigma.ast.{AND, ErgoTree, TrueLeaf}
+import sigma.ast.{AND, ErgoTree, TrueLeaf, UnsignedBigIntConstant}
 import sigma.interpreter.{ContextExtension, ProverResult}
 import sigma.serialization.ErgoTreeSerializer.DefaultSerializer
 import sigmastate.helpers.TestingHelpers._
 
+import java.math.BigInteger
 import scala.util.{Random, Try}
 
 class ErgoNodeTransactionSpec extends ErgoCorePropertyTest with ErgoCompilerHelpers with ErgoStateContextHelpers {
@@ -569,6 +570,19 @@ class ErgoNodeTransactionSpec extends ErgoCorePropertyTest with ErgoCompilerHelp
 
     val f = utx.statefulValidity(IndexedSeq(b), IndexedSeq.empty, stateContext, 0)(defaultProver)
     f.isSuccess shouldBe false
+  }
+
+  property("Boxes containing 6.0 data types") {
+    val ubic = UnsignedBigIntConstant(new BigInteger("2"))
+    val b = new ErgoBox(1000000000L, ErgoTreePredef.TrueProp(ErgoTree.defaultHeaderWithVersion(3.toByte)), Colls.emptyColl,
+      Map(R4 -> ubic), ModifierId @@ "c95c2ccf55e03cac6659f71ca4df832d28e2375569cec178dcb17f3e2e5f7742",
+      0, 0)
+
+    VersionContext.withVersions(3, 3) {
+      val bytes = ErgoBoxSerializer.toBytes(b)
+      println(Base16.encode(bytes))
+      ErgoBoxSerializer.parseBytes(bytes) shouldBe b
+    }
   }
 
 }

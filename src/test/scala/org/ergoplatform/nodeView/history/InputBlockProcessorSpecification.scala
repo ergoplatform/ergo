49acb70d89159218c1e41264553feadaf6f8fc51
Alexander Chepurnoy <alex.chepurnoy@iohk.io>
2025-02-04 11:51:16 +0300

    input blocks application tests plan

---
+++ src/test/scala/org/ergoplatform/nodeView/history/InputBlockProcessorSpecification.scala
@@ -0,0 +1,39 @@
+package org.ergoplatform.nodeView.history
+
+import org.ergoplatform.utils.ErgoCorePropertyTest
+
+class InputBlockProcessorSpecification extends ErgoCorePropertyTest {
+
+  property("apply first input block after ordering block") {
+
+  }
+
+  property("apply child input block of best input block") {
+
+  }
+
+  property("apply input block with parent input block not available") {
+
+  }
+
+  property("apply input block with parent ordering block not available") {
+
+  }
+
+  property("apply input block with parent ordering block in the past") {
+
+  }
+
+  property("apply input block with parent ordering block in the past") {
+
+  }
+
+  property("apply input block with non-best parent input block") {
+
+  }
+
+  property("apply new best input block (input blocks chain switch)") {
+
+  }
+
+}

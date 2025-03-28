473531f4517cd82d461a759f103b90c67609b8e8
Alexander Chepurnoy <alex.chepurnoy@iohk.io>
2025-03-28 13:32:13 +0300

    option[header] test improved

--- src/test/scala/org/ergoplatform/modifiers/mempool/ErgoNodeTransactionSpec.scala
+++ src/test/scala/org/ergoplatform/modifiers/mempool/ErgoNodeTransactionSpec.scala
@@ -489,9 +489,11 @@ class ErgoNodeTransactionSpec extends ErgoCorePropertyTest with ErgoCompilerHelp
       |  ub >= 0
       | } """.stripMargin,
     """{
-      |   val bs = fromBase16("014201ad7fffba807080ce7f808001b4771880bdffbd7fff857f8db17fdc89b0015bff0189486d80934a84015e03c064c6d100c17fae55bf01f380007fbb00ff01d9000000013f0680d10015ff11c4b7ffff01ffb2d27f0001050064010080ffb93e7fff4cc15a2bffdd770480007f8080ff8026802f7f51007f004b01ff0080002380a581e8c0ca81dfca62007fa7ff01860019f37f3c4491067cd1001ee07e570fe8689b80ff11017aff7408703927a2bbc4fa0200000000032bdb4c610ddf0078b4f31f5a36bff7b003704dfc93b201fedd8fe331d9ff1102008d7fff010d80ff")
-      |   val oh = Global.deserializeTo[Option[Header]](bs)
-      |   sigmaProp(oh.isDefined)
+      |   val bs1 = fromBase16("014201ad7fffba807080ce7f808001b4771880bdffbd7fff857f8db17fdc89b0015bff0189486d80934a84015e03c064c6d100c17fae55bf01f380007fbb00ff01d9000000013f0680d10015ff11c4b7ffff01ffb2d27f0001050064010080ffb93e7fff4cc15a2bffdd770480007f8080ff8026802f7f51007f004b01ff0080002380a581e8c0ca81dfca62007fa7ff01860019f37f3c4491067cd1001ee07e570fe8689b80ff11017aff7408703927a2bbc4fa0200000000032bdb4c610ddf0078b4f31f5a36bff7b003704dfc93b201fedd8fe331d9ff1102008d7fff010d80ff")
+      |   val oh1 = Global.deserializeTo[Option[Header]](bs1)
+      |   val bs2 = fromBase16("00")
+      |   val oh2 = Global.deserializeTo[Option[Header]](bs2)
+      |   sigmaProp(oh1.isDefined && oh2.isEmpty)
       | } """.stripMargin
   )
 

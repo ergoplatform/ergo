3394616a2dee15e96ba55f307ecb45fb00bf7c6e
Alexander Chepurnoy <alex.chepurnoy@iohk.io>
2025-06-09 18:34:22 +0300

    test for applying input block with ok transaction

--- src/test/scala/org/ergoplatform/nodeView/history/modifierprocessors/InputBlockProcessorSpecification.scala
+++ src/test/scala/org/ergoplatform/nodeView/history/modifierprocessors/InputBlockProcessorSpecification.scala
@@ -452,11 +452,43 @@ class InputBlockProcessorSpecification extends ErgoCorePropertyTest with ErgoCom
     h.bestInputBlocksChain() shouldBe Seq()
   }
 
-  property("apply new best input block on another ordering block on the same height") {
+  property("apply input block with normal transaction") {
+    val bh = BoxHolder(Seq(eb1))
+    val us = UtxoState.fromBoxHolder(bh, None, createTempDir, settings, parameters)
+    val tx1 = validTransactionsFromBoxHolder(bh, new RandomWrapper(Some(1)), 201)._1
 
+    val h = generateHistory(verifyTransactions = true, StateType.Utxo, PoPoWBootstrap = false, blocksToKeep = -1,
+      epochLength = 10000, useLastEpochs = 3, initialDiffOpt = None, None)
+    val c1 = genChain(height = 2, history = h, stateOpt = Some(us)).toList
+    applyChain(h, c1)
+
+    val c2 = genChain(2, h, stateOpt = Some(us)).tail
+    c2.head.header.parentId shouldBe h.bestHeaderOpt.get.id
+    h.bestFullBlockOpt.get.id shouldBe c1.last.id
+
+    val ib1 = InputBlockInfo(1, c2(0).header, InputBlockFields.empty)
+    val r1 = h.applyInputBlock(ib1)
+    r1 shouldBe None
+    h.getInputBlock(ib1.id) shouldBe Some(ib1)
+    h.getOrderingBlockTips(h.bestHeaderOpt.get.id).get should contain(ib1.id)
+    h.getOrderingBlockTipHeight(h.bestHeaderOpt.get.id).get shouldBe 1
+    h.isAncestor(ib1.id, ib1.id).isEmpty shouldBe true
+
+    val c3 = genChain(height = 2, history = h, stateOpt = Some(us)).tail
+    c3.head.header.parentId shouldBe h.bestHeaderOpt.get.id
+    h.bestFullBlockOpt.get.id shouldBe c1.last.id
+
+    // apply transactions
+    // input block should be rejected
+    h.applyInputBlockTransactions(ib1.id, tx1, us) shouldBe Seq(ib1.id)
+    h.bestInputBlocksChain() shouldBe Seq(ib1.id)
   }
 
   property("apply input block with double spending") {
+
+  }
+
+  property("apply new best input block on another ordering block on the same height") {
 
   }
 

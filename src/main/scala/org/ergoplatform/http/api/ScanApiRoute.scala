8438c90e83da4d88835c21dc93903b9e6630eb79
kushti <kushtech@yahoo.com>
2025-11-11 12:52:26 +0300

    auto shorten scan name in /scan/p2sRule

--- src/main/scala/org/ergoplatform/http/api/ScanApiRoute.scala
+++ src/main/scala/org/ergoplatform/http/api/ScanApiRoute.scala
@@ -107,7 +107,12 @@ case class ScanApiRoute(readersHolder: ActorRef, ergoSettings: ErgoSettings)
         val script = p2sAddr.script
         val scriptBytes = ByteArrayConstant(ErgoTreeSerializer.DefaultSerializer.serializeErgoTree(script))
         val trackingRule = EqualsScanningPredicate(R1, scriptBytes)
-        val request = ScanRequest(p2s, trackingRule, Some(ScanWalletInteraction.Off), Some(true))
+        val scanName = if (p2s.getBytes("UTF-8").length > Scan.MaxScanNameLength) {
+          new String(p2s.getBytes("UTF-8"), 0, Scan.MaxScanNameLength, "UTF-8")
+        } else {
+          p2s
+        }
+        val request = ScanRequest(scanName, trackingRule, Some(ScanWalletInteraction.Off), Some(true))
         withWalletOp(_.addScan(request).map(_.response)) {
           case Failure(e) => BadRequest(s"Bad request $request. ${Option(e.getMessage).getOrElse(e.toString)}")
           case Success(app) => ApiResponse(ScanIdWrapper(app.scanId))

9323f2a870fbfacfefa56a6310a4417874291662
Alexander Chepurnoy <alex.chepurnoy@iohk.io>
2026-03-16 20:57:47 +0300

    more cfor

--- ergo-core/src/main/scala/org/ergoplatform/network/message/inputblocks/OrderingBlockAnnouncementMessageSpec.scala
+++ ergo-core/src/main/scala/org/ergoplatform/network/message/inputblocks/OrderingBlockAnnouncementMessageSpec.scala
@@ -2,16 +2,23 @@ package org.ergoplatform.network.message.inputblocks
 
 import org.ergoplatform.modifiers.history.extension.Extension
 import org.ergoplatform.modifiers.history.header.HeaderSerializer
-import org.ergoplatform.modifiers.mempool.ErgoTransactionSerializer
+import org.ergoplatform.modifiers.mempool.{ErgoTransaction, ErgoTransactionSerializer}
 import org.ergoplatform.network.message.MessageConstants.MessageCode
 import org.ergoplatform.network.message.MessageSpecInputBlocks
-import scorex.util.{bytesToId, idToBytes}
+import scorex.util.{bytesToId, idToBytes, ModifierId}
 import scorex.util.serialization.{Reader, Writer}
 import scorex.util.Extensions._
+import spire.syntax.all.cfor
 
 object OrderingBlockAnnouncementMessageSpec extends MessageSpecInputBlocks[OrderingBlockAnnouncement] {
 
   private val maxSize = 32000
+  
+  /**
+    * Current protocol version for OrderingBlockAnnouncement messages
+    */
+  private val CurrentVersion: Byte = 1.toByte
+
   /**
     * Code which identifies what message type is contained in the payload
     */
@@ -23,18 +30,19 @@ object OrderingBlockAnnouncementMessageSpec extends MessageSpecInputBlocks[Order
   override val messageName: String = "OrderingBlockAnnouncement"
 
   override def serialize(ann: OrderingBlockAnnouncement, w: Writer): Unit = {
-    w.put(1.toByte) // todo: named constant
+    w.put(CurrentVersion)
     HeaderSerializer.serialize(ann.header, w)
     w.putUInt(ann.nonBroadcastedTransactions.length)
-    ann.nonBroadcastedTransactions.foreach{ tx =>  // todo: replace with cfor
-      ErgoTransactionSerializer.serialize(tx, w)
+    cfor(0)(_ < ann.nonBroadcastedTransactions.length, _ + 1) { i =>
+      ErgoTransactionSerializer.serialize(ann.nonBroadcastedTransactions(i), w)
     }
     w.putUInt(ann.broadcastedTransactionIds.length)
-    ann.broadcastedTransactionIds.foreach { txId => // todo: replace with cfor
-      w.putBytes(idToBytes(txId))
+    cfor(0)(_ < ann.broadcastedTransactionIds.length, _ + 1) { i =>
+      w.putBytes(idToBytes(ann.broadcastedTransactionIds(i)))
     }
     w.putUShort(ann.extensionFields.size)
-    ann.extensionFields.foreach { case (key, value) =>
+    cfor(0)(_ < ann.extensionFields.length, _ + 1) { i =>
+      val (key, value) = ann.extensionFields(i)
       w.putBytes(key)
       w.putUByte(value.length)
       w.putBytes(value)
@@ -46,22 +54,26 @@ object OrderingBlockAnnouncementMessageSpec extends MessageSpecInputBlocks[Order
     val startPosition = r.position
     val version = r.getByte()
     val header = HeaderSerializer.parse(r)
-    val nbtCount = r.getUInt().toIntExact
-    val txs = (1 to nbtCount).map { _ =>
-      ErgoTransactionSerializer.parse(r)
-    }.toArray // todo: replace with cfor
+    val nbtCount = r.getUInt().toIntExact // todo: check for spam, ie too big value
+    val txs = new Array[ErgoTransaction](nbtCount)
+    cfor(0)(_ < nbtCount, _ + 1) { i =>
+      txs(i) = ErgoTransactionSerializer.parse(r)
+    }
+    require(r.position - startPosition < maxSize)
     val txIdsCount = r.getUInt().toIntExact
-    val txIds = (1 to txIdsCount).map { _ => // todo: replace with cfor
-      bytesToId(r.getBytes(32))
-    }.toArray
+    val txIds = new Array[ModifierId](txIdsCount)
+    cfor(0)(_ < txIdsCount, _ + 1) { i =>
+      txIds(i) = bytesToId(r.getBytes(32))
+    }
+    require(r.position - startPosition < maxSize)
     val fieldsSize = r.getUShort()
-    val fieldsView = (1 to fieldsSize).toStream.map { _ =>
+    val fields = new Array[(Array[Byte], Array[Byte])](fieldsSize)
+    cfor(0)(_ < fieldsSize, _ + 1) { i =>
       val key = r.getBytes(Extension.FieldKeySize)
       val length = r.getUByte()
       val value = r.getBytes(length)
-      (key, value)
+      fields(i) = (key, value)
     }
-    val fields = fieldsView.takeWhile(_ => r.position - startPosition < maxSize)
     require(r.position - startPosition < maxSize)
     OrderingBlockAnnouncement(header, txs, txIds, fields)
     // todo: consider versioning by skipping unparsed bytes if version > 1

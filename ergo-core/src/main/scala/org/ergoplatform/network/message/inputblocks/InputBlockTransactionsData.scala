1953d45bfb46d1cf0538f6270217c55533d13e46
Alexander Chepurnoy <alex.chepurnoy@iohk.io>
2026-05-02 00:17:20 +0300

    InputBlockTransactionsDataSerializer removed

--- ergo-core/src/main/scala/org/ergoplatform/network/message/inputblocks/InputBlockTransactionsData.scala
+++ ergo-core/src/main/scala/org/ergoplatform/network/message/inputblocks/InputBlockTransactionsData.scala
@@ -14,29 +14,3 @@ import spire.syntax.all.cfor
 case class InputBlockTransactionsData(inputBlockId: ModifierId,
                                       transactions: Seq[ErgoTransaction],
                                       sizeOpt: Option[Int] = None)
-
-object InputBlockTransactionsDataSerializer extends ErgoSerializer[InputBlockTransactionsData] {
-
-  override def serialize(obj: InputBlockTransactionsData, w: Writer): Unit = {
-    w.putBytes(idToBytes(obj.inputBlockId))
-    w.putUInt(obj.transactions.size.toLong)
-    cfor(0)(_ < obj.transactions.length, _ + 1) { i =>
-      ErgoTransactionSerializer.serialize(obj.transactions(i), w)
-    }
-  }
-
-  override def parse(r: Reader): InputBlockTransactionsData = {
-    //todo: consider max message size
-    val startPos = r.position
-
-    val headerId: ModifierId = bytesToId(r.getBytes(Constants.ModifierIdSize))
-    val txCount = r.getUInt().toIntExact
-
-    val txs = new Array[ErgoTransaction](txCount)
-    cfor(0)(_ < txCount, _ + 1) { i =>
-      txs(i) = ErgoTransactionSerializer.parse(r)
-    }
-    InputBlockTransactionsData(headerId, txs, Some(r.position - startPos))
-  }
-
-}

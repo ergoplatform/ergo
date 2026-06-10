0c0bdb167fa81ac0546f75ab8aaf890857dfce6b
a-shannon <upwindstrategyproton.me>
2026-06-10 22:46:15 +0200

    test: stabilize mining candidate cache spec

--- src/test/scala/org/ergoplatform/mining/CandidateGeneratorSpec.scala
+++ src/test/scala/org/ergoplatform/mining/CandidateGeneratorSpec.scala
@@ -864,10 +864,16 @@ class CandidateGeneratorSpec extends AnyFlatSpec with Matchers with ErgoTestHelp
       .proveCandidate(initCandidate.candidateBlock, defaultMinerSecret.w, 0, 1000)
       .get
     candidateGenerator.tell(initBlock.header.powSolution, testProbe.ref)
-    // Wait for block application - can receive either StatusReply or FullBlockApplied first
+    // Wait for both the direct mining ACK and the async local block application
+    var ackSeen = false
+    var appliedSeen = false
     testProbe.fishForMessage(blockValidationDelay) {
-      case StatusReply.Success(()) => true
-      case _: FullBlockApplied => true
+      case StatusReply.Success(()) =>
+        ackSeen = true
+        ackSeen && appliedSeen
+      case FullBlockApplied(header) if header.id == initBlock.header.id =>
+        appliedSeen = true
+        ackSeen && appliedSeen
       case _ => false
     }
 
@@ -897,11 +903,9 @@ class CandidateGeneratorSpec extends AnyFlatSpec with Matchers with ErgoTestHelp
     // Submit solution - should succeed because candidate1 should be in cachedPreviousCandidate
     candidateGenerator.tell(solvedBlock.header.powSolution, testProbe.ref)
 
-    // Should successfully apply the block
-    testProbe.fishForMessage(blockValidationDelay) {
-      case StatusReply.Success(()) => true
-      case _: FullBlockApplied => true
-      case _ => false
+    // CandidateGenerator should accept the solution against cachedPreviousCandidate.
+    testProbe.expectMsgPF(blockValidationDelay) {
+      case StatusReply.Success(()) =>
     }
 
     system.terminate()

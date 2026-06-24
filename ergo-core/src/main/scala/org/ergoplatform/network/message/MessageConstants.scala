d58915d2b572d83949d85d75387cdbce74daaf4f
Alexander Chepurnoy <alex.chepurnoy@iohk.io>
2026-06-24 18:32:23 +0300

    improvedMaxMessageSize comment

--- ergo-core/src/main/scala/org/ergoplatform/network/message/MessageConstants.scala
+++ ergo-core/src/main/scala/org/ergoplatform/network/message/MessageConstants.scala
@@ -13,9 +13,16 @@ object MessageConstants {
   val HeaderLength: Int = MagicLength + 5
 
   /**
-    * Max message in p2p networking protocol, including headers. Reasonably bigger than max modifier size (8M),
-    * defined by `ModifiersSpec.maxMsgSizeWithReserve`. Also enough to have reasonable guaranteed max memory consumption
-    * by p2p messaging buffers (30 peers by default * 16M = 480M).
+    * Max serialized payload length accepted for any P2P message, including message headers.
+    *
+    * Set to twice `ModifiersSpec.maxMsgSizeWithReserve` (~16.4 MB), which itself is 4x the
+    * `ModifiersSpec.maxMessageSize` base (~2 MB) to accommodate large ADProofs. This bound
+    * comfortably exceeds the largest legitimate Ergo messages:
+    *   - `Modifiers` messages: up to `ModifiersSpec.maxMsgSizeWithReserve` (~8.4 MB payload)
+    *   - UTXO snapshot manifest/chunk messages: up to ~4 MB
+    *   - Inv/Request/Sync/Peers messages: kilobytes
+    *
+    * It also bounds per-peer buffering: 30 peers by default * 16 MB ≈ 480 MB worst-case memory.
     */
   val MaxMessageSize: Int = ModifiersSpec.maxMsgSizeWithReserve * 2
 
--- src/main/scala/org/ergoplatform/nodeView/ErgoNodeViewHolder.scala
+++ src/main/scala/org/ergoplatform/nodeView/ErgoNodeViewHolder.scala
@@ -232,7 +232,7 @@ abstract class ErgoNodeViewHolder[State <: ErgoState[State]](settings: ErgoSetti
       case (success@Success(updateInfo), modToApply) =>
         if (updateInfo.failedMod.isEmpty) {
           val chainTipOpt = history.estimatedTip()
-          // todo: make cleaner ADProofs dump instead of pmodModify , see https://github.com/ergoplatform/ergo/issues/2413 
+          // todo: make cleaner ADProofs dump instead of pmodModify , see https://github.com/ergoplatform/ergo/issues/2413
           updateInfo.state.applyModifier(modToApply, chainTipOpt)(lm => pmodModify(lm.pmod, local = true)) match {
             case Success(stateAfterApply) =>
               history.reportModifierIsValid(modToApply).map { newHis =>

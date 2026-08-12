4d93c8a2ddfdfa996fcc1dc00bd4c93074567ea2
A. Shannon <217575710+a-shannon@users.noreply.github.com>
2026-08-12 14:49:25 +0200

    Fix ExtraIndexer checkpoint recovery

--- src/main/scala/org/ergoplatform/nodeView/history/storage/HistoryStorage.scala
+++ src/main/scala/org/ergoplatform/nodeView/history/storage/HistoryStorage.scala
@@ -14,7 +14,8 @@ import scala.util.{Failure, Success, Try}
 import spire.syntax.all.cfor
 
 import java.io.File
-import java.nio.file.Files
+import java.nio.file.{Files, Path}
+import java.util.concurrent.locks.ReentrantReadWriteLock
 import scala.jdk.CollectionConverters.asScalaIteratorConverter
 
 /**
@@ -50,6 +51,20 @@ class HistoryStorage(indexStore: LDBKVStore, objectsStore: LDBKVStore, extraStor
     Caffeine.newBuilder()
       .maximumSize(config.history.indexesCacheSize)
       .build[ByteArrayWrapper, Array[Byte]]
+
+  private val extraCacheLock = new ReentrantReadWriteLock()
+
+  private def withExtraCacheReadLock[A](body: => A): A = {
+    extraCacheLock.readLock().lock()
+    try body
+    finally extraCacheLock.readLock().unlock()
+  }
+
+  private def withExtraCacheWriteLock[A](body: => A): A = {
+    extraCacheLock.writeLock().lock()
+    try body
+    finally extraCacheLock.writeLock().unlock()
+  }
 
   private def cacheModifier(mod: BlockSection): Unit = mod.modifierTypeId match {
     case Header.modifierTypeId => headersCache.put(mod.id, mod)
@@ -89,7 +104,7 @@ class HistoryStorage(indexStore: LDBKVStore, objectsStore: LDBKVStore, extraStor
       }
     }
 
-  def getExtraIndex(id: ModifierId): Option[ExtraIndex] = {
+  def getExtraIndex(id: ModifierId): Option[ExtraIndex] = withExtraCacheReadLock {
     Option(extraCache.getIfPresent(id)) orElse extraStore.get(idToBytes(id)).flatMap { bytes =>
       ExtraIndexSerializer.parseBytesTry(bytes) match {
         case Success(pm) =>
@@ -150,16 +165,46 @@ class HistoryStorage(indexStore: LDBKVStore, objectsStore: LDBKVStore, extraStor
 
   def insertExtra(indexesToInsert: Array[(Array[Byte], Array[Byte])],
                   objectsToInsert: Array[ExtraIndex]): Unit = {
-    extraStore.insert(
-      objectsToInsert.map(mod => mod.serializedId),
-      objectsToInsert.map(mod => ExtraIndexSerializer.toBytes(mod))
-    )
-    cfor(0)(_ < indexesToInsert.length, _ + 1) { i => extraStore.insert(indexesToInsert(i)._1, indexesToInsert(i)._2)}
+    insertExtraTry(indexesToInsert, objectsToInsert).failed.foreach { error =>
+      log.error("Failed to insert extra indexes", error)
+    }
+  }
+
+  private[history] def invalidateExtraCache(ids: Iterable[ModifierId]): Unit = withExtraCacheWriteLock {
+    ids.foreach(extraCache.invalidate)
+  }
+
+  def insertExtraTry(indexesToInsert: Array[(Array[Byte], Array[Byte])],
+                     objectsToInsert: Array[ExtraIndex]): Try[Unit] = {
+    val objectIds = objectsToInsert.iterator.flatMap(obj => Try(obj.id).toOption).toArray
+    Try {
+      val keys = objectsToInsert.map(_.serializedId) ++ indexesToInsert.map(_._1)
+      val values = objectsToInsert.map(ExtraIndexSerializer.toBytes) ++ indexesToInsert.map(_._2)
+      keys -> values
+    }.flatMap { case (keys, values) =>
+      withExtraCacheWriteLock {
+        extraStore.insert(keys, values).map { _ =>
+          objectIds.foreach(extraCache.invalidate)
+        }
+      }
+    }.recoverWith { case error =>
+      invalidateExtraCache(objectIds)
+      Failure(error)
+    }
   }
 
   def removeExtra(indexesToRemove: Array[ModifierId]) : Unit = {
-    extraStore.remove(indexesToRemove.map(idToBytes))
-    cfor(0)(_ < indexesToRemove.length, _ + 1) { i => removeModifier(indexesToRemove(i)) }
+    removeExtraTry(indexesToRemove).failed.foreach { error =>
+      log.error("Failed to remove extra indexes", error)
+    }
+  }
+
+  def removeExtraTry(indexesToRemove: Array[ModifierId]): Try[Unit] = {
+    withExtraCacheWriteLock {
+      extraStore.remove(indexesToRemove.map(idToBytes)).map { _ =>
+        cfor(0)(_ < indexesToRemove.length, _ + 1) { i => removeModifier(indexesToRemove(i)) }
+      }
+    }
   }
 
   /**
@@ -205,26 +250,37 @@ class HistoryStorage(indexStore: LDBKVStore, objectsStore: LDBKVStore, extraStor
     * Delete the extra index database and reopen it.
     *
     * @param ergoSettings - settings to use
-    * @return new HistoryStorage instance with empty extra database, or this instance in case of failure
+    * @return new HistoryStorage instance with an empty extra database
     */
-  def deleteExtraDB(ergoSettings: ErgoSettings): HistoryStorage = {
+  def deleteExtraDB(ergoSettings: ErgoSettings): HistoryStorage =
+    deleteExtraDBTry(ergoSettings).get
+
+  /**
+    * Delete the extra index database and reopen it, preserving deletion failures.
+    */
+  def deleteExtraDBTry(ergoSettings: ErgoSettings): Try[HistoryStorage] = {
     log.warn(s"Removing extra index database due to old schema.")
-    close()
-    // org.ergoplatform.wallet.utils.FileUtils
     val root = new File(s"${ergoSettings.directory}/history/extra")
-    if (root.exists()) {
-      Files.walk(root.toPath).iterator().asScala.toSeq.reverse.foreach(path => Try(Files.delete(path)))
-    }else {
-      log.error(s"Could not delete ${root.toString}")
-      return this
+    Try(close()).flatMap { _ =>
+      HistoryStorage.deleteRecursively(root.toPath, Files.delete)
+    }.map { _ =>
+      log.info(s"Deleted ${root.toString}")
+      HistoryStorage.apply(ergoSettings)
     }
-    log.info(s"Deleted ${root.toString}")
-    HistoryStorage.apply(ergoSettings)
   }
 
 }
 
 object HistoryStorage {
+  private[storage] def deleteRecursively(root: Path, deletePath: Path => Unit): Try[Unit] = Try {
+    if (Files.exists(root)) {
+      val paths = Files.walk(root)
+      try paths.iterator().asScala.toSeq.reverse.foreach(deletePath)
+      finally paths.close()
+    }
+    require(!Files.exists(root), s"Could not delete $root")
+  }
+
   def apply(ergoSettings: ErgoSettings): HistoryStorage = {
     val indexStore = LDBFactory.createKvDb(s"${ergoSettings.directory}/history/index")
     val objectsStore = LDBFactory.createKvDb(s"${ergoSettings.directory}/history/objects")
